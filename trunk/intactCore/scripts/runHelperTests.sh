178f801a8e9cb100049daa50d77633e956d42f5d
clewing <clewing@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-10-15 14:25:42 +0000

    fixed scripts to run in Unix or windows JVM - properly this time!

--- trunk/intactCore/scripts/runHelperTests.sh
+++ trunk/intactCore/scripts/runHelperTests.sh
@@ -5,7 +5,17 @@ CLASSPATH=classes/:$CLASSPATH
 
 #if cygwin used (ie on a Windows machine), make sure the paths
 #are converted from Unix to run correctly with the windows JVM
-if [ $CYGWIN ] ; then
+cygwin=false;
+
+case "`uname`" in
+
+CYGWIN*) cygwin=true 
+         echo "running in a Windows JVM (from cygwin).." ;;
+*) echo "running in a Unix JVM..." ;;
+
+esac
+
+if $cygwin; then
 CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
 
 fi
