35bef6d3d4577deb707995f87ea0b8242bf41823
aceol <arnaud.ceol@gmail.com>
2002-10-21 13:33:55 +0000

    add the good reference for the EBI institution

--- trunk/intactCore/sql/postgres/create_nodes.sql
+++ trunk/intactCore/sql/postgres/create_nodes.sql
@@ -1,15 +1,13 @@
 -- insert nodes into database for testing synchron package
 
-insert into institution(ac,fullName) values ('EBI-10','EBI Outstation');
+insert into institution(shortlabel, fullname, postaladdress, url, ac) values ('EBI',
+    'European Bioinformatics Institute',
+    'European Bioinformatics Institute
+    Wellcome Trust Genome Campus Hinxton, Cambridge
+    CB10 1SD United Kingdom' ,
+    'http://www.ebi.ac.uk',
+    'EBI-11');
 
-
--- you can add here informations about ftp server you want to use (e.g. your computer, your login and your password)
--- if you want to use the collector
-
--- node for the EBI, you can add others node on the same exemple
 insert into intactnode(ac, ftpaddress, ftplogin, ftppassword, ftpdirectory, ownerprefix,owner_ac)
-    values ('EBI-node1', '', '', '', '', 'EBI','EBI-10');
-
-
---don't forget to specify your id (e.g. EBI) in config/Properties.pro
+    values ('EBI-11', 'brandt.ebi.ac.uk', 'amueller', 'iZ111sU+A', '/scratch/PseudoFtpServer/EBI', 'EBI','EBI-10');
 
