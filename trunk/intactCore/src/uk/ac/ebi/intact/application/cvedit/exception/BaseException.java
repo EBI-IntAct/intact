455e97b5f903d6dbca8249448270c13dae149957
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-10-31 18:22:47 +0000

    Moved from web/src under main Intact src

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/exception/BaseException.java
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.  
+All rights reserved. Please see the file LICENSE 
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.cvedit.exception;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+/**
+ * This is the common superclass for all application.exception; This
+ * class and its subclasses support the chained exception facility.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class BaseException extends Exception {
+
+    /**
+     * The root exception.
+     */
+    private Throwable myRootException;
+
+    // Constructors
+
+    /**
+     * Default constructor.
+     */
+    public BaseException() {}
+
+    /**
+     * Constructor with a message.
+     * @param msg the message explaing the exception.
+     */
+    public BaseException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructor that takes a root exception.
+     * @param casue the root exception.
+     */
+    public BaseException(Throwable cause) {
+        myRootException = cause;
+    }
+
+    /**
+     * Print both the normal and root stack traces.
+     * @param writer the writer to print the stack trace.
+     */
+    public void printStackTrace(PrintWriter writer) {
+        super.printStackTrace(writer);
+        if (getRootCause() != null) {
+            getRootCause().printStackTrace(writer);
+        }
+        writer.flush();
+    }
+
+    /**
+     * Print both the normal and root stack traces.
+     * @param out the output stream.
+     */
+    public void printStackTrace(PrintStream out) {
+        printStackTrace(new PrintWriter(out));
+    }
+
+    /**
+     * Print both the normal and root stack traces to the std err.
+     */
+    public void printStackTrace() {
+        printStackTrace(System.err);
+    }
+
+    /**
+     * Return the root exception, if one exists.
+     * @return the root exception if it exists or else null.
+     */
+    public Throwable getRootCause() {
+        return myRootException;
+    }
+}
