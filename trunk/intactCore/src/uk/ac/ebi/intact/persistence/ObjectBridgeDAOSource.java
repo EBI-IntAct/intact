3fac244468e432ca61b65421c9b4503db54f167c
clewing <clewing@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-11-07 15:52:45 +0000

    updated to include user/password support

--- trunk/intactCore/src/uk/ac/ebi/intact/persistence/ObjectBridgeDAOSource.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/persistence/ObjectBridgeDAOSource.java
@@ -9,6 +9,7 @@ import java.util.*;
 import java.io.*;
 
 import org.apache.ojb.broker.*;
+import org.apache.ojb.broker.metadata.*;
 import org.apache.ojb.broker.util.logging.*;
 
 import uk.ac.ebi.intact.model.Constants;
@@ -27,9 +28,24 @@ public class ObjectBridgeDAOSource implements DAOSource, Serializable {
     //holds the filename containing the OR mapping data
     private String repositoryFile;
 
+    //holds any username that overrides a default user
+    private String user;
+
+    //used to override any default password specified
+    private String password;
+
+    private Logger logger;
+
     public ObjectBridgeDAOSource() {
 
-        //don't need to do anything!
+        //just set up a logger
+        try {
+            logger = LoggerFactory.getLogger(Class.forName("org.apache.ojb.broker.util.logging.Log4jLoggerImpl"));
+        }
+        catch(ClassNotFoundException ce) {
+
+            //do nothing - not finding a logger is not a failure condition
+        }
     }
 
     /**
@@ -57,6 +73,37 @@ public class ObjectBridgeDAOSource implements DAOSource, Serializable {
     }
 
     /**
+     * Used to specify a user for connecting to the persistent store. If this
+     * method is not used, default user details will be obtained from details supplied
+     * in the setConfig method (eg from a supplied config file).
+     * @param user the username to use for connection (overrides any default)
+     */
+    public void setUser(String user) {
+
+        this.user = user;
+    }
+
+    public String getUser() {
+
+        return user;
+    }
+
+    /**
+     * Used to define a password which overrides any default supplied via config data.
+     * Should typically be used in conjunction with the setUser method for consistency.
+     * @param passowrd the password to be used for persistent store connection.
+     */
+    public void setPassword(String password) {
+
+        this.password = password;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    /**
      *  This method returns a connection to the data source, ie in this case
      * a broker instance which provides database connection.
      *
@@ -78,13 +125,60 @@ public class ObjectBridgeDAOSource implements DAOSource, Serializable {
             //could be any of parser, SAX or IO exceptions.....
             String msg = "error - unable to create a DAO object; see stack trace...";
 
-            throw new DataSourceException(e.toString() + e.getLocalizedMessage() + e.getMessage() + e.getClass().getName());
+            throw new DataSourceException(msg, e);
         }
 
         //create an ObjectBridgeDAO, passing the initialised broker as a param
-        //NB can we set a logfile too *NO!*? Need to change the DAO I/F to take a
-        //Logger instead...And what about the following methods -
-        //they aren't supported in the OJB factory.......
+        return new ObjectBridgeDAO(broker);
+
+    }
+
+    /**
+     *  Returns a connection to the data source, and connects to it using
+     * the supplied username and password. If the username is null or the details
+     * are invalid then a default connection will be attempted.
+     *
+     * @param user - the username to connect with
+     * @param password - user's password (null values allowed)
+     * @return a Data Access Object (connection)
+     */
+     public DAO getDAO(String user, String password) throws DataSourceException {
+
+        PersistenceBroker broker = null;
+        if (repositoryFile == null) {
+
+            throw new DataSourceException("cannot obtain a data connection - no mapping file!");
+        }
+        if(user == null) {
+
+            //no valid username supplied - will connect with default user..
+            logger.debug("cannot connect to data store with username of null - using default user details..");
+            return this.getDAO();
+        }
+        try {
+
+            //if username is non-null, just get a broker based on supplied details..
+            //NB null passwords allowed
+            PBKey key = new PBKey(repositoryFile, user, password);
+            broker = PersistenceBrokerFactory.createPersistenceBroker(key);
+        }
+        catch(Exception e) {
+
+            logger.debug("failed to obtain a connection with username [" + user + "] and password [" + password + "]; exception details:");
+            logger.debug(e.toString());
+            logger.debug("attempting default connection instead...");
+            //first try and get a default connection
+            try {
+                return this.getDAO();
+            }
+            catch(DataSourceException de) {
+
+                String msg = "invalid user details supplied - even failed to obtain a default connection!";
+                throw new DataSourceException(msg, de);
+            }
+        }
+
+        //create an ObjectBridgeDAO, passing the initialised broker as a param
         return new ObjectBridgeDAO(broker);
 
     }
@@ -92,7 +186,7 @@ public class ObjectBridgeDAOSource implements DAOSource, Serializable {
 
     public String getDataSourceName() {
 
-        //not supported by OJB broker interface
+        //not important - will implement later if required...
         return null;
     }
 
@@ -109,32 +203,23 @@ public class ObjectBridgeDAOSource implements DAOSource, Serializable {
 
     public void setAutoSave(boolean val) {
 
-        //not supported by OJB broker interface
+        //not important - will implement if required later...
     }
 
     public boolean isAutoSaveSet() {
 
-        //not supported by OJB broker interface
+        //not important - will implement later if required...
         return false;
     }
 
     public void setLogger(org.apache.ojb.broker.util.logging.Logger l) {
 
-        //not required...
+        this.logger = l;
     }
 
     public Logger getLogger() {
 
-        //NB the Castor datasource will need changing too..
-        try {
-
-            return LoggerFactory.getLogger(Class.forName("org.apache.ojb.broker.util.logging.Log4jLoggerImpl"));
-        }
-        catch(ClassNotFoundException c) {
-
-            //set up a basic logger and return that instead (TBD...)
-            return null;
-        }
+        return logger;
 
     }
 
