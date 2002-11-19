3108542498c991ba596b24443ea54ec009ba59e0
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-11-19 16:58:02 +0000

    Moved all the persistent system calls to this class; all the transactions are performed here

--- trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/controller/CvEditAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/controller/CvEditAction.java
@@ -9,13 +9,21 @@ package uk.ac.ebi.intact.application.cvedit.struts.controller;
 import uk.ac.ebi.intact.application.cvedit.struts.framework.IntactBaseAction;
 import uk.ac.ebi.intact.application.cvedit.struts.framework.util.WebIntactConstants;
 import uk.ac.ebi.intact.application.cvedit.struts.view.CvEditForm;
+import uk.ac.ebi.intact.application.cvedit.struts.view.CommentBean;
+import uk.ac.ebi.intact.application.cvedit.struts.view.XreferenceBean;
 import uk.ac.ebi.intact.application.cvedit.business.IntactUserIF;
 import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.model.CvObject;
+import uk.ac.ebi.intact.model.Annotation;
+import uk.ac.ebi.intact.model.Xref;
+import uk.ac.ebi.intact.persistence.SearchException;
 
 import org.apache.struts.action.*;
 import org.apache.commons.lang.exception.ExceptionUtils;
 
 import javax.servlet.http.*;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * This class is called by Struts when Submit, Delete or Cancel is called from
@@ -53,35 +61,122 @@ public class CvEditAction extends IntactBaseAction {
         IntactUserIF user = super.getIntactUser(request);
 
         if (theForm.isSubmitted()) {
-            // Commit all the changes.
-            try {
-                user.commit();
-            }
-            catch (IntactException ie) {
-                // Unable to create an annotation.
-                super.addError("error.transaction.commit", ie.getNestedMessage());
-                super.saveErrors(request);
+            // Sumbit butoon pressed.
+            formSubmitted(request);
+
+            // Any errors in committing the transaction?
+            if (super.hasErrors()) {
                 return mapping.findForward(WebIntactConstants.FORWARD_FAILURE);
             }
-            super.log("After committing the update: " + user.isActive());
         }
         else if (theForm.isDeleted()) {
             super.log("Cv Object is deleted");
             // Confirm the delete.
             return mapping.findForward(WebIntactConstants.FORWARD_DEL_CONFIRM);
         }
-        else {
-            // Assume Cancel is chosen.
+        // Either Cancel or form is sumbitted without errors (all changes are
+        // committed successfully).
+        return mapping.findForward(super.fwdResultsOrSearch(request));
+    }
+
+    /**
+     * This method is responsible for handling the sequence of events when the
+     * user presses Submit button (committing changes to the database).
+     *
+     * @param request the request to access various objects saved under a
+     * session.
+     */
+    private void formSubmitted(HttpServletRequest request) {
+        // Handler to the Intact User.
+        IntactUserIF user = super.getIntactUser(request);
+
+        // The object we are editing at the moment.
+        CvObject cvobj = user.getCurrentEditObject();
+
+        // Save the session to access session objects.
+        HttpSession session = super.getSession(request);
+
+        // Holds the annotations to add.
+        Collection addcomments = (Collection) session.getAttribute(
+            WebIntactConstants.ANNOTS_TO_ADD);
+
+        // Holds the annotations to delete.
+        Collection delcomments = (Collection) session.getAttribute(
+            WebIntactConstants.ANNOTS_TO_DELETE);
+
+        // Holds the xrefs to add.
+        Collection addxrefs = (Collection) session.getAttribute(
+            WebIntactConstants.XREFS_TO_ADD);
+
+        // Holds the xrefs to delete.
+        Collection delxrefs = (Collection) session.getAttribute(
+            WebIntactConstants.XREFS_TO_DELETE);
+
+        // Clear any previous errors.
+        super.clearErrors();
+
+        try {
+            // Begin the transaction.
+            user.begin();
+
+            // Create annotations and add them to CV object.
+            for (Iterator iter = addcomments.iterator(); iter.hasNext();) {
+                Annotation annot = ((CommentBean)iter.next()).getAnnotation();
+                user.create(annot);
+                cvobj.addAnnotation(annot);
+            }
+            // Delete annotations and remove them from CV object.
+            for (Iterator iter = delcomments.iterator(); iter.hasNext();) {
+                Annotation annot = ((CommentBean)iter.next()).getAnnotation();
+                user.delete(annot);
+                cvobj.removeAnnotation(annot);
+            }
+
+            // Create xrefs and add them to CV object.
+            for (Iterator iter = addxrefs.iterator(); iter.hasNext();) {
+                Xref xref = ((XreferenceBean)iter.next()).getXref();
+                user.create(xref);
+                cvobj.addXref(xref);
+            }
+            // Delete xrefs and remove them from CV object.
+            for (Iterator iter = delxrefs.iterator(); iter.hasNext();) {
+                Xref xref = ((XreferenceBean)iter.next()).getXref();
+                user.delete(xref);
+                cvobj.removeXref(xref);
+            }
+            user.update(cvobj);
+
+            // Commit all the changes.
+            user.commit();
+
+            // Need to update the drop down lists for certain types.
+            user.updateList(cvobj.getClass());
+        }
+        catch (IntactException ie1) {
             try {
                 user.rollback();
             }
-            catch (IntactException ie) {
-                // Unable to rollback the transaction.
-                super.addError("error.transaction.rollback", ie.getNestedMessage());
-                super.saveErrors(request);
-                return mapping.findForward(WebIntactConstants.FORWARD_FAILURE);
+            catch (IntactException ie2) {
+                // Oops! Problems with rollback; ignore this as this
+                // error is reported via the main exception (ie1).
             }
+            // Log the stack trace.
+            super.log(ExceptionUtils.getStackTrace(ie1));
+            // Error with committing changes.
+            super.addError("error.transaction.commit", ie1.getNestedMessage());
+            super.saveErrors(request);
         }
-        return mapping.findForward(super.fwdResultsOrSearch(request));
+        catch (SearchException se) {
+            // Unable to construct the lists.
+            super.addError("error.search", se.getNestedMessage());
+            super.saveErrors(request);
+        }
+        finally {
+            // Clear containers; regradless of the outcome.
+            addcomments.clear();
+            delcomments.clear();
+            addxrefs.clear();
+            delxrefs.clear();
+        }
     }
 }
