e4600debf23745aa5062d944bccb77a6916c0a3b
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-11-29 13:37:13 +0000

    Added equals method, so we can use remove() method from a collection

--- trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/view/ListObject.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/view/ListObject.java
@@ -1,12 +1,13 @@
 /*
-Copyright (c) 2002 The European Bioinformatics Institute, and others.  
-All rights reserved. Please see the file LICENSE 
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
 in the root directory of this distribution.
 */
 
 package uk.ac.ebi.intact.application.cvedit.struts.view;
 
 import java.io.Serializable;
+import org.apache.commons.collections.Predicate;
 
 import uk.ac.ebi.intact.model.CvObject;
 
@@ -20,6 +21,34 @@ import uk.ac.ebi.intact.model.CvObject;
  */
 
 public class ListObject implements Serializable {
+
+    // Static Inner class
+
+    // ------------------------------------------------------------------------
+    private static class ListObjectPredicate implements Predicate {
+
+        /**
+         * The ac to filter out.
+         */
+        private String myFilterAc;
+
+        private ListObjectPredicate(String ac) {
+            myFilterAc = ac;
+        }
+
+        /**
+         * Returns true if the input object matches this predicate.
+         * @param object the object to get the AC.
+         * @return  true if <code>object</code>'s AC does not match with the
+         * filter AC.
+         */
+        public boolean evaluate(Object object) {
+            return !myFilterAc.equals(((ListObject) object).getAc());
+        }
+    }
+    // ------------------------------------------------------------------------
+
+    // End of inner classes.
 
     /**
      * Reference to CV object.
@@ -49,4 +78,39 @@ public class ListObject implements Serializable {
     public String getShortLabel() {
         return myCvObject.getShortLabel();
     }
+
+    /**
+     * Returns the predicate for this class.
+     * @param ac the AC number to filter.
+     * @return the <code>Predicate</code> for list operations.
+     */
+    public static Predicate getPredicate(String ac) {
+        return new ListObjectPredicate(ac);
+    }
+
+    // Override Objects's equal method.
+
+    /**
+     * Compares <code>obj</code> with this object according to
+     * Java's equals() contract. Only returns <tt>true</tt> if the AC for both
+     * objects match.
+     *
+     * @param obj the object to compare.
+     */
+    public final boolean equals(Object obj) {
+        // Identical to this?
+        if (obj == this) {
+            return true;
+        }
+        if ((obj != null) && (getClass() == obj.getClass())) {
+            // Can safely cast it.
+            return myCvObject.getAc().equals(((ListObject) obj).getAc());
+        }
+        return false;
+    }
+
+    // For debugging only.
+//    public String toString() {
+//        return "AC: " + myCvObject.getAc() + " SL: " + myCvObject.getShortLabel();
+//    }
 }
