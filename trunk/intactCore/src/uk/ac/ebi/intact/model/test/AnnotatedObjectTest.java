e0de415e9ce80c85bfd9354812221b74607e29ec
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2002-12-05 17:10:21 +0000

    Added test case for xrefAdd, general Junit framework setup.

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/test/AnnotatedObjectTest.java
@@ -0,0 +1,101 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: hhe
+ * Date: Dec 5, 2002
+ * Time: 11:54:37 AM
+ * To change this template use Options | File Templates.
+ */
+package uk.ac.ebi.intact.model.test;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import uk.ac.ebi.intact.test.FirstTest;
+import uk.ac.ebi.intact.util.TestCaseHelper;
+import uk.ac.ebi.intact.model.Protein;
+import uk.ac.ebi.intact.model.Xref;
+import uk.ac.ebi.intact.model.CvDatabase;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.business.IntactHelper;
+
+public class AnnotatedObjectTest extends TestCase {
+
+    /**
+     *
+     */
+    IntactHelper helper;
+    TestCaseHelper testHelper;
+
+    /**
+     * Constructor
+     *
+     * @param name the name of the test.
+     */
+    public AnnotatedObjectTest(String name) throws Exception {
+        super(name);
+        testHelper = new TestCaseHelper();
+        helper = testHelper.getHelper();
+    }
+
+    /**
+     * Sets up the test fixture. Called before every test case method.
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        testHelper.setUp();
+
+    }
+
+    /**
+     * Tears down the test fixture. Called after every test case method.
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        testHelper.tearDown();
+    }
+
+    /** Test the addition and removal of Xref objects.
+     *
+     * @throws IntactException
+     */
+    public void testXref() throws IntactException {
+        // Set up required objects
+        Protein p1 = (Protein) helper.getObjectByLabel(Protein.class, "prot1");
+        Xref x1 = new Xref();
+        Xref x2 = new Xref();
+        CvDatabase db1 = (CvDatabase) helper.getObjectByLabel(CvDatabase.class,"testCvDb");
+        x1.setCvDatabase(db1);
+        x1.setPrimaryId("xx1");
+        x2.setCvDatabase(db1);
+        x2.setPrimaryId("xx1");
+
+        // get the initial state
+        int xrefCount = p1.getXref().size();
+
+        // check addition of xref
+        p1.addXref(x1);
+        super.assertEquals(xrefCount + 1, p1.getXref().size());
+
+        // x2 should not be added, it has the same content as x1
+        p1.addXref(x2);
+        super.assertEquals(xrefCount + 1, p1.getXref().size());
+
+        // change x2, now it should be added
+        x2.setPrimaryId("xx2");
+        p1.addXref(x2);
+        super.assertEquals(xrefCount + 2, p1.getXref().size());
+
+        // Test removal
+        p1.removeXref(x2);
+        p1.removeXref(x1);
+        super.assertEquals(xrefCount, p1.getXref().size());
+    }
+
+    /**
+     * Returns this test suite. Reflection is used here to add all
+     * the testXXX() methods to the suite.
+     */
+    public static Test suite() {
+        return new TestSuite(AnnotatedObjectTest.class);
+    }
+}
