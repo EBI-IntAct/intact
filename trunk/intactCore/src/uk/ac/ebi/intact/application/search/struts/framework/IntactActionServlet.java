7f903922fc8e87cbddbff3983fe17373964139b2
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-01-15 18:54:50 +0000

    Passes the config dir location; uses SearchConstants instead of WebIntactConstants

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/framework/IntactActionServlet.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/framework/IntactActionServlet.java
@@ -14,18 +14,10 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 
 import uk.ac.ebi.intact.application.search.business.IntactServiceImpl;
 import uk.ac.ebi.intact.application.search.business.IntactServiceIF;
-import uk.ac.ebi.intact.application.search.struts.framework.util.WebIntactConstants;
+import uk.ac.ebi.intact.application.search.struts.framework.util.SearchConstants;
 import uk.ac.ebi.intact.application.search.exception.MissingIntactTypesException;
-import uk.ac.ebi.intact.persistence.DataSourceException;
-import uk.ac.ebi.intact.persistence.SearchException;
-import uk.ac.ebi.intact.model.Constants;
-import uk.ac.ebi.intact.business.IntactException;
 
-import java.util.ResourceBundle;
-import java.util.Map;
-import java.beans.IntrospectionException;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.io.IOException;
 
 /**
  * This is Intact specific action servlet class. This class provides our own
@@ -43,32 +35,25 @@ public class IntactActionServlet extends ActionServlet {
         // Save the context to avoid repeat calls.
         ServletContext ctx = super.getServletContext();
 
-        // The Intact Types file.
-        String types = ctx.getInitParameter(WebIntactConstants.INTACT_TYPES_FILE);
+        // The configuration dir.
+        String configDir = ctx.getInitParameter(SearchConstants.CONFIG_DIR);
         // Create an instance of IntactService.
         IntactServiceIF service = null;
         try {
             // Load the Intact Types resources.
-            service = new IntactServiceImpl(types);
-            //service.setIntactTypes(types);
+            service = new IntactServiceImpl(configDir);
+        }
+        catch (IOException ioe) {
+            // Unable to load the properties file.
+            super.log(ExceptionUtils.getStackTrace(ioe));
+            throw new ServletException();
         }
         catch (MissingIntactTypesException mite) {
-            // Unable to find the resource file.
+            // An empty intact types file.
             super.log(ExceptionUtils.getStackTrace(mite));
             throw new ServletException();
         }
-        catch (ClassNotFoundException cnfe) {
-            // Unable to load intact objects.
-            super.log(ExceptionUtils.getStackTrace(cnfe));
-            // Carry on; may cause problems later on.
-        }
-        catch (IntrospectionException ie) {
-            // Failed to get reflection data on object"
-            super.log(ExceptionUtils.getStackTrace(ie));
-            // Carry on; may cause problems later on.
-        }
-
         // Store the service into the session scope.
-        ctx.setAttribute(WebIntactConstants.INTACT_SERVICE, service);
+        ctx.setAttribute(SearchConstants.INTACT_SERVICE, service);
     }
 }
