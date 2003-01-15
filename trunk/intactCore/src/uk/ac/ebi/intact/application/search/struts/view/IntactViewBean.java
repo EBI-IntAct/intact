2980e526b0f27e475e86291b68d6f3dd393c4904
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-01-15 18:26:35 +0000

    Moved constants out of this class to TreeViewAction class; stylesheet is passed as an arg to the constructor; transformer factory is created instead of an arg for tranform method

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/view/IntactViewBean.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/view/IntactViewBean.java
@@ -7,10 +7,6 @@ in the root directory of this distribution.
 package uk.ac.ebi.intact.application.search.struts.view;
 
 import java.io.Serializable;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Iterator;
 
 import org.w3c.dom.*;
@@ -23,8 +19,9 @@ import org.exolab.castor.xml.ValidationException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.dom.DOMSource;
+
+import uk.ac.ebi.intact.application.search.struts.framework.util.TreeViewAction;
 
 /**
  * Bean to display an Intact object. This bean is used by results.jsp to display
@@ -34,29 +31,6 @@ import javax.xml.transform.dom.DOMSource;
  * @version $Id$
  */
 public class IntactViewBean implements Serializable {
-
-    // Class Data.
-
-    /**
-     * A list of element names we want the status attributes for.
-     */
-    private static List expElementTags = Arrays.asList(
-            new String[]{"experiment", "interaction", "interactor"});
-
-    /**
-     * The name of the status attribute.
-     */
-    private static String STATUS_ATTRIBUTE = "status";
-
-    /**
-     * True string.
-     */
-    private static String TRUE_STR = Boolean.TRUE.toString();
-
-    /**
-     * False string.
-     */
-    private static String FALSE_STR = Boolean.FALSE.toString();
 
     // Instance Data.
 
@@ -72,28 +46,23 @@ public class IntactViewBean implements Serializable {
     private Object wrappedObject;
 
     /**
-     * The name of the stylesheet to use to transform this object.
+     * The stylesheet.
      */
     private String stylesheet;
 
     /**
      * Construct an instance of this class for given object.
      * @param object the object to contruct the view.
-     */
-    public IntactViewBean(Object object) {
-        this.wrappedObject = object;
-    }
-
-    /**
-     * Sets the stylesheet.
      * @param xslt the name of the stylesheet to use for transformation.
+     * @exception TransformerException for errors in creating a new transformer.
      */
-    public void setStylesheet(String xslt) {
+    public IntactViewBean(Object object, String xslt) throws TransformerException {
+        this.wrappedObject = object;
         this.stylesheet = xslt;
     }
 
     /**
-     * Marshsasls the wrapped object to an XML node.
+     * Marshals the wrapped object to an XML node using Castor.
      * @param mapping the mapping file for castor to do mapping.
      * @param db the document builder to create an XML node.
      * @exception MappingException for errrors with setting the map file.
@@ -113,14 +82,13 @@ public class IntactViewBean implements Serializable {
 
     /**
      * Transforms this bean using given stylesheet.
-     * @param factory the factory to create a transformer.
      * @param id the id for the table parameter (for XSL).
      * @param out holder for the transformation result.
      * @exception TransformerConfigurationException thrown during the parse
      *  when it is constructing the Templates object and fails.
      */
-    public void transform(TransformerFactory factory, String id, Result out)
-            throws TransformerException {
+    public void transform(String id, Result out) throws TransformerException {
+        TransformerFactory factory = TransformerFactory.newInstance();
         Transformer transformer =
                 factory.newTransformer(new StreamSource(this.stylesheet));
         // Set the global parameters.
@@ -130,90 +98,42 @@ public class IntactViewBean implements Serializable {
     }
 
     /**
-     * This method adds the status attribute to all the elements with AC as an
-     * attribute except for Xref elements.
+     * This method adds the status attribute to the root node and to all the
+     * elements defined in
+     * {@link uk.ac.ebi.intact.application.search.struts.framework.util.TreeViewAction#ELEMENT_TAGS}.
+     * The attribute is set to false.
      */
     public void addStatusNodes() {
         // Set the status in the root element.
-        this.rootNode.getDocumentElement().setAttribute(STATUS_ATTRIBUTE, FALSE_STR);
+        this.rootNode.getDocumentElement().setAttribute(
+                TreeViewAction.STATUS_ATTRIBUTE, TreeViewAction.FALSE_STR);
         // Now go through the expandable list.
-        for (Iterator iter = expElementTags.iterator(); iter.hasNext(); ) {
+        for (Iterator iter = TreeViewAction.ELEMENT_TAGS.iterator(); iter.hasNext(); ) {
             NodeList elements = this.rootNode.getElementsByTagName((String) iter.next());
             // Process each element.
-            //int length = elements.getLength();
             for (int length = elements.getLength(), i = 0; i < length; i++) {
                 Element element = (Element) elements.item(i);
-                element.setAttribute(STATUS_ATTRIBUTE, FALSE_STR);
+                element.setAttribute(TreeViewAction.STATUS_ATTRIBUTE,
+                        TreeViewAction.FALSE_STR);
             }
         }
     }
 
     /**
-     * Changes the status for given AC.
-     * @param ac the AC to search for the element.
+     * Apply the action for the root and all its subnodes.
+     * @param action the action to apply.
      */
-    public void changeElementStatus(String ac) {
-        // Set the status in the root element.
-        Element rootElement = this.rootNode.getDocumentElement();
-        if (rootElement.getAttribute("ac").equals(ac)) {
-            toggle(rootElement);
-            // Changed the status at root; no need to check for other nodes
-            // (assuming that root AC is unique).
-            return;
-        }
-        // Now go through the expandable list.
-        for (Iterator iter = expElementTags.iterator(); iter.hasNext(); ) {
-            NodeList elements = this.rootNode.getElementsByTagName((String) iter.next());
-            // Process each element.
-            int length = elements.getLength();
-            for (int i = 0; i < length; i++) {
-                Element element = (Element) elements.item(i);
-                if (element.getAttribute("ac").equals(ac)) {
-                    toggle(element);
-                }
-            }
-        }
-    }
-
-    // Helper methods.
+//    public void setTreeStatus(TreeViewAction action) {
+//        Element rootElement = this.rootNode.getDocumentElement();
+//        String ac = rootElement.getAttribute("ac");
+//        this.setTreeStatus(action, ac);
+//    }
 
     /**
-     * Changes the status of given element.
-     * @param element the element to change the status.
+     * Apply the action for given ac.
+     * @param action the action to apply.
      */
-    private void toggle(Element element) {
-        String attValue = element.getAttribute(STATUS_ATTRIBUTE);
-        String value = attValue.equals(FALSE_STR) ? TRUE_STR : FALSE_STR;
-        element.setAttribute(STATUS_ATTRIBUTE, value);
+    public void setTreeStatus(TreeViewAction action, String ac) {
+        action.apply(this.rootNode, ac);
     }
-
-    // All the following methods for debugging purposes only.
-    /**
-     * Helper method to dump the XML tree.
-     * @param filename the name of the file to dump the XML tree.
-     */
-//    public void dumpXML(String filename) {
-//        StreamResult sr = new StreamResult(new File(filename));
-//        DOMSource dom = new DOMSource(this.rootNode);
-//        TransformerFactory tFactory = TransformerFactory.newInstance();
-//        Transformer transformer = null;
-//        try {
-//            transformer = tFactory.newTransformer();
-//            transformer.transform(dom, sr);
-//        }
-//        catch (TransformerConfigurationException e) {
-//            e.printStackTrace();
-//        }
-//        catch (TransformerException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    public Document root() {
-//        return this.rootNode;
-//    }
-//
-//    public List getList() {
-//        return IntactViewBean.expElementTags;
-//    }
-}
+ }
