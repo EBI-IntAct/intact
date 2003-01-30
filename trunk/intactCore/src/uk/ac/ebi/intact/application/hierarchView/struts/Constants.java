56f736724087bb55ef7e0b142605d61b8cd5b674
skerrien <samuel.kerrien@gmail.com>
2003-01-30 15:22:51 +0000

    Clean the code

--- trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/Constants.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/Constants.java
@@ -69,54 +69,6 @@ public final class Constants {
      */
     public static final String ATTRIBUTE_GRAPH = "graph";
 
-
-
-
-    // ******************************************************* Attributes of the graph components
-
-    // /**
-//    * The attribute which allows to access the width of the image graph.
-//    */
-//   public static final String ATTRIBUTE_LENGTH = "length";
-
-//   /**
-//    * The attribute which allows to access the heigth of the image graph.
-//    */
-//   public static final String ATTRIBUTE_HEIGHT = "height";
-
-//   /**
-//    * The X coordinate of a Node int the image.
-//    */
-//   public static final String ATTRIBUTE_COORDINATE_X = "x";
-
-//   /**
-//    * The Y coordinate of a Node int the image.
-//    */
-//   public static final String ATTRIBUTE_COORDINATE_Y = "y";
-
-//   /**
-//    * The attribute which allows to access the label of a Node.
-//    */
-//   public static final String ATTRIBUTE_LABEL = "label";
-
-//   /**
-//    * The attribute which allows to access the color of a Node. 
-//    */
-//   public static final String ATTRIBUTE_COLOR_NODE = "colorNode";
-
-//   /**
-//    * The attribute which allows to access the color of a Label. 
-//    */
-//   public static final String ATTRIBUTE_COLOR_LABEL = "colorLabel";
-
-//   /**
-//    * The attribute which allows to access the visibility of a Node
-//    */
-//   public static final String ATTRIBUTE_VISIBLE = "visible";
-
-
-
-
 } // Constants
 
 
