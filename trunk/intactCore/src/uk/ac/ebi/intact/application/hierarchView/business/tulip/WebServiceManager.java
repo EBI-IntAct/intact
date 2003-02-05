09b4260cf60a3d62b56a04069fd63ee0a265dc05
skerrien <samuel.kerrien@gmail.com>
2003-02-05 14:50:12 +0000

    Automation of the web service managment (deploy, undeploy)

--- trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/business/tulip/WebServiceManager.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/business/tulip/WebServiceManager.java
@@ -9,18 +9,27 @@ package uk.ac.ebi.intact.application.hierarchView.business.tulip;
 import org.apache.axis.utils.Options;
 import org.apache.axis.client.AdminClient;
 
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
 import java.io.InputStream;
+import java.util.Properties;
+
+import uk.ac.ebi.intact.application.hierarchView.business.PropertyLoader;
+import uk.ac.ebi.intact.application.hierarchView.struts.Constants;
 
 /**
- * Allow to deploy and undeploy the web service
+ * Allows to deploy and undeploy the web service
  *
  * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  */
 
-public class WebServiceManager {
+public class WebServiceManager implements ServletContextListener {
 
-    private String deploymentFile;
-    private String undeploymentFile;
+    private String deploymentFile   = null;
+    private String undeploymentFile = null;
+
+    private boolean started = false;
+
 
     public String getDeploymentFile() {
         return deploymentFile;
@@ -39,6 +48,55 @@ public class WebServiceManager {
     }
 
 
+    /**
+     * Tells is the Manager is properly initialized
+     * @return true is the manager is initialized, false in the other way round.
+     */
+    public boolean isInitialized () {
+        return ((null != deploymentFile) && (null != undeploymentFile));
+    }
+
+    /**
+     * Initialize the web service manager by reading the properties file
+     * and setting needed files names.
+     * @throws Exception
+     */
+    public void init () throws Exception {
+
+        // The configuration file.
+        String configFile = Constants.WEB_SERVICE_PROPERTY_FILE;
+
+        System.out.println ("Loading web service's properties");
+        Properties props = PropertyLoader.load (configFile);
+
+        if (null != props) {
+            String deploymentFile   = props.getProperty ("webService.deployment");
+            String undeploymentFile = props.getProperty ("webService.undeployment");
+
+            System.out.println ("Properties Loaded :" +
+                                "\nwebService.deployment = "   + deploymentFile +
+                                "\nwebService.undeployment = " + undeploymentFile);
+
+            if ((null == deploymentFile) || (null == undeploymentFile)) {
+                String msg = "Error, can't initialize WebServiceManager : unable to read properties file.";
+                throw new Exception (msg);
+            }
+
+            this.setDeploymentFile (deploymentFile);
+            this.setUndeploymentFile (undeploymentFile);
+
+            System.out.println ("Properties loaded.");
+        }
+    } // init
+
+
+    /**
+     * Tells is the web service is running.
+     * @return true is the web service is running, or false in the other way round.
+     */
+    public boolean isRunning () {
+        return started;
+    }
 
     /**
      * Undeploy the web service according to collected data
@@ -46,8 +104,14 @@ public class WebServiceManager {
      */
     public void undeploy () throws Exception {
 
-        processWsddFile (this.undeploymentFile);
-
+        try {
+            if (false == isInitialized()) init();
+            processWsddFile (this.undeploymentFile);
+            // web service stopped
+            started = false;
+        } catch (Exception e) {
+          throw e;
+        }
     } // undeploy
 
 
@@ -57,8 +121,14 @@ public class WebServiceManager {
      */
     public void deploy () throws Exception {
 
-        processWsddFile (this.deploymentFile);
-
+        try {
+            if (false == isInitialized()) init();
+            processWsddFile (this.deploymentFile);
+            // web service started
+            started = true;
+        } catch (Exception e) {
+          throw e;
+        }
     } // deploy
 
 
@@ -103,5 +173,28 @@ public class WebServiceManager {
         else
             throw (new Exception ("Unable to open the WSDD file: " + wsddFile));
     }  // processWsddFile
+
+
+    // ****************************************
+    // Implementation of ServletContextListener
+    public void contextInitialized (ServletContextEvent event) {
+    }
+
+
+    public void contextDestroyed (ServletContextEvent event) {
+
+        // UNDEPLOY THE WEB SERVICE
+        // Could throw an exception if Axis is running on the same Tomcat server,
+        // seems that connection with Axis are already closed when we try to stop
+        // the web service.
+        try {
+            this.undeploy();
+            System.out.println ("Tulip web service undeployed successfully");
+        } catch (Exception e) {
+            // TODO : log it
+            System.out.println ("Unable to undeploy the web service\n" + e.getMessage());
+            e.printStackTrace();
+        }
+    } // contextDestroyed
 
 }  // WebServiceManager
