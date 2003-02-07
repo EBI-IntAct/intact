d77077b9abb22f1778acd3fe565ba3e4d27e9f71
skerrien <samuel.kerrien@gmail.com>
2003-02-07 11:54:21 +0000

    clean code

--- trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/business/servlet/GenerateImage.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/business/servlet/GenerateImage.java
@@ -16,99 +16,87 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
-   Purpose: <br>
-   For a bean given in the session, forward an image (png) to be displayed.
-*/
+ Purpose: <br>
+ For a bean given in the session, forward an image (png) to be displayed.
+ */
 public class GenerateImage extends HttpServlet {
 
-  // * Public servlet methods
+    // * Public servlet methods
 
-  public void init(ServletConfig config)
-    throws ServletException {
-    
-    // MANDATORY!
-    super.init(config);
-    
-    // get init parameters
-    // todo ...
-  }
-  
-  
-  /**
-   *
-   *
-   *
-   */
-  public void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse)
-    throws ServletException{
-        
-    try
-      {
-    // binary output
-    ServletOutputStream out = aResponse.getOutputStream();
-    
-    // take the parameter in the request
-    String format = (String) aRequest.getParameter("format");
+    public void init(ServletConfig config)
+            throws ServletException {
 
-    String className = null;
-    Properties propertiesBusiness = PropertyLoader.load (uk.ac.ebi.intact.application.hierarchView.business.Constants.PROPERTY_FILE);
+        // MANDATORY!
+        super.init(config);
 
-    if (null != propertiesBusiness) {
-      className = propertiesBusiness.getProperty ("hierarchView.image.format." + format + ".class" );
+        // get init parameters
+        // todo ...
     }
 
-    ConvertSVG convert = ConvertSVG.getConvertSVG(className);
 
-    // Encode the off-screen image into a JPEG and send it to the client
+    /**
+     *
+     *
+     *
+     */
+    public void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse)
+            throws ServletException{
 
-    String typeMime = convert.getMimeType();
-    aResponse.setContentType(typeMime);
-   
+        try
+        {
+            // binary output
+            ServletOutputStream out = aResponse.getOutputStream();
 
-    // get the current user session
-    HttpSession session = aRequest.getSession();
-    ImageBean ib = (ImageBean) session.getAttribute (uk.ac.ebi.intact.application.hierarchView.struts.Constants.ATTRIBUTE_IMAGE_BEAN);
+            // take the parameter in the request
+            String format = aRequest.getParameter("format");
 
-    if (null == ib) {
-      System.out.println("ib == null");
-      return;
-    }
+            String className = null;
+            Properties propertiesBusiness = PropertyLoader.load (uk.ac.ebi.intact.application.hierarchView.business.Constants.PROPERTY_FILE);
 
-    Document document = ib.getDocument();
-    if (null != document) {
-      try {
-	byte[] imageData = convert.convert(document);
-	if (null != imageData) {
-	  System.out.println("lenght = " + imageData.length);
-	  out.write (imageData);
-	}
-	else System.out.println("imageData is null");
-      }
-      catch (Exception e) {
-	e.printStackTrace(); 
-	System.out.println("Impossible to convert the document");
-      }
-    }
-    else System.out.println("document is null");
-    
+            if (null != propertiesBusiness) {
+                className = propertiesBusiness.getProperty ("hierarchView.image.format." + format + ".class" );
+            }
 
-    out.flush ();
-    out.close ();
-      }
-    catch (IOException e) {return;}
+            ConvertSVG convert = ConvertSVG.getConvertSVG(className);
 
-  } // doGet
-  
+            // Encode the off-screen image into a JPEG and send it to the client
 
-  /**
-   *
-   *
-   *
-   */
-  public void
-    destroy(){}
+            String typeMime = convert.getMimeType();
+            aResponse.setContentType(typeMime);
 
-  
+
+            // get the current user session
+            HttpSession session = aRequest.getSession();
+            ImageBean ib = (ImageBean) session.getAttribute (uk.ac.ebi.intact.application.hierarchView.struts.Constants.ATTRIBUTE_IMAGE_BEAN);
+
+            if (null == ib) {
+                System.out.println("ib == null");
+                return;
+            }
+
+            Document document = ib.getDocument();
+            if (null != document) {
+                try {
+                    byte[] imageData = convert.convert(document);
+                    if (null != imageData) {
+                        System.out.println("lenght = " + imageData.length);
+                        out.write (imageData);
+                    }
+                    else System.out.println("imageData is null");
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Impossible to convert the document");
+                }
+            }
+            else System.out.println("document is null");
+
+            out.flush ();
+            out.close ();
+        }
+        catch (IOException e) {return;}
+
+    } // doGet
 
 } // GenerateImage
 
