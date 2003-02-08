010f21363c0cb1485747a41ab5d7ebe552dc2526
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-02-08 11:25:04 +0000

    Replace Constants

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/StrutsConstants.java
@@ -0,0 +1,97 @@
+package uk.ac.ebi.intact.application.hierarchView.struts;
+
+
+/**
+ * Manifest constants for the hierarchView application.
+ *
+ * @author Samuel Kerrien - skerrien@ebi.ac.uk
+ */
+
+public final class StrutsConstants {
+
+
+    // ******************************************************* Properties file
+
+    /**
+     * Where to find the properties file
+     */
+    public static final String WEB_SERVICE_PROPERTY_FILE = "/config/WebService.properties";
+
+    public static final String PROPERTY_FILE = "/config/Struts.properties";
+
+    public static final String PROPERTY_FILE_HIGHLIGHTING = "/config/Highlighting.properties";
+
+
+    // ******************************************************* Session Attributes
+
+    /**
+     * The name of the AC attribute set in the session.
+     */
+    public static final String ATTRIBUTE_AC = "AC";
+
+    /**
+     * The name of the depth attribute set in the session.
+     */
+    public static final String ATTRIBUTE_DEPTH = "depth";
+
+    /**
+     * The name of the noDepthLimit flag set in the session.
+     */
+    public static final String ATTRIBUTE_NO_DEPTH_LIMIT = "noDepthLimit";
+
+    /**
+     * The name of the methodLabel attribute set in the session.
+     */
+    public static final String ATTRIBUTE_METHOD_LABEL = "methodLabel";
+
+    /**
+     * The name of the methodClass attribute set in the session.
+     */
+    public static final String ATTRIBUTE_METHOD_CLASS = "methodClass";
+
+    /**
+     * The name of the behaviour attribute set in the session.
+     */
+    public static final String ATTRIBUTE_BEHAVIOUR = "behaviour";
+
+    /**
+     * The name of the keys attribute set int the session.
+     */
+    public static final String ATTRIBUTE_KEYS = "keys";
+
+    /**
+     * The name of the imageBean attribute set in the session.
+     */
+    public static final String ATTRIBUTE_IMAGE_BEAN = "imageBean";
+
+    /**
+     * The name of an attribute set int the session.
+     */
+    public static final String ATTRIBUTE_GRAPH = "graph";
+
+} // StrutsConstants
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
