64092c90842517a4b599a30cbc85fc4a51aab003
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-02-14 09:24:31 +0000

    Update. Now deleting old data before loading new data. Improved command line interface.

--- trunk/intactCore/src/uk/ac/ebi/intact/util/GoTools.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/GoTools.java
@@ -29,12 +29,6 @@ import org.apache.regexp.RESyntaxException;
  */
 public class GoTools {
 
-
-    /**
-     * If true, delete old references from a CvObject during an update.
-     */
-    private static final boolean DELETEREFERENCES = true;
-
     /**
      * Maximum length of a shortLabel
      */
@@ -111,16 +105,20 @@ public class GoTools {
         Update references depending on constant setting.
         */
 
-        CvObject current = (CvObject)helper.getObjectByXref(targetClass,
-                               ((Vector)definition.get("goid")).elementAt(0).toString());
-
-
-
         // Get or create CvObject
+        CvObject current;
+        current = (CvObject)helper.getObjectByXref(targetClass,
+                                       ((Vector)definition.get("goid")).elementAt(0).toString());
+
         if (null == current){
             current = (CvObject) targetClass.newInstance();
             current.setOwner((Institution) helper.getObjectByLabel(Institution.class, "EBI"));
-            helper.create(current);
+        } else {
+            // Delete all old data
+            helper.deleteAllElements(current.getXref());
+            current.getXref().removeAll(current.getXref());
+            helper.deleteAllElements(current.getAnnotation());
+            current.getAnnotation().removeAll(current.getAnnotation());
         }
 
         // Update shortLabel. Label has to be unique!
@@ -133,6 +131,10 @@ public class GoTools {
 
         // Update fullName
         current.setFullName(goTerm.substring(0,Math.min(goTerm.length(), MAX_NAME_LEN)));
+
+        // Now we have a valid database object with all constraints
+        // fulfilled. Update or create object.
+        helper.update(current);
 
         // Update GO term
         // The term needs to be stored separately, as the length may exceed the maximum fullName length.
@@ -225,9 +227,10 @@ public class GoTools {
      * Read a GO definition flat file from the given URL,
      * insert or update all terms into aTargetClass.
      */
-    public static void insertGoDefinitions (String aSourceUrl,
+    public static void insertGoDefinitions (Class aTargetClass,
                                             IntactHelper helper,
-                                            Class aTargetClass) throws Exception {
+                                            String aSourceUrl)
+                                             throws Exception {
 
         Hashtable goRecord;
 
@@ -250,9 +253,9 @@ public class GoTools {
 
                 // Insert the definition
                 try {
-                    helper.startTransaction();
+                    //helper.startTransaction();
                     insertDefinition(goRecord, helper, aTargetClass);
-                    helper.finishTransaction();
+                    //helper.finishTransaction();
                 }
                 catch (IntactException e){
                     // helper.undoTransaction();
@@ -266,8 +269,8 @@ public class GoTools {
     /**
      * Write a Controlled vocabulary in GO definition format flat file.
      */
-    public static void writeGoDefinitions(IntactHelper helper,
-                                          Class aTargetClass)
+    public static void writeGoDefinitions(Class aTargetClass,
+                                          IntactHelper helper)
         throws IntactException {
 
         // Get all members of the class
@@ -326,6 +329,16 @@ public class GoTools {
     public static String toGoString(CvObject current){
         StringBuffer buf = new StringBuffer();
 
+        Collection annotation = current.getAnnotation();
+        for (Iterator iterator = annotation.iterator(); iterator.hasNext();) {
+            Annotation a = (Annotation) iterator.next();
+            if (a.getCvTopic().getShortLabel().equals("GO term")){
+                buf.append("term: ");
+                buf.append(a.getAnnotationText());
+                buf.append("\n");
+            }
+        }
+
         Collection xref = current.getXref();
         for (Iterator iterator = xref.iterator(); iterator.hasNext();) {
             Xref x = (Xref) iterator.next();
@@ -336,14 +349,10 @@ public class GoTools {
             }
         }
 
-        Collection annotation = current.getAnnotation();
+        annotation = current.getAnnotation();
         for (Iterator iterator = annotation.iterator(); iterator.hasNext();) {
             Annotation a = (Annotation) iterator.next();
-            if (a.getCvTopic().getShortLabel().equals("GO term")){
-                buf.append("term: ");
-                buf.append(a.getAnnotationText());
-                buf.append("\n");
-            }
+
             if (a.getCvTopic().getShortLabel().equals("GO description")){
                 buf.append("definition: ");
                 buf.append(a.getAnnotationText());
@@ -361,18 +370,57 @@ public class GoTools {
         return buf.toString();
     }
 
-
+    /** Load or unload Controlled Vocabularies in GO format.
+     *  Usage:
+     *  GoTools upload IntAct_classname Go_File_URL  |
+     *  GoTools download IntAct_classname
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
-        // Create database access object
-        IntactHelper helper = new IntactHelper();
+        final String usage = "Usage:\n" +
+                             "GoTools upload IntAct_classname Go_File_URL    OR\n" +
+                             "GoTools download IntAct_classname";
+        Class targetClass;
 
-        // Read definitins
-        insertGoDefinitions("file:///homes/hhe/tmp/GO_small.defs", helper, CvInteractionType.class);
+        try {
+            // Check parameters
+            if (args.length < 2 || args.length > 3) {
+                throw new IntactException("Invalid number of arguments.\n" + usage);
+            }
 
-        // Write definitions for verification
-        writeGoDefinitions(helper,
-                CvInteractionType.class);
+            try {
+                targetClass = Class.forName(args[1]);
+            } catch (ClassNotFoundException e) {
+                throw new IntactException("Class " + args[1] + " not found.\n" + usage);
+            }
 
+
+            if (args[0].equals("upload")) {
+                if (args.length != 3)
+                    throw new IntactException("Invalid number of arguments.\n" + usage);
+
+                // Create database access object
+                IntactHelper helper = new IntactHelper();
+
+                // Read definitins
+                insertGoDefinitions(targetClass, helper, args[2]);
+
+            } else if (args[0].equals("download")) {
+
+                // Create database access object
+                IntactHelper helper = new IntactHelper();
+
+                // Write definitions
+                writeGoDefinitions(targetClass, helper);
+
+            } else
+                throw new IntactException("Invalid argument " + args[0] + "\n" + usage);
+
+        } catch (IntactException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
