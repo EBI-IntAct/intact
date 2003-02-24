dc0fb57dd4afc054a01d892d927ed486ac30c67f
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-02-24 17:50:37 +0000

    CvInteraction can now model GO style DAG controlled vocabularies.

--- trunk/intactCore/src/uk/ac/ebi/intact/model/CvInteraction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/CvInteraction.java
@@ -1,22 +1,32 @@
-/*
-Copyright (c) 2002 The European Bioinformatics Institute, and others.  
-All rights reserved. Please see the file LICENSE 
-in the root directory of this distribution.
-*/
+/** Java class "CvInteraction.java" generated from Poseidon for UML.
+ *  Poseidon for UML is developed by <A HREF="http://www.gentleware.com">Gentleware</A>.
+ *  Generated with <A HREF="http://jakarta.apache.org/velocity/">velocity</A> template engine.
+ */
 package uk.ac.ebi.intact.model;
 
 import java.util.*;
 
 /**
+ * <p>
  * The method by which the Interaction has been determined.
+ * </p>
+ * <p>
  * 
- * @intact.example co-immunoprecipitation
+ * </p>
+ * <p>
+ * 
+ * @example co-immunoprecipitation
+ * </p>
+ * <p>
  * @author hhe
+ * </p>
+ * </p>
  */
-public class CvInteraction extends CvObject {
+public class CvInteraction extends CvDagObject {
 
 
 } // end CvInteraction
+
 
 
 
