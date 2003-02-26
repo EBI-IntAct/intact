eed6356b81fb113b7c0dce690ce37e7393bb3628
clewing <clewing@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-02-26 15:42:45 +0000

    updates to XmlBuilder

--- trunk/intactCore/src/uk/ac/ebi/intact/util/XmlBuilder.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/XmlBuilder.java
@@ -305,6 +305,14 @@ public class XmlBuilder implements Serializable {
                         System.out.println("Where I am in the tree: " + ((Element)walker.getCurrentNode()).getAttribute("ac"));
                         System.out.println("setting walker back to replaced node.....");
                         walker.setCurrentNode(newNode);
+
+                        //finish - design decision to end on first match
+                        //BUT this means that if later the second or later occurrence
+                        //of this AC in the tree is clicked, then only the
+                        //first (wrong) one (and not the one we picked!) will change!!
+                        //If we don't break though, only the last one gets changed
+                        //and all the others disappear!!
+                        break;
                     }
                     //carry on - may be more than one element with this AC...
                 }
@@ -606,103 +614,5 @@ public class XmlBuilder implements Serializable {
         }
         return result;
     }
-
-     //------------------------ other stuff ---------------------------
-
-  /**
-   Searchs and returns the first node which is identified by the input
-   parameters, localname and namespaceURI. The localname and namespaceURI
-   should uniquely identify a certain element type. Performs a depth first
-   search.
-   @param nodename The nodename of an element
-   @param root The DOM tree element to be searched
-   @returns null if no matching element found, otherwise a reference to the first
-   matching element.
-   **/
-   /*public Node findElementNode(String searchnodename, Node root){
-
-     Node matchingNode = null;
-
-
-     //Check to see if root is the desired element. If so return a root.
-     String nodeName = root.getNodeName();
-
-     if((nodeName != null) & (nodeName.equals(searchnodename)))
-                return root;
-
-     //Check to see if root has any children if not return null
-     if(!(root.hasChildNodes()))
-                return null;
-
-     //Root has children, so continue searching for them
-     NodeList childNodes = root.getChildNodes();
-     int noChildren = childNodes.getLength();
-     for(int i = 0; i < noChildren; i++){
-         if(matchingNode == null){
-                Node child = childNodes.item(i);
-                matchingNode = findElementNode(searchnodename,child);
-         } else break;
-
-     }
-
-     return matchingNode;
-   }
-*/
-
-   /**
-    Will create a documentFragment of the replacingDocument, will import the
-    replacingDocument as a node of the replacedDocument, and then will replace
-    the replaceNode with the documentFragment of replacingDocument.
-    @param replacedDocument The document which will have a node replace
-    @param replacingDocument The document that will replace a node
-    @param replacedNode The node in replacedDocument that will be replaced
-    @return The new version of replacedDocument will replacedNode replaced
-    **/
-    /*public Node replaceNode(Document replacedDocument,
-                                Document replacingDocument,
-                                Node replacedNode){
-
-    //Create a documentFragment of the replacingDocument
-    DocumentFragment docFrag = replacingDocument.createDocumentFragment();
-    Element rootElement = replacingDocument.getDocumentElement();
-    docFrag.appendChild(rootElement);
-
-
-    //Import docFrag under the ownership of replacedDocument
-    Node replacingNode =
-        ((replacedDocument).importNode(docFrag, true));
-
-
-    //In order to replace the node need to retrieve replacedNode's parent
-    Node replaceNodeParent = replacedNode.getParentNode();
-    replaceNodeParent.replaceChild(replacingNode, replacedNode);
-    return replacedDocument;
-    }
-*/
-
-    //------- just for info on usage ----------------
-
-        //The first xml file is the replacedDocument, and the second is the
-        //replacingDocument
-
-       /*try {
-        ReplacingNode replaceFunction = new ReplacingNode();
-        Document replacingDocument = replaceFunction.parseFileToDom(args[1]);
-        Document replacedDocument = replaceFunction.parseFileToDom(args[0]);
-        Node replacedNode = replaceFunction.
-                        findElementNode(args[2], replacedDocument.getDocumentElement());
-
-        if(replacedNode != null){
-        Node modifiedReplacedDocument = replaceFunction.
-                replaceNode(replacedDocument, replacingDocument, replacedNode);
-        replaceFunction.printOutDocument(replacedDocument, "output.xml");
-        }else
-                System.out.println("replace node is null");
-
-       }catch(Exception e){
-        e.printStackTrace();
-       }
-     }*/
-
 
 }
