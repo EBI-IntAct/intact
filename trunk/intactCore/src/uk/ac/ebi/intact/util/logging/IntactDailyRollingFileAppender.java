99d914db73d7daa168f7f7d85a2fae35bd50df74
skerrien <samuel.kerrien@gmail.com>
2003-03-03 09:40:23 +0000

    Moved from application.commons.logging

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/logging/IntactDailyRollingFileAppender.java
@@ -0,0 +1,53 @@
+package uk.ac.ebi.intact.util.logging;
+
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+import java.io.IOException;
+
+public class IntactDailyRollingFileAppender extends org.apache.log4j.DailyRollingFileAppender {
+
+    /**
+     * The option the user can use to add the hostname in the file path
+     */
+    private static final String HOSTNAME_FLAG = "$hostname";
+
+    private static String hostname;
+
+    static {
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            hostname="noHostnameFound";
+        }
+    }
+
+    /**
+     * Over definition of that method inherited from FileAppender.
+     * It allows to specify in the file path the hostname by using $hostname
+     *
+     * e.g. if myAppender.File=/tmp/$hostname_myfile.log and your host is mycomputer.ebi.ac.uk,
+     * it will become /tmp/mycomputer.ebi.ac.uk_myfile.log,
+     *
+     * @throws IOException
+     */
+    public void setFile(String fileName,
+                        boolean append,
+                        boolean bufferedIO,
+                        int bufferSize)
+            throws IOException {
+
+        int indexOfFlag = fileName.indexOf(HOSTNAME_FLAG);
+        if (indexOfFlag >= -1) {
+            fileName = fileName.substring(0, indexOfFlag) +
+                       hostname +
+                       fileName.substring(indexOfFlag + HOSTNAME_FLAG.length(), fileName.length());
+        }
+
+        super.setFile (fileName,
+                append,
+                bufferedIO,
+                bufferSize);
+
+    }
+}
