9f936151f2868c683b8643b21126dc437fe2821d
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-03-05 11:37:20 +0000

    Added the form to fill CV info data; moved the common code to provate methods to keep the execute method simple

--- trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/controller/CvEditPreAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/cvedit/struts/controller/CvEditPreAction.java
@@ -7,11 +7,17 @@ in the root directory of this distribution.
 package uk.ac.ebi.intact.application.cvedit.struts.controller;
 
 import org.apache.struts.action.*;
+import org.apache.struts.config.ModuleConfig;
+import org.apache.struts.config.FormBeanConfig;
+import org.apache.struts.Globals;
+import org.apache.commons.beanutils.DynaBean;
+
 import javax.servlet.http.*;
 
 import uk.ac.ebi.intact.application.cvedit.struts.framework.util.CvEditConstants;
 import uk.ac.ebi.intact.application.cvedit.struts.framework.CvAbstractAction;
 import uk.ac.ebi.intact.application.cvedit.struts.view.EditForm;
+import uk.ac.ebi.intact.application.cvedit.struts.view.CvViewBean;
 import uk.ac.ebi.intact.application.cvedit.business.IntactUserIF;
 
 /**
@@ -30,15 +36,12 @@ public class CvEditPreAction extends CvAbstractAction {
      * control should be forwarded, or null if the response has
      * already been completed.
      *
-     * @param mapping - The <code>ActionMapping</code> used to select this instance
-     * @param form - The optional <code>ActionForm</code> bean for this request (if any)
-     * @param request - The HTTP request we are processing
-     * @param response - The HTTP response we are creating
-     *
-     * @return - represents a destination to which the controller servlet,
-     * <code>ActionServlet</code>, might be directed to perform a RequestDispatcher.forward()
-     * or HttpServletResponse.sendRedirect() to, as a result of processing
-     * activities of an <code>Action</code> class
+     * @param mapping the <code>ActionMapping</code> used to select this instance
+     * @param form the optional <code>ActionForm</code> bean for this request (if any)
+     * @param request the HTTP request we are processing
+     * @param response the HTTP response we are creating
+     * @return always returns successful mapping as this action class sets data for
+     * a JSP to display.
     */
    public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
@@ -49,41 +52,78 @@ public class CvEditPreAction extends CvAbstractAction {
         // Handler to the Intact User.
         IntactUserIF user = super.getIntactUser(session);
 
-//        ModuleConfig appConfig = (ModuleConfig) request.getAttribute(Globals.MODULE_KEY);
-//        FormBeanConfig config = appConfig.findFormBeanConfig("commentEditForm");
-//        DynaActionFormClass dynaClass =
-//            DynaActionFormClass.createDynaActionFormClass(config);
-//
-//        DynaBean dynaForm = dynaClass.newInstance();
-//        dynaForm.set("annotations", user.getView().getAnnotations().toArray(new CommentBean[0]));
-//        super.log("In edit pre action 2");
-//        Object[] annots = (Object[]) dynaForm.get("annotations");
-//        for (int i = 0; i < annots.length; i++) {
-//            super.log("Annot: " + i + " - " + ((CommentBean) annots[i]).getDescription());
-//        }
-//        super.log("In edit pre action 3 -- " + annots.length);
+        // The current user view.
+        CvViewBean viewbean = user.getView();
 
-//        request.setAttribute("commentEditForm", dynaForm);
+        // Fill the form to edit short label and full name.
+        DynaBean dynaForm = this.getCvInfoForm(request, session);
+        dynaForm.set("ac", viewbean.getAc());
+        dynaForm.set("shortLabel", viewbean.getShortLabel());
+        dynaForm.set("fullName", viewbean.getFullName());
 
         // Fill data for annotations.
-        EditForm ceForm = (EditForm) session.getAttribute(
+        EditForm ceForm = this.getEditForm(session,
                 CvEditConstants.COMMENT_EDIT_FORM);
-        if (ceForm == null) {
-            ceForm = new EditForm();
-            session.setAttribute(CvEditConstants.COMMENT_EDIT_FORM, ceForm);
-        }
-        ceForm.setItems(user.getView().getAnnotations());
+        ceForm.setItems(viewbean.getAnnotations());
 
         // Fill data for xrefs.
-        EditForm xeForm = (EditForm) session.getAttribute(
-                CvEditConstants.XREF_EDIT_FORM);
-        if (xeForm == null) {
-            xeForm = new EditForm();
-            session.setAttribute(CvEditConstants.XREF_EDIT_FORM, xeForm);
-        }
-        xeForm.setItems(user.getView().getXrefs());
+        EditForm xeForm = this.getEditForm(session, CvEditConstants.XREF_EDIT_FORM);
+        xeForm.setItems(viewbean.getXrefs());
 
         // Move to the results page.
         return mapping.findForward(CvEditConstants.FORWARD_SUCCESS);
+    }
+
+    /**
+     * Returns a DynaBean from the session; new bean is created if the bean
+     * doesn't exist in the session (the new bean is stored in a session).
+     * @param request the HTTP request to get the application configuration.
+     * @param session the HTTP session object to access and store the bean.
+     * @return <code>DynaBean</code> instance either retrieved from
+     * <code>session</code> or a new object.
+     * @throws InstantiationException errors in creating the bean
+     * @throws IllegalAccessException errors in creating the bean
+     *
+     * <pre>
+     * post: session.getAttribute(CvEditConstants.CV_INFO_FORM) != Null
+     * </pre>
+     */
+    private DynaBean getCvInfoForm(HttpServletRequest request, HttpSession session)
+            throws InstantiationException, IllegalAccessException {
+        // Fill the form to edit short label and full name.
+        DynaBean dynaForm = (DynaBean) session.getAttribute(
+                CvEditConstants.CV_INFO_FORM);
+        if (dynaForm == null) {
+            ModuleConfig appConfig =
+                    (ModuleConfig) request.getAttribute(Globals.MODULE_KEY);
+            FormBeanConfig config = appConfig.findFormBeanConfig(
+                    CvEditConstants.CV_INFO_FORM);
+            DynaActionFormClass dynaClass =
+                DynaActionFormClass.createDynaActionFormClass(config);
+            dynaForm = dynaClass.newInstance();
+            session.setAttribute(CvEditConstants.CV_INFO_FORM, dynaForm);
+            super.log("Created cvinfo instance ");
+        }
+        return dynaForm;
+    }
+
+    /**
+     * Returns the edit form for given type.
+     * @param session the HTTP session object to access and store the bean.
+     * @param type the form type; CvEditConstants.COMMENT_EDIT_FORM or
+     * CvEditConstants.XREF_EDIT_FORM
+     * @return the form created for given <code>type</code>.
+     *
+     * <pre>
+     * post: session.getAttribute(type) != Null
+     * </pre>
+     */
+    private EditForm getEditForm(HttpSession session, String type) {
+        EditForm ceForm = (EditForm) session.getAttribute(type);
+        if (ceForm == null) {
+            ceForm = new EditForm();
+            session.setAttribute(type, ceForm);
+        }
+        return ceForm;
     }
 }
