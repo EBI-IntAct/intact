c37754c958c761bb34709c4a3c35191554ffb40c
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-03-25 11:44:44 +0000

    Initial checkin - managing display of Struts errors with a framed web application

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/commons/struts/taglibs/SaveErrorsTag.java
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.application.commons.struts.taglibs;
+
+import uk.ac.ebi.intact.application.hierarchView.business.Constants;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.log4j.Logger;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.Globals;
+
+/**
+ * That class allows to save in the session scope any ActionErrors
+ * available in the request scope (in pageRequest)
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+
+public class SaveErrorsTag extends TagSupport {
+
+    static Logger logger = Logger.getLogger (Constants.LOGGER_NAME);
+
+    /**
+     * Skip the body content.
+     */
+    public int doStartTag() throws JspTagException {
+        return SKIP_BODY;
+    }
+
+
+    /**
+     * In case some errors (ActionErrors) are discovered, let's save them in the
+     * session for later displaying. The page in charge of the displaying will
+     * have to check it any errors are stored and need to be displayed.
+     *
+     * @throws JspException
+     */
+    public int doEndTag() throws JspException {
+
+        ActionErrors errors = (ActionErrors) pageContext.findAttribute(Globals.ERROR_KEY);
+
+        if ( null != errors ) {
+            HttpSession session = pageContext.getSession();
+            session.setAttribute(Globals.ERROR_KEY, errors);
+        }
+
+        return EVAL_PAGE; // the rest of the calling JSP is evaluated
+    }
+}
