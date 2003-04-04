1a983d258a7b7d6ad7b2946fb3d9aebf72a696b9
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-04 13:39:42 +0000

    bugfix

--- trunk/intactCore/sql/postgres/list_interactions.sql
+++ trunk/intactCore/sql/postgres/list_interactions.sql
@@ -22,8 +22,8 @@
 --- Setup layout
 
 --- Main query
-SELECT e.shortlabel Experiment, i.shortLabel Interaction, 
-       m.shortLabel Molecule, r.shortLabel Role
+SELECT e.shortlabel, i.shortLabel, 
+       m.shortLabel, r.shortLabel
   FROM interactor i, interactor m, component c, ControlledVocab r, 
        experiment e, int2exp i2e 
  WHERE i.ac=c.interaction_ac 
