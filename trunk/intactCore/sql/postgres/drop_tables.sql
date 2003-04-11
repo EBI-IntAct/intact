6d7068d6e33c24afae7d466ac9f7e7b9e2ec1d41
markrijnbeek <markrijnbeek@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-11 10:14:30 +0000

    more readable

--- trunk/intactCore/sql/postgres/drop_tables.sql
+++ trunk/intactCore/sql/postgres/drop_tables.sql
@@ -18,35 +18,49 @@
 
   *************************************************************/
 
-  drop table ia_annotation_audit cascade ; 
-  drop table ia_biosource_audit cascade ;
-  drop table ia_biosource2annot_audit cascade ;
-  drop table ia_component_audit cascade ;
-  drop table ia_controlledvocab_audit cascade ;
-  drop table ia_cv2cv_audit cascade ;
-  drop table ia_cvobject2annot_audit cascade ;
-  drop table ia_exp2annot_audit cascade ;
-  drop table ia_experiment_audit cascade ;
-  drop table ia_institution_audit cascade ;
-  drop table ia_int2annot_audit cascade ;
-  drop table ia_int2exp_audit cascade ;
-  drop table ia_intactnode_audit cascade ;
-  drop table ia_interactor_audit cascade ;
-  drop table ia_xref_audit cascade ;
   drop table ia_annotation cascade ;
+  drop table ia_annotation_audit cascade ; 
+
   drop table ia_biosource cascade ;
   drop table ia_biosource2annot cascade ;
+
+  drop table ia_biosource2annot_audit cascade ;
+  drop table ia_biosource_audit cascade ;
+
   drop table ia_component cascade ;
+  drop table ia_component_audit cascade ;
+
   drop table ia_controlledvocab cascade ;
+  drop table ia_controlledvocab_audit cascade ;
+
   drop table ia_cv2cv cascade ;
+  drop table ia_cv2cv_audit cascade ;
+
   drop table ia_cvobject2annot cascade ;
+  drop table ia_cvobject2annot_audit cascade ;
+
   drop table ia_exp2annot cascade ;
+  drop table ia_exp2annot_audit cascade ;
+
   drop table ia_experiment cascade ;
+  drop table ia_experiment_audit cascade ;
+
   drop table ia_institution cascade ;
+  drop table ia_institution_audit cascade ;
+
   drop table ia_int2annot cascade ;
+  drop table ia_int2annot_audit cascade ;
+
   drop table ia_int2exp cascade ;
+  drop table ia_int2exp_audit cascade ;
+
   drop table ia_intactnode cascade ;
+  drop table ia_intactnode_audit cascade ;
+
   drop table ia_interactor cascade ;
+  drop table ia_interactor_audit cascade ;
+
   drop table ia_xref cascade ;
+  drop table ia_xref_audit cascade ;
 
   drop sequence intact_ac;
