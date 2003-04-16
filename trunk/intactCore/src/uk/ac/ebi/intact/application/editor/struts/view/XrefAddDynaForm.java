9c3815690864a26f4d44a35082d151ec6cea910d
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-16 08:49:36 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/XrefAddDynaForm.java
@@ -0,0 +1,68 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.view;
+
+import org.apache.struts.action.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import uk.ac.ebi.intact.application.editor.struts.framework.util.EditorMenuFactory;
+
+/**
+ * The form to add new Xref.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class XrefAddDynaForm extends DynaActionForm {
+
+    /**
+     * Validate the properties that have been set from the HTTP request.
+     *
+     * @param mapping the mapping used to select this instance
+     * @param request the servlet request we are processing
+     * @return <tt>ActionErrors</tt> object that contains validation errors. If
+     * no errors are found, <tt>null</tt> or an empty <tt>ActionErrors</tt>
+     * object is returned.
+     */
+    public ActionErrors validate(ActionMapping mapping,
+                                 HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        String database = ((String) get("database")).trim();
+        String qualifier = ((String) get("qualifier")).trim();
+
+        // They must have selected a database.
+        if (database.equals(EditorMenuFactory.SELECT_LIST_ITEM)) {
+            errors.add("xref.database", new ActionError("error.dropdown.list"));
+        }
+        // They must have selected a qualifier.
+        else if (qualifier.equals(EditorMenuFactory.SELECT_LIST_ITEM)) {
+            errors.add("xref.qualifier", new ActionError("error.dropdown.list"));
+        }
+        if (errors.isEmpty()) {
+            // No errors; set the trimmed values (no need to trim again in the
+            // action class.
+            set("database", database);
+            set("qualifier", qualifier);
+        }
+        return errors;
+    }
+
+    /**
+     * Reset all properties to their default values.
+     *
+     * @param mapping the mapping used to select this instance
+     * @param request the servlet request we are processing
+     */
+    public void reset(ActionMapping mapping, HttpServletRequest request) {
+        set("database", EditorMenuFactory.SELECT_LIST_ITEM);
+        set("primaryId", "");
+        set("secondaryId", "");
+        set("releaseNumber", "");
+        set("qualifier", EditorMenuFactory.SELECT_LIST_ITEM);
+    }
+}
