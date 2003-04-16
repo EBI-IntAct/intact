ff2c4356cb44cdf2fda0a6adbb9a85bef870bc27
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-16 09:21:14 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/exception/ValidationException.java
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.exception;
+
+/**
+ * Super exception class for all the validation exceptions.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class ValidationException extends BaseException {
+}
