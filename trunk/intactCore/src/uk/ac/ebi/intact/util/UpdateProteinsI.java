f00c82364716b2daf3d6f9f0e465d17d20d27e6a
danwu <danwu@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-17 17:19:33 +0000

    new methods added to interface class

--- trunk/intactCore/src/uk/ac/ebi/intact/util/UpdateProteinsI.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/UpdateProteinsI.java
@@ -7,7 +7,23 @@
  */
 package uk.ac.ebi.intact.util;
 
+import java.io.*;
+import java.util.*;
+import gnu.regexp.* ;
+import org.apache.regexp.RE;
+import org.apache.regexp.RESyntaxException;
+
+//re: YASP
 import uk.ac.ebi.intact.business.IntactHelper;
+import uk.ac.ebi.sptr.flatfile.yasp.*;
+import uk.ac.ebi.interfaces.sptr.*;
+
+//re: IntAct OJB
+import uk.ac.ebi.intact.business.*;
+import uk.ac.ebi.intact.persistence.*;
+import uk.ac.ebi.intact.model.*;
+
+
 
 /**
  * Defines the functionality of protein import utilities.
@@ -31,9 +47,53 @@ public interface UpdateProteinsI {
     *                   else,    skip existing Protein objects.
     * @return           The number of protein objects created.
     */
+
     public int insertSPTrProteins(String sourceUrl,
                                   String taxid,
                                   IntactHelper helper,
                                   boolean update);
+
+    /**
+     * From a given sptr AC, returns a full URL from where a flatfile format SPTR entry
+     * will be fetched. Note, the SRS has several format of data output, the URLs which
+     * outputs html format SPTR entry CANNOT be used, since YASP does't have html parsing 
+     * function.
+     *
+     *@param sptrAC, a SPTR AC 
+     *@return a full URL. 
+     */
+    public String getUrl(String sptrAC) ;
+
+
+    /**
+     * From a given URL, returns a string of a SPTR entry. 
+     * 
+     *@param url, a URL which outputs flatfile of   
+     *@return a full URL. 
+     */
+    public String getAnEntry(String url) ;
+
+
+    /**
+     * from a given string and a given pattern(string), to find all matches. The matched are  
+     * retured as a list. This method uses gnu.regexp.* package, not the org.apache.regexp.*
+     *@param textin A string from which some pattern will be matched. 
+     *@param pattern A string as a pattern.
+     *@return A list of matched pattern.
+     */
+    public REMatch[] match(String textin, String pattern ) ;
+
+
+    public void addNewXref(AnnotatedObject current, 
+                           Xref xref) ;
+
+
+    /**
+     * add (not update) a new BioSource to the db
+     *@param orgName Organism name 
+     *@param taxId Taxonomy ID 
+     */
+    public void addBioSource(String orgName, String taxId) ;
+
 
 }
