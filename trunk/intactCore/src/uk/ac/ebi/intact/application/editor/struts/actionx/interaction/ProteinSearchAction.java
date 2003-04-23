d2e4ea7c5a4fbeea912c0f1c003d0ffbb36806f0
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-04-23 15:10:45 +0000

    Completed this class; it now adds Proteins to the list

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/actionx/interaction/ProteinSearchAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/actionx/interaction/ProteinSearchAction.java
@@ -8,17 +8,17 @@ package uk.ac.ebi.intact.application.editor.struts.actionx.interaction;
 
 import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorAction;
 import uk.ac.ebi.intact.application.editor.struts.framework.util.EditorConstants;
-import uk.ac.ebi.intact.application.editor.struts.framework.util.AbstractEditViewBean;
-import uk.ac.ebi.intact.application.editor.struts.view.CommentBean;
-import uk.ac.ebi.intact.application.editor.struts.view.EditForm;
-import uk.ac.ebi.intact.application.editor.struts.viewx.interaction.ProteinBean;
 import uk.ac.ebi.intact.application.editor.struts.viewx.interaction.InteractionViewBean;
 import uk.ac.ebi.intact.application.editor.business.EditUserI;
+import uk.ac.ebi.intact.model.Protein;
+import uk.ac.ebi.intact.persistence.SearchException;
 
 import org.apache.struts.action.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * The action class to search a Protein.
@@ -51,11 +51,82 @@ public class ProteinSearchAction extends AbstractEditorAction {
             throws Exception {
         DynaActionForm theForm = (DynaActionForm) form;
 
-        log("Short Label: " + (String) theForm.get("shortLabel"));
-        log("SP AC: " + (String) theForm.get("spAc"));
-        log("AC : " + (String) theForm.get("ac"));
-        log("Full Text: " + (String) theForm.get("fullText"));
+        // The search parameter.
+        String searchParam = getSearchParam(theForm);
+
+        // The search value.
+        String searchValue = (String) theForm.get(searchParam);
+        if (searchValue.length() == 0) {
+            ActionErrors errors = new ActionErrors();
+            errors.add("protein.search",
+                    new ActionError("error.protein.search.input"));
+            saveErrors(request, errors);
+            return new ActionForward(mapping.getInput());
+        }
+        // Handler to the current user.
+        EditUserI user = super.getIntactUser(request);
+//        log("Searching for: " + searchParam + " and value: " + searchValue);
+
+        // The collection to hold proteins.
+        Collection proteins;
+        try {
+            proteins = user.search(Protein.class.getName(), searchParam,
+                    searchValue);
+        }
+        catch (SearchException se) {
+            ActionErrors errors = new ActionErrors();
+            errors.add("protein.search",
+                    new ActionError("error.search", se.getMessage()));
+            saveErrors(request, errors);
+            return new ActionForward(mapping.getInput());
+        }
+        // Search found any results?
+        if (proteins.isEmpty()) {
+            ActionErrors errors = new ActionErrors();
+            errors.add("protein.search",
+                    new ActionError("error.protein.search.empty", searchParam));
+            saveErrors(request, errors);
+            return new ActionForward(mapping.getInput());
+        }
+        // The number of Prteins retrieved from the search.
+        int psize = proteins.size();
+
+        // Just an arbitrary number for the moment.
+        if (psize > 10) {
+            ActionErrors errors = new ActionErrors();
+            errors.add("protein.search",
+                    new ActionError("error.protein.search.many",
+                            Integer.toString(psize), searchParam, "10"));
+            saveErrors(request, errors);
+            return new ActionForward(mapping.getInput());
+        }
+        // Can safely cast it as we have the correct editor view bean.
+        InteractionViewBean view = (InteractionViewBean) user.getView();
+
+        for (Iterator iter = proteins.iterator(); iter.hasNext();) {
+            view.addProtein((Protein) iter.next());
+        }
 
         return mapping.findForward(EditorConstants.FORWARD_SUCCESS);
+    }
+
+    /**
+     * Returns the search parameter.
+     * @param form the form to get search parameter values.
+     * @return the search parameter; the most sepecific search
+     * is preferred over the least specific one. For example, 'ac' is preferred
+     * over any other value.
+     */
+    private String getSearchParam(DynaActionForm form) {
+        if (((String) form.get("ac")).length() != 0) {
+            return "ac";
+        }
+        if (((String) form.get("spAc")).length() != 0) {
+            return "spAc";
+        }
+        if (((String) form.get("shortLabel")).length() != 0) {
+            return "shortLabel";
+        }
+        return "fullName";
     }
 }
