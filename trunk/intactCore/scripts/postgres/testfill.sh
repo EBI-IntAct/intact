d596a14c2aeb84a664a11cf931851e8376d2d4b6
skerrien <samuel.kerrien@gmail.com>
2003-04-28 09:55:34 +0000

    allow to let the user free to choose its db name

--- trunk/intactCore/scripts/postgres/testfill.sh
+++ trunk/intactCore/scripts/postgres/testfill.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
-echo "Usage: testfill.sh user/pw database small|large"
+echo "Usage: testfill.sh user/pw database small|medium|large"
+
+
+# extract username from the user/pw parameter
+DBUSER=${1%%/*}
+
+# set default dataset if needed
+if [ "$3" = "" ]
+then
+   DATASET="small"
+   DEFAULT_WARN="(default)"
+else
+   DATASET=$3
+   DEFAULT_WARN=""
+fi
+
+# display parameters summary
+echo
+echo "user name     : $DBUSER"
+echo "database name : $2"
+echo "data set      : ${DATASET} ${DEFAULT_WARN}"
 
 echo
 echo You need to create a db with:
-echo initdb -D /scratch/$USER
-echo postmaster -i -S -D /scratch/$USER
-echo createdb $USER
+echo initdb -D /scratch/$DBUSER
+echo postmaster -i -S -D /scratch/$DBUSER
+echo createdb $2
 echo
 
 # wait until server has properly started up
 sleep 2
 
-psql -f sql/postgres/drop_tables.sql          
-psql -f sql/postgres/create_tables.sql          
-psql -f sql/postgres/create_dummy.sql          
+psql -U $DBUSER -d $2 -f sql/postgres/drop_tables.sql
+psql -U $DBUSER -d $2 -f sql/postgres/create_tables.sql
+psql -U $DBUSER -d $2 -f sql/postgres/create_dummy.sql
 
 echo "Inserting controlled vocabularies"
 
@@ -21,17 +41,11 @@ scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvTopic data/controlled
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvXrefQualifier data/controlledVocab/CvXrefQualifier.def
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvDatabase data/controlledVocab/CvDatabase.def
 
-
-if [ "$3" = "" ]
-then
-   $3="small"
-fi
-
 echo "Inserting Proteins and their Xrefs ..."
-scripts/javaRun.sh InsertGo data/go_$3.dat "http://www.geneontology.org/doc/GO.defs"
+scripts/javaRun.sh InsertGo data/go_${DATASET}.dat "http://www.geneontology.org/doc/GO.defs"
 
 echo "Inserting Complexes ..."
-scripts/javaRun.sh InsertComplex data/ho_gavin_$3.dat
+scripts/javaRun.sh InsertComplex data/ho_gavin_${DATASET}.dat
 
 #end
 
