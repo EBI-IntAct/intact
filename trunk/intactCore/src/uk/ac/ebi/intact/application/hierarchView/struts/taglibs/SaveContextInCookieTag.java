31448bc43a43cc496bc484e4621f3ffdec2f5ed2
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-05-08 15:25:10 +0000

    BugFix: don't save all queries in distinct cookie but in one single. 20 is the limit

--- trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/taglibs/SaveContextInCookieTag.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/taglibs/SaveContextInCookieTag.java
@@ -20,12 +20,18 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
 
 /**
  * That tag allows to save in a cookie (stored in the client side)
  * all needed data to restore later the user context.
  * It stores the selected protein, the method and the current depth
  * of the graph.
+ *
+ * Be aware that the cookie specification allows to store a maximum of 20 cookies per session,
+ * the length of a cookie including name and content shouldn't exceed 4Kbytes.
+ * cf. http://wp.netscape.com/newsref/std/cookie_spec.html
  *
  * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
@@ -105,14 +111,25 @@ public class SaveContextInCookieTag extends TagSupport {
         // save our context
         ArrayList criterias = network.getCriteria();
         int max = criterias.size();
+        StringBuffer sb = new StringBuffer (128);
         for (int i = 0; i < max; i++) {
-            String query = ( (String[]) criterias.get(i) )[0];
-            saveCookie (applicationPath, "Q"+i, query);
+            sb.append ( ( (String[]) criterias.get(i) )[0] ).append(',');
         }
 
-        saveCookie (applicationPath, "QUERY_COUNT", ""+max);
+        // save queries without the last comma
+        String queryString = null;
+        try {
+            queryString = URLEncoder.encode (sb.substring(0, sb.length()-1), "UTF-8");
+        } catch (UnsupportedEncodingException uee) {
+            logger.error ("Unsupported encoding system");
+            return EVAL_PAGE;
+        }
+
+        saveCookie (applicationPath, "QUERY", queryString);
         saveCookie (applicationPath, "DEPTH",       ""+user.getCurrentDepth());
         saveCookie (applicationPath, "METHOD",      user.getMethodLabel());
+
+        // saveCookie (applicationPath, "QUERY_COUNT", ""+max);
 
         return EVAL_PAGE;
     }
