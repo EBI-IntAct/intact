7df2a01c37a39e4c5d294b606b1bac70b45f464a
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-05-20 13:49:07 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/cv/CvROViewBean.java
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.view.cv;
+
+import uk.ac.ebi.intact.application.editor.struts.framework.util.AbstractROViewBean;
+
+/**
+ * The CV read only bean. Currently, this class does not provide any additional
+ * functionalities (extends the super abstract class to allow to create
+ * an instance of this class).
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class CvROViewBean extends AbstractROViewBean {
+}
