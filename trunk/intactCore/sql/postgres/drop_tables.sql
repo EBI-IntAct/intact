d82cd8ba8632ba3d2bb55e58c8d72aae20db7c81
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-05-28 15:18:34 +0000

    fix wrong droping order and comment out all audit table (not yet used)

--- trunk/intactCore/sql/postgres/drop_tables.sql
+++ trunk/intactCore/sql/postgres/drop_tables.sql
@@ -18,49 +18,49 @@
 
   *************************************************************/
 
-  drop table ia_annotation;
-  drop table ia_annotation_audit; 
-
-  drop table ia_biosource;
-  drop table ia_biosource2annot;
-
-  drop table ia_biosource2annot_audit;
-  drop table ia_biosource_audit;
-
-  drop table ia_component;
-  drop table ia_component_audit;
-
-  drop table ia_controlledvocab;
-  drop table ia_controlledvocab_audit;
-
-  drop table ia_cv2cv;
-  drop table ia_cv2cv_audit;
-
   drop table ia_cvobject2annot;
-  drop table ia_cvobject2annot_audit;
-
-  drop table ia_exp2annot;
-  drop table ia_exp2annot_audit;
-
-  drop table ia_experiment;
-  drop table ia_experiment_audit;
-
-  drop table ia_institution;
-  drop table ia_institution_audit;
+--  drop table ia_cvobject2annot_audit;
 
   drop table ia_int2annot;
-  drop table ia_int2annot_audit;
+--  drop table ia_int2annot_audit;
+
+  drop table ia_exp2annot;
+--  drop table ia_exp2annot_audit;
+
+  drop table ia_biosource2annot;
+--  drop table ia_biosource2annot_audit;
+
+  drop table ia_annotation;
+--  drop table ia_annotation_audit;
+
+  drop table ia_component;
+--  drop table ia_component_audit;
+
+  drop table ia_cv2cv;
+--  drop table ia_cv2cv_audit;
 
   drop table ia_int2exp;
-  drop table ia_int2exp_audit;
+--  drop table ia_int2exp_audit;
+
+  drop table ia_experiment;
+--  drop table ia_experiment_audit;
 
   drop table ia_intactnode;
-  drop table ia_intactnode_audit;
-
-  drop table ia_interactor;
-  drop table ia_interactor_audit;
+--  drop table ia_intactnode_audit;
 
   drop table ia_xref;
-  drop table ia_xref_audit;
+--  drop table ia_xref_audit;
+
+  drop table ia_interactor;
+--  drop table ia_interactor_audit; 
+
+  drop table ia_biosource;
+--  drop table ia_biosource_audit;
+
+  drop table ia_controlledvocab;
+--  drop table ia_controlledvocab_audit; 
+
+  drop table ia_institution;
+--  drop table ia_institution_audit;
 
   drop sequence intact_ac;
