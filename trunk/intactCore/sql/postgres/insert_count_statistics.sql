7aca4bf6554bd9da2f2a2a01a1a91d4e45bcc3de
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-06-26 08:33:49 +0000

    was not working under postgres 7.0.3, now ok

--- trunk/intactCore/sql/postgres/insert_count_statistics.sql
+++ trunk/intactCore/sql/postgres/insert_count_statistics.sql
@@ -26,16 +26,18 @@ LOCK TABLE IA_Statistics IN EXCLUSIVE MODE;
 INSERT INTO IA_Statistics (protein_number)
     SELECT count(*)
     FROM IA_Interactor
-    WHERE objclass='uk.ac.ebi.intact.model.Protein'
-UPDATE IA_Statistics
+    WHERE objclass='uk.ac.ebi.intact.model.Protein';
 
+
+UPDATE IA_Statistics
 SET interaction_number = (SELECT count(*)
                           FROM IA_Interactor
                           WHERE objclass='uk.ac.ebi.intact.model.Interaction'
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
-UPDATE IA_Statistics
 
+
+UPDATE IA_Statistics
 SET binary_interactions = (SELECT count(distinct interaction_ac)
                            FROM IA_Component c1
                            WHERE (SELECT count(*)
@@ -43,25 +45,32 @@ SET binary_interactions = (SELECT count(distinct interaction_ac)
                                   WHERE c1.interaction_ac = c2.interaction_ac) = 2)
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
-UPDATE IA_Statistics
 
+
+UPDATE IA_Statistics
 SET complex_interactions = (SELECT count(distinct interaction_ac)
-                           FROM IA_Component c1
-                           WHERE (SELECT count(*)
-                                  FROM IA_Component c2
-                                  WHERE c1.interaction_ac = c2.interaction_ac) > 2)
+                            FROM IA_Component c1
+                            WHERE (SELECT count(*)
+                                   FROM IA_Component c2
+                                   WHERE c1.interaction_ac = c2.interaction_ac) > 2)
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
-UPDATE IA_Statistics
 
+
+UPDATE IA_Statistics
 SET experiment_number = (SELECT count(*)
                          FROM IA_Experiment)
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
-UPDATE IA_Statistics
 
+
+UPDATE IA_Statistics
 SET term_number = (SELECT count(*)
-                          FROM IA_ControlledVocab)
+                   FROM IA_ControlledVocab)
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
+
+
+
+-- END WORK;
 COMMIT;
