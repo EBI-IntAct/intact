70f7ce46235ce499703989b5a86a3a76dd1af747
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-06-26 08:36:30 +0000

    was not working under postgres 7.0.3, now ok

--- trunk/intactCore/sql/postgres/insert_count_statistics.sql
+++ trunk/intactCore/sql/postgres/insert_count_statistics.sql
@@ -14,9 +14,7 @@
 
   $Date$
   $Author$
-  $Locker$
-
-  Note:
+  $Id$
 
 *************************************************************/
 
@@ -32,45 +30,27 @@ INSERT INTO IA_Statistics (protein_number)
 UPDATE IA_Statistics
 SET interaction_number = (SELECT count(*)
                           FROM IA_Interactor
-                          WHERE objclass='uk.ac.ebi.intact.model.Interaction'
-WHERE timestamp=(SELECT max(timestamp)
-                 FROM IA_Statistics);
+                          WHERE objclass='uk.ac.ebi.intact.model.Interaction')
 
-
-UPDATE IA_Statistics
-SET binary_interactions = (SELECT count(distinct interaction_ac)
+   ,binary_interactions = (SELECT count(distinct interaction_ac)
                            FROM IA_Component c1
                            WHERE (SELECT count(*)
                                   FROM IA_Component c2
                                   WHERE c1.interaction_ac = c2.interaction_ac) = 2)
-WHERE timestamp=(SELECT max(timestamp)
-                 FROM IA_Statistics);
 
-
-UPDATE IA_Statistics
-SET complex_interactions = (SELECT count(distinct interaction_ac)
+   ,complex_interactions = (SELECT count(distinct interaction_ac)
                             FROM IA_Component c1
                             WHERE (SELECT count(*)
                                    FROM IA_Component c2
                                    WHERE c1.interaction_ac = c2.interaction_ac) > 2)
-WHERE timestamp=(SELECT max(timestamp)
-                 FROM IA_Statistics);
 
-
-UPDATE IA_Statistics
-SET experiment_number = (SELECT count(*)
+   ,experiment_number = (SELECT count(*)
                          FROM IA_Experiment)
-WHERE timestamp=(SELECT max(timestamp)
-                 FROM IA_Statistics);
 
-
-UPDATE IA_Statistics
-SET term_number = (SELECT count(*)
+   ,term_number = (SELECT count(*)
                    FROM IA_ControlledVocab)
+
 WHERE timestamp=(SELECT max(timestamp)
                  FROM IA_Statistics);
 
-
-
--- END WORK;
 COMMIT;
