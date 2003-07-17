df7e54fe3bdd111450758b61465213a1def1856a
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-07-17 10:49:38 +0000

    Changed some property names

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/exception/validation/ShortLabelException.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/exception/validation/ShortLabelException.java
@@ -17,6 +17,6 @@ import uk.ac.ebi.intact.application.editor.exception.validation.ValidationExcept
 public class ShortLabelException extends ValidationException {
 
     public ShortLabelException() {
-        super("error.cvinfo.label.validation", "cvinfo");
+        super("error.cvinfo.label", "cvinfo");
     }
 }
