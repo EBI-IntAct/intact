17c223b99e85ffcf85828a3317d13dd80f715331
skerrien <samuel.kerrien@gmail.com>
2003-07-17 15:58:04 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/view/utils/SourceBean.java
@@ -0,0 +1,101 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.application.hierarchView.struts.view.utils;
+
+/**
+ * Simple JavaBean to represent a GO source data.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class SourceBean {
+
+    // ----------------------------------------------------------- Instance variables
+
+    private String id;
+
+    private String description;
+
+    private String SourceBrowserUrl;
+
+    private String directHighlightUrl;
+
+    private boolean selected;
+
+    // ----------------------------------------------------------- Constructors
+
+    public SourceBean (String id,
+                       String description,
+                       String sourceBrowserUrl,
+                       String directHighlightUrl,
+                       boolean clickable) {
+        this.id = id;
+        this.description = description;
+        this.SourceBrowserUrl = sourceBrowserUrl;
+        this.directHighlightUrl = directHighlightUrl;
+        this.selected = clickable;
+    }
+
+    // ------------------------------------------------------------- Properties
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSourceBrowserUrl() {
+        return SourceBrowserUrl;
+    }
+
+    public void setSourceBrowserUrl(String sourceBrowserUrl) {
+        this.SourceBrowserUrl = sourceBrowserUrl;
+    }
+
+    public String getDirectHighlightUrl() {
+        return directHighlightUrl;
+    }
+
+    public void setDirectHighlightUrl(String directHighlightUrl) {
+        this.directHighlightUrl = directHighlightUrl;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    // --------------------------------------------------------- Public Methods
+
+
+    /**
+     * Return a string representation of this object.
+     */
+    public String toString() {
+        StringBuffer sb = new StringBuffer("SourceBean[Id=");
+        sb.append(id);
+        sb.append(", description=");
+        sb.append(description);
+        sb.append(", selected=");
+        sb.append(selected);
+        sb.append("]");
+        return (sb.toString());
+    }
+}
+
+
