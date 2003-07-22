39ced0d709bc6fb671f64e1dcf87b8da72e65930
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-07-22 23:00:26 +0000

    Moved form init parts to separate classes

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/SetUpEditorAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/SetUpEditorAction.java
@@ -6,21 +6,17 @@ in the root directory of this distribution.
 
 package uk.ac.ebi.intact.application.editor.struts.action;
 
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorAction;
 import uk.ac.ebi.intact.application.editor.struts.framework.util.EditorConstants;
-import uk.ac.ebi.intact.application.editor.struts.framework.util.AbstractEditViewBean;
-import uk.ac.ebi.intact.application.editor.struts.view.EditForm;
-import uk.ac.ebi.intact.application.editor.business.EditUserI;
-import org.apache.struts.action.*;
-import org.apache.commons.beanutils.DynaBean;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Sets up the editor form type using the selected editor topic. The common forms
- * for all the topics (such as annotations, xrefs) are populated.
+ * Sets up the editor form type using the selected editor topic.
  *
  * @author Sugath Mudali (smudali@ebi.ac.uk)
  * @version $Id$
@@ -32,36 +28,8 @@ public class SetUpEditorAction  extends AbstractEditorAction {
                                  HttpServletRequest request,
                                  HttpServletResponse response)
             throws Exception {
-        // Handler to the Intact User.
-        EditUserI user = super.getIntactUser(request);
-
-        // The view of the current object we are editing at the moment.
-        AbstractEditViewBean view = user.getView();
-
-        String cvFormName = EditorConstants.FORM_CVINFO;
-        DynaBean cvInfoBean = user.getDynaBean(cvFormName, request);
-        cvInfoBean.set("ac", view.getAc());
-        cvInfoBean.set("shortLabel", view.getShortLabel());
-        cvInfoBean.set("fullName", view.getFullName());
-        request.setAttribute(cvFormName, cvInfoBean);
-
-        // The session to retrieve forms.
-        HttpSession session = getSession(request);
-
-        // The annotation form.
-        EditForm commentForm =
-                getEditForm(session, EditorConstants.FORM_COMMENT_EDIT);
-        // Populate with annotations.
-        commentForm.setItems(view.getAnnotations());
-
-        // The xref form.
-        EditForm xrefForm = getEditForm(session, EditorConstants.FORM_XREF_EDIT);
-        // Populate with xrefs.
-        xrefForm.setItems(view.getXrefs());
-//        LOGGER.info("Annotation ITEMS: " + commentForm.getItems().length);
-//        LOGGER.info("Xref ITEMS: " + xrefForm.getItems().length);
-
-        String topic = user.getSelectedTopic();
+        // The topic to choose where to go.
+        String topic = getIntactUser(request).getSelectedTopic();
         if (topic.equals("BioSource")) {
             return mapping.findForward("biosource");
         }
@@ -71,7 +39,7 @@ public class SetUpEditorAction  extends AbstractEditorAction {
         if (topic.equals("Interaction")) {
             return mapping.findForward("interaction");
         }
-        // Strainght to the editor.
+        // Straight to the editor.
         return mapping.findForward(EditorConstants.FORWARD_EDITOR);
     }
 }
