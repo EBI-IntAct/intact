ec9427ae9d22abd710028832601ae14ff4c2c40f
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-07-23 17:01:03 +0000

    CvInteractionType now derived from CvDagObject

--- trunk/intactCore/src/uk/ac/ebi/intact/model/CvInteractionType.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/CvInteractionType.java
@@ -14,7 +14,7 @@ import java.util.*;
  * @intact.example phosphorylation
  * @author hhe
  */
-public class CvInteractionType extends CvObject implements Editable {
+public class CvInteractionType extends CvDagObject implements Editable {
 
     /**
      * Cache a Vector of all shortLabels of the class, e.g. for menus.
