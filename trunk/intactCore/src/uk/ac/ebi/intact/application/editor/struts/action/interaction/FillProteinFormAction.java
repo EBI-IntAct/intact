79a5f4ef0a7503cb53e94d349f94e8b7c364227b
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-07-30 18:04:45 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/interaction/FillProteinFormAction.java
@@ -0,0 +1,41 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action.interaction;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorAction;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.InteractionViewBean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Action to populate the form with proteins.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class FillProteinFormAction extends AbstractEditorAction {
+
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+        // The view of the current object we are editing at the moment.
+        InteractionViewBean view = (InteractionViewBean)
+                getIntactUser(request).getView();
+
+        // Populate the form.
+        PropertyUtils.setSimpleProperty(form, "proteins", view.getProteins());
+
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+}
