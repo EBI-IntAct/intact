6a3804bc1dbf76a0b02e4fa345f27a2424ec94f1
clewing <clewing@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-08-01 16:16:44 +0000

    updated to support new help pages

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/controller/DetailsAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search/struts/controller/DetailsAction.java
@@ -72,7 +72,12 @@ public class DetailsAction extends IntactBaseAction {
         // The search and help links for links (used by style sheet).
         String relativeSearchLink = getServlet().getServletContext().getInitParameter("searchLink");
         String relativeHelpLink = getServlet().getServletContext().getInitParameter("helpLink");
-        String helpLink = request.getContextPath() + relativeHelpLink;
+        //build the help link out of the context path - strip off the 'search' bit...
+        String ctxtPath = (request.getContextPath());
+        String relativePath = ctxtPath.substring(0, ctxtPath.lastIndexOf("search"));
+        String helpLink = relativePath.concat(relativeHelpLink);
+
+        //String helpLink = request.getContextPath() + relativeHelpLink.;
         String searchLink = request.getContextPath() + relativeSearchLink;
 
 
