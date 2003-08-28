561245d5f18d3d4a16bc02ab1c1b173714532d98
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-08-28 15:41:01 +0000

    Added validation for sp and intact ac values

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/interaction/ProteinSearchDynaForm.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/interaction/ProteinSearchDynaForm.java
@@ -6,12 +6,14 @@ in the root directory of this distribution.
 
 package uk.ac.ebi.intact.application.editor.struts.view.interaction;
 
+import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.validator.DynaValidatorForm;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The form to search for Proteins. Overrides the validate method to provide
@@ -21,6 +23,17 @@ import javax.servlet.http.HttpServletRequest;
  * @version $Id$
  */
 public class ProteinSearchDynaForm extends DynaValidatorForm {
+
+    /**
+     * Must start with either o or P or Q, followed by 5 characters consists of
+     * either A - Z or 0 - 9.
+     */
+    private static final Pattern ourSpAcPat = Pattern.compile("[O|P|Q][A-Z0-9]{5}$");
+
+    /**
+     * Must start with either uppercase characters, followed by '-' and a number.
+     */
+    private static final Pattern ourIntactAcPat = Pattern.compile("[A-Z]+\\-[0-9]+$");
 
     /**
      * Validates the search input values.
@@ -53,10 +66,24 @@ public class ProteinSearchDynaForm extends DynaValidatorForm {
         String value = shortLabel;
         String param = "shortLabel";
         if (acLen != 0) {
+            Matcher matcher = ourIntactAcPat.matcher(ac);
+            if (!matcher.matches()) {
+                ActionErrors errors = new ActionErrors();
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                        new ActionError("error.int.protein.search.ac"));
+                return errors;
+            }
             value = ac;
             param = "ac";
         }
         else if (spAcLen != 0) {
+            Matcher matcher = ourSpAcPat.matcher(spAc);
+            if (!matcher.matches()) {
+                ActionErrors errors = new ActionErrors();
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                        new ActionError("error.int.protein.search.sp"));
+                return errors;
+            }
             value = spAc;
             param = "spAc";
         }
