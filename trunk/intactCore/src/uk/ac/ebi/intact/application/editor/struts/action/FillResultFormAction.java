05aadecea3f5898a6c0128a43a9a147fc6573cd4
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-09-05 16:13:53 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/FillResultFormAction.java
@@ -0,0 +1,41 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorAction;
+import uk.ac.ebi.intact.application.editor.struts.framework.util.AbstractEditViewBean;
+import uk.ac.ebi.intact.application.editor.business.EditUserI;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Sets up the editor form type using the selected editor topic. The common forms
+ * for all the topics (such as annotations, xrefs) are populated.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class FillResultFormAction  extends AbstractEditorAction {
+
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+        // Fill the form with search results.
+        EditUserI user = getIntactUser(request);
+        user.fillSearchResult((DynaActionForm) form);
+
+        // Straight to the results page.
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+}
