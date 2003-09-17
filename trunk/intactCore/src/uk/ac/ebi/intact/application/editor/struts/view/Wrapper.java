ddfd6d97056dab74092bc72fd26e942f66b34b0e
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-09-17 23:16:59 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/Wrapper.java
@@ -0,0 +1,31 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.view;
+
+import org.apache.taglibs.display.TableDecorator;
+
+/**
+ * This class is a decorator for the display library.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class Wrapper extends TableDecorator {
+
+    /**
+     * @return the link for the result page; clicking on this link, the
+     * user will be taken into the edit page.
+     */
+    public String getLink() {
+        ResultBean bean = (ResultBean) getObject();
+        String ac = bean.getAc();
+        String className = bean.getClassName();
+        int lastPos = className.lastIndexOf('.');
+        return "<a href=results?ac=" + ac + "&searchClass="
+                + className.substring(lastPos + 1) + ">" + ac + "</a>";
+    }
+}
