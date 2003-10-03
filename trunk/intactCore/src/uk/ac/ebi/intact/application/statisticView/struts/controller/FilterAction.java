fc1fe06b6f5bd5f10539d1176b458fd0d9f3e6cc
skerrien <samuel.kerrien@gmail.com>
2003-10-03 11:01:44 +0000

    initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/statisticView/struts/controller/FilterAction.java
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.application.statisticView.struts.controller;
+
+import uk.ac.ebi.intact.application.statisticView.struts.view.FilterForm;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.Action;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Implementation of <strong>Action</strong> that validates a visualize submisson.
+ *
+ * @author Samuel Kerrien
+ * @version $Id$
+ */
+public final class FilterAction extends Action {
+
+    /**
+     * Process the specified HTTP request, and create the corresponding HTTP
+     * response (or forward to another web component that will create it).
+     * Return an <code>ActionForward</code> instance describing where and how
+     * control should be forwarded, or <code>null</code> if the response has
+     * already been completed.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param form The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet exception occurs
+     */
+    public ActionForward execute (ActionMapping mapping,
+                                  ActionForm form,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String start = null;
+        String stop = null;
+
+        FilterForm filterForm = (FilterForm) form;
+
+        if (null != form) {
+            // read form values from the bean
+            start = filterForm.getStart ();
+            if (start != null) request.setAttribute("start", start);
+
+            stop  = filterForm.getStop ();
+            if (stop != null) request.setAttribute("stop", stop);
+        }
+
+        // Forward control to the specified success URI
+        return (mapping.findForward("success"));
+    }
+}
+
+
+
+
