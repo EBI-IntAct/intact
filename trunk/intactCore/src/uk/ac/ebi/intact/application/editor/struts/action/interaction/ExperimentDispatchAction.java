8dfe255d0adf110681fec0a0e8b40382e4ece7aa
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-10-16 09:38:06 +0000

    As a result of moving to single forms

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/interaction/ExperimentDispatchAction.java
@@ -0,0 +1,156 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action.interaction;
+
+import org.apache.struts.action.*;
+import uk.ac.ebi.intact.application.editor.business.EditUserI;
+import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorDispatchAction;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.InteractionViewBean;
+import uk.ac.ebi.intact.model.Experiment;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The action class to search an Experiment (in the context of an Interaction).
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class ExperimentDispatchAction extends AbstractEditorDispatchAction {
+
+    // Implements super's abstract methods.
+
+    /**
+     * Provides the mapping from resource key to method name.
+     * @return Resource key / method name map.
+     */
+    protected Map getKeyMethodMap() {
+        Map map = new HashMap();
+        map.put("int.exp.button.recent", "recent");
+        map.put("int.exp.button.search", "search");
+        return map;
+    }
+
+    /**
+     * Process the specified HTTP request, and create the corresponding
+     * HTTP response (or forward to another web component that will create
+     * it). Return an ActionForward instance describing where and how
+     * control should be forwarded, or null if the response has
+     * already been completed.
+     *
+     * @param mapping - The <code>ActionMapping</code> used to select this instance
+     * @param form - The optional <code>ActionForm</code> bean for this request (if any)
+     * @param request - The HTTP request we are processing
+     * @param response - The HTTP response we are creating
+     *
+     * @return - represents a destination to which the action servlet,
+     * <code>ActionServlet</code>, might be directed to perform a RequestDispatcher.forward()
+     * or HttpServletResponse.sendRedirect() to, as a result of processing
+     * activities of an <code>Action</code> class
+     */
+    public ActionForward recent(ActionMapping mapping,
+                                ActionForm form,
+                                HttpServletRequest request,
+                                HttpServletResponse response)
+            throws Exception {
+        // Handler to the Intact User.
+        EditUserI user = getIntactUser(request);
+
+        // The current view of the edit session.
+        InteractionViewBean view = (InteractionViewBean) user.getView();
+
+        Set recentExps = user.getCurrentExperiments();
+        if (recentExps.isEmpty()) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.int.exp.search.recent.empty"));
+            saveErrors(request, errors);
+            return mapping.getInputForward();
+        }
+        // We have edited/added experiments in the current session.
+        view.addExperimentToHold(recentExps);
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+
+    public ActionForward search(ActionMapping mapping,
+                                ActionForm form,
+                                HttpServletRequest request,
+                                HttpServletResponse response)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // The search parameter.
+        String searchParam = getSearchParam(dynaform);
+
+        // The search value.
+        String searchValue = (String) dynaform.get(searchParam);
+        if (searchValue.length() == 0) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.int.exp.search.input"));
+            saveErrors(request, errors);
+            return mapping.getInputForward();
+        }
+        // Handler to the Intact User.
+        EditUserI user = getIntactUser(request);
+
+        // Normalize the search parameter.
+        searchParam = searchParam.endsWith("AC") ? "ac" : "shortLabel";
+
+        // The collection to hold experiments.
+        Collection experiments = user.search(Experiment.class.getName(), searchParam,
+                searchValue);
+        // Search found any results?
+        if (experiments.isEmpty()) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.int.exp.search.empty", searchParam));
+            saveErrors(request, errors);
+            return mapping.getInputForward();
+        }
+        // The number of Experiments retrieved from the search.
+        int size = experiments.size();
+
+        // Just an arbitrary number for the moment.
+        if (size > 10) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.int.exp.search.many",
+                            Integer.toString(size), searchParam, "10"));
+            saveErrors(request, errors);
+            return mapping.getInputForward();
+        }
+        // The current view of the edit session.
+        InteractionViewBean view = (InteractionViewBean) user.getView();
+
+        // Add the search result to the holder.
+        view.addExperimentToHold(experiments);
+
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+
+    /**
+     * Returns the search parameter.
+     * @param form the form to get search parameter values.
+     * @return the search parameter; the most sepecific search
+     * is preferred over the least specific one. For example, 'ac' is preferred
+     * over any other value.
+     */
+    private String getSearchParam(DynaActionForm form) {
+        String ac = (String) form.get("expSearchAC");
+        if ((ac != null) && (ac.length() > 0)) {
+            return "expSearchAC";
+        }
+        return "expSearchLabel";
+    }
+}
---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/interaction/FillInteractionFormAction.java
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action.interaction;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+import uk.ac.ebi.intact.application.editor.struts.action.FillCvFormAction;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.ExperimentBean;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.InteractionViewBean;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.ProteinBean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Populates Interaction form for display.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class FillInteractionFormAction extends FillCvFormAction {
+
+    // Override Actions's execure method.
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+        // Fill the form with common info.
+        super.execute(mapping, form, request, response);
+
+        // The view of the current object we are editing at the moment.
+        InteractionViewBean view = (InteractionViewBean)
+                getIntactUser(request).getView();
+
+        // Poplulate with experiment data; first try the form first, so it
+        // will save the last typed values.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // Assumption: We are creating a new record if the current view has no
+        // short label. Reset the previous values.
+        if (view.getShortLabel() == null) {
+            dynaform.set("kD", Float.valueOf("0.0"));
+            dynaform.set("organism", null);
+            dynaform.set("interactionType", null);
+
+            // Clear the search forms as well.
+            dynaform.set("expSearchLabel", null);
+            dynaform.set("expSearchAC", null);
+            dynaform.set("protSearchLabel", null);
+            dynaform.set("protSearchSpAC", null);
+            dynaform.set("protSearchAC", null);
+        }
+        else {
+            if (isPropertyNull(dynaform, "kD")) {
+                dynaform.set("kD", view.getKD());
+            }
+            if (isPropertyNullOrEmpty(dynaform, "organism")) {
+                dynaform.set("organism", view.getOrganism());
+            }
+            if (isPropertyNullOrEmpty(dynaform, "interactionType")) {
+                // Special case this is a dag menu object.
+                dynaform.set("interactionType", view.getSelectedInterationType());
+            }
+        }
+        // Populate with the experiments for the interaction.
+        List exps = view.getExperiments();
+        dynaform.set("exps", exps.toArray(new ExperimentBean[0]));
+        dynaform.set("expCmd", new String[exps.size()]);
+
+        // Populate with experiments on hold.
+        List expshold = view.getHoldExperiments();
+        dynaform.set("expshold", expshold.toArray(new ExperimentBean[0]));
+        dynaform.set("expsholdCmd", new String[expshold.size()]);
+
+        // Populate with Proteins
+        List proteins = view.getProteins();
+        dynaform.set("proteins", proteins.toArray(new ProteinBean[0]));
+        dynaform.set("protCmd", new String[proteins.size()]);
+
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+}
