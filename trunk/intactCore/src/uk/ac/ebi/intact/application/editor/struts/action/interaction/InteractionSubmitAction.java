deb2ee3342415cb989994cde59d8b708b4e45cd1
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2003-10-16 09:38:06 +0000

    As a result of moving to single forms

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/interaction/InteractionSubmitAction.java
@@ -0,0 +1,85 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action.interaction;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+import org.apache.struts.util.MessageResources;
+import uk.ac.ebi.intact.application.editor.struts.action.SubmitFormAction;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.InteractionViewBean;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * An action to handle Interaction specific events.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class InteractionSubmitAction extends SubmitFormAction {
+
+    // Override the super method to handle the event for pressing the tax id.
+    protected ActionForward handle(ActionMapping mapping,
+                                   ActionForm form,
+                                   HttpServletRequest request)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // List of buttons to check which experiment was selected.
+        String[] expCmds = (String[]) dynaform.get("expCmd");
+        int expIdx = getCurrentSelection(expCmds);
+
+        if (expIdx != -1 ) {
+            // Selected an experiment to delete; set the index.
+            dynaform.set("idx", new Integer(expIdx));
+            return mapping.findForward("expDel");
+        }
+        // List of buttons to check which experiment was added/hidden.
+        String[] expholdCmds = (String[]) dynaform.get("expsholdCmd");
+        int expholdIdx = getCurrentSelection(expholdCmds);
+
+        if (expholdIdx != -1 ) {
+            // Selected an experiment to add/hide.
+            dynaform.set("idx", new Integer(expholdIdx));
+            return mapping.findForward("expHold");
+        }
+        // List of buttons to check which proten was edited/saved/deleted.
+        String[] protCmds = (String[]) dynaform.get("protCmd");
+        int protIdx = getCurrentSelection(protCmds);
+
+        if (protIdx != -1 ) {
+            // Selected a Protein.
+            dynaform.set("idx", new Integer(protIdx));
+            return mapping.findForward("prot");
+        }
+        return mapping.findForward(FORWARD_FAILURE);
+    }
+
+    // Handle events with dispatch parameter.
+    protected ActionForward handleDispatch(ActionMapping mapping,
+                                           ActionForm form,
+                                           HttpServletRequest request,
+                                           String dispatch)
+            throws Exception {
+        // Message resources to access button labels.
+        MessageResources msgres = getResources(request);
+
+        if (dispatch.equals(msgres.getMessage("int.exp.button.recent"))) {
+            return mapping.findForward("expSearch");
+        }
+        if (dispatch.equals(msgres.getMessage("int.exp.button.search"))) {
+            return mapping.findForward("expSearch");
+        }
+        if (dispatch.equals(msgres.getMessage("int.proteins.button.search"))) {
+            return mapping.findForward("protSearch");
+        }
+        return null;
+    }
+}
---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/action/interaction/ProteinDispatchAction.java
@@ -0,0 +1,162 @@
+/*
+Copyright (c) 2002-2003 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.action.interaction;
+
+import org.apache.struts.action.*;
+import org.apache.struts.util.MessageResources;
+import uk.ac.ebi.intact.application.editor.struts.framework.AbstractEditorAction;
+import uk.ac.ebi.intact.application.editor.struts.view.AbstractEditBean;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.InteractionViewBean;
+import uk.ac.ebi.intact.application.editor.struts.view.interaction.ProteinBean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The action class for editing a Protein.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class ProteinDispatchAction extends AbstractEditorAction {
+
+    /**
+     * This method dispatches calls to various methods using the 'dispatch' parameter.
+     *
+     * @param mapping - The <code>ActionMapping</code> used to select this instance
+     * @param form - The optional <code>ActionForm</code> bean for this request (if any)
+     * @param request - The HTTP request we are processing
+     * @param response - The HTTP response we are creating
+     *
+     * @return - represents a destination to which the action servlet,
+     * <code>ActionServlet</code>, might be directed to perform a RequestDispatcher.forward()
+     * or HttpServletResponse.sendRedirect() to, as a result of processing
+     * activities of an <code>Action</code> class
+     */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // The index position of the xref.
+        int idx = ((Integer) dynaform.get("idx")).intValue();
+
+        // The command associated with the index.
+        String cmd = ((String[]) dynaform.get("protCmd"))[idx];
+
+        // Message resources to access button labels.
+        MessageResources msgres = getResources(request);
+
+        if (cmd.equals(msgres.getMessage("int.proteins.button.edit"))) {
+            return edit(mapping, form, request, response);
+        }
+        if (cmd.equals(msgres.getMessage("int.proteins.button.save"))) {
+            return save(mapping, form, request, response);
+        }
+        // default is delete.
+        return delete(mapping, form, request, response);
+    }
+
+    public ActionForward edit(ActionMapping mapping,
+                              ActionForm form,
+                              HttpServletRequest request,
+                              HttpServletResponse response)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // The index position of the annotation.
+        int idx = ((Integer) dynaform.get("idx")).intValue();
+
+        // The current view of the edit session.
+        InteractionViewBean viewbean =
+                (InteractionViewBean) getIntactUser(request).getView();
+
+        // The protein we are editing at the moment.
+        ProteinBean pb = ((ProteinBean[]) dynaform.get("proteins"))[idx];
+
+        // We must have the protein bean.
+        assert pb != null;
+
+        // Must save this bean.
+        pb.setEditState(AbstractEditBean.SAVE);
+
+        // Update the form.
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+
+    public ActionForward save(ActionMapping mapping,
+                              ActionForm form,
+                              HttpServletRequest request,
+                              HttpServletResponse response)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // The index position of the annotation.
+        int idx = ((Integer) dynaform.get("idx")).intValue();
+
+        // The current view of the edit session.
+        InteractionViewBean viewbean =
+                (InteractionViewBean) getIntactUser(request).getView();
+
+        // The protein we are editing at the moment.
+        ProteinBean pb = ((ProteinBean[]) dynaform.get("proteins"))[idx];
+
+        // We must have the protein bean.
+        assert pb != null;
+
+        if (viewbean.hasDuplicates(pb)) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.int.protein.edit.dup",
+                            pb.getShortLabel(), pb.getRole()));
+            saveErrors(request, errors);
+            pb.setEditState(ProteinBean.ERROR);
+            return mapping.findForward(FORWARD_FAILURE);
+        }
+        // The protein to update.
+        viewbean.addProteinToUpdate(pb);
+
+        // Back to the view mode.
+        pb.setEditState(AbstractEditBean.VIEW);
+
+        // Update the form.
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+
+    public ActionForward delete(ActionMapping mapping,
+                                ActionForm form,
+                                HttpServletRequest request,
+                                HttpServletResponse response)
+            throws Exception {
+        // The dyna form.
+        DynaActionForm dynaform = (DynaActionForm) form;
+
+        // The index position of the annotation.
+        int idx = ((Integer) dynaform.get("idx")).intValue();
+
+        // The current view of the edit session.
+        InteractionViewBean view =
+                (InteractionViewBean) getIntactUser(request).getView();
+
+        // The protein we are editing at the moment.
+        ProteinBean pb = ((ProteinBean[]) dynaform.get("proteins"))[idx];
+
+        // We must have the protein bean.
+        assert pb != null;
+
+        // Delete this Protein from the view.
+        view.delProtein(idx);
+
+        // Update the form.
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
+}
