902f1ddc2034a4649b851141ed0a4cdf43997c2c
skerrien <samuel.kerrien@gmail.com>
2003-11-13 15:14:52 +0000

    Stores the logger name

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search2/business/Constants.java
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.  
+All rights reserved. Please see the file LICENSE 
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.application.search2.business;
+
+/**
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class Constants {
+
+    /**
+     * Name of the Log4J logger
+     */
+    public static final String LOGGER_NAME = "search";
+}
