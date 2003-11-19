c854da5f5ce87d33116e0f6c7ced5ed6093992f0
skerrien <samuel.kerrien@gmail.com>
2003-11-19 11:07:19 +0000

    return right exit code

--- trunk/intactCore/scripts/oracle/testfill.sh
+++ trunk/intactCore/scripts/oracle/testfill.sh
@@ -22,8 +22,22 @@ sleep 2
 
 cd sql/oracle
 sqlplus $1@$2 @create_all.sql
+if [ $? != 0 ]
+then
+    exit 1 # if something went wrong in the previous command line, exit with an error code
+fi
+
 sqlplus $1@$2 @create_dummy.sql
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 sqlplus $1@$2 @create_privs.sql
+if [ $? != 0 ]
+then
+    exit 1
+fi
 cd ../../
 
 
@@ -34,42 +48,82 @@ echo "Inserting controlled vocabularies"
 echo ""
 echo "Insert CvTopic"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvTopic data/controlledVocab/CvTopic.def
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvXrefQualifier"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvXrefQualifier data/controlledVocab/CvXrefQualifier.def
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvAliasType"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvAliasType data/controlledVocab/CvAliasType.def
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvDatabase"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvDatabase data/controlledVocab/CvDatabase.def
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvComponentRole"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvComponentRole data/controlledVocab/CvComponentRole.def
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvIdentification"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvIdentification data/controlledVocab/CvIdentification.def data/controlledVocab/CvIdentification.dag
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvInteraction"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvInteraction data/controlledVocab/CvInteraction.def data/controlledVocab/CvInteraction.dag
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Insert CvInteractionType"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvInteractionType data/controlledVocab/CvInteractionType.def data/controlledVocab/CvInteractionType.dag
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Inserting Proteins ..."
 scripts/javaRun.sh UpdateProteins file:data/yeast_test.sp
- 
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 echo ""
 echo "Inserting Complexes ..."
 scripts/javaRun.sh InsertComplex -file data/ho_gavin_${DATASET}.dat -taxId 4932 -interactionType aggregation
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 #end
 
--- trunk/intactCore/scripts/postgres/testfill.sh
+++ trunk/intactCore/scripts/postgres/testfill.sh
@@ -33,8 +33,22 @@ echo
 sleep 2
 
 psql -U $DBUSER -d $2 -f sql/postgres/drop_tables.sql
+if [ $? != 0 ]
+then
+    exit 1 # if something went wrong in the previous command line, exit with an error code
+fi
+
 psql -U $DBUSER -d $2 -f sql/postgres/create_tables.sql
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
 psql -U $DBUSER -d $2 -f sql/postgres/create_dummy.sql
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 
 echo ""
@@ -44,43 +58,84 @@ echo "Inserting controlled vocabularies"
 echo ""
 echo "Insert CvTopic"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvTopic data/controlledVocab/CvTopic.def
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvXrefQualifier"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvXrefQualifier data/controlledVocab/CvXrefQualifier.def
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvAliasType"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvAliasType data/controlledVocab/CvAliasType.def
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvDatabase"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvDatabase data/controlledVocab/CvDatabase.def
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvComponentRole"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvComponentRole data/controlledVocab/CvComponentRole.def
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvIdentification"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvIdentification data/controlledVocab/CvIdentification.def data/controlledVocab/CvIdentification.dag
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvInteraction"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvInteraction data/controlledVocab/CvInteraction.def data/controlledVocab/CvInteraction.dag
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Insert CvInteractionType"
 scripts/javaRun.sh GoTools upload uk.ac.ebi.intact.model.CvInteractionType data/controlledVocab/CvInteractionType.def data/controlledVocab/CvInteractionType.dag
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Inserting Proteins ..."
 scripts/javaRun.sh UpdateProteins file:data/yeast_test.sp
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 echo ""
 echo "Inserting Complexes ..."
 
 scripts/javaRun.sh InsertComplex -file data/ho_gavin_${DATASET}.dat -taxId 4932 -interactionType aggregation
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 #end
 
+exit 0
