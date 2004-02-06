da3738388de48f9d6d0cf674b5ca0031ffdaa998
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-02-06 17:39:00 +0000

    Removed commented out code; changed the order for isAssignable method to accomodate for Interaction interface

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/framework/util/EditViewBeanFactory.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/framework/util/EditViewBeanFactory.java
@@ -32,26 +32,6 @@ public class EditViewBeanFactory {
     private Map myNameToView = new HashMap();
 
     /**
-     * CV editor is stored under this key.
-     */
-//    private static final String CV_EDITOR = "0";
-//
-//    /**
-//     * BioSource editor is stored under this key.
-//     */
-//    private static final String BIOSRC_EDITOR = "1";
-//
-//    /**
-//     * Experiment editor is stored under this key.
-//     */
-//    private static final String EXPERIMENT_EDITOR = "2";
-//
-//    /**
-//     * Interaction editor is stored under this key.
-//     */
-//    private static final String INTERACTION_EDITOR = "3";
-
-    /**
      * The factory to create various menus (single menu factory for a user).
      */
     private EditorMenuFactory myMenuFactory;
@@ -64,121 +44,36 @@ public class EditViewBeanFactory {
         myMenuFactory = new EditorMenuFactory(helper);
     }
 
+    /**
+     * Returns a view bean constructed from given class.
+     * @param clazz the Class to determine the view bean to retun.
+     * @return a view bean from the cache is returned for an existing
+     * view or a new view bean is returned (cached). CV view bean is returned
+     * for an unknown <code>clazz</code> (other than Experiment, Interaction
+     * and  BioSource).
+     */
     public AbstractEditViewBean factory(Class clazz) {
         if (myNameToView.containsKey(clazz)) {
             return (AbstractEditViewBean) myNameToView.get(clazz);
         }
         // Create a a new view put it in the cache.
         AbstractEditViewBean viewbean;
-        if (clazz.isAssignableFrom(BioSource.class)) {
+        if (BioSource.class.isAssignableFrom(clazz)) {
             viewbean = new BioSourceViewBean();
         }
-        else if (clazz.isAssignableFrom(Experiment.class)) {
+        else if (Experiment.class.isAssignableFrom(clazz)) {
             viewbean = new ExperimentViewBean();
         }
-        else if (clazz.isAssignableFrom(Interaction.class)) {
+        else if (Interaction.class.isAssignableFrom(clazz)) {
             viewbean = new InteractionViewBean();
         }
         else {
             // Assume it is an CV object.
             viewbean = new CvViewBean();
         }
+        // New view bean, set the factory and cache it.
         viewbean.setMenuFactory(myMenuFactory);
         myNameToView.put(clazz, viewbean);
         return viewbean;
-
     }
-
-    /**
-     * Factory method to return an instance of an Edit view bean.
-     * @param annot the Annotated object to initialize the edit view bean.
-     * @return an instance of <code>AbstractEditViewBean</code>; the type to return
-     * is determined by <code>annot</code>. This method does not create new
-     * instances of beans; instead it recyles them.
-     */
-//    public AbstractEditViewBean factory(AnnotatedObject annot) {
-//        // The view bean to return.
-//        AbstractEditViewBean viewbean;
-//        Class clazz = annot.getClass();
-//        if (clazz.isAssignableFrom(BioSource.class)) {
-//            viewbean = getBioSourceEditor((BioSource) annot);
-//        }
-//        else if (clazz.isAssignableFrom(Experiment.class)) {
-//            viewbean = getExperimentEditor((Experiment) annot);
-//        }
-//        else if (clazz.isAssignableFrom(Interaction.class)) {
-//            viewbean = getInteractionEditor((Interaction) annot);
-//        }
-//        else {
-//            // Assume it is an CV object.
-//            viewbean = getCvEditor((CvObject) annot);
-//        }
-//        return viewbean;
-//    }
-
-    // Helper methods.
-
-//    private AbstractEditViewBean getBioSourceEditor(BioSource biosrc) {
-//        String name = BIOSRC_EDITOR;
-//        BioSourceViewBean view;
-//        // Check the cache first.
-//        if (myNameToView.containsKey(name)) {
-//            view = (BioSourceViewBean) myNameToView.get(name);
-//        }
-//        else {
-//            view = new BioSourceViewBean(myHelper);
-////            view.setMenuFactory(myMenuFactory);
-//            myNameToView.put(name, view);
-//        }
-//        view.setAnnotatedObject(biosrc);
-//        return view;
-//    }
-//
-//    private AbstractEditViewBean getExperimentEditor(Experiment exp) {
-//        String name = EXPERIMENT_EDITOR;
-//        ExperimentViewBean view;
-//        // Check the cache first.
-//        if (myNameToView.containsKey(name)) {
-//            view = (ExperimentViewBean) myNameToView.get(name);
-//        }
-//        else {
-//            view = new ExperimentViewBean(myHelper);
-////            view.setMenuFactory(myMenuFactory);
-//            myNameToView.put(name, view);
-//        }
-//        view.setAnnotatedObject(exp);
-//        return view;
-//    }
-//
-//    private AbstractEditViewBean getInteractionEditor(Interaction intact) {
-//        String name = INTERACTION_EDITOR;
-//        InteractionViewBean view;
-//        // Check the cache first.
-//        if (myNameToView.containsKey(name)) {
-//            view = (InteractionViewBean) myNameToView.get(name);
-//        }
-//        else {
-//            view = new InteractionViewBean(myHelper);
-////            view.setMenuFactory(myMenuFactory);
-//            myNameToView.put(name, view);
-//        }
-//        view.setAnnotatedObject(intact);
-//        return view;
-//    }
-//
-//    private AbstractEditViewBean getCvEditor(CvObject cvobj) {
-//        String name = CV_EDITOR;
-//        CvViewBean view;
-//        // Check the cache first.
-//        if (myNameToView.containsKey(name)) {
-//            view = (CvViewBean) myNameToView.get(name);
-//        }
-//        else {
-//            view = new CvViewBean(myHelper);
-////            view.setMenuFactory(myMenuFactory);
-//            myNameToView.put(name, view);
-//        }
-//        view.setAnnotatedObject(cvobj);
-//        return view;
-//    }
 }
