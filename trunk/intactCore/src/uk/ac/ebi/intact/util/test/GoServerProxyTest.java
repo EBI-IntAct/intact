c54c0c4b1324b75e059620b3649d048e6fc28bdc
skerrien <samuel.kerrien@gmail.com>
2004-02-23 14:29:30 +0000

    Enhanced unit test

--- trunk/intactCore/src/uk/ac/ebi/intact/util/test/GoServerProxyTest.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/test/GoServerProxyTest.java
@@ -11,18 +11,24 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import uk.ac.ebi.intact.util.GoServerProxy;
+import uk.ac.ebi.intact.util.test.mocks.MockInputStream;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class GoServerProxyTest extends TestCase {
 
 
+    private static final String NEW_LINE = System.getProperty( "line.separator" );
+
+
     /**
      * Constructs a NewtServerProxyTest instance with the specified name.
+     *
      * @param name the name of the test.
      */
-    public GoServerProxyTest(String name) {
-        super(name);
+    public GoServerProxyTest( String name ) {
+        super( name );
     }
 
     /**
@@ -30,37 +36,56 @@ public class GoServerProxyTest extends TestCase {
      * the testXXX() methods to the suite.
      */
     public static Test suite() {
-        return new TestSuite(GoServerProxyTest.class);
+        return new TestSuite( GoServerProxyTest.class );
     }
 
-    public void testGetGoResponse() {
-        try {
-            getGoResponse();
-        }
-        catch (IOException ex) {
-            // Uncomment this line to find out where it went wrong!
-            ex.printStackTrace();
-            fail(ex.getMessage());
-        }
+
+    private GoServerProxy server;
+
+    /**
+     * Sets up the test fixture. Called before every test case method.
+     */
+    protected void setUp() {
+
+        this.server = new GoServerProxy();
     }
 
-    private void getGoResponse() throws IOException {
-        // The server to connect to.
-        GoServerProxy server = new GoServerProxy();
+    public static final String GO0000074 = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>" + NEW_LINE +
+                                           "<page>" + NEW_LINE +
+                                           "  <name>" + NEW_LINE +
+                                           "     regulation of cell cycle" + NEW_LINE +
+                                           "  </name>" + NEW_LINE +
+                                           "  <category>" + NEW_LINE +
+                                           "     process" + NEW_LINE +
+                                           "  </category>" + NEW_LINE +
+                                           "</page>";
+
+    public static final String GO0008645 = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>" + NEW_LINE +
+                                           "<page>" + NEW_LINE +
+                                           "  <name>" + NEW_LINE +
+                                           "     hexose transport" + NEW_LINE +
+                                           "  </name>" + NEW_LINE +
+                                           "  <category>" + NEW_LINE +
+                                           "     process" + NEW_LINE +
+                                           "  </category>" + NEW_LINE +
+                                           "</page>";
+
+
+    public void testGetGoResponseFromInputStream() {
+
         GoServerProxy.GoResponse response = null;
 
-        // Test 1: existing GO term.
         try {
-            response = server.query( "GO:0000074" );
+            MockInputStream is = new MockInputStream();
+            is.setBuffer( GO0000074 );
+            response = server.query( (InputStream) is, "GO:0000074" );
 
-            assertEquals(response.getGoId(),     "GO:0000074");
-            assertEquals(response.getName(),     "regulation of cell cycle");
-            assertEquals(response.getCategory(), "process");
-        }
-        catch (IOException ex) {
+            assertEquals( response.getGoId(),     "GO:0000074" );
+            assertEquals( response.getName(),     "regulation of cell cycle" );
+            assertEquals( response.getCategory(), "process" );
+        } catch( IOException ex ) {
             fail( "Could not connect to the GO server !" );
-        }
-        catch (GoServerProxy.GoIdNotFoundException ex) {
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
             fail( "Could not find GO:0000074" );
         }
 
@@ -68,27 +93,65 @@ public class GoServerProxyTest extends TestCase {
         try {
             response = server.query( "GO:0008645" );
 
-            assertEquals(response.getGoId(),     "GO:0008645");
-            assertEquals(response.getName(),     "hexose transport");
-            assertEquals(response.getCategory(), "process");
-        }
-        catch (IOException ex) {
+            MockInputStream is = new MockInputStream();
+            is.setBuffer( GO0008645 );
+            response = server.query( (InputStream) is, "GO:0008645" );
+
+            assertEquals( response.getGoId(),     "GO:0008645" );
+            assertEquals( response.getName(),     "hexose transport" );
+            assertEquals( response.getCategory(), "process" );
+        } catch( IOException ex ) {
             fail( "Could not connect to the GO server !" );
-        }
-        catch (GoServerProxy.GoIdNotFoundException ex) {
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
             fail( "Could not find GO:0008645" );
         }
+    }
+
+
+
+    public void testGetGoResponseWithUrl() {
+        getGoResponse();
+    }
+
+    private void getGoResponse() {
+        GoServerProxy.GoResponse response = null;
+
+        // Test 1: existing GO term.
+        try {
+            response = server.query( "GO:0000074" );
+
+            assertEquals( response.getGoId(),     "GO:0000074" );
+            assertEquals( response.getName(),     "regulation of cell cycle" );
+            assertEquals( response.getCategory(), "process" );
+        } catch( IOException ex ) {
+            fail( "Could not connect to the GO server !" );
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
+            fail( "Could not find GO:0000074" );
+        }
+
+        // Test 2: existing GO term.
+        try {
+            response = server.query( "GO:0008645" );
+
+            assertEquals( response.getGoId(),     "GO:0008645" );
+            assertEquals( response.getName(),     "hexose transport" );
+            assertEquals( response.getCategory(), "process" );
+        } catch( IOException ex ) {
+            fail( "Could not connect to the GO server !" );
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
+            fail( "Could not find GO:0008645" );
+        }
+
+        // CA BUG ICI !!!!!!!!!!!!!
 
         // Test 3: no matches.
         try {
             response = server.query( "GO:0000000" );
             // We shouldn't get here as there are no matches for this.
             fail( "Found GO:0000000 where it should have crashed !" );
-        }
-        catch (IOException ex) {
+        } catch( IOException ex ) {
             fail( "Could not connect to the GO server !" );
-        }
-        catch (GoServerProxy.GoIdNotFoundException ex) {
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
             assertTrue( true );
         }
 
@@ -99,11 +162,9 @@ public class GoServerProxyTest extends TestCase {
             response = server.query( "GO:0000074" );
             // Invalid URL.
             fail( "Succeded to parse the URL: " + url + " where it should have crashed !" );
-        }
-        catch (IOException ex) {
+        } catch( IOException ex ) {
             assertTrue( true );
-        }
-        catch (GoServerProxy.GoIdNotFoundException ex) {
+        } catch( GoServerProxy.GoIdNotFoundException ex ) {
             fail( "Succeded to parse the URL: " + url + " where it should have crashed !" );
         }
     }
