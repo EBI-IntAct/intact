abb720034728b7c6a06b5cd9489e1787916cb23e
clewing <clewing@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-03-12 16:38:41 +0000

    AnnotatedObjectTest no longer necessary

--- trunk/intactCore/src/uk/ac/ebi/intact/model/test/AllJUnitTests.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/test/AllJUnitTests.java
@@ -37,7 +37,7 @@ public class AllJUnitTests extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite();
         // Add tests one by one.
-        suite.addTest(AnnotatedObjectTest.suite());
+        //suite.addTest(AnnotatedObjectTest.suite());
         suite.addTest(EqualityTests.suite());
         //suite.addTest(SecondTest.suite());
         return suite;
