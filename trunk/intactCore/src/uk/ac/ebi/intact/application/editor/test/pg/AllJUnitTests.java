195100f2377f5f2f66d1b32cfff71e97adf5f3ae
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-03-16 23:18:12 +0000

    Initial checkin

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/test/pg/AllJUnitTests.java
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2002-2004 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.application.editor.test.pg;
+
+// JUnit classes
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import uk.ac.ebi.intact.application.editor.test.pg.security.LoginTest;
+
+/**
+ * Testsuite for Postgres database.
+ *
+ * @author Sugath Mudali
+ * @version $Id$
+ */
+public class AllJUnitTests extends TestCase {
+
+    /**
+     * The constructor with the test name.
+     *
+     * @param name the name of the test.
+     */
+    public AllJUnitTests(String name) {
+        super(name);
+    }
+
+    /**
+     * Returns a suite containing tests.
+     * @return a suite containing tests.
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(LoginTest.suite());
+        suite.addTest(SearchTest.suite());
+        return suite;
+    }
+}
