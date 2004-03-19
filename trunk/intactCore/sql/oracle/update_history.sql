2fcb79caf430dabfc08a0b8f649c9e9b2dc2a876
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-03-19 10:16:21 +0000

    initial checkin

---
+++ trunk/intactCore/sql/oracle/update_history.sql
@@ -0,0 +1,91 @@
+set doc off
+
+/**************************************************************************************************************************
+
+  Package:    IntAct
+
+  Purpose:    Update scripts to go from database version to database version.
+
+  Usage:      sqlplus username/password @update_history.sql
+
+
+  $Date$
+
+  $Locker$
+
+
+  **************************************************************************************************************************/
+
+/* From 2004-02-29 to 2004-03-15 */
+
+alter table ia_biosource
+add (tissue_ac VARCHAR2(30) CONSTRAINT fk_Biosource$tissue REFERENCES   
+IA_ControlledVocab(ac) )
+;
+
+alter table ia_biosource add ( celltype_ac VARCHAR2(30) CONSTRAINT
+fk_Biosource$celltype REFERENCES IA_ControlledVocab(ac) )
+;
+
+
+
+alter table ia_biosource_audit
+add (tissue_ac VARCHAR2(30))
+;
+
+
+alter table ia_biosource_audit add ( celltype_ac VARCHAR2(30))
+;
+
+
+
+CREATE OR REPLACE TRIGGER TRGAUD_IA_BIOSOURCE
+ BEFORE UPDATE OR DELETE
+ ON ia_biosource
+ FOR EACH ROW
+declare
+        l_event char(1);
+begin
+        if deleting then
+                l_event := 'D';
+        elsif updating then
+                l_event := 'U';
+                :new.timestamp := sysdate;
+                :new.userstamp := user;
+        end if ;
+
+
+        insert into ia_biosource_audit
+                ( event
+                , ac
+                , deprecated
+                , created
+                , updated
+                , timestamp
+                , userstamp
+                , taxid
+                , owner_ac
+                , shortlabel
+                , fullname
+                , tissue_ac
+                , celltype_ac
+                )
+        values
+                ( l_event
+                , :old.ac
+                , :old.deprecated
+                , :old.created
+                , :old.updated
+                , :old.timestamp
+                , :old.userstamp
+                , :old.taxid
+                , :old.owner_ac
+                , :old.shortlabel
+                , :old.fullname
+                , :old.tissue_ac
+                , :old.celltype_ac
+                );
+end;
+/
+
+
