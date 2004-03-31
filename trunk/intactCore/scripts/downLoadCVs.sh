0a5571e7fb24e44de1d349aa80045962540c0348
hhe <hhe@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-03-31 12:17:22 +0000

    Update.

--- trunk/intactCore/scripts/downLoadCVs.sh
+++ trunk/intactCore/scripts/downLoadCVs.sh
@@ -7,23 +7,24 @@
 # downLoadCVs.sh targetDirectory
 #
 
+
 # write simple lists which are not part of PSI
 for cv in CvAliasType CvComponentRole CvDatabase CvTopic CvXrefQualifier 
 do
    scripts/javaRun.sh GoTools download uk.ac.ebi.intact.model.$cv - $1/$cv.def 
-   perl -w scripts/dag2html.pl -stems $1/$cv
+   perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
 done
 
 # write DAG CVs which are not part of PSI
 for cv in CvInteractionType
 do scripts/javaRun.sh GoTools download uk.ac.ebi.intact.model.$cv - $1/$cv.def $1/$cv.dag 
-   perl -w scripts/dag2html.pl -stems $1/$cv
+   perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
 done
 
 # write CVs which are part of PSI
 for cv in CvIdentification CvInteraction
 do scripts/javaRun.sh GoTools download uk.ac.ebi.intact.model.$cv psi-mi $1/$cv.def $1/$cv.dag 
-   perl -w scripts/dag2html.pl -stems $1/$cv
+   perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
 done
 
 # end
