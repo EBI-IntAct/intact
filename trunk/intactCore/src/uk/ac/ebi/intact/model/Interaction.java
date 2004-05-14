7c5579e5e08999391cac71414bac21fe5a8ee30e
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-05-14 16:08:39 +0000

    Removed copy method and replaced it with clone

--- trunk/intactCore/src/uk/ac/ebi/intact/model/Interaction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/Interaction.java
@@ -60,14 +60,4 @@ public interface Interaction extends Interactor {
     public int hashCode();
 
     public String toString();
-
-    /**
-     * Returns a copy of the current interaction with few exceptions as utlined
-     * below.
-     * @return the copy of the current interaction. The copy inherits
-     * values for Interaction type and host organism of the current interaction. 'x' will
-     * be appneded to the short label. The copy also inherits same Proteins with
-     * new components and a copy of new Xrefs. Annotations are not inherited.   
-     */
-    public Interaction copy();
 }
