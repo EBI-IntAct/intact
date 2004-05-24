ac3485b10e175790cba4b0c8aeeb6ce3aa12d108
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-05-24 17:38:06 +0000

    Removed copy method

--- trunk/intactCore/src/uk/ac/ebi/intact/model/Xref.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/Xref.java
@@ -108,18 +108,6 @@ public class Xref extends BasicObjectImpl {
         setCvXrefQualifier(aCvXrefQualifier);
     }
 
-
-    /**
-     * Returns a copy of the current Xref with few exceptions as utlined
-     * below.
-     * @return the copy of the current Xref. The copy inherits values for
-     *  CvDatabase, Owner and CvXrefQualifier. Primary id, Secondary Id
-     * and database release are copied.    
-     */
-    public Xref copy() {
-        return new Xref(getOwner(), getCvDatabase(), getPrimaryId(),
-            getSecondaryId(), getDbRelease(), getCvXrefQualifier());
-    }
     
     ///////////////////////////////////////
     //access methods for attributes
