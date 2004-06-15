402651d655c8a8c306184dba936439446c34f701
skerrien <samuel.kerrien@gmail.com>
2004-06-15 15:19:48 +0000

    BUGFIX: Add the creation of the in vitro biosource, including the owner.

--- trunk/intactCore/sql/oracle/create_dummy.sql
+++ trunk/intactCore/sql/oracle/create_dummy.sql
@@ -69,8 +69,13 @@ INSERT INTO IA_ControlledVocab (ac, objClass, shortLabel, fullName, owner_ac)
          FROM IA_Institution
         WHERE shortLabel='EBI';
 
-INSERT INTO IA_biosource (ac, taxid, shortlabel)
-values (SELECT 'EBI-' || Intact_ac.nextval, '-1', 'in vitro');        
+INSERT INTO IA_biosource (ac, taxid, shortlabel, owner_ac)
+       SELECT 'EBI-' || Intact_ac.nextval,
+              '-1',
+              'in vitro',
+	          ac
+       FROM IA_Institution
+       WHERE shortLabel='EBI';
 
 commit;
 exit;
