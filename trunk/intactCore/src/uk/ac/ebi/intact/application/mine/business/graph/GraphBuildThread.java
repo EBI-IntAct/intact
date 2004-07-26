a87e90ec8af622e0abfcf34a5488c15fafc1cb7e
groscurth <groscurth@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-07-26 15:54:08 +0000

    Structures to store the graphs and organize the grapg building process

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/mine/business/graph/GraphBuildThread.java
@@ -0,0 +1,121 @@
+/*
+ * Created on 26.07.2004
+ */
+
+package uk.ac.ebi.intact.application.mine.business.graph;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Map;
+
+import jdsl.graph.api.Vertex;
+import jdsl.graph.ref.IncidenceListGraph;
+
+import org.shiftone.cache.Cache;
+
+import uk.ac.ebi.intact.application.mine.business.IntactUserI;
+import uk.ac.ebi.intact.application.mine.business.graph.model.EdgeObject;
+import uk.ac.ebi.intact.application.mine.business.graph.model.GraphData;
+
+/**
+ * @author Andreas Groscurth
+ */
+public class GraphBuildThread extends Thread {
+    // the static values are set by the GraphManager !
+    static IntactUserI intactUser;
+    static Cache cache;
+    static Collection running;
+
+    private Integer toProcceed;
+
+    /**
+     * Creates new Thread to build a graph for the given ID
+     * 
+     * @param i the graphID
+     */
+    public GraphBuildThread(Integer i) {
+        toProcceed = i;
+    }
+
+    public void run() {
+        GraphData gd;
+        try {
+            // build the graph
+            gd = buildGraph( toProcceed );
+
+            // the removal of the graphid and the adding of the graph is
+            // synchronized to avoid that another thread wants to read the
+            // running structure and build a graph whilst this thread removed it
+            // and pushed it into the cache
+            synchronized ( running ) {
+                // the data is stored in the cache
+                cache.addObject( toProcceed, gd );
+                // work is done -> remove it from the running structure
+                running.remove( toProcceed );
+            }
+        }
+        catch ( SQLException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Method to build the graph for the provided graphid - works on database
+     * level
+     * 
+     * @param graphid the graphID
+     * @return the graphData
+     * @throws SQLException
+     */
+    private GraphData buildGraph(Integer graphid) throws SQLException {
+        System.out.println( "building " + graphid );
+        Statement stm = intactUser.getDBConnection().createStatement();
+        ResultSet set = null;
+        IncidenceListGraph graph = null;
+        Vertex v1, v2;
+        String protein1_ac, protein2_ac, interaction_ac;
+        Map nodeLabelMap = new Hashtable();
+
+        set = stm.executeQuery( "SELECT * FROM ia_interactions WHERE graphid="
+                + graphid );
+        // the graph is initialised
+        graph = new IncidenceListGraph();
+        while ( set.next() ) {
+            // the two interactors are fetched
+            protein1_ac = set.getString( 1 ).trim().toUpperCase();
+            protein2_ac = set.getString( 2 ).trim().toUpperCase();
+            // the interaction_ac of the interactions
+            interaction_ac = set.getString( 5 );
+
+            // if the map does not contain the interactor_ac
+            // this means the interactor is not yet in the graph
+            if ( !nodeLabelMap.containsKey( protein1_ac ) ) {
+                v1 = graph.insertVertex( protein1_ac );
+                nodeLabelMap.put( protein1_ac, v1 );
+            }
+            // if the map does not contain the interactor_ac
+            // this means the interactor is not yet in the graph
+            if ( !nodeLabelMap.containsKey( protein2_ac ) ) {
+                v2 = graph.insertVertex( protein2_ac );
+                nodeLabelMap.put( protein2_ac, v2 );
+            }
+            // because it can happens that just one of the if tests
+            // is succesful which means the protein1_ac may be old and
+            // different
+            // to the node v1 - the correct node for the given
+            // interactor_ac has to be fetched from the map
+            v1 = (Vertex) nodeLabelMap.get( protein1_ac );
+            v2 = (Vertex) nodeLabelMap.get( protein2_ac );
+            // the edge between these two nodes is inserted
+            graph.insertEdge( v1, v2, new EdgeObject( interaction_ac, set
+                    .getDouble( 6 ) ) );
+        }
+        set.close();
+        stm.close();
+        return new GraphData( graph, nodeLabelMap );
+    }
+}
