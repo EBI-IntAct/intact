8efaad13d1b512240f946f659d57cdec09971dc4
groscurth <groscurth@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-08-18 15:29:42 +0000

    bugfixing added null tests before calling a method of several collections

--- trunk/intactCore/src/uk/ac/ebi/intact/application/mine/struts/view/AmbiguousBean.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/mine/struts/view/AmbiguousBean.java
@@ -27,260 +27,288 @@ import uk.ac.ebi.intact.model.Protein;
  * @author Andreas Groscurth
  */
 public class AmbiguousBean {
-    private static final String HELP_LINK = "/displayDoc.jsp?section=";
-    private Collection proteins = null;
-    private Collection interactions = null;
-    private Collection experiments = null;
-    private String searchAc;
-    private String context;
+	private static final String HELP_LINK = "/displayDoc.jsp?section=";
+	private Collection proteins = null;
+	private Collection interactions = null;
+	private Collection experiments = null;
+	private String searchAc;
+	private String context;
 
-    public int hashCode() {
-        return searchAc.hashCode();
-    }
+	public int hashCode() {
+		return searchAc.hashCode();
+	}
 
-    public boolean equals(Object o) {
-        if ( this == o ) {
-            return true;
-        }
-        if ( !( o instanceof AmbiguousBean ) ) {
-            return false;
-        }
-        AmbiguousBean element = (AmbiguousBean) o;
-        return element.searchAc.equals( searchAc );
-    }
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof AmbiguousBean)) {
+			return false;
+		}
+		AmbiguousBean element = (AmbiguousBean) o;
+		return element.searchAc.equals(searchAc);
+	}
 
-    /**
-     * @param searchAc The searchAc to set.
-     */
-    public void setSearchAc(String searchAc) {
-        this.searchAc = searchAc;
-    }
+	/**
+	 * @param searchAc The searchAc to set.
+	 */
+	public void setSearchAc(String searchAc) {
+		this.searchAc = searchAc;
+	}
 
-    /**
-     * @return Returns the searchAc.
-     */
-    public String getSearchAc() {
-        return searchAc;
-    }
+	/**
+	 * @return Returns the searchAc.
+	 */
+	public String getSearchAc() {
+		return searchAc;
+	}
 
-    /**
-     * Returns the number of all found results. This is the sum of the size of
-     * all three collections.
-     * 
-     * @return the number of found results.
-     */
-    public int getNumberOfResults() {
-        return proteins.size() + interactions.size() + experiments.size();
-    }
+	/**
+	 * Returns the number of all found results. This is the sum of the size of
+	 * all three collections.
+	 * 
+	 * @return the number of found results.
+	 */
+	public int getNumberOfResults() {
+		int protSize = proteins == null ? 0 : proteins.size();
+		int interSize = interactions == null ? 0 : interactions.size();
+		int expSize = experiments == null ? 0 : experiments.size();
+		return protSize + interSize + expSize;
+	}
 
-    /**
-     * Returns all found results which are an <tt>Experiment</tt>
-     * 
-     * @return Returns the experiments.
-     */
-    public Collection getExperiments() {
-        return experiments;
-    }
+	/**
+	 * Returns all found results which are an <tt>Experiment</tt>
+	 * 
+	 * @return Returns the experiments.
+	 */
+	public Collection getExperiments() {
+		return experiments;
+	}
 
-    /**
-     * Sets the found results which are an <tt>Experiment</tt>
-     * 
-     * @param experiments The experiments to set.
-     */
-    public void setExperiments(Collection experiments) {
-        this.experiments = experiments;
-    }
+	/**
+	 * Sets the found results which are an <tt>Experiment</tt>
+	 * 
+	 * @param experiments The experiments to set.
+	 */
+	public void setExperiments(Collection experiments) {
+		this.experiments = experiments;
+	}
 
-    /**
-     * Returns all found results which are an <tt>Interaction</tt>
-     * 
-     * @return Returns the interactions.
-     */
-    public Collection getInteractions() {
-        return interactions;
-    }
+	/**
+	 * Returns all found results which are an <tt>Interaction</tt>
+	 * 
+	 * @return Returns the interactions.
+	 */
+	public Collection getInteractions() {
+		return interactions;
+	}
 
-    /**
-     * Sets the found results which are an <tt>Interaction</tt>
-     * 
-     * @param interactions The interactions to set.
-     */
-    public void setInteractions(Collection interactions) {
-        this.interactions = interactions;
-    }
+	/**
+	 * Sets the found results which are an <tt>Interaction</tt>
+	 * 
+	 * @param interactions The interactions to set.
+	 */
+	public void setInteractions(Collection interactions) {
+		this.interactions = interactions;
+	}
 
-    /**
-     * Returns all found results which are a <tt>Protein</tt>
-     * 
-     * @return Returns the proteins.
-     */
-    public Collection getProteins() {
-        return proteins;
-    }
+	/**
+	 * Returns all found results which are a <tt>Protein</tt>
+	 * 
+	 * @return Returns the proteins.
+	 */
+	public Collection getProteins() {
+		return proteins;
+	}
 
-    /**
-     * Returns whether the search returned an ambigious result. This is the case
-     * when more than one protein was found or at least one interaction or on
-     * experiment.
-     * 
-     * @return whether an ambiguous result exists.
-     */
-    public boolean hasAmbiguousResult() {
-        return !( proteins.size() == 1 && interactions.size() == 0 && experiments
-                .size() == 0 );
-    }
+	/**
+	 * Returns whether the search returned an ambigious result. This is the case
+	 * when more than one protein was found or at least one interaction or on
+	 * experiment.
+	 * 
+	 * @return whether an ambiguous result exists.
+	 */
+	public boolean hasAmbiguousResult() {
+		// if an interaction was found we have an ambiguous result
+		if (interactions != null && interactions.size() > 0) {
+			return true;
+		}
 
-    /**
-     * Sets the found results which are a <tt>Protein</tt>
-     * 
-     * @param proteins The proteins to set.
-     */
-    public void setProteins(Collection proteins) {
-        this.proteins = proteins;
-    }
+		// if an experiment was found we have an ambiguous result
+		if (experiments != null && experiments.size() > 0) {
+			return true;
+		}
 
-    /**
-     * Prints the results as html to the given writer.
-     * 
-     * @param out the writer to write html
-     * @param contextPath the contextpath of the application
-     */
-    public void printHTML(Writer out, String contextPath) {
-        try {
-            out.write( searchAc + " returned<br>" );
-            context = contextPath;
-            int interactorNumber = getNumberOfResults();
-            // if no results where found
-            if ( interactorNumber == 0 ) {
-                out.write( "<i>no results</i><br>" );
-            }
-            // if more results than allowed were found
-            else if ( interactorNumber > Constants.MAX_NUMBER_RESULTS ) {
-                out.write( "<i>more than " + Constants.MAX_NUMBER_RESULTS );
-                out.write( " results, please refine your query</i><br>" );
-            }
-            else {
-                out.write( "<table width=\"100%\" bgcolor=\"#336666\">" );
-                Iterator iter;
-                // all found proteins are displayed
-                for (iter = proteins.iterator(); iter.hasNext();) {
-                    printProtein( out, (Protein) iter.next() );
-                }
-                // all found interactions are displayed
-                for (iter = interactions.iterator(); iter.hasNext();) {
-                    printInteraction( out, (Interaction) iter.next() );
-                }
-                // all found eperiments are displayed
-                for (iter = experiments.iterator(); iter.hasNext();) {
-                    printExperiment( out, (Experiment) iter.next() );
-                }
-                out.write( "</table><br>\n" );
-            }
-            out.write( "<br>" );
-        }
-        catch ( IOException e ) {
-            e.printStackTrace();
-        }
-    }
+		// if more than one protein was found we have an ambiguous result
+		return proteins != null && proteins.size() > 1;
+	}
 
-    /**
-     * Prints all results which are an <tt>Experimeriment</tt>. Each
-     * experiment is decomposed into its interaction which are then decomposed
-     * into their components.
-     * 
-     * @param out the writer
-     * @param exp the experiment to display
-     * @throws IOException when something failed with the writer
-     */
-    private void printExperiment(Writer out, Experiment exp) throws IOException {
-        Collection interactions = exp.getInteractions();
-        // if the number of interactions is greater than allowed
-        if ( interactions.size() > Constants.MAX_INTERACTION_SIZE ) {
-            out.write( "<i>" + exp.getAc() + " has more than "
-                    + Constants.MAX_INTERACTION_SIZE
-                    + " number of interactions</i>" );
-            return;
-        }
+	/**
+	 * Sets the found results which are a <tt>Protein</tt>
+	 * 
+	 * @param proteins The proteins to set.
+	 */
+	public void setProteins(Collection proteins) {
+		this.proteins = proteins;
+	}
 
-        // every interaction is displayed
-        for (Iterator iter = interactions.iterator(); iter.hasNext();) {
-            printInteraction( out, (Interaction) iter.next() );
-        }
-    }
+	/**
+	 * Prints the results as html to the given writer.
+	 * 
+	 * @param out the writer to write html
+	 * @param contextPath the contextpath of the application
+	 */
+	public void printHTML(Writer out, String contextPath) {
+		try {
+			out.write(searchAc + " returned<br>");
+			context = contextPath;
+			int interactorNumber = getNumberOfResults();
+			// if no results where found
+			if (interactorNumber == 0) {
+				out.write("<i>no results</i><br>");
+			}
+			// if more results than allowed were found
+			else if (interactorNumber > Constants.MAX_NUMBER_RESULTS) {
+				out.write("<i>more than " + Constants.MAX_NUMBER_RESULTS);
+				out.write(" results, please refine your query</i><br>");
+			} else {
+				out.write("<table width=\"100%\" bgcolor=\"#336666\">");
+				Iterator iter;
+				// all found proteins are displayed
+				if (proteins != null) {
+					for (iter = proteins.iterator(); iter.hasNext();) {
+						printProtein(out, (Protein) iter.next());
+					}
+				}
+				// all found interactions are displayed				
+				if (interactions != null) {
+					for (iter = interactions.iterator(); iter.hasNext();) {
+						printInteraction(out, (Interaction) iter.next());
+					}
+				}
+				if (experiments != null) {
+					// all found eperiments are displayed
+					for (iter = experiments.iterator(); iter.hasNext();) {
+						printExperiment(out, (Experiment) iter.next());
+					}
+				}
+				out.write("</table><br>\n");
+			}
+			out.write("<br>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Prints all results which are an <tt>Interaction</tt>. Each interaction
-     * is decomposed into its components.
-     * 
-     * @param out the writer
-     * @param in the interaction to display
-     * @throws IOException when something failed with the writer
-     */
-    private void printInteraction(Writer out, Interaction in)
-            throws IOException {
-        Collection components = in.getComponents();
-        // if an interaction has more than the allowed number of components
-        if ( components.size() > Constants.MAX_INTERACTION_SIZE ) {
-            out.write( "<i>" + in.getAc() + " has more than "
-                    + Constants.MAX_INTERACTION_SIZE
-                    + " number of interactors</i>" );
-            return;
-        }
-        Interactor inter;
-        // every component is displayed
-        for (Iterator iter = components.iterator(); iter.hasNext();) {
-            inter = ( (Component) iter.next() ).getInteractor();
-            if ( inter instanceof Protein ) {
-                printProtein( out, (Protein) inter );
-            }
-            else if ( inter instanceof Interaction ) {
-                printInteraction( out, (Interaction) inter );
-            }
-        }
+	/**
+	 * Prints all results which are an <tt>Experimeriment</tt>. Each
+	 * experiment is decomposed into its interaction which are then decomposed
+	 * into their components.
+	 * 
+	 * @param out the writer
+	 * @param exp the experiment to display
+	 * @throws IOException when something failed with the writer
+	 */
+	private void printExperiment(Writer out, Experiment exp)
+		throws IOException {
+		Collection interactions = exp.getInteractions();
+		// if the number of interactions is greater than allowed
+		if (interactions.size() > Constants.MAX_INTERACTION_SIZE) {
+			out.write(
+				"<i>"
+					+ exp.getAc()
+					+ " has more than "
+					+ Constants.MAX_INTERACTION_SIZE
+					+ " number of interactions</i>");
+			return;
+		}
 
-    }
+		// every interaction is displayed
+		for (Iterator iter = interactions.iterator(); iter.hasNext();) {
+			printInteraction(out, (Interaction) iter.next());
+		}
+	}
 
-    /**
-     * Prints the information of a protein to the writer. The text is formatted
-     * via HTML.
-     * 
-     * @param out The writer
-     * @param prot the protein to display
-     * @throws IOException when something failed with the writer
-     */
-    private void printProtein(Writer out, Protein prot) throws IOException {
-        String ac = prot.getAc();
+	/**
+	 * Prints all results which are an <tt>Interaction</tt>. Each interaction
+	 * is decomposed into its components.
+	 * 
+	 * @param out the writer
+	 * @param in the interaction to display
+	 * @throws IOException when something failed with the writer
+	 */
+	private void printInteraction(Writer out, Interaction in)
+		throws IOException {
+		Collection components = in.getComponents();
+		// if an interaction has more than the allowed number of components
+		if (components.size() > Constants.MAX_INTERACTION_SIZE) {
+			out.write(
+				"<i>"
+					+ in.getAc()
+					+ " has more than "
+					+ Constants.MAX_INTERACTION_SIZE
+					+ " number of interactors</i>");
+			return;
+		}
+		Interactor inter;
+		// every component is displayed
+		for (Iterator iter = components.iterator(); iter.hasNext();) {
+			inter = ((Component) iter.next()).getInteractor();
+			if (inter instanceof Protein) {
+				printProtein(out, (Protein) inter);
+			} else if (inter instanceof Interaction) {
+				printInteraction(out, (Interaction) inter);
+			}
+		}
 
-        out.write( "<tr bgcolor=\"#eeeeee\">" );
-        out.write( "<td class=\"objectClass\"><nobr>" );
-        // a checkbox is added so that the user can select the protein for the
-        // algorithm
-        out.write( "<input type=\"checkbox\" name=\"" + ac + "\"" );
-        // if only one protein is given the checkbox is checked by default
-        if ( !hasAmbiguousResult() ) {
-            out.write( " checked" );
-        }
-        out.write( ">" );
+	}
 
-        out.write( "<b>Protein</b>" );
-        out.write( "<a href=\"" + context + HELP_LINK
-                + "search.TableLayout\"" );
-        out.write( " target=\"new\"><sup><b><font color=\"red\">?</font>" );
-        out.write( "</b></sup></a></nobr></td>" );
+	/**
+	 * Prints the information of a protein to the writer. The text is formatted
+	 * via HTML.
+	 * 
+	 * @param out The writer
+	 * @param prot the protein to display
+	 * @throws IOException when something failed with the writer
+	 */
+	private void printProtein(Writer out, Protein prot) throws IOException {
+		String ac = prot.getAc();
 
-        // the ac number is added
-        out.write( "<td><a href=\"" + context + HELP_LINK
-                + "BasicObject.ac\"" );
-        out.write( "target=\"new\">Ac:</a>" + ac + "</td>" );
+		out.write("<tr bgcolor=\"#eeeeee\">");
+		out.write("<td class=\"objectClass\"><nobr>");
+		// a checkbox is added so that the user can select the protein for the
+		// algorithm
+		out.write("<input type=\"checkbox\" name=\"" + ac + "\"");
+		// if only one protein is given the checkbox is checked by default
+		if (!hasAmbiguousResult()) {
+			out.write(" checked");
+		}
+		out.write(">");
 
-        // the shortlabel is added
-        out.write( "<td><a href=\"" + context + HELP_LINK
-                + "AnnotatedObject.shortLabel\"" );
-        out.write( " target=\"new\">Name:</a> " );
-        out.write( "<a href=\"" + context + "/search/do/search?searchString="
-                + ac + "&searchClass=Protein\">" );
-        out.write( "<b><i>" + prot.getShortLabel() + "</i></b></a></td>" );
-        out.write( "<td>" + prot.getFullName() + "</td>" );
-        out.write( "</tr>" );
-    }
+		out.write("<b>Protein</b>");
+		out.write("<a href=\"" + context + HELP_LINK + "search.TableLayout\"");
+		out.write(" target=\"new\"><sup><b><font color=\"red\">?</font>");
+		out.write("</b></sup></a></nobr></td>");
+
+		// the ac number is added
+		out.write("<td><a href=\"" + context + HELP_LINK + "BasicObject.ac\"");
+		out.write("target=\"new\">Ac:</a>" + ac + "</td>");
+
+		// the shortlabel is added
+		out.write(
+			"<td><a href=\""
+				+ context
+				+ HELP_LINK
+				+ "AnnotatedObject.shortLabel\"");
+		out.write(" target=\"new\">Name:</a> ");
+		out.write(
+			"<a href=\""
+				+ context
+				+ "/search/do/search?searchString="
+				+ ac
+				+ "&searchClass=Protein\">");
+		out.write("<b><i>" + prot.getShortLabel() + "</i></b></a></td>");
+		out.write("<td>" + prot.getFullName() + "</td>");
+		out.write("</tr>");
+	}
 }
