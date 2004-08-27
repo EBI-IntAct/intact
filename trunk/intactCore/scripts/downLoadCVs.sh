6fca6c00292f5cc376208a601b1fdc9abf85feee
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-08-27 16:15:24 +0000

    generate CvInteractionType with PSI-MI ids

--- trunk/intactCore/scripts/downLoadCVs.sh
+++ trunk/intactCore/scripts/downLoadCVs.sh
@@ -15,14 +15,8 @@ do
    perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
 done
 
-# write DAG CVs which are not part of PSI
-for cv in CvInteractionType
-do scripts/javaRun.sh GoTools download uk.ac.ebi.intact.model.$cv - $1/$cv.def $1/$cv.dag 
-   perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
-done
-
 # write CVs which are part of PSI
-for cv in CvIdentification CvInteraction
+for cv in CvIdentification CvInteraction CvInteractionType
 do scripts/javaRun.sh GoTools download uk.ac.ebi.intact.model.$cv psi-mi $1/$cv.def $1/$cv.dag 
    perl -w scripts/dag2html.pl -targetDir $1 -stems $cv
 done
