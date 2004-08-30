0c9bd8acc80dec5b50b1e14e49b389f770543749
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-08-30 18:08:57 +0000

    As so far there is no PSI-MI ID for CellType and Tissue, we rely on shortlabel.

--- trunk/intactCore/src/uk/ac/ebi/intact/application/dataConversion/psiUpload/checker/AbstractOrganismChecker.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/dataConversion/psiUpload/checker/AbstractOrganismChecker.java
@@ -8,6 +8,7 @@ package uk.ac.ebi.intact.application.dataConversion.psiUpload.checker;
 import uk.ac.ebi.intact.application.dataConversion.psiUpload.model.CellTypeTag;
 import uk.ac.ebi.intact.application.dataConversion.psiUpload.model.Constants;
 import uk.ac.ebi.intact.application.dataConversion.psiUpload.model.TissueTag;
+import uk.ac.ebi.intact.application.dataConversion.psiUpload.util.CommandLineOptions;
 import uk.ac.ebi.intact.application.dataConversion.psiUpload.util.report.Message;
 import uk.ac.ebi.intact.application.dataConversion.psiUpload.util.report.MessageHolder;
 import uk.ac.ebi.intact.business.IntactException;
@@ -24,8 +25,9 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * That class .
- * 
+ * That class handles the common behaviour of OrganismChecker and HostOrganismChecker.
+ * eg. find if a protein having such tissue and/or cell type exists.
+ *
  * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
  */
@@ -34,9 +36,12 @@ public abstract class AbstractOrganismChecker {
     // will avoid to have to search again later !
     protected static final Map cache = new HashMap();
 
+    private final static boolean DEBUG = CommandLineOptions.getInstance().isDebugEnabled();
+
     protected static BioSource getBioSource( final String taxid,
                                              final CellTypeTag cellType,
                                              final TissueTag tissue ) {
+
         String id = buildID( taxid, cellType, tissue );
         return (BioSource) cache.get( id );
     }
@@ -57,15 +62,21 @@ public abstract class AbstractOrganismChecker {
         sb.append( taxid );
 
         if( null != cellType ) {
+            sb.append( '#' );
             if( null != cellType.getPsiDefinition() ) {
-                sb.append( '#' );
+                sb.append( cellType.getShortlabel() );
+            } else {
                 sb.append( cellType.getPsiDefinition().getId() );
             }
         }
 
         if( null != tissue ) {
             sb.append( '@' );
-            sb.append( tissue.getPsiDefinition().getId() );
+            if( tissue.getPsiDefinition() == null ) {
+                sb.append( tissue.getShortlabel() );
+            } else {
+                sb.append( tissue.getPsiDefinition().getId() );
+            }
         }
 
         return sb.toString();
@@ -77,12 +88,20 @@ public abstract class AbstractOrganismChecker {
                                       final IntactHelper helper,
                                       final BioSourceFactory bioSourceFactory ) {
 
+        if( DEBUG ) {
+            System.out.println( "Looking for: " + taxid + ", cellType:" + cellType + ", Tissue:" + tissue );
+        }
+
         final String cacheId = buildID( taxid, cellType, tissue );
         BioSource bioSource = (BioSource) cache.get( cacheId );
 
         // TODO enhance here because in case there is no mapping for an ID we will keep searching for it. Once is enough.
 
         if( bioSource == null ) {
+            if( DEBUG ) {
+                System.out.println( "No found in cache" );
+            }
+
             if( null == tissue && null == cellType ) {
 
                 // original BioSource wanted !
@@ -122,11 +141,18 @@ public abstract class AbstractOrganismChecker {
 
                 Collection biosources = null;
                 try {
+                    if( DEBUG ) {
+                        System.out.println( "Look for all BioSources having taxid: " + taxid );
+                    }
+
                     biosources = helper.search( BioSource.class.getName(), "taxId", taxid );
+
+                    if( DEBUG ) {
+                        System.out.println( biosources.size() + " found." );
+                    }
                 } catch ( IntactException e ) {
-                    MessageHolder.getInstance().addCheckerMessage( new Message( "Could not find BioSource for " +
-                                                                                "the taxid: " + taxid + " in IntAct. " +
-                                                                                "Error was: " + e.getMessage() ) );
+                    String msg = "Could not find BioSource for the taxid: " + taxid + ". Error was: " + e.getMessage();
+                    MessageHolder.getInstance().addCheckerMessage( new Message( msg ) );
                 }
 
                 // Now try to find the one that has the same Tissue and CellType.
@@ -148,11 +174,19 @@ public abstract class AbstractOrganismChecker {
                     sb.append( ", Tissue: " ).append( tissue );
 
                     MessageHolder.getInstance().addCheckerMessage( new Message( sb.toString() ) );
+                } else {
+                    if( DEBUG ) {
+                        System.out.println( "BioSource found: " + bioSource );
+                    }
                 }
             }
 
             // cache the result of the search.
             cache.put( cacheId, bioSource );
+        } else {
+            if( DEBUG ) {
+                System.out.println( "Found in cache" );
+            }
         }
 
         return bioSource;
@@ -175,39 +209,39 @@ public abstract class AbstractOrganismChecker {
         if( bsTissue == null && tissue == null ) {
             answer = true;
         } else if( bsTissue != null && tissue != null ) {
-            // we'll check first the PSI-MI definition and if not present, go for the shortlabel.
-            String psiId = tissue.getPsiDefinition().getId();
 
-            String intactId = null;
-            for ( Iterator iterator = bsTissue.getXrefs().iterator(); iterator.hasNext() && null == intactId; ) {
-                Xref xref = (Xref) iterator.next();
-                if( Constants.PSI_DB_SHORTLABEL.equals( xref.getCvDatabase().getShortLabel() ) ) {
-                    if( ControlledVocabularyRepository.getPrimaryXrefQualifier().equals( xref.getCvXrefQualifier() ) ) {
-                        // found it !
-                        intactId = xref.getPrimaryId(); // PSI ID
+            // TODO temporarily, we check also the shortlabel of the cellType, that should disappear when we have
+            // TODO proper ID as Xref in PSI.
+
+            if( tissue.getPsiDefinition() != null ) {
+                String intactId = null;
+                String psiId = tissue.getPsiDefinition().getId();
+
+                for ( Iterator iterator = bsTissue.getXrefs().iterator(); iterator.hasNext() && null == intactId; ) {
+                    Xref xref = (Xref) iterator.next();
+                    if( Constants.PSI_DB_SHORTLABEL.equals( xref.getCvDatabase().getShortLabel() ) ) {
+                        if( ControlledVocabularyRepository.getPrimaryXrefQualifier().equals( xref.getCvXrefQualifier() ) ) {
+                            // found it !
+                            intactId = xref.getPrimaryId(); // PSI ID
+                        }
                     }
+                } // xrefs
+
+                // we found a PSI Xref ... check if it matches.
+                if( psiId != null && psiId.equalsIgnoreCase( intactId ) ) {
+                    answer = true;
                 }
-            } // xrefs
 
-            // as there is not yet PSI Xrefs for CvTissue, we may have to rely on the shortlabel.
-            if( intactId == null ) {
+            } else {
 
-                // check on shortlabel.
+                // as there is not yet PSI Xrefs for CvTissue, we may have to rely on the shortlabel.
                 if( tissue.getShortlabel() != null ) {
 
                     if( tissue.getShortlabel().equalsIgnoreCase( bsTissue.getShortLabel() ) ) {
                         answer = true;
                     }
                 }
-
-            } else {
-
-                // we found a PSI Xref ... check if it matches.
-                if( psiId.equalsIgnoreCase( intactId ) ) {
-                    answer = true;
-                }
             }
-
         }
 
         return answer;
@@ -238,9 +272,6 @@ public abstract class AbstractOrganismChecker {
             // TODO temporarily, we check also the shortlabel of the cellType, that should disappear when we have
             // TODO proper ID as Xref in PSI.
 
-            String intactId = null;
-            String psiId = null;
-
             if( cellType.getShortlabel() != null ) {
 
                 if( cellType.getShortlabel().equalsIgnoreCase( bsCellType.getShortLabel() ) ) {
@@ -249,8 +280,7 @@ public abstract class AbstractOrganismChecker {
 
             } else if( null != cellType.getPsiDefinition() ) {
 
-                psiId = cellType.getPsiDefinition().getId();
-
+                String intactId = null;
                 for ( Iterator iterator = bsCellType.getXrefs().iterator(); iterator.hasNext() && null == intactId; ) {
                     Xref xref = (Xref) iterator.next();
                     if( Constants.PSI_DB_SHORTLABEL.equals( xref.getCvDatabase().getShortLabel() ) ) {
@@ -262,40 +292,11 @@ public abstract class AbstractOrganismChecker {
                 } // xrefs
 
                 // we found a PSI Xref ... check if it matches.
+                String psiId = cellType.getPsiDefinition().getId();
                 if( psiId.equalsIgnoreCase( intactId ) ) {
                     answer = true;
                 }
             }
-
-//            if( null != cellType.getPsiDefinition() ) {
-//
-//                psiId = cellType.getPsiDefinition().getId();
-//
-//                for ( Iterator iterator = bsCellType.getXrefs().iterator(); iterator.hasNext() && null == intactId; ) {
-//                    Xref xref = (Xref) iterator.next();
-//                    if( Constants.PSI_DB_SHORTLABEL.equals( xref.getCvDatabase().getShortLabel() ) ) {
-//                        if( ControlledVocabularyRepository.getPrimaryXrefQualifier().equals( xref.getCvXrefQualifier() ) ) {
-//                            // found it !
-//                            intactId = xref.getPrimaryId();
-//                        }
-//                    }
-//                } // xrefs
-//
-//                // we found a PSI Xref ... check if it matches.
-//                if( psiId.equalsIgnoreCase( intactId ) ) {
-//                    answer = true;
-//                }
-//
-//            } else {
-//
-//                // check on shortlabel.
-//                if( cellType.getShortlabel() != null ) {
-//
-//                    if( cellType.getShortlabel().equalsIgnoreCase( bsCellType.getShortLabel() ) ) {
-//                        answer = true;
-//                    }
-//                }
-//            }
         }
 
         return answer;
