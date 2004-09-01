9ebe7c82aff4d7907226458a73b837f06c433887
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-09-01 12:03:21 +0000

    Removed commented out code

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/biosrc/BioSourceActionForm.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/biosrc/BioSourceActionForm.java
@@ -62,33 +62,4 @@ public class BioSourceActionForm extends EditorActionForm {
     public void setTissue(String tissue) {
         myTissue = tissue;
     }
-
-    // TODO Remove this later
-    /**
-     * Validate the properties that have been set from the HTTP request.
-     *
-     * @param mapping the mapping used to select this instance
-     * @param request the servlet request we are processing
-     * @return <tt>ActionErrors</tt> object that contains validation errors. If
-     * no errors are found, <tt>null</tt> or an empty <tt>ActionErrors</tt>
-     * object is returned.
-     */
-//    public ActionErrors validate(ActionMapping mapping,
-//                                 HttpServletRequest request) {
-//        ActionErrors errors = super.validate(mapping, request);
-//
-//        // Only proceed if super method does not find any errors.
-//        if ((errors != null) && !errors.isEmpty()) {
-//            return errors;
-//        }
-//       // The tax id must be an integer.
-//        try {
-//            Integer.parseInt(getTaxId());
-//        }
-//        catch (NumberFormatException nfe) {
-//            errors = new ActionErrors();
-//            errors.add("bs.taxid", new ActionError("error.taxid.mask", getTaxId()));
-//        }
-//        return errors;
-//    }
 }
