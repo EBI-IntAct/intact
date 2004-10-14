1d8ed905de702985e989498f1e32154af340d96d
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2004-10-14 15:17:23 +0000

    Moved from the view package. LockManager is retrieved from the app context

---
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/view/wrappers/ResultDisplayWrapper.java
@@ -0,0 +1,119 @@
+/*
+Copyright (c) 2002-2004 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.view.wrappers;
+
+import org.apache.taglibs.display.TableDecorator;
+import uk.ac.ebi.intact.application.editor.business.EditUserI;
+import uk.ac.ebi.intact.application.editor.struts.framework.util.EditorConstants;
+import uk.ac.ebi.intact.application.editor.util.LockManager;
+import uk.ac.ebi.intact.model.AnnotatedObject;
+import uk.ac.ebi.intact.business.IntactHelper;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * This class is the wrapper class for the display library to display
+ * results from a search page.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class ResultDisplayWrapper extends TableDecorator {
+
+    /**
+     * The formatter for the date.
+     */
+    private static SimpleDateFormat ourDateFormatter =
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+
+    /**
+     * @return the link to the search application; clicking on this link, the
+     * user will be taken into the search application.
+     */
+    public String getAc() {
+        AnnotatedObject annotobj = (AnnotatedObject) getObject();
+        String topic = IntactHelper.getDisplayableClassName(annotobj);
+        return "<a href=\"" + "javascript:show('" + topic + "', '"
+                + annotobj.getShortLabel() + "')\">" + annotobj.getAc() + "</a>";
+    }
+
+    /**
+     * Returns the short label.
+     * @return short label.
+     */
+    public String getShortLabel() {
+        AnnotatedObject annotbj = (AnnotatedObject) getObject();
+
+        // The lock manager to check the lock.
+        LockManager lmr = getLockManager();
+
+        if (lmr.hasLock(annotbj.getAc())) {
+            // This object has a lock. Get the current user.
+            EditUserI user = (EditUserI) getPageContext().getSession().getAttribute(
+                    EditorConstants.INTACT_USER);
+
+            // The current lock associated with the object.
+            LockManager.LockObject lock = lmr.getLock(annotbj.getAc());
+
+            if (lock != null) {
+                // Check the lock owner with the current user.
+                if (lock.getOwner().equals(user.getUserName())) {
+                    // Locked by the same user.
+                    return "<span class=\"owner\">" + getEditorLink(annotbj) + "</span>";
+                }
+                // Locked by someone else.
+                return "<span class=\"error\">" + annotbj.getShortLabel() + "</span>";
+            }
+        }
+        // Not locked.
+        return getEditorLink(annotbj);
+    }
+
+    /**
+     * Returns the full name.
+     * @return the full name as a <code>String</code> for the wrapped object.
+     */
+    public String getFullName() {
+        AnnotatedObject annotbj = (AnnotatedObject) getObject();
+        return annotbj.getFullName();
+    }
+
+    /**
+     * @return the owner for the current bean. "---" is returned if the current
+     * bean is not locked.
+     */
+    public String getLock() {
+        AnnotatedObject annotbj = (AnnotatedObject) getObject();
+        LockManager.LockObject lock = getLockManager().getLock(annotbj.getAc());
+        if (lock == null) {
+            // No owner; no need for the title
+            return "<input type=\"text\" size=\"7\" value=\"  ---  \" readonly>";
+        }
+        // Get the owner and the time stamp.
+        String owner = lock.getOwner();
+        String title = ourDateFormatter.format(lock.getLockDate());
+        return "<input type=\"text\" size=\"7\" value=\"" + owner + "\" title=\""
+                + title + "\" readonly>";
+    }
+
+    // Helper methods.
+
+    /**
+     * @return the link for the result page; clicking on this link, the
+     * user will be taken into the edit page.
+     */
+    private String getEditorLink(AnnotatedObject annotobj) {
+        String topic = IntactHelper.getDisplayableClassName(annotobj);
+        return "<a href=secure/edit?ac=" + annotobj.getAc() + "&type="
+                + topic + ">" + annotobj.getShortLabel() + "</a>";
+    }
+
+    private LockManager getLockManager() {
+        return (LockManager) getPageContext().getServletContext().getAttribute(
+                EditorConstants.LOCK_MGR);
+    }
+}
