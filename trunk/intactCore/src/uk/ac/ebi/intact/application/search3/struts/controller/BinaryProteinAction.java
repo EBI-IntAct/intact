0691289dc455d64442e8ee63b49316020e956d05
mkleen <mkleen@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-01-31 22:39:14 +0000

    update for use with self interactions

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/BinaryProteinAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/BinaryProteinAction.java
@@ -4,8 +4,8 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import uk.ac.ebi.intact.application.search3.business.IntactUserIF;
+import uk.ac.ebi.intact.application.search3.struts.framework.IntactBaseAction;
 import uk.ac.ebi.intact.application.search3.struts.framework.util.SearchConstants;
-import uk.ac.ebi.intact.application.search3.struts.view.beans.AbstractViewBean;
 import uk.ac.ebi.intact.application.search3.struts.view.beans.SimpleViewBean;
 import uk.ac.ebi.intact.model.Component;
 import uk.ac.ebi.intact.model.Interaction;
@@ -17,15 +17,13 @@ import javax.servlet.http.HttpSession;
 import java.util.*;
 
 /**
- *
- * /**
- * This class provides the actions required  for an url based search query for 2 proteins.
- * The action recieve 2 proteins from the Search Action
+ * /** This class provides the actions required  for an url based search query for 2 proteins. The
+ * action recieve 2 proteins from the Search Action
  *
  * @author Michael Kleen
  * @version BinaryProteinAction.java Date: Jan 14, 2005 Time: 12:53:45 PM
  */
-public class BinaryProteinAction extends AbstractResultAction {
+public class BinaryProteinAction extends IntactBaseAction {
 
 
     public ActionForward execute(ActionMapping mapping, ActionForm form,
@@ -48,7 +46,7 @@ public class BinaryProteinAction extends AbstractResultAction {
         Collection someProteins = (Collection) request.getAttribute(SearchConstants.SEARCH_RESULTS);
 
         // forward to error page if we got lesser than 1 protein
-        if (someProteins.size() < 2) {
+        if (someProteins.size() < 1) {
             return mapping.findForward(SearchConstants.FORWARD_NO_MATCHES);
         }
 
@@ -91,22 +89,18 @@ public class BinaryProteinAction extends AbstractResultAction {
 
     }
 
-    /** Provides an Collection of binary Interactions based on the 2 proteins for calculating. 
-     *
-     * @param results contains 2 Proteins
-     * @return a Collection of Interactions which holds all binary interactions based on the spoke model
-     */
+
     private Collection processtParticipatedInteractions(Collection someProteins) {
 
         logger.info("process ParticaptedInteractions");
 
-        // throw an Exception here if something went wrong
-
-        Iterator iterator = someProteins.iterator();
-        Protein protein1 = (Protein) iterator.next();
-        Protein protein2 = (Protein) iterator.next();
-
         Collection result = new HashSet();
+        Iterator iterator = someProteins.iterator();
+
+
+        // get all interactions from protein1
+
+        Protein protein1 = (Protein) iterator.next();
         Set componentSet1 = new HashSet(protein1.getActiveInstances());
         Set component1Interactions = new HashSet();
 
@@ -115,23 +109,48 @@ public class BinaryProteinAction extends AbstractResultAction {
             component1Interactions.add(component.getInteraction());
         }
 
-        Set componentSet2 = new HashSet(protein2.getActiveInstances());
+        // if we got another protein, get also all interactions from protein2
+        if (someProteins.size() == 2) {
+            Protein protein2 = (Protein) iterator.next();
+            Set componentSet2 = new HashSet(protein2.getActiveInstances());
 
-        for (Iterator iterator2 = componentSet2.iterator(); iterator2.hasNext();) {
-            Component component2 = (Component) iterator2.next();
-            Interaction component2Interaction = component2.getInteraction();
+            for (Iterator iterator2 = componentSet2.iterator(); iterator2.hasNext();) {
+                Component component2 = (Component) iterator2.next();
+                Interaction component2Interaction = component2.getInteraction();
 
-            if (component1Interactions.contains(component2Interaction)) {
-                result.add(component2Interaction);
+                if (component1Interactions.contains(component2Interaction)) {
+                    result.add(component2Interaction);
+                }
+            }
+        } else {
+
+            // check now for self Interactions from protein1
+            for (Iterator iterator3 = component1Interactions.iterator();
+                 iterator3.hasNext();) {
+                Interaction interaction = (Interaction) iterator3.next();
+                Collection interactionComponents = interaction.getComponents();
+
+                if (interactionComponents.size() == 2) {
+                    Iterator iterator4 = interactionComponents.iterator();
+                    Component component1 = (Component) iterator4.next();
+                    Component component2 = (Component) iterator4.next();
+                    if (component1.getStoichiometry() == 1 &&
+                            component2.getStoichiometry() == 1) {
+                        result.add(interaction);
+                    }
+                } else if (interactionComponents.size() == 1) {
+                    Iterator iterator5 = interactionComponents.iterator();
+                    Component component1 = (Component) iterator5.next();
+                    if (component1.getStoichiometry() == 2) {
+                        result.add(interaction);
+                    }
+                }
             }
         }
         return result;
     }
 
-
-    protected AbstractViewBean getAbstractViewBean(Object result, IntactUserIF user,
-                                                   String contextPath) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
 }
+
+
 
