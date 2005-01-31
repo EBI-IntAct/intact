397d1e8be670496109a4f0a7cd6a378e642c7f42
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-01-31 11:13:58 +0000

    Added a method to get the DAO by specifying a persistence type

--- trunk/intactCore/src/uk/ac/ebi/intact/persistence/DAOSource.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/persistence/DAOSource.java
@@ -8,6 +8,7 @@ package uk.ac.ebi.intact.persistence;
 import java.util.*;
 
 import org.apache.log4j.Logger;
+import uk.ac.ebi.intact.business.IntactHelper;
 
 /**
  *  <p>This interface defines the functionality usually provided by
@@ -21,36 +22,7 @@ import org.apache.log4j.Logger;
  */
 public interface DAOSource {
 
-
-    /**
-     *  <p>sets the configuration data for the data source. If configuration
-     * fails then an exception is thrown back to the caller.</p>
-     *
-     * @param configFiles - the collection of configuration files to be used
-     * @exception DataSourceException - thrown if there are configuration problems
-     * @deprecated Configuration data, if required, should be set using
-     * individual implementation constructors - not through this method.
-     *
-     */
-    public void setConfig(Map configFiles) throws DataSourceException;
-
-    /**
-     * Used to specify a user for connecting to the persistent store. If this
-     * method is not used, default user details will be obtained from details supplied
-     * in the setConfig method (eg from a supplied config file).
-     * @param user the username to use for connection (overrides any default)
-     */
-    public void setUser(String user);
-
     public String getUser();
-
-    /**
-     * Used to define a password which overrides any default supplied via config data.
-     * Should typically be used in conjunction with the setUser method for consistency.
-     * @param password the password to be used for persistent store connection.
-     */
-    public void setPassword(String password);
-
     public String getPassword();
 
     /**
@@ -69,12 +41,14 @@ public interface DAOSource {
      *
      * @param user username to make a connection with
      * @param password the user's password (null values are allowed)
+     * @param type the persistence type (i.e, ODMG or PB)
      *
      * @return a Data Access Object
      *
      * @exception DataSourceException - thrown if a DAO cannot be obtained
     */
-    public DAO getDAO(String user, String password) throws DataSourceException;
+    public DAO getDAO(String user, String password, IntactHelper.PersistenceType type)
+            throws DataSourceException;
 
     /**
      *  @return the name of the data source (as per configuration)
@@ -85,11 +59,6 @@ public interface DAOSource {
      *   @return The class loader used to load the data source
      */
     public ClassLoader getClassLoader();
-
-    /**
-     *    @return the configuration information about the data source
-     */
-    public String getConfig();
 
     /**
      *   Defines whether or not data are saved automatically.
