ec205c0e69a3a80ff8475a01cf6d41c634119707
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-02-04 15:58:59 +0000

    Fixed the type pragma; output id instead of goid, comments are printed without end pipe chars

--- trunk/intactCore/scripts/toDagEditFormat.pl
+++ trunk/intactCore/scripts/toDagEditFormat.pl
@@ -10,7 +10,10 @@
 #                           -targetDagFile string 
 #                           -targetDefFile string
 #
-
+# WARNING: This script assumes that concatenated def/dag files are in 'new' dag
+# format (i.e., goid: is replaced with id:). This is OK as files are downloaded
+# in 'new' format by default.
+# - Sugath 04/02/2005.
 
 use Getopt::Long;
 use Data::Dumper;
@@ -39,7 +42,7 @@ sub leftTrim($);
 open (TARGETDAG, ">$opt_targetDagFile") || die "Could not open target dag file $opt_targetDagFile\n";
 
 # Add this tag for the Dag editor to load this file properly.
-print TARGETDAG "!type: % is_a is a\n";
+print TARGETDAG "!type: \@is_a\@ is_a is a\n";
 print TARGETDAG "\$intactCVs ; PSEUDOAC:$pseudoAC\n";
 $pseudoAC++;
 
@@ -109,72 +112,76 @@ for (my $i=0; $i <= $#opt_sourceDefFile; $i++) {
     my %entry = ();
 
     while (<SOURCEDEF>){
-	# Read one entry into memory
-	# ignore comments
-	if (/^\!/){
-	    next;
-	}
+        # Read one entry into memory
+        # ignore comments
+        if (/^\!/){
+            next;
+        }
 
-	# read content line
-	unless (/^\s*\n*$/){
-	    my ($key) = /(\w+\:)/;
-	    push @{$entry{$key}}, $_;
-	    next;
-	}
+        # read content line
+        unless (/^\s*\n*$/){
+            my ($key) = /(\w+\:)/;
+            push @{$entry{$key}}, $_;
+            next;
+        }
 
-	# A blank line has been found, write modified entry
+        # A blank line has been found, write modified entry
 
-	# write term
-	print TARGETDEF @{$entry{"term:"}}[0];
-	
-	# write goid
-	if (! defined $entry{"goid:"}){
-	    if (defined $pseudoAcAssignments{@{$entry{"term:"}}[0]}){
-		$entry{"goid:"}[0] = "goid: " . $pseudoAcAssignments{@{$entry{"term:"}}[0]} . "\n";
-	    } else {
-		$entry{"goid:"}[0] = "goid: PSEUDOAC:" . $pseudoAC . "\n";
-		$pseudoAC++;
-	    }
-	}
-	print TARGETDEF $entry{"goid:"}[0];
-	delete $entry{"term:"};
-	delete $entry{"goid:"};
+        # write term
+        print TARGETDEF @{$entry{"term:"}}[0];
 
-	#write definition
-	if (defined $entry{"definition:"}){
-	    print TARGETDEF $entry{"definition:"}[0];
-	}
-	delete $entry{"definition:"};
-	
-	#write definition_reference
-	if (defined $entry{"definition_reference:"}){
-	    foreach my $ref (@{$entry{"definition_reference:"}}){
-		print TARGETDEF $ref;
-	    }
-	} else {
-	    print TARGETDEF  "definition_reference: PMID:-\n";
-	}
-	delete $entry{"definition_reference:"};
-
-
-	# Print the non-GO lines as comments
-	my $comment = "";
-	foreach my $key (keys %entry){
-	    foreach my $keyterms (@{$entry{$key}}){
-		chomp $keyterms;
-		$comment .= $keyterms . "  ||  ";
+        # write goid
+        if (! defined $entry{"id:"}){
+            if (defined $pseudoAcAssignments{@{$entry{"term:"}}[0]}){
+                $entry{"id:"}[0] = "id: " . $pseudoAcAssignments{@{$entry{"term:"}}[0]} . "\n";
+            } else {
+                $entry{"id:"}[0] = "id: PSEUDOAC:" . $pseudoAC . "\n";
+                $pseudoAC++;
             }
-	}
-	if ($comment){
-	    print TARGETDEF "comment: " . $comment . "\n";
-	}
+        }
+        print TARGETDEF $entry{"id:"}[0];
+        delete $entry{"term:"};
+        delete $entry{"id:"};
 
-	print TARGETDEF "\n";
-	
-	# Reinitialise entry
-	%entry = ();
-	
-    } 
+        #write definition
+        if (defined $entry{"definition:"}){
+            print TARGETDEF $entry{"definition:"}[0];
+        }
+        delete $entry{"definition:"};
+
+        #write definition_reference
+        if (defined $entry{"definition_reference:"}){
+            foreach my $ref (@{$entry{"definition_reference:"}}) {
+                print TARGETDEF $ref;
+            }
+        } else {
+            print TARGETDEF  "definition_reference: PMID:-\n";
+        }
+        delete $entry{"definition_reference:"};
+
+
+        # Print the non-GO lines as comments
+        my $comment = "";
+        foreach my $key (keys %entry){
+            foreach my $keyterms (@{$entry{$key}}) {
+                chomp $keyterms;
+                if ($comment eq "") {
+                    $comment .= $keyterms;
+                }
+                else {
+                    $comment .= "  ||  ". $keyterms;
+                }
+            }
+        }
+        if ($comment){
+            print TARGETDEF "comment: " . $comment . "\n";
+        }
+
+        print TARGETDEF "\n";
+
+        # Reinitialise entry
+        %entry = ();
+    }
 }
 close TARGETDEF;
 
