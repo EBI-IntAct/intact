8eadb9b50bb9d4f87561df6952a9fb0b4d842e9a
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-03-01 11:24:17 +0000

    Latest definition of IA_SEARCH

--- trunk/intactCore/sql/oracle/search.sql
+++ trunk/intactCore/sql/oracle/search.sql
@@ -10,6 +10,9 @@
   Purpose:    materialized view for search3 for the initial request
               after creating that view you have to grant right to user
               on it and to create a public view
+              Note: having all the 'value' field lowercased allow us to
+                    get ealily a non case sensitive search only by doing
+                    a lowercase to the search query.
 
   Usage:      psql -f search.sql
 
@@ -24,38 +27,47 @@
 DROP MATERIALIZED VIEW IA_SEARCH;
 
 CREATE MATERIALIZED VIEW IA_SEARCH AS (
-    SELECT ac, shortlabel as value, objclass, 'shortlabel' as type
+    SELECT ac, LOWER(shortlabel) as value, objclass, 'shortlabel' as type
     FROM ia_interactor
 UNION
     SELECT ac, LOWER(fullname) as value, objclass, 'fullname' as type
     FROM ia_interactor
 UNION
-    SELECT I.ac, X.primaryid as value, objclass, 'xref' as type
+    SELECT ac, LOWER(ac) as value, objclass, 'ac' as type
+    FROM ia_interactor
+UNION
+    SELECT I.ac, LOWER(X.primaryid) as value, objclass, 'xref' as type
     FROM ia_interactor I, ia_xref X
     WHERE I.ac = X.parent_ac
 UNION
-    SELECT I.ac, A.name as value, I.objclass, CV.shortlabel as type
+    SELECT I.ac, LOWER(A.name) as value, I.objclass, CV.shortlabel as type
     FROM ia_interactor I, ia_alias A, ia_controlledvocab CV
     WHERE I.ac = A.parent_ac and
           A.aliastype_ac = CV.ac
 UNION
-    SELECT E.ac, X.primaryid as value, 'uk.ac.ebi.intact.model.Experiment', 'xref' as type
+    SELECT E.ac, LOWER(X.primaryid) as value, 'uk.ac.ebi.intact.model.Experiment', 'xref' as type
     FROM ia_experiment E, ia_xref X
     WHERE E.ac = X.parent_ac
 UNION
-    SELECT ac, shortlabel as value, 'uk.ac.ebi.intact.model.Experiment', 'shortlabel' as type
+    SELECT ac, LOWER(shortlabel) as value, 'uk.ac.ebi.intact.model.Experiment', 'shortlabel' as type
     FROM ia_experiment
 UNION
     SELECT ac, LOWER(fullname) as value, 'uk.ac.ebi.intact.model.Experiment', 'fullname' as type
     FROM ia_experiment
 UNION
+    SELECT ac,LOWER(ac) as value, 'uk.ac.ebi.intact.model.Experiment', 'ac' as type
+    FROM ia_experiment
+UNION
     SELECT ac, LOWER(fullname) as value, objclass, 'fullname' as type
     FROM ia_controlledvocab
 UNION
-    SELECT ac, shortlabel as value, objclass, 'shortlabel' as type
+    SELECT ac, LOWER(ac) as value, objclass, 'ac' as type
     FROM ia_controlledvocab
 UNION
-    SELECT CV.ac, X.primaryid as value, objclass, 'xref' as type
+    SELECT ac, LOWER(shortlabel) as value, objclass, 'shortlabel' as type
+    FROM ia_controlledvocab
+UNION
+    SELECT CV.ac, LOWER(X.primaryid) as value, objclass, 'xref' as type
     FROM ia_controlledvocab CV, ia_xref X
     WHERE CV.ac = X.parent_ac
 UNION
