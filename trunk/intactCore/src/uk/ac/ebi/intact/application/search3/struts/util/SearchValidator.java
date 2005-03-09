ba2569d86ea24e045f81d92107ea43a87d83b9ec
mkleen <mkleen@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-03-09 17:19:28 +0000

    update with comments

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/util/SearchValidator.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/util/SearchValidator.java
@@ -3,6 +3,8 @@ package uk.ac.ebi.intact.application.search3.struts.util;
 import uk.ac.ebi.intact.model.*;
 
 /**
+ * This Class provides basic validations for the search3 search struts action class.
+ *
  * @author Michael Kleen
  * @version SearchClassValidator.java Date: Jan 25, 2005 Time: 08:21:01 AM
  */
@@ -11,7 +13,8 @@ public class SearchValidator {
     public static boolean isValid(String searchValue) {
         if (searchValue.equalsIgnoreCase("%") || searchValue.equalsIgnoreCase("")) {
             return false;
-        } else {
+        }
+        else {
             return true;
         }
     }
@@ -20,7 +23,8 @@ public class SearchValidator {
         Class clazz = null;
         try {
             clazz = Class.forName("uk.ac.ebi.intact.model." + searchClass);
-        } catch (ClassNotFoundException e1) {
+        }
+        catch (ClassNotFoundException e1) {
             // not a valid className
             return false;
         }
@@ -32,11 +36,18 @@ public class SearchValidator {
 
     }
 
+    /**
+     * Returns true if
+     *
+     * @param searchClass
+     * @return a boolean which is true if the searchClass searchable and false if it's not searchable
+     */
     public static boolean isSearchable(String searchClass) {
         Class clazz = null;
         try {
             clazz = Class.forName("uk.ac.ebi.intact.model." + searchClass);
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e) {
             // not a valid className
             return false;
         }
@@ -52,6 +63,9 @@ public class SearchValidator {
                 return true;
             }
             if (CvObject.class.isAssignableFrom(clazz)) {
+                return true;
+            }
+            if (Interactor.class.isAssignableFrom(clazz)) {
                 return true;
             }
         }
