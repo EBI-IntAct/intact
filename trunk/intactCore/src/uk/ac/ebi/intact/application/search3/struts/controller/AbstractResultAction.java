ef4bf6d635a525160b7ef997615381c7a380578c
mkleen <mkleen@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-03-09 17:25:58 +0000

    update for new search version

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/AbstractResultAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/AbstractResultAction.java
@@ -10,35 +10,25 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import uk.ac.ebi.intact.application.search3.business.IntactUserIF;
-import uk.ac.ebi.intact.application.search3.struts.framework.IntactBaseAction;
 import uk.ac.ebi.intact.application.search3.struts.framework.util.SearchConstants;
-import uk.ac.ebi.intact.application.search3.struts.view.beans.*;
-import uk.ac.ebi.intact.model.Protein;
-import uk.ac.ebi.intact.model.Experiment;
-import uk.ac.ebi.intact.model.Interaction;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Perform the common behaviour of the result Action.
  * <p/>
- * TODO: This class needs refactoring/removing when all new views are in place - it may be possible
  * to integrate the new 'simple' view into here, but currently it needs to generate a LIST of
  * viewbeans, not a SINGLE one (but maybe see partners view as this is similar but is included here
  * in the 'hacked' code below!)
  *
- * @author IntAct Team
+ * @author Michael Kleen
  * @version $Id$
  */
-public abstract class AbstractResultAction extends IntactBaseAction {
+public abstract class AbstractResultAction extends SearchBaseAction {
 
     /**
      * Process the specified HTTP request, and create the corresponding HTTP response (or forward to
@@ -58,12 +48,10 @@ public abstract class AbstractResultAction extends IntactBaseAction {
                                  HttpServletResponse response)
             throws IOException, ServletException {
 
-        //New view - Sept 2004:
-        //To get to here we must have something to display, and that we have come to
-        //this Action via a link click from the 'simple' JSP (that display is handled by
-        //a completely seperate Action not in the AbstractResultAction hierarchy). So
-        //we can process the request and send to either a single, partner or detail view..
+        // first set the request
 
+        super.setRequest(request);
+        
         // Session to access various session objects. This will create
         //a new session if one does not exist.
         HttpSession session = super.getSession(request);
@@ -76,179 +64,19 @@ public abstract class AbstractResultAction extends IntactBaseAction {
             if (user == null) return mapping.findForward(SearchConstants.FORWARD_FAILURE);
         }
 
-        //first check for a tabbed page request on existing data - in this
-        //case there are no search results in the request.
-        //TODO: Complete the refactoring so all subclasses just implement 'processResults'
-        //TODO: This will remove the need for the check here..
-        
-        String selectedPage = request.getParameter("selectedChunk");
-        if ((selectedPage != null) && (!selectedPage.equals(""))) {
-            return mapping.findForward(processResults(request, user.getHelpLink()));
-        }
+        //use the procesRequest method instead....
+        return mapping.findForward(processResults(request, user.getHelpLink()));
 
-        //get the search results from the request
-        Collection results = (Collection) request.getAttribute(SearchConstants.SEARCH_RESULTS);
-
-        logger.info("Collection contains " + results.size() + " items.");
-
-        AbstractViewBean bean = null; //used for single results
-        List beanList = null;         //used for multiple results
-
-        //**** TESTS ****
-        //- checking some refactoring of this classes' code - hopefully we can remove
-        //most of the code in this class as it will be handled in the processResults
-        //method of subclasses...
-        Class resultType = results.iterator().next().getClass();
-        //checking refactoring for Protein
-        if (Protein.class.isAssignableFrom(resultType)) {
-            //use the procesRequest method instead....
-            return mapping.findForward(processResults(request, user.getHelpLink()));
-        }
-
-        //checking refactoring for Exp/Interaction
-        if ((Experiment.class.isAssignableFrom(resultType)) ||
-                (Interaction.class.isAssignableFrom(resultType))) {
-          
-            //use the procesRequest method instead....
-            return mapping.findForward(processResults(request, user.getHelpLink()));
-        }
-
-        //**** END OF REFACTORING ***
-
-        //handle a single result first (simplest)..
-        if (results.size() == 1) {
-            // look here !!
-
-            bean = getAbstractViewBean(results.iterator().next(), user, request.getContextPath());
-
-            if (bean == null) {
-                logger.warn("something went wrong - no viewBean created  ");
-                super.clearErrors();
-                super.addError("error.search", "No data collected in the database.");
-                super.saveErrors(request);
-                return mapping.findForward(SearchConstants.FORWARD_FAILURE);
-            }
-
-            logger.info(bean.getClass() + " created");
-
-            //TODO - Forwards: This can be handled by subclasses which can add view items
-            //to a session and return the appropriate forwaard code to here. The
-            //explicit bits below should go when the refactoring is done...
-
-            //if we need to display a Protein detail view, go there now
-            //TODO this should be removed when the other views are finished
-            if (bean instanceof ProteinViewBean) {
-                //testing stuff - save the bean and send to the protein view JSP
-                logger.info("AbstractResult: going to single Protein view");
-                session.setAttribute(SearchConstants.VIEW_BEAN, bean);
-                return mapping.findForward("singleProtein");
-            }
-
-            //if we need to display a Protein partner view,
-            //put the single view bean inside a Collection and go there now
-            //(it's too messy to have the JSP check for single objects or Collections
-            //and then do basically the same thing in each case)....
-            //TODO This should go when the other views are finished
-            if (bean instanceof PartnersViewBean) {
-                //testing stuff - send to the protein partners view JSP
-                beanList = new ArrayList();
-                beanList.add(bean);
-                session.setAttribute(SearchConstants.VIEW_BEAN_LIST, beanList);
-                logger.info("AbstractResult: going to partners view");
-                return mapping.findForward("partners");
-            }
-
-            //TODO this is not a nice to do that but at the moemnt there's no better way
-            //sould remove after the complete refactoring
-            if (bean instanceof CvObjectViewBean) {
-                //testing stuff - save the bean and send to the protein view JSP
-                logger.info(
-                        "Content of the Bean " + ((CvObjectViewBean) bean).getObject().toString());
-
-                logger.info("AbstractResult: going to single Object view");
-                session.setAttribute(SearchConstants.VIEW_BEAN, bean);
-                return mapping.findForward("cvObject");
-
-            }
-
-            //TODO needs reforcatoring
-            //sould remove after the complete refactoring
-            if (bean instanceof BioSourceViewBean) {
-                //testing stuff - save the bean and send to the protein view JSP
-                logger.info(
-                        "Content of the Bean " + ((BioSourceViewBean) bean).getObject().toString());
-
-                logger.info("AbstractResult: going to single Object view");
-                session.setAttribute(SearchConstants.VIEW_BEAN, bean);
-                return mapping.findForward("bioSource");
-
-            }
-
-            //the view bean is from old code - just do as before....
-            session.setAttribute(SearchConstants.VIEW_BEAN, bean);
-        } else {
-
-            //got a multiple result list - if they are Proteins build views for
-            //interaction partner summary, but just do as above if not
-            //(for now, because the beans views are not yet ready - this will be
-            //changed later...)
-            if (Protein.class.isAssignableFrom(results.iterator().next().getClass())) {
-                beanList = new ArrayList();
-                for (Iterator it = results.iterator(); it.hasNext();) {
-                    beanList.add(getAbstractViewBean(it.next(), user, request.getContextPath()));
-                }
-                logger.info("Collection of " + beanList.iterator().next().getClass() + " created");
-                session.setAttribute(SearchConstants.VIEW_BEAN_LIST, beanList);
-
-                //send to the protein partners view JSP
-                return mapping.findForward("partners");
-            } else {
-                //multiple result list of something not yet reengineered - proceed
-                //as before ....
-                //TODO get rid of this code when the other views are done...
-                bean = getAbstractViewBean(results, user, request.getContextPath());
-                if (bean == null) {
-                    logger.warn("something went wrong - no viewBean created");
-                    super.clearErrors();
-                    super.addError("error.search", "No data collected in the database.");
-                    super.saveErrors(request);
-                    return mapping.findForward(SearchConstants.FORWARD_FAILURE);
-                }
-
-                logger.info(bean.getClass() + " created");
-                session.setAttribute(SearchConstants.VIEW_BEAN, bean);
-
-            }
-
-        }
-
-        return mapping.findForward(SearchConstants.FORWARD_RESULTS);
     }
-
-    //default implementation for now to use in testing - avoids subclass rewrites..
-    protected String processResults(HttpServletRequest request, String helpLink) {
-        return null;
-    }
-
-    //////////////////////
-    // Abstract methods
-    //////////////////////
 
     /**
-     * Builds a view bean depending upon the search result data (eg Experiment, single Protein,
-     * etc). Specific subclasses should have a means of creating the appropriate view bean to be
-     * forwarded.
+     * This abstract method is process the request more effectively. It avoids making any
+     * assumptions about the beans or the size of search result list and keep all of the
+     * processing in a single place for each Action type.
      *
-     * @param result      The search result - left as type Object to allow subclasses to handle
-     *                    either Collections or single items as they require.
-     * @param user        The User object, used to obtain various beans.
-     * @param contextPath The context path of the servlet - used to build machine-independent
-     *                    links.
-     * @return AbstractViewBean a view bean appropriate for the display item(s), or null if there
-     *         was a problem with either the parameters or in building a bean
+     * @param request  The request to be processed
+     * @param helpLink The contextual help link
+     * @return String the forward code for the parent execute method to return.
      */
-    protected abstract AbstractViewBean getAbstractViewBean(Object result, IntactUserIF user,
-                                                            String contextPath);
-
-
+    protected abstract String processResults(HttpServletRequest request, String helpLink);
 }
--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/DetailsResultAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/controller/DetailsResultAction.java
@@ -6,38 +6,34 @@ in the root directory of this distribution.
 
 package uk.ac.ebi.intact.application.search3.struts.controller;
 
-import uk.ac.ebi.intact.application.search3.business.IntactUserIF;
-import uk.ac.ebi.intact.application.search3.struts.view.beans.AbstractViewBean;
-import uk.ac.ebi.intact.application.search3.struts.view.ViewBeanFactory;
-import uk.ac.ebi.intact.application.search3.struts.view.beans.MainDetailViewBean;
 import uk.ac.ebi.intact.application.search3.struts.framework.util.SearchConstants;
-import uk.ac.ebi.intact.model.Interaction;
+import uk.ac.ebi.intact.application.search3.struts.view.beans.MainDetailViewBean;
 import uk.ac.ebi.intact.model.Experiment;
+import uk.ac.ebi.intact.model.Interaction;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ArrayList;
 
 /**
- * Performs the beans view for Proteins.
+ * Performs the beans view for the Experiement View
+ * <p/>
+ * This Action can be called in case of a Experiement or an Interaction
  *
- * @author IntAct Team
+ * @author Michael Kleen
  * @version $Id$
  */
 public class DetailsResultAction extends AbstractResultAction {
 
-    ///////////////////////////////////
-    // Abstract methods implementation
-    ///////////////////////////////////
-
     /**
      * This method overrides the parent one to process the request more effectively. It avoids
-     * making any assumptions about the beans or the size of search result list and
-     * keep all of the processing in a single place for each Action type.
-     * @param request The request to be processed
+     * making any assumptions about the beans or the size of search result list and keep all of the
+     * processing in a single place for each Action type.
+     *
+     * @param request  The request to be processed
      * @param helpLink The contextual help link
      * @return String the forward code for the parent execute method to return.
      */
@@ -52,98 +48,76 @@ public class DetailsResultAction extends AbstractResultAction {
         //set the index of the Interaction page to that given in the request
         //send back to detail.jsp.
         String selectedPage = request.getParameter("selectedChunk");
-        if((selectedPage != null) && (!selectedPage.equals(""))) {
+        if ((selectedPage != null) && (!selectedPage.equals(""))) {
 
             logger.error("doing next page request..next page is " + selectedPage);
             //got a request for a tabbed page from a detail view -
             //so use the request info on the specified view bean to change the
             //Interaction list to be viewed, and go back to the detail JSP afterwards...
-            MainDetailViewBean bean = (MainDetailViewBean)session.getAttribute(SearchConstants.LARGE_EXPERIMENT_BEAN);
-            if(bean != null) {
+
+            MainDetailViewBean bean = (MainDetailViewBean) session.getAttribute(SearchConstants.LARGE_EXPERIMENT_BEAN);
+            if (bean != null) {
                 bean.setInteractionPage(Integer.parseInt(selectedPage));
                 logger.error("bean index set OK");
+                //TODO use a Constant here 
                 return "detailPage";   //done
             }
-
             //something wrong here - no viewbean present to modify...
             return SearchConstants.FORWARD_FAILURE;
-
         }
 
         //new info to process, so get the search results from the request
         Collection results = (Collection) request.getAttribute(SearchConstants.SEARCH_RESULTS);
         //initial sanity check - empty results should be just ignored
-        if ((results == null) || (results.isEmpty())) return SearchConstants.FORWARD_NO_MATCHES;
+        if ((results == null) || (results.isEmpty())) {
+            return SearchConstants.FORWARD_NO_MATCHES;
+        }
 
         logger.info("DetailAction: result Collection contains " + results.size() + " items.");
         List beanList = null;
-        //useful URL info
-        String appPath = getServlet().getServletContext().getInitParameter("searchLink");
-        String searchURL = request.getContextPath().concat(appPath);
+        
+        // String appPath = getServlet().getServletContext().getInitParameter("searchLink");
+        // String searchURL = request.getContextPath().concat(appPath);
+        String searchURL = super.getSearchURL();
 
         //regardless of the result size, just build a viewbean for each result and put into
         //a Collection for use by the JSP - but first check we have the correct type for this
         //Action to process..
         Class resultType = results.iterator().next().getClass();
 
-        //NB either have a single Experiment to display, OR if we have an Interaction
-        //then we need to build an Experiment view bean for each Experiment that the
-        //Interaction is linked to.....
-        //TODO: November 2004 - NEW REQUIREMENT: For Interaction search results, the Experiment
-        //TODO: context should display ONLY the Interaction and its Experiment details, and
-        //TODO: NOT the whole Experiment.
         Collection experiments = null;
-        Interaction interactionResult = null;    //used to tell the viewbean what needs displaying
-        if ((Interaction.class.isAssignableFrom(resultType))) {
+        Interaction interactionResult = null;   //used to tell the viewbean what needs displaying
 
-            //NOTE: by the time we get to this Action we should only ever have ONE
-            //Interaction to deal with (multiple search results go to the main 'simple' view
-            //first
+        if ((Interaction.class.isAssignableFrom(resultType))) {
             interactionResult = (Interaction) results.iterator().next();
             experiments = interactionResult.getExperiments();
 
-        } else if (Experiment.class.isAssignableFrom(resultType)) {
+        }
+        else if (Experiment.class.isAssignableFrom(resultType)) {
             experiments = results;  //got Experiments in the first place
         }
 
-        //now process, assuming we have something to do..
         if (experiments != null) {
             beanList = new ArrayList();
             for (Iterator it = experiments.iterator(); it.hasNext();) {
-                MainDetailViewBean bean = new MainDetailViewBean((Experiment) it.next(), helpLink, searchURL, request.getContextPath());
-                if(interactionResult != null) bean.setWrappedInteraction(interactionResult);   //tell bean display info
+                MainDetailViewBean bean = new MainDetailViewBean((Experiment) it.next(), helpLink,
+                                                                 searchURL, request.getContextPath());
+                if (interactionResult != null) bean.setWrappedInteraction(interactionResult);   //tell bean display info
                 beanList.add(bean);
             }
-            logger.info("DetailAction: Collection of " + beanList.iterator().next().getClass() + " created");
+            logger.info("DetailAction: Collection of " + beanList.iterator().next().getClass() +
+                        " created");
 
-            //save in the session (****REQUEST**** would be better!) and return..
-            //session.setAttribute(SearchConstants.VIEW_BEAN_LIST, beanList);
             request.setAttribute(SearchConstants.VIEW_BEAN_LIST, beanList);
             //send to the detail view JSP
             logger.info("detailsAction: forwarding to 'details' JSP..");
+            // TODO use a Constant here
             return "detailPage";
         }
-
-        //something wrong here - wrong result type for this Action...
-        return SearchConstants.FORWARD_FAILURE;
-
+        else {
+            //something is wrong here - forward to error page 
+            return SearchConstants.FORWARD_FAILURE;
+        }
     }
 
-
-    /**
-     * @see AbstractResultAction#getAbstractViewBean
-     * @param result
-     * @param user
-     * @param contextPath
-     * @return
-     */
-    protected AbstractViewBean getAbstractViewBean (Object result, IntactUserIF user, String contextPath ) {
-
-        logger.info( "beans action: building view beans..." );
-
-        //OLD CODE....
-        //first check we have a Collection type...
-        if(!Collection.class.isAssignableFrom(result.getClass())) return null;
-        return ViewBeanFactory.getInstance().getDetailsViewBean ( (Collection)result, user.getHelpLink(), contextPath );
-    }
 }
