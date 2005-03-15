849a609a2f71f819158419c10d29cd1fe03db85b
skerrien <samuel.kerrien@gmail.com>
2005-03-15 14:58:41 +0000

    an interactor should have a BioSource set. added a check in the setBioSource method and modified the equals/hashCode accordingly.

--- trunk/intactCore/src/uk/ac/ebi/intact/model/InteractorImpl.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/InteractorImpl.java
@@ -59,15 +59,15 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
      * created without at least a shortLabel and an owner specified. NOTE: It is
      * assumed that subclasses of Interactor will supply a valid BioSource; this
      * is initially set to null but <b>other classes may expect it to be non-null</b>.
+     *
      * @param shortLabel The memorable label to identify this Interactor
-     * @param owner The Institution which owns this Interactor
-     * @exception NullPointerException thrown if either parameters are not specified
+     * @param owner      The Institution which owns this Interactor
+     * @throws NullPointerException thrown if either parameters are not specified
      */
-    protected InteractorImpl(String shortLabel, Institution owner) {
+    protected InteractorImpl( String shortLabel, Institution owner ) {
 
         //NB is more than this required to define a valid Interactor?
-        super(shortLabel, owner);
-
+        super( shortLabel, owner );
     }
 
 
@@ -77,44 +77,56 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     public BioSource getBioSource() {
         return bioSource;
     }
+
     public void setBioSource( BioSource bioSource ) {
-        if (bioSource == null) throw new NullPointerException("valid Interactor must have a BioSource!");
+
+        if( bioSource == null ) {
+            throw new NullPointerException( "valid Interactor must have a BioSource!" );
+        }
+
         this.bioSource = bioSource;
     }
 
     ///////////////////////////////////////
     // access methods for associations
-    public void setActiveInstances(Collection someActiveInstance) {
+    public void setActiveInstances( Collection someActiveInstance ) {
         this.activeInstances = someActiveInstance;
     }
+
     public Collection getActiveInstances() {
         return activeInstances;
     }
-    public void addActiveInstance(Component component) {
-        if (! this.activeInstances.contains(component)) {
-            this.activeInstances.add(component);
-            component.setInteractor(this);
+
+    public void addActiveInstance( Component component ) {
+        if( !this.activeInstances.contains( component ) ) {
+            this.activeInstances.add( component );
+            component.setInteractor( this );
         }
     }
-    public void removeActiveInstance(Component component) {
-        boolean removed = this.activeInstances.remove(component);
-        if (removed) component.setInteractor(null);
+
+    public void removeActiveInstance( Component component ) {
+        boolean removed = this.activeInstances.remove( component );
+        if( removed ) component.setInteractor( null );
     }
-    public void setProducts(Collection someProduct) {
+
+    public void setProducts( Collection someProduct ) {
         this.products = someProduct;
     }
+
     public Collection getProducts() {
         return products;
     }
-    public void addProduct(Product product) {
-        if (! this.products.contains(product)) {
-            this.products.add(product);
-            product.setInteractor(this);
+
+    public void addProduct( Product product ) {
+        if( !this.products.contains( product ) ) {
+            this.products.add( product );
+            product.setInteractor( this );
         }
     }
-    public void removeProduct(Product product) {
-        boolean removed = this.products.remove(product);
-        if (removed) product.setInteractor(null);
+
+    public void removeProduct( Product product ) {
+        boolean removed = this.products.remove( product );
+        if( removed ) product.setInteractor( null );
     }
 
     //attributes used for mapping BasicObjects - project synchron
@@ -122,25 +134,26 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     public String getBioSourceAc() {
         return this.bioSourceAc;
     }
-    public void setBioSourceAc(String bioSourceAc) {
+
+    public void setBioSourceAc( String bioSourceAc ) {
         this.bioSourceAc = bioSourceAc;
     }
 
     ///////////////////////////////////////
     // instance methods
 
-    public String toString(){
+    public String toString() {
         String result;
         Iterator i;
 
         result = "Interactor: " + this.getAc()
-                + " Owner: " + this.getOwner().getShortLabel()
-                + " Label: " + this.getShortLabel()
-                + "[";
+                 + " Owner: " + this.getOwner().getShortLabel()
+                 + " Label: " + this.getShortLabel()
+                 + "[";
 
-        if (null != this.getXrefs()){
+        if( null != this.getXrefs() ) {
             i = this.getXrefs().iterator();
-            while(i.hasNext()){
+            while( i.hasNext() ) {
                 result = result + i.next();
             }
         }
@@ -151,49 +164,46 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     /**
      * Equality for Interactors is currently based on equality for
      * <code>AnnotatedObjects</code>, BioSources and Products.
-     * @see uk.ac.ebi.intact.model.AnnotatedObject
+     *
      * @param o The object to check
      * @return true if the parameter equlas this object, false otherwise
+     * @see uk.ac.ebi.intact.model.AnnotatedObject
      */
-    public boolean equals (Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Interactor)) return false;
-        if (!super.equals(o)) return false;
+    public boolean equals( Object o ) {
+        if( this == o ) return true;
+        if( !( o instanceof Interactor ) ) return false;
+        if( !super.equals( o ) ) return false;
 
         final Interactor interactor = (Interactor) o;
 
         // possible cycle ...
         // if (!activeInstances.equals(interactor.activeInstances)) return false;
-        if(bioSource != null) {
-            if (! bioSource.equals(interactor.getBioSource())) return false;
+
+        if( !bioSource.equals( interactor.getBioSource() ) ) {
+            return false;
         }
-        else {
-            if (interactor.getBioSource() != null) return false;
-        }
-        return CollectionUtils.isEqualCollection(interactor.getProducts(), products);
+
+        return CollectionUtils.isEqualCollection( interactor.getProducts(), products );
     }
 
     public int hashCode() {
 
         int code = 29 * super.hashCode();
 
-        //BioSource should NEVER be null as subclasses must set it -
-        //however just check it here to be safe....
-        if(bioSource != null) code = code + bioSource.hashCode();
+        code = 29 * code + bioSource.hashCode();
 
-   	    /*
-         * Can't take into account activeInstances since it happens that an object
-         * references himself into that collection ... it would never ends.
-         */
-        if(!products.isEmpty()) {
+        // Can't take into account activeInstances since it happens that an object
+        // references himself into that collection ... it would never ends.
+        if( !products.isEmpty() ) {
             //add the codes for those too....
             int productsCode = 17;
-            for(Iterator it = products.iterator(); it.hasNext();) {
-                Product prod = (Product)it.next(); //controlled here - must be valid cast
-                productsCode = 29*productsCode + prod.hashCode();
+            for( Iterator it = products.iterator(); it.hasNext(); ) {
+                Product prod = (Product) it.next(); //controlled here - must be valid cast
+                productsCode = 29 * productsCode + prod.hashCode();
             }
-            code = 29*code + productsCode;
+            code = 29 * code + productsCode;
         }
+
         return code;
     }
 
