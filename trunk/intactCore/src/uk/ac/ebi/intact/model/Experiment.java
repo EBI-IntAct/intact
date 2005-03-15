9c6fff6d9a844a192c9c2df2919921a6331bc2f4
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-03-15 15:29:02 +0000

    an experiment should have a BioSource set. added a check in the setBioSource method and modified the equals/hashCode accordingly.

--- trunk/intactCore/src/uk/ac/ebi/intact/model/Experiment.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/Experiment.java
@@ -45,7 +45,7 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
      * However, one experiment might explicitely involve different
      * conditions, for example a time series, or before and after a stimulus.
      * This association can establish this relation.
-     *
+     * <p/>
      * This might be extended into an association class
      * which could state the type of relationship.
      */
@@ -71,6 +71,7 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
      * This constructor should <b>not</b> be used as it could
      * result in objects with invalid state. It is here for object mapping
      * purposes only and if possible will be made private.
+     *
      * @deprecated Use the full constructor instead
      */
     private Experiment() {
@@ -94,9 +95,7 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
 
         //TODO Q: does it make sense to create an Experiment without interactions? If not then all the Cv stuff (eg ident method etc) needs to be set also...
         super( shortLabel, owner );
-        if( source == null ) throw new NullPointerException( "valid Experiment must have a BioSource!" );
-        this.bioSource = source;
-
+        setBioSource( source );
     }
 
     /**
@@ -104,18 +103,19 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
      * new Experiment instance is returned, but you should be aware that the
      * object references it contains <b>point to the objects referenced in the
      * original object </b>.
+     *
      * @param experiment The Experiment you want a shallow copy of
      * @return Experiment a new Experiment instance containing new references
-     * to the parameter object's attributes.
-     * @exception NullPointerException thrown if required items are not present
-     * in the parameter Experiment instance.
+     *         to the parameter object's attributes.
+     * @throws NullPointerException thrown if required items are not present
+     *                              in the parameter Experiment instance.
      */
-    public static Experiment getShallowCopy(Experiment experiment) {
+    public static Experiment getShallowCopy( Experiment experiment ) {
 
-        Experiment ex = new Experiment ( experiment.getOwner(),
-                                         experiment.getShortLabel(),
-                                         experiment.getBioSource() );
-        ex.ac = (experiment.getAc());
+        Experiment ex = new Experiment( experiment.getOwner(),
+                                        experiment.getShortLabel(),
+                                        experiment.getBioSource() );
+        ex.ac = ( experiment.getAc() );
         ex.setAnnotation( experiment.getAnnotations() );
         ex.setCurator( experiment.getCurator() );
         ex.setCvInteraction( experiment.getCvInteraction() );
@@ -134,79 +134,97 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
 
     // TODO could wipe all existing interaction ... maybe dangerous.
     // TODO shold also allow to have no interaction if the collection is null or empty.
-    public void setInteractions(Collection someInteraction) {
+    public void setInteractions( Collection someInteraction ) {
         this.interactions = someInteraction;
     }
+
     public Collection getInteractions() {
         return interactions;
     }
 
     // TODO comments
-    public void addInteraction(Interaction interaction) {
-        if (! this.interactions.contains(interaction)) {
-            this.interactions.add(interaction);
-            interaction.addExperiment(this);
+    public void addInteraction( Interaction interaction ) {
+        if( !this.interactions.contains( interaction ) ) {
+            this.interactions.add( interaction );
+            interaction.addExperiment( this );
         }
     }
-    public void removeInteraction(Interaction interaction) {
-        boolean removed = this.interactions.remove(interaction);
-        if (removed) interaction.removeExperiment(this);
+
+    public void removeInteraction( Interaction interaction ) {
+        boolean removed = this.interactions.remove( interaction );
+        if( removed ) interaction.removeExperiment( this );
     }
+
     public Experiment getRelatedExperiment() {
         return relatedExperiment;
     }
 
-    public void setRelatedExperiment(Experiment experiment) {
+    public void setRelatedExperiment( Experiment experiment ) {
         this.relatedExperiment = experiment;
     }
+
     public CvIdentification getCvIdentification() {
         return cvIdentification;
     }
 
-    public void setCvIdentification(CvIdentification cvIdentification) {
+    public void setCvIdentification( CvIdentification cvIdentification ) {
         this.cvIdentification = cvIdentification;
     }
+
     public CvInteraction getCvInteraction() {
         return cvInteraction;
     }
 
-    public void setCvInteraction(CvInteraction cvInteraction) {
+    public void setCvInteraction( CvInteraction cvInteraction ) {
         this.cvInteraction = cvInteraction;
     }
+
     public BioSource getBioSource() {
         return bioSource;
     }
 
     // TODO Q: does it make sense now to change the BioSource which is already set
     // in the cosntructor.
-    public void setBioSource(BioSource bioSource) {
+    public void setBioSource( BioSource bioSource ) {
+
+        if( bioSource == null ) {
+            throw new NullPointerException( "valid Experiment must have a BioSource!" );
+        }
+
         this.bioSource = bioSource;
     }
 
     //attributes used for mapping BasicObjects - project synchron
     // TODO: should be move out of the model.
-    public String getRelatedExperimentAc(){
+    public String getRelatedExperimentAc() {
         return this.relatedExperimentAc;
     }
-    public void setRelatedExperimentAc(String ac){
+
+    public void setRelatedExperimentAc( String ac ) {
         this.relatedExperimentAc = ac;
     }
-    public String getCvIdentificationAc(){
+
+    public String getCvIdentificationAc() {
         return this.cvIdentificationAc;
     }
-    public void setCvIdentificationAc(String ac){
+
+    public void setCvIdentificationAc( String ac ) {
         this.cvIdentificationAc = ac;
     }
-    public String getCvInteractionAc(){
+
+    public String getCvInteractionAc() {
         return this.cvInteractionAc;
     }
-    public void setCvInteractionAc(String ac){
+
+    public void setCvInteractionAc( String ac ) {
         this.cvInteractionAc = ac;
     }
-    public String getBioSourceAc(){
+
+    public String getBioSourceAc() {
         return this.bioSourceAc;
     }
-    public void setBioSourceAc(String ac){
+
+    public void setBioSourceAc( String ac ) {
         this.bioSourceAc = ac;
     }
 
@@ -214,22 +232,24 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
     /**
      * Equality for Experiments is currently based on equality for
      * <code>AnnotatedObjects</code> and BioSources only.
-     * @see uk.ac.ebi.intact.model.AnnotatedObject
+     *
      * @param o The object to check
      * @return true if the parameter equals this object, false otherwise
+     * @see uk.ac.ebi.intact.model.AnnotatedObject
      */
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Experiment)) return false;
-        if (!super.equals(o)) return false;
+    public boolean equals( Object o ) {
+        if( this == o ) return true;
+        if( !( o instanceof Experiment ) ) return false;
+        if( !super.equals( o ) ) return false;
 
         final Experiment experiment = (Experiment) o;
 
-        //need check because we still have no-arg constructor...
-        if(bioSource != null) {
-            return (bioSource.equals(experiment.bioSource));
+
+        if( !bioSource.equals( experiment.bioSource ) ) {
+            return false;
         }
-        return experiment.bioSource == null;
+
+        return true;
 
         // TODO Q: do we compare all interaction ?
         /* YES
@@ -242,20 +262,21 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
         int result = super.hashCode();
 
         //need check as we still have no-arg constructor...
-        if(bioSource != null) result = 29 * result + bioSource.hashCode();
+        result = 29 * result + bioSource.hashCode();
         return result;
     }
 
     /**
      * Returns a cloned version of the current object.
+     *
      * @return a cloned version of the current Experiment with following
-     * exceptions.
-     * <ul>
-     * <li>Interactions are not cloned. The interactions for the cloned
-     * experiment is empty</li>
-     * <li>New components but with the same proteins. The new components has the
-     * cloned interaction as their interaction.</li>
-     * </ul>
+     *         exceptions.
+     *         <ul>
+     *         <li>Interactions are not cloned. The interactions for the cloned
+     *         experiment is empty</li>
+     *         <li>New components but with the same proteins. The new components has the
+     *         cloned interaction as their interaction.</li>
+     *         </ul>
      * @throws CloneNotSupportedException
      */
     public Object clone() throws CloneNotSupportedException {
@@ -273,14 +294,14 @@ public class Experiment extends AnnotatedObjectImpl implements Editable {
     public String toString() {
         StringBuffer result = new StringBuffer( 128 );
 
-        result.append("Experiment [AC: " + this.getAc() + " Shortlabel: " + getShortLabel() );
-        result.append(" BioSource: " + (getBioSource() == null ? "-" : getBioSource().getShortLabel() ) );
+        result.append( "Experiment [AC: " + this.getAc() + " Shortlabel: " + getShortLabel() );
+        result.append( " BioSource: " + ( getBioSource() == null ? "-" : getBioSource().getShortLabel() ) );
         result.append( NEW_LINE );
-        result.append("CvIdentification: " + ( cvIdentification == null ? "-" : cvIdentification.getShortLabel() ));
+        result.append( "CvIdentification: " + ( cvIdentification == null ? "-" : cvIdentification.getShortLabel() ) );
         result.append( NEW_LINE );
-        result.append("CvInteraction: " + ( cvInteraction == null ? "NOT SPECIFIED" : cvInteraction.getShortLabel() ) );
+        result.append( "CvInteraction: " + ( cvInteraction == null ? "NOT SPECIFIED" : cvInteraction.getShortLabel() ) );
         result.append( NEW_LINE );
-        result.append("Interactions (" );
+        result.append( "Interactions (" );
         if( null != getInteractions() ) {
             for( Iterator iterator = interactions.iterator(); iterator.hasNext(); ) {
                 Interaction interaction = (Interaction) iterator.next();
