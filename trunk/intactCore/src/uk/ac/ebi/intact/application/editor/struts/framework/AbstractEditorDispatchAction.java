922253fe22a1287e86ca3c6341e58f2573f0ead8
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-03-29 14:21:51 +0000

    Added a common search method - moved from SidebarDispatchAction class

--- trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/framework/AbstractEditorDispatchAction.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/editor/struts/framework/AbstractEditorDispatchAction.java
@@ -7,22 +7,28 @@ in the root directory of this distribution.
 package uk.ac.ebi.intact.application.editor.struts.framework;
 
 import org.apache.log4j.Logger;
-import org.apache.struts.Globals;
-import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionError;
-import org.apache.struts.actions.LookupDispatchAction;
+import org.apache.ojb.broker.query.LikeCriteria;
 import org.apache.ojb.broker.query.Query;
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.actions.LookupDispatchAction;
+import uk.ac.ebi.intact.application.commons.search.ResultWrapper;
+import uk.ac.ebi.intact.application.commons.search.SearchHelper;
+import uk.ac.ebi.intact.application.commons.search.SearchHelperI;
 import uk.ac.ebi.intact.application.editor.business.EditUserI;
 import uk.ac.ebi.intact.application.editor.business.EditorService;
 import uk.ac.ebi.intact.application.editor.exception.SessionExpiredException;
 import uk.ac.ebi.intact.application.editor.struts.framework.util.EditorConstants;
 import uk.ac.ebi.intact.application.editor.struts.framework.util.ForwardConstants;
 import uk.ac.ebi.intact.application.editor.struts.framework.util.OJBQueryFactory;
+import uk.ac.ebi.intact.application.editor.struts.view.wrappers.ResultRowData;
 import uk.ac.ebi.intact.application.editor.util.LockManager;
+import uk.ac.ebi.intact.business.IntactException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The super class for all the dispatch actions.
@@ -156,6 +162,71 @@ public abstract class AbstractEditorDispatchAction extends LookupDispatchAction
     }
 
     /**
+     * Performs the search using given query array.
+     * @param queries an array of queries. The first query is to get a count and
+     * the secodn query for the actual search.
+     * @param max the max allowed records
+     * @param request the request to store the ActionError
+     * @return a list of search results or an empty list for any errors, too large
+     * result set or search produces no output.
+     */
+    protected List search(Query[] queries, int max, HttpServletRequest request) {
+        // The search helper to do the searching.
+        SearchHelperI searchHelper = new SearchHelper(Logger.getLogger(
+                EditorConstants.LOGGER));
+
+        // The result wrapper returned from the search.
+        ResultWrapper rw = null;
+        try {
+            rw = searchHelper.searchByQuery(queries, max);
+        }
+        catch (IntactException ie) {
+            // This can only happen when problems with creating an internal helper
+            // This error is already logged from the User class.
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("error.intact"));
+            saveErrors(request, errors);
+            return Collections.EMPTY_LIST;
+        }
+
+        // Too large result set?
+        if (rw.isTooLarge()) {
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.search.large",
+                            Integer.toString(rw.getPossibleResultSize())));
+            saveErrors(request, errors);
+            return Collections.EMPTY_LIST;
+        }
+
+        // Nothing found?
+        if (rw.isEmpty()) {
+            // The topic for errors.
+            String topic = EditorService.getTopic(queries[1].getSearchClass());
+
+            // Extract the search parameter.
+            String searchParam = null;
+
+            for (Enumeration enum = queries[1].getCriteria().getElements();
+                 enum.hasMoreElements();) {
+                Object nextCritera = enum.nextElement();
+                if (LikeCriteria.class.isAssignableFrom(nextCritera.getClass())) {
+                    LikeCriteria crit = (LikeCriteria) nextCritera;
+                    searchParam = (String) crit.getValue();
+                    break;
+                }
+            }
+            // No matches found - forward to a suitable page
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                    new ActionError("error.search.nomatch", searchParam, topic));
+            saveErrors(request, errors);
+            return Collections.EMPTY_LIST;
+        }
+        return makeRowData(rw.getResult().iterator(), queries[1].getSearchClass());
+    }
+
+    /**
      * Return an array of general search queries.
      * @param searchClass the search class
      * @param searchString the search value
@@ -176,6 +247,23 @@ public abstract class AbstractEditorDispatchAction extends LookupDispatchAction
         queries[1] = qf.getSearchQuery(searchClass, searchString);
 
         return queries;
+    }
+
+    /**
+     * Returns an array of row data
+     * @param iter the iterator to loop.
+     * @param searchClass the search class to construct an instance of RowData
+     * @return a list consists of RowData objects.
+     */
+    protected List makeRowData(Iterator iter, Class searchClass) {
+        // The results to return.
+        List results = new ArrayList();
+
+        // Convert to result row data.
+        while (iter.hasNext()) {
+            results.add(new ResultRowData((Object[]) iter.next(), searchClass));
+        }
+        return results;
     }
 
     /**
