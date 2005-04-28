7d62c216e527e4b49e0a6a3011dbe58a457ae2d6
afrie <afrie@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-04-28 18:36:54 +0000

    script to create the index

---
+++ trunk/intactCore/scripts/createLuceneIndex.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Usage:
+# createLuceneIndex.sh targetDirectory
+#
+
+CLASSPATH=`echo lib/*.jar | tr ' ' ':'`:$CLASSPATH
+CLASSPATH=classes/:$CLASSPATH
+
+#if cygwin used (ie on a Windows machine), make sure the paths
+#are converted from Unix to run correctly with the windows JVM
+cygwin=false;
+
+case "`uname`" in
+
+CYGWIN*) cygwin=true
+         echo "running in a Windows JVM (from cygwin).." ;;
+*) echo "running in a Unix JVM..." ;;
+
+esac
+
+if $cygwin; then
+CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+
+fi
+
+if [ "$JAVA_HOME" ]; then
+    $JAVA_HOME/bin/java -Xms256m -Xmx1024m -classpath $CLASSPATH uk.ac.ebi.intact.application.search3.searchEngine.lucene.Indexer $1
+else
+    echo Please set JAVA_HOME for this script to exceute
+fi
+
+# end
