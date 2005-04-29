ee600968da4768de9e2976b4a8197f13de279f7a
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-04-29 08:59:42 +0000

    added constant of Databases

--- trunk/intactCore/src/uk/ac/ebi/intact/model/CvDatabase.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/CvDatabase.java
@@ -7,9 +7,8 @@ package uk.ac.ebi.intact.model;
 
 
 /**
- * Represents an external database and contains all the
- * information necessary to retrieve an object from it
- * by a given primary identifier.
+ * Represents an external database and contains all the information necessary to retrieve an object from it by a given
+ * primary identifier.
  *
  * @author hhe
  * @version $Id$
@@ -19,10 +18,19 @@ public class CvDatabase extends CvObject implements Editable {
     ////////////////////////////////
     // Constants
 
-    public static final String UNIPROT = "uniprot";
+    public static final String CABRI = "cabri";
+    public static final String FLYBASE = "flybase";
+    public static final String GO = "go";
+    public static final String HUGE = "huge";
+    public static final String INTACT = "intact";
+    public static final String INTERPRO = "interpro";
+    public static final String NEWT = "newt";
+    public static final String REACTOME_PROTEIN = "reactome-protein";
     public static final String PUBMED = "pubmed";
     public static final String PSI_MI = "psi-mi";
-    public static final String INTERPRO = "interpro";
+    public static final String RESID = "resid";
+    public static final String SGD = "sgd";
+    public static final String UNIPROT = "uniprot";
 
 
     /**
@@ -34,9 +42,8 @@ public class CvDatabase extends CvObject implements Editable {
 //    protected static Vector menuList = null;
 
     /**
-     * This constructor should <b>not</b> be used as it could
-     * result in objects with invalid state. It is here for object mapping
-     * purposes only and if possible will be made private.
+     * This constructor should <b>not</b> be used as it could result in objects with invalid state. It is here for
+     * object mapping purposes only and if possible will be made private.
      *
      * @deprecated Use the full constructor instead
      */
@@ -46,11 +53,11 @@ public class CvDatabase extends CvObject implements Editable {
     }
 
     /**
-     * Creates a valid CvDatabase instance. Requires at least a shortLabel and an
-     * owner to be specified.
+     * Creates a valid CvDatabase instance. Requires at least a shortLabel and an owner to be specified.
      *
      * @param shortLabel The memorable label to identify this CvDatabase
      * @param owner      The Institution which owns this CvDatabase
+     *
      * @throws NullPointerException thrown if either parameters are not specified
      */
     public CvDatabase( Institution owner, String shortLabel ) {
