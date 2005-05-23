bb09c2ed300412a7343439de4e91df2c45897bf5
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-05-23 14:08:17 +0000

    Overrides equals and hashcode methods to avoid recursive behaviour

--- trunk/intactCore/src/uk/ac/ebi/intact/model/CvObject.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/CvObject.java
@@ -5,11 +5,12 @@ in the root directory of this distribution.
 */
 package uk.ac.ebi.intact.model;
 
+import java.util.Iterator;
+
 /**
  * Represents a controlled vocabulary object. CvObject is derived from
  * AnnotatedObject to allow to store annotation of the term within the
  * object itself, thus allowing to build an integrated dictionary.
- *
  *
  * @author Henning Hermjakob
  * @version $Id$
@@ -28,17 +29,81 @@ public abstract class CvObject extends AnnotatedObjectImpl {
     /**
      * Constructor for subclass use only. Ensures that CvObjects cannot be
      * created without at least a shortLabel and an owner specified.
+     *
      * @param shortLabel The memorable label to identify this CvObject
      * @param owner The Institution which owns this CvObject
-     * @exception NullPointerException thrown if either parameters are not specified
+     * @throws NullPointerException thrown if either parameters are not specified
      */
     protected CvObject(Institution owner, String shortLabel) {
-
         //super call sets up a valid AnnotatedObject (and also CvObject, as there is
         //nothing more to add)
         super(shortLabel, owner);
     }
 
+    /**
+     * Equality for CvObject is currently based on equality for primary id of Xref
+     * having the qualifier of identity and short label if there is xref of identity.
+     * We need to equals method to avoid circular references when invoking
+     * equals methods
+     *
+     * @param obj The object to check
+     * @return true if given object has an identity xref and its primary id matches
+     *         to this' object's primary id or short label if there is no identity xref.
+     * @see uk.ac.ebi.intact.model.Xref
+     */
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof CvObject)) {
+            return false;
+        }
+
+        final CvObject other = (CvObject) obj;
+
+        // Check this object has an identity xref first.
+        Xref idXref = getIdentityXref();
+        Xref idOther = other.getIdentityXref();
+
+        if ((idXref != null) && (idOther != null)) {
+            // Both objects have the identity xrefs
+            return idXref.getPrimaryId().equals(idOther.getPrimaryId());
+        }
+        if ((idXref == null) && (idOther == null)) {
+            // Revert to short labels.
+            return getShortLabel().equals(other.getShortLabel());
+        }
+        return false;
+    }
+
+    /**
+     * This class overwrites equals. To ensure proper functioning of HashTable,
+     * hashCode must be overwritten, too.
+     *
+     * @return hash code of the object.
+     */
+    public int hashCode() {
+        Xref idXref = getIdentityXref();
+        return idXref != null ? 29 * idXref.getPrimaryId().hashCode()
+                : 29 * getShortLabel().hashCode();
+    }
+
+    /**
+     * Returns the Identity xref
+     *
+     * @return the Identity xref or null if there is no Identity xref found.
+     */
+    public Xref getIdentityXref() {
+        for (Iterator iter = getXrefs().iterator(); iter.hasNext();) {
+            Xref xref = (Xref) iter.next();
+            CvXrefQualifier xq = (CvXrefQualifier) xref.getCvXrefQualifier();
+            if ((xq != null) && CvXrefQualifier.IDENTITY.equals(xq.getShortLabel())) {
+                return xref;
+            }
+        }
+        return null;
+    }
 } // end CvObject
 
 
