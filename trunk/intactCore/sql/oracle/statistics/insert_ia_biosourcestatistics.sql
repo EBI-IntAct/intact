61e5608b88fbacde3471536a6bb8378a593f501b
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-06-02 14:19:28 +0000

    reformating code for better readability and added / in the end to execute the script when called from create_all_statistics.sh

--- trunk/intactCore/sql/oracle/statistics/insert_ia_biosourcestatistics.sql
+++ trunk/intactCore/sql/oracle/statistics/insert_ia_biosourcestatistics.sql
@@ -19,35 +19,55 @@
 
 declare
   cursor c_bin (b_taxid ia_biosource.taxid%TYPE)
-  IS SELECT count(distinct(component1.interaction_ac)) AS binary_interactions
-          FROM ia_interactor i1
-             , ia_component component1
-          WHERE 2=(SELECT COUNT(*)
-                   FROM ia_component component2
-                   WHERE component1.interaction_ac = component2.interaction_ac)
-          AND i1.biosource_ac in (SELECT ac FROM ia_biosource WHERE taxid = b_taxid)
-          AND component1.interactor_ac = i1.ac
-   ;
+  IS
+          SELECT count(distinct(component1.interaction_ac)) AS binary_interactions
+          FROM ia_interactor i1, ia_component component1
+          WHERE 2 = (SELECT COUNT(*)
+                     FROM ia_component component2
+                     WHERE component1.interaction_ac = component2.interaction_ac) AND
+                i1.biosource_ac in (SELECT ac
+                                    FROM ia_biosource
+                                    WHERE taxid = b_taxid) AND
+                component1.interactor_ac = i1.ac ;
+
    cursor c_prot (b_taxid ia_biosource.taxid%TYPE)
    IS
-     SELECT count(distinct(i2.ac)) AS proteins
+          SELECT count(distinct(i2.ac)) AS proteins
           FROM ia_interactor i2
-          WHERE i2.biosource_ac in (SELECT ac FROM ia_biosource where taxid = b_taxid)
-     AND  i2.objclass ='uk.ac.ebi.intact.model.ProteinImpl'
-   ;
+          WHERE i2.biosource_ac in (SELECT ac
+                                    FROM ia_biosource
+                                    WHERE taxid = b_taxid) AND
+                i2.objclass ='uk.ac.ebi.intact.model.ProteinImpl' ;
+
    cnt_bin INTEGER;
    cnt_prot INTEGER;
+
 BEGIN
-   FOR r in (SELECT DISTINCT taxid,shortlabel FROM ia_biosource b1 where length(shortlabel) =
-(select min(length(shortlabel)) from ia_biosource b2 where b1.taxid=b2.taxid) order by 2) LOOP
-	OPEN c_bin (r.taxid);
+
+   dbms_output.enable ( 1000000 );
+
+   FOR r in (SELECT DISTINCT taxid, shortlabel
+             FROM ia_biosource b1
+             WHERE length(shortlabel) = (SELECT min(length(shortlabel))
+                                         FROM ia_biosource b2
+                                         WHERE b1.taxid=b2.taxid)
+             order by shortlabel)
+   LOOP
+       dbms_output.put_line ( 'Processing taxid: ' || r.taxid );
+
+       OPEN c_bin (r.taxid);
        FETCH c_bin INTO cnt_bin;
        CLOSE c_bin;
-	OPEN c_prot (r.taxid);
+
+       OPEN c_prot (r.taxid);
        FETCH c_prot INTO cnt_prot;
        CLOSE c_prot;
-     INSERT INTO ia_biosourcestatistics(ac, taxid, shortlabel, protein_number, binary_interactions)
-     VALUES (Intact_statistics_seq.nextval, r.taxid, r.shortlabel, cnt_bin, cnt_prot);
-   END LOOP;
-END;
 
+       dbms_output.put_line ( 'Inserting statement...' );
+       INSERT INTO ia_biosourcestatistics(ac, taxid, shortlabel, protein_number, binary_interactions)
+       VALUES (Intact_statistics_seq.nextval, r.taxid, r.shortlabel, cnt_bin, cnt_prot);
+
+   END LOOP;
+
+END;
+/
