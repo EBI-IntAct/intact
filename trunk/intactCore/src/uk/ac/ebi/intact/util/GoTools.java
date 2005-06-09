584b7cf4b2b50da598c6940020e17a419ef15d82
skerrien <samuel.kerrien@gmail.com>
2005-06-09 14:48:02 +0000

    enhanced way of inserting the CVs, the CvDatabase( psi-mi ) and CvXrefQualifier( identity ) are checked to be present and created if needed. [BUGFIX] internal-remark are not exported

--- trunk/intactCore/src/uk/ac/ebi/intact/util/GoTools.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/GoTools.java
@@ -6,91 +6,244 @@ in the root directory of this distribution.
 
 package uk.ac.ebi.intact.util;
 
+import uk.ac.ebi.intact.business.IntactException;
 import uk.ac.ebi.intact.business.IntactHelper;
+import uk.ac.ebi.intact.model.CvDatabase;
+import uk.ac.ebi.intact.model.CvObject;
+import uk.ac.ebi.intact.model.CvXrefQualifier;
+import uk.ac.ebi.intact.model.Xref;
 import uk.ac.ebi.intact.util.go.GoUtils;
+
+import java.io.File;
+import java.util.Iterator;
 
 /**
  * Utilities to read and write files in GO format
  *
- * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @author Sugath Mudali (smudali@ebi.ac.uk), Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
  */
 public class GoTools {
 
+    public static final String NEW_LINE = System.getProperty( "line.separator" );
+
     /**
-     * Load or unload Controlled Vocabularies in GO format.
-     * Usage:
-     * GoTools upload   IntAct_classname goid_db Go_DefinitionFile [Go_DagFile] |
-     * GoTools download[v14] IntAct_classname goid_db Go_DefinitionFile [Go_DagFile]
+     * Load or unload Controlled Vocabularies in GO format. Usage: GoTools upload   IntAct_classname goid_db
+     * Go_DefinitionFile [Go_DagFile] | GoTools download[v14] IntAct_classname goid_db Go_DefinitionFile [Go_DagFile]
      * <p/>
-     * goid_db is the shortLabel of the database which is to be used to establish
-     * object identity by mapping it to goid: in the GO flat file.
-     * Example: If goid_db is psi-mi, an CvObject with an xref "psi-mi; MI:123" is
-     * considered to be the same object as an object from the flat file with goid MI:123.
-     * If goid_db is '-', the shortLabel will be used if present.
+     * goid_db is the shortLabel of the database which is to be used to establish object identity by mapping it to goid:
+     * in the GO flat file. Example: If goid_db is psi-mi, an CvObject with an xref "psi-mi; MI:123" is considered to be
+     * the same object as an object from the flat file with goid MI:123. If goid_db is '-', the shortLabel will be used
+     * if present.
      *
      * @param args
+     *
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main( String[] args ) throws Exception {
 
-        String nl = System.getProperty("line.separator");
-
-        String usage = "Usage: GoTools upload IntAct_classname goid_db "
-                + "Go_DefinitionFile [Go_DagFile] OR" + nl
-                + "GoTools download IntAct_classname goid_db "
-                + "Go_DefinitionFile [Go_DagFile]" + nl + "goid_db is the "
-                + "shortLabel of the database which is to be used to establish"
-                + nl + "object identity by mapping it to goid: in the GO flat file."
-                + nl +  "Example: If goid_db is psi-mi, an CvObject with an xref "
-                + "psi-mi; MI:123 is" + nl + "considered to be the same object "
-                + "as an object from the flat file with goid MI:123." + nl
-                + "If goid_db is '-', the short label will be used if present.";
+        String usage = "Usage: GoTools upload IntAct_classname goid_db Go_DefinitionFile [Go_DagFile] " + NEW_LINE +
+                       "OR" + NEW_LINE +
+                       "GoTools download IntAct_classname goid_db Go_DefinitionFile [Go_DagFile]" + NEW_LINE +
+                       "goid_db is the shortLabel of the database which is to be used to establish" + NEW_LINE +
+                       "object identity by mapping it to goid: in the GO flat file." + NEW_LINE +
+                       "Example: If goid_db is psi-mi, an CvObject with an xref psi-mi; MI:123 is" + NEW_LINE +
+                       "considered to be the same object as an object from the flat file with goid MI:123." + NEW_LINE +
+                       "If goid_db is '-', the short label will be used if present.";
 
         // Check parameters
-        if ((args.length < 4) || (args.length > 5)) {
-            throw new IllegalArgumentException("Invalid number of arguments.\n" + usage);
+        if ( ( args.length < 4 ) || ( args.length > 5 ) ) {
+            throw new IllegalArgumentException( "Invalid number of arguments." + NEW_LINE + usage );
         }
+
+        // retreive parameters
+        String mode = args[ 0 ];
+        String cvClazz = args[ 1 ];
+        String id_db = args[ 2 ];
+        String defFilename = args[ 3 ];
+
+        String dagFile = null;
+        if ( args.length == 5 ) {
+            dagFile = args[ 4 ];
+        }
+
         // The first argument must be either upload or download
-        if (args[0].equals("upload") || args[0].equals("download")) {
+        if ( mode.equals( "upload" ) || mode.equals( "download" ) ) {
             // The target as a Class object
-            Class targetClass = Class.forName(args[1]);
+            Class targetClass = Class.forName( cvClazz );
 
             // Create database access object
             IntactHelper helper = new IntactHelper();
 
+            createNecessaryCvTerms( helper );
+
             try {
                 // args[2] is the go id database.
-                GoUtils goUtils = new GoUtils(helper, args[2], targetClass);
+                GoUtils goUtils = new GoUtils( helper, id_db, targetClass );
 
-                if (args[0].equals("upload")) {
+                if ( mode.equals( "upload" ) ) {
                     // Insert definitions
-                    goUtils.insertGoDefinitions(args[3]);
+                    File defFile = new File( defFilename );
+                    goUtils.insertGoDefinitions( defFile );
 
                     // Insert DAG
-                    if (args.length == 5) {
-                        goUtils.insertGoDag(args[4]);
+                    if ( args.length == 5 ) {
+                        goUtils.insertGoDag( dagFile );
                     }
-                }
-                else if (args[0].equals("download")) {
+                } else if ( mode.equals( "download" ) ) {
                     // Write definitions
-                    System.out.println("Writing GO definitons to " + args[3] + " ...");
-                    goUtils.writeGoDefinitions(args[3]);
+                    System.out.println( "Writing GO definitons to " + defFilename + " ..." );
+                    goUtils.writeGoDefinitions( defFilename );
 
                     // Write go dag format
-                    if (args.length == 5) {
-                        System.out.println("Writing GO DAG to " + args[4] + " ...");
-                        goUtils.writeGoDag(args[4]);
-                        System.out.println("Done.");
+                    if ( args.length == 5 ) {
+                        System.out.println( "Writing GO DAG to " + dagFile + " ..." );
+                        goUtils.writeGoDag( dagFile );
+                        System.out.println( "Done." );
                     }
                 }
-            }
-            finally {
+            } finally {
                 helper.closeStore();
             }
+        } else {
+            throw new IllegalArgumentException( "Invalid argument " + mode + NEW_LINE + usage );
         }
-        else {
-            throw new IllegalArgumentException("Invalid argument " + args[0] + "\n" + usage);
+    }
+
+    //////////////////////////////////////////////////////
+    // Constants related to the identity and psi-mi CVs
+
+    public static final String IDENTITY_MI_REFERENCE = "MI:0356";
+    public static final String IDENTITY_SHORTLABEL = "identity";
+
+    public static final String PSI_MI_REFERENCE = "MI:0488";
+    public static final String PSI_SHORTLABEL = "psi-mi";
+
+
+    /**
+     * Assures that necessary Controlled vocabulary terms are present prior to manipulation of other terms.
+     *
+     * @param helper access to the database.
+     *
+     * @throws IntactException
+     */
+    private static void createNecessaryCvTerms( IntactHelper helper ) throws IntactException {
+
+        // 1. check that the CvXrefQualifier 'identity'/'MI:0356' is present...
+        boolean identityCreated = false;
+        CvXrefQualifier identity = (CvXrefQualifier) helper.getObjectByXref( CvXrefQualifier.class,
+                                                                             IDENTITY_MI_REFERENCE );
+        if ( identity == null ) {
+            // then look for shortlabel
+            identity = (CvXrefQualifier) helper.getObjectByLabel( CvXrefQualifier.class, IDENTITY_SHORTLABEL );
+
+            if ( identity == null ) {
+
+                System.out.println( "CvXrefQualifier( " + IDENTITY_SHORTLABEL + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                identity = new CvXrefQualifier( helper.getInstitution(), IDENTITY_SHORTLABEL );
+                helper.create( identity );
+
+                // That flag will allow the creation of the PSI MI Xref after checking that the CvDatabase exists.
+                identityCreated = true;
+            }
+        }
+
+        // 2. check that the CvDatabase 'psi-mi'/'MI:0356' is present...
+        boolean psiAlreadyExists = false;
+        CvDatabase psi = (CvDatabase) helper.getObjectByXref( CvDatabase.class,
+                                                              PSI_MI_REFERENCE );
+        if ( psi == null ) {
+            // then look for shortlabel
+            psi = (CvDatabase) helper.getObjectByLabel( CvDatabase.class, PSI_SHORTLABEL );
+
+            if ( psi == null ) {
+
+                System.out.println( "CvDatabase( " + PSI_SHORTLABEL + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                psi = new CvDatabase( helper.getInstitution(), PSI_SHORTLABEL );
+                helper.create( psi );
+
+                Xref xref = new Xref( helper.getInstitution(), psi, PSI_MI_REFERENCE, null, null, identity );
+                psi.addXref( xref );
+
+                helper.create( xref );
+            } else {
+                psiAlreadyExists = true;
+            }
+        } else {
+            psiAlreadyExists = true;
+        }
+
+        if ( psiAlreadyExists ) {
+            // check that it has the right MI reference.
+            updatePsiXref( psi, PSI_MI_REFERENCE, helper, psi, identity );
+        }
+
+        if ( identityCreated ) {
+
+            // add the psi reference to the identity term.
+            Xref xref = new Xref( helper.getInstitution(), psi, IDENTITY_MI_REFERENCE, null, null, identity );
+            identity.addXref( xref );
+
+            helper.create( xref );
+        } else {
+            // identity already existed, check that it has the right PSI Xref.
+            updatePsiXref( identity, IDENTITY_MI_REFERENCE, helper, psi, identity );
+        }
+    }
+
+    /**
+     * Update the given CvObject with the given PSI MI reference. update existing Xref if necessary, or create a new one
+     * if none exists.
+     *
+     * @param cv       the Controlled Vocabulary term to update.
+     * @param mi       the MI reference to be found.
+     * @param helper   Database access
+     * @param psi      the CvDatabase( psi-mi )
+     * @param identity the CvXrefQualifier( identity )
+     *
+     * @throws IntactException
+     */
+    private static void updatePsiXref( CvObject cv, String mi,
+                                       IntactHelper helper,
+                                       CvDatabase psi,
+                                       CvXrefQualifier identity ) throws IntactException {
+
+        // Note: we assume that there is at most one psi ref per term.
+        boolean psiRefFound = false;
+
+        for ( Iterator iterator = cv.getXrefs().iterator(); iterator.hasNext() && false == psiRefFound; ) {
+            Xref xref = (Xref) iterator.next();
+
+            if ( psi.equals( xref.getCvDatabase() ) ) {
+                if ( false == mi.equals( xref.getPrimaryId() ) ) {
+
+                    System.out.println( "Updating " + cv.getShortLabel() + "'s MI reference (" +
+                                        xref.getPrimaryId() + " becomes " + mi + ")" );
+                    xref.setPrimaryId( mi );
+                    helper.update( xref );
+                }
+
+                if ( false == identity.equals( xref.getCvXrefQualifier() ) ) {
+                    System.out.println( "Updating " + cv.getShortLabel() + "'s CvXrefQualifier to identity." );
+
+                    xref.setCvXrefQualifier( identity );
+                    helper.update( xref );
+                }
+
+                psiRefFound = true;
+            }
+        } // for
+
+        if ( !psiRefFound ) {
+            // then create the PSI ref
+            Xref xref = new Xref( helper.getInstitution(), psi, mi, null, null, identity );
+            psi.addXref( xref );
+
+            helper.create( xref );
         }
     }
 }
