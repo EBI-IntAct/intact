267ddd89b821fb0c30937c7e3342c869395d570f
smudali <smudali@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-07-14 12:48:22 +0000

    Initiali checkin

--- trunk/intactCore/src/uk/ac/ebi/intact/model/NucleicAcid.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/NucleicAcid.java
@@ -1,20 +1,16 @@
 /*
-Copyright (c) 2002 The European Bioinformatics Institute, and others.  
+Copyright (c) 2002-2005 The European Bioinformatics Institute, and others.
 All rights reserved. Please see the file LICENSE 
 in the root directory of this distribution.
 */
 package uk.ac.ebi.intact.model;
 
 /**
- * A nucleic acid sequence.
- * 
- * todo Further specification.
- * @author hhe
+ * A marker interface to differentiate from other Polymer types (namely Protein).
+ * This is keeping in consistent with existing interfaces such as Protein.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
  * @version $Id$
  */
-public class NucleicAcid extends InteractorImpl {
-} // end NucleicAcid
-
-
-
-
+public interface NucleicAcid extends Polymer {
+}
