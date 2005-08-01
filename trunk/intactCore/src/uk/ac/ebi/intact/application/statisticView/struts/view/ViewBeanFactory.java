8aa5cd5708182c18d6da27817f27abb7d2cc036a
skerrien <samuel.kerrien@gmail.com>
2005-08-01 16:19:24 +0000

    exception handling + logging

--- trunk/intactCore/src/uk/ac/ebi/intact/application/statisticView/struts/view/ViewBeanFactory.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/statisticView/struts/view/ViewBeanFactory.java
@@ -9,9 +9,13 @@ import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.entity.StandardEntityCollection;
 import org.jfree.chart.servlet.ServletUtilities;
+import org.apache.log4j.Logger;
 import uk.ac.ebi.intact.application.statisticView.business.data.StatisticHelper;
+import uk.ac.ebi.intact.application.statisticView.business.util.Constants;
+import uk.ac.ebi.intact.business.IntactException;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * User: Michael Kleen mkleen@ebi.ac.uk
@@ -19,6 +23,8 @@ import javax.servlet.http.HttpSession;
  * Time: 4:24:29 PM
  */
 public class ViewBeanFactory {
+
+    private static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
 
     private StatisticHelper helper;
     private String contextPath;
@@ -29,7 +35,8 @@ public class ViewBeanFactory {
     }
 
 
-    public IntactStatisticsBean createViewBean(String start, String stop, HttpSession session) throws Exception {
+    public IntactStatisticsBean createViewBean(String start, String stop, HttpSession session) throws IntactException,
+                                                                                                      IOException {
 
         final JFreeChart cvChart = helper.getCvChart(start, stop);
         final JFreeChart experimentChart = helper.getExperimentChart(start, stop);
