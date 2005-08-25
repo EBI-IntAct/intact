b67c02acfb6c3772f0a7916b755f325520ef59e2
alibanfr <alibanfr@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-08-25 13:56:30 +0000

    New features for Decorator related to the new structure of sourceBean

--- trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/view/utils/SourceDecorator.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/hierarchView/struts/view/utils/SourceDecorator.java
@@ -2,10 +2,16 @@
 Copyright (c) 2002 The European Bioinformatics Institute, and others.
 All rights reserved. Please see the file LICENSE
 in the root directory of this distribution.
+*
+*
+* - ** XHTML/F ready **
+*
 */
 package uk.ac.ebi.intact.application.hierarchView.struts.view.utils;
 
-import org.apache.taglibs.display.Decorator;
+import org.displaytag.decorator.TableDecorator;
+import org.apache.log4j.Logger;
+import uk.ac.ebi.intact.application.hierarchView.business.Constants;
 
 
 /**
@@ -27,13 +33,19 @@ import org.apache.taglibs.display.Decorator;
  *   </p>
  * </p>
  *
- * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk) & Alexandre Liban (aliban@ebi.ac.uk)
  * @version $Id$
+ *
+ * UPDATE Alexandre Liban (aliban@ebi.ac.uk) | 13/08/2005
+ *        Class SourceDecorator modified to be compatible with new "display.tld"
+ *
  */
-public class SourceDecorator extends Decorator {
+public class SourceDecorator extends TableDecorator {
+
+    static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
 
     /**
-     * Transform the label and value data contained by the LabelValueBean beiing
+     * Transform the label and value data contained by the LabelValueBean being
      * displayed by the Display:* tag into a link.
      *
      * e.g. label = XY:0000123 and value = http://www.mysourceXY.com/displayIt?id=XY:0000123
@@ -41,14 +53,39 @@ public class SourceDecorator extends Decorator {
      *
      * @return data properly formated in order to be displayed by the display taglib
      */
-    public String getId() {
-        SourceBean bean = (SourceBean) this.getObject();
-        if (bean == null) return "";
+    public String getid() {
+        SourceBean bean = (SourceBean) this.getCurrentRowObject();
 
         final String id = bean.getId();
         final String sourceBrowserUrl = bean.getSourceBrowserUrl();
 
-        return "<a href=\"" + sourceBrowserUrl + "\" target=\"selectedSourcetFrame\">" + id + "</a>";
+        logger.info("getId() for " + id);
+
+        return "<a href=\"javascript:self.location();\" onClick=\"javascript:window.open('"
+                    + sourceBrowserUrl + "','" + id + "',"
+                    + "'width=600,height=500,scrollbars=yes');\">" + id + "</a>";
+    }
+
+
+    /**
+     * Format the link "Browse" to the GO-diagram when the souce list is displayed
+     *
+     * @return a link "Browse" which displays the GO-diagram in the "selectSourceFrame"
+     */
+    public String getGraph() {
+        SourceBean bean = (SourceBean) this.getCurrentRowObject();
+        if ( bean == null ) return "";
+
+        if ( bean.getType().equals("Go") ) {
+            final String sourceBrowserGraphUrl = bean.getSourceBrowserGraphUrl();
+
+            String applicationPath = bean.getApplicationPath();
+
+            return "<a href=\"" + sourceBrowserGraphUrl + "\" target=\"selectedSourcetFrame\" ><img src=\""+ applicationPath +"/images/graph.png\" border=\"0\" height=\"25\" width=\"25\" align=\"middle\" alt=\"(*)\" /></a>";
+        }
+        else {
+            return null;
+        }
     }
 
 
@@ -62,36 +99,53 @@ public class SourceDecorator extends Decorator {
      * @return data properly formated in order to be displayed by the display taglib
      */
     public String getDirectHighlightUrl() {
-        SourceBean bean = (SourceBean) this.getObject();
-        if (bean == null) return "";
+        SourceBean bean = (SourceBean) this.getCurrentRowObject();
+        if ( bean == null ) return "";
 
         String applicationPath = bean.getApplicationPath();
 
         if (bean.isSelected() == false) {
             final String url = bean.getDirectHighlightUrl();
-            return "<center><a href=\"" + url + "\" target=\"_top\"><img src=\""+ applicationPath +
-                   "/images/ok-grey.png\" border=\"0\" alt=\"(*)\"></a></center>";
-        } else {
-            return "<center><img src=\""+ applicationPath +
-                   "/images/ok-red.png\" border=\"0\" alt=\"(-)\"></center>";
+            return "<a href=\"" + url + "\" target=\"_top\"><img src=\""+ applicationPath +
+                   "/images/ok-grey.png\" border=\"0\" align=\"middle\" alt=\"(*)\" /></a>";
         }
+        else {
+            return "<img src=\""+ applicationPath +
+                   "/images/ok-red.png\" border=\"0\" align=\"middle\" alt=\"(-)\" />";
+            }
     }
 
 
     /**
      * Send back the desciption of the source and in case it doesn't exists,
      * give a meaningfull message.
-     * @return the soruce description or an explanation messages.
+     * @return the source description or an explanation messages.
      */
     public String getDescription () {
-        SourceBean bean = (SourceBean) this.getObject();
+        SourceBean bean = (SourceBean) this.getCurrentRowObject();
         if (bean == null) return "";
 
         String description = bean.getDescription();
+
         if (description == null || description.trim().length() == 0) {
-            description = "<font color=\"#898989\"> No description available </font>";
+            description = "<span style=\"font-color:#898989;text-align:left;\"> No description available </span>";
         }
 
         return description;
     }
+
+
+    /**
+     * Send back the count of the source (proteins related)
+     * @return the source count
+     */
+    public int getCount () {
+        SourceBean bean = (SourceBean) this.getCurrentRowObject();
+        if (bean == null) return 0;
+
+        int count = bean.getCount();
+
+        return count;
+    }
+
 }
