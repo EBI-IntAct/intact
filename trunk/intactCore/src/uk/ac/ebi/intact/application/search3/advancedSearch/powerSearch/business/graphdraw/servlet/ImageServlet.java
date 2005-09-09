0c1aa22038c9977215b3060455cd646d4dd8e820
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-09-09 16:00:11 +0000

    optimization

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/business/graphdraw/servlet/ImageServlet.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/business/graphdraw/servlet/ImageServlet.java
@@ -28,38 +28,40 @@ import java.io.IOException;
  */
 public class ImageServlet extends HttpServlet {
 
-    private static Logger logger = Logger.getLogger(Constants.LOGGER_NAME);
+    private static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
             throws IOException, ServletException {
 
-        ImageBean imageBean = (ImageBean) getServletContext().getAttribute(SearchConstants.IMAGE_BEAN);
+        ImageBean imageBean = (ImageBean) getServletContext().getAttribute( SearchConstants.IMAGE_BEAN );
         BufferedImage image = imageBean.getImageData();
 
         // encode the image and put it on the stream
         try {
-            JimiWriter writer = Jimi.createJimiWriter("image/png", response.getOutputStream());
-            writer.setSource(image);
-            writer.putImage(response.getOutputStream());
-        } catch (JimiException je) {
-            throw new IOException(je.getMessage());
+            JimiWriter writer = Jimi.createJimiWriter( "image/png", response.getOutputStream() );
+            writer.setSource( image );
+            writer.putImage( response.getOutputStream() );
+        } catch ( JimiException je ) {
+            throw new IOException( je.getMessage() );
         }
 
-        // Get current size of heap in bytes
-        long heapSize = Runtime.getRuntime().totalMemory();
+        if ( logger.isInfoEnabled() ) {
+            // Get current size of heap in bytes
+            long heapSize = Runtime.getRuntime().totalMemory();
 
-        // Get maximum size of heap in bytes. The heap cannot grow beyond this size.
-        // Any attempt will result in an OutOfMemoryException.
-        long heapMaxSize = Runtime.getRuntime().maxMemory();
+            // Get maximum size of heap in bytes. The heap cannot grow beyond this size.
+            // Any attempt will result in an OutOfMemoryException.
+            long heapMaxSize = Runtime.getRuntime().maxMemory();
 
-        // Get amount of free memory within the heap in bytes. This size will increase
-        // after garbage collection and decrease as new objects are created.
-        long heapFreeSize = Runtime.getRuntime().freeMemory();
+            // Get amount of free memory within the heap in bytes. This size will increase
+            // after garbage collection and decrease as new objects are created.
+            long heapFreeSize = Runtime.getRuntime().freeMemory();
 
-        logger.info("in Servlet: current size of heap: " + heapSize);
-        logger.info("maximum heap size: " + heapMaxSize);
-        logger.info("free heap size: " + heapFreeSize);
+            logger.info( "in Servlet: current size of heap: " + heapSize );
+            logger.info( "maximum heap size: " + heapMaxSize );
+            logger.info( "free heap size: " + heapFreeSize );
+        }
 
-        response.setContentType("image/png");
+        response.setContentType( "image/png" );
     }
 }
