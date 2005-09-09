8bae35fae2554386aa069441c200cda3781cb648
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-09-09 15:47:51 +0000

    Now Supports all 6 CV DAGs that are in IntAct

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/business/graphdraw/CvGraph.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/business/graphdraw/CvGraph.java
@@ -6,30 +6,26 @@
 package uk.ac.ebi.intact.application.search3.advancedSearch.powerSearch.business.graphdraw;
 
 import org.apache.log4j.Logger;
- import uk.ac.ebi.intact.application.search3.advancedSearch.powerSearch.business.graphdraw.graph.*;
+import uk.ac.ebi.intact.application.search3.advancedSearch.powerSearch.business.graphdraw.graph.*;
 import uk.ac.ebi.intact.application.search3.business.Constants;
 import uk.ac.ebi.intact.business.IntactException;
 import uk.ac.ebi.intact.business.IntactHelper;
-import uk.ac.ebi.intact.model.CvDagObject;
-import uk.ac.ebi.intact.model.CvIdentification;
-import uk.ac.ebi.intact.model.CvInteraction;
-import uk.ac.ebi.intact.model.CvInteractionType;
+import uk.ac.ebi.intact.model.*;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * This class provides methods to create graph images for the controlled vocabulary
  *
- * @author Anja Friedrichsen, Michael Kleen
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk), Anja Friedrichsen, Michael Kleen
  * @version $id$
  */
 public class CvGraph {
+
+    // TODO cache image on the hard drive
 
     private static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
 
@@ -43,19 +39,41 @@ public class CvGraph {
     private long lastImageGeneratedTime = -1;
     private HashMap imageCache = new HashMap();
 
+    private static final String NEW_LINE = System.getProperty( "line.separator" );
+
+    /**
+     * List of all supported CVs.
+     */
+    private static final Set SUPPORTED_CV = new HashSet();
+
+    static{
+        // declare all supported CVs
+        SUPPORTED_CV.add( CvInteraction.class );
+        SUPPORTED_CV.add( CvInteractionType.class );
+        SUPPORTED_CV.add( CvInteractorType.class );
+        SUPPORTED_CV.add( CvIdentification.class );
+        SUPPORTED_CV.add( CvFeatureType.class );
+        SUPPORTED_CV.add( CvFeatureIdentification.class );
+    }
+
 
     /**
      * First this method retrieves all CVs, that belong to the CV class specified in the argument, out of the database.
      * Afterwards it creates for each Cv object one node for the graph and finds all parent relations between these
      * nodes.
      *
-     * @param cvName name of the CV which should be drawn
+     * @param cvClass Class of the CV which should be drawn
      *
      * @return image showing the DAG of the CVs
      *
      * @throws IOException
      */
-    public synchronized Image createImage( String cvName ) throws IOException {
+    public synchronized Image createImage( Class cvClass ) throws IOException, IntactException {
+
+        // check if the given CV is allowed
+        if( !SUPPORTED_CV.contains( cvClass ) ) {
+            throw new IntactException( "Unsupported Controlled Vocabulary: " + cvClass.getName() );
+        }
 
         if ( lastImageGeneratedTime == -1 ) {
             lastImageGeneratedTime = System.currentTimeMillis();
@@ -68,106 +86,106 @@ public class CvGraph {
         logger.info( "timeout: " + timeout );
 
         // if the image is not in the cache, create a new one
-        if ( !imageCache.containsKey( cvName ) || timeout ) {
+        if ( !imageCache.containsKey( cvClass ) || timeout ) {
 
-            logger.info( "Generate the picture" );
+            logger.info( "Generating image for " + cvClass.getName() );
 
             Stroke thinStroke = new BasicStroke( 1 );
             Graph g = new Graph();
-            IntactHelper iahelper = null;
-            // collection that holds all cv objects as nodes
-            Collection nodes = null;
-            Map cvobjectToNode = new HashMap();
+            IntactHelper helper = null;
+            BufferedImage image;
+            StringBuffer sb = null;
 
-            // get all CvObjects of the specified Cv out of the database
             try {
-                iahelper = new IntactHelper();
-                if ( cvName.equalsIgnoreCase( "CvInteraction" ) ) {
-                    nodes = iahelper.search( CvInteraction.class.getName(), "ac", null );
-                } else if ( cvName.equalsIgnoreCase( "CvInteractionType" ) ) {
-                    nodes = iahelper.search( CvInteractionType.class.getName(), "ac", null );
-                } else if ( cvName.equalsIgnoreCase( "CvIdentification" ) ) {
-                    nodes = iahelper.search( CvIdentification.class.getName(), "ac", null );
-                } else {
-                    throw new IntactException( "unknown cvName: " + cvName );
+                helper = new IntactHelper();
+                // collection that holds all cv objects as nodes
+                Collection nodes = null;
+                Map cvobjectToNode = new HashMap();
+
+                // get all CvObjects of the specified Cv out of the database
+                nodes = helper.search( cvClass, "ac", null );
+
+                // create for all cvDagObject a node and add these to the graph
+                for ( Iterator iterator = nodes.iterator(); iterator.hasNext(); ) {
+                    CvDagObject o = (CvDagObject) iterator.next();
+                    RectangularNode node = new RectangularNode( 40, 40, o.getFullName(), "javascript:SendInfo('" + o.getShortLabel() + "')", null, o.getShortLabel(), Color.white, Color.blue, thinStroke );
+                    g.nodes.add( node );
+                    cvobjectToNode.put( o, node );
                 }
 
-            } catch ( IntactException e ) {
-                e.printStackTrace();
+                // find all parent-relations between the cvDagObjects
+                for ( Iterator iterator = nodes.iterator(); iterator.hasNext(); ) {
+                    CvDagObject o = (CvDagObject) iterator.next();
+                    findParentRelations( o, cvobjectToNode, g );
+                }
+
+                HierarchicalLayout layout = new HierarchicalLayout( g, HierarchicalLayout.PARENT_TOP );
+                layout.layout();
+
+                final int width = layout.getWidth();
+                final int height = layout.getHeight();
+                image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB );
+
+                final Graphics2D g2 = image.createGraphics();
+
+                g2.setColor( Color.white );
+                g2.fillRect( 0, 0, width, height );
+                g2.setColor( Color.black );
+
+                // Set antialising on
+                g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+
+                for ( Iterator it = g.edges.iterator(); it.hasNext(); ) {
+                    StrokeEdge edge = (StrokeEdge) it.next();
+                    edge.render( g2 );
+                }
+
+                // string that builds the imageMap
+                sb = new StringBuffer( 16384 ); // Starts with a big buffer
+
+                // iterate through the nodes and build the imageMap
+                for ( Iterator it = g.nodes.iterator(); it.hasNext(); ) {
+                    RectangularNode node = (RectangularNode) it.next();
+                    node.render( g2 );
+                    sb.append( node.getImageMap() ).append( NEW_LINE );
+                }
+
+            } finally {
+                if ( helper != null ) {
+                    helper.closeStore();
+                }
             }
 
-            // create for all cvDagObject a node and add these to the graph
-            for ( Iterator iterator = nodes.iterator(); iterator.hasNext(); ) {
-                CvDagObject o = (CvDagObject) iterator.next();
-                RectangularNode node = new RectangularNode( 40, 40, o.getFullName(), "javascript:SendInfo('" + o.getShortLabel() + "')", null, o.getShortLabel(), Color.white, Color.blue, thinStroke );
-                g.nodes.add( node );
-                cvobjectToNode.put( o, node );
+
+            if ( logger.isInfoEnabled() ) {
+
+                // Get current size of heap in bytes
+                long heapSize = Runtime.getRuntime().totalMemory();
+
+                // Get maximum size of heap in bytes. The heap cannot grow beyond this size.
+                // Any attempt will result in an OutOfMemoryException.
+                long heapMaxSize = Runtime.getRuntime().maxMemory();
+
+                // Get amount of free memory within the heap in bytes. This size will increase
+                // after garbage collection and decrease as new objects are created.
+                long heapFreeSize = Runtime.getRuntime().freeMemory();
+
+                logger.info( "in Producer: current size of heap: " + heapSize );
+                logger.info( "maximum heap size: " + heapMaxSize );
+                logger.info( "free heap size: " + heapFreeSize );
             }
-            // find all parent-relations between the cvDagObjects
-            for ( Iterator iterator = nodes.iterator(); iterator.hasNext(); ) {
-                CvDagObject o = (CvDagObject) iterator.next();
-                findParentRelations( o, cvobjectToNode, g );
-            }
-
-
-            HierarchicalLayout layout = new HierarchicalLayout( g, HierarchicalLayout.PARENT_TOP );
-            layout.layout();
-
-            final int width = layout.getWidth();
-            final int height = layout.getHeight();
-            BufferedImage image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB );
-
-            final Graphics2D g2 = image.createGraphics();
-
-            g2.setColor( Color.white );
-
-            g2.fillRect( 0, 0, width, height );
-
-            g2.setColor( Color.black );
-
-            g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
-
-            for ( Iterator it = g.edges.iterator(); it.hasNext(); ) {
-                StrokeEdge edge = (StrokeEdge) it.next();
-                edge.render( g2 );
-            }
-
-            // string that builds the imageMap
-            StringBuffer sb = new StringBuffer();
-
-            // iterate through the nodes and build the imageMap
-            for ( Iterator it = g.nodes.iterator(); it.hasNext(); ) {
-                RectangularNode node = (RectangularNode) it.next();
-                node.render( g2 );
-                sb.append( node.getImageMap() + "\n" );
-            }
-
-            // Get current size of heap in bytes
-            long heapSize = Runtime.getRuntime().totalMemory();
-
-            // Get maximum size of heap in bytes. The heap cannot grow beyond this size.
-            // Any attempt will result in an OutOfMemoryException.
-            long heapMaxSize = Runtime.getRuntime().maxMemory();
-
-            // Get amount of free memory within the heap in bytes. This size will increase
-            // after garbage collection and decrease as new objects are created.
-            long heapFreeSize = Runtime.getRuntime().freeMemory();
-
-            logger.info( "in Producer: current size of heap: " + heapSize );
-            logger.info( "maximum heap size: " + heapMaxSize );
-            logger.info( "free heap size: " + heapFreeSize );
 
             // put the image into cache
-            imageCache.put( cvName, image );
+            imageCache.put( cvClass, image );
             // put the imageMap into cache
-            imageCache.put( cvName + "Map", sb.toString() );
+            imageCache.put( cvClass + "Map", sb.toString() );
         } else {
             logger.info( "Use the cache" );
         }
         // set the imageMap, so it can be retrieved from another class
-        this.setImageMap( (String) imageCache.get( cvName + "Map" ) );
+        this.setImageMap( (String) imageCache.get( cvClass + "Map" ) );
 
-        return (Image) imageCache.get( cvName );
+        return (Image) imageCache.get( cvClass );
     }
 
     /**
