edb6c10ffccad1762faab7f2944f005bf8a286e7
skerrien <samuel.kerrien@gmail.com>
2005-09-09 15:52:31 +0000

    no border on the generated image. updated removed useless code.

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/struts/taglibs/DisplayCvDagObjectTag.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/advancedSearch/powerSearch/struts/taglibs/DisplayCvDagObjectTag.java
@@ -11,21 +11,20 @@ import uk.ac.ebi.intact.application.search3.business.Constants;
 import uk.ac.ebi.intact.application.search3.struts.util.SearchConstants;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.tagext.TagSupport;
 import java.io.IOException;
 
 /**
- * This class builds the HTMl code which is needed to view the CvDag graphs.
+ * This class builds the HTML code which is needed to view the CvDag graphs.
  *
- * @author Anja Friedrichsen
- * @version $id$
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk), Anja Friedrichsen
+ * @version $Id$
  */
 public class DisplayCvDagObjectTag extends TagSupport {
 
-    static Logger logger = Logger.getLogger(Constants.LOGGER_NAME);
+    static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
 
 
     /**
@@ -40,36 +39,33 @@ public class DisplayCvDagObjectTag extends TagSupport {
      * Called when the JSP encounters the end of a tag.
      */
     public int doEndTag() throws JspException {
-        HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
-        ImageBean imageBean = (ImageBean) pageContext.getServletContext().getAttribute(SearchConstants.IMAGE_BEAN);
 
+        ImageBean imageBean = (ImageBean) pageContext.getServletContext().getAttribute( SearchConstants.IMAGE_BEAN );
 
         // Display the HTML code map
         try {
-            pageContext.getOut().write("<map name='graph'>" + imageBean.getImageMap() + "<map>");
+            pageContext.getOut().write( "<map name='graph'>" + imageBean.getImageMap() + "<map>" );
 
             String cvName = imageBean.getCvName();
-            String contextPath = ((HttpServletRequest) pageContext.getRequest()).getContextPath();
-            /* The context parameter in the URL is also given to prevent some browser
-            * (eg. Netscape 4.7) to cache image wrongly.
-            * If the image link were /hierarchView/GenerateImage, netscape don't even
-            * call the servlet and display cached image.
-            */
+            String contextPath = ( (HttpServletRequest) pageContext.getRequest() ).getContextPath();
+            /* The context parameter in the URL is also given to prevent some browser (eg. Netscape 4.7) to cache
+             * image wrongly. If the image link were /hierarchView/GenerateImage, netscape don't even call the
+             * servlet and display cached image.
+             */
             String msg = "<p align=\"left\">\n"
-                    + "  <center>"
-                    + "     <img src=\"" + contextPath + "/image?" + cvName + "\""
-                    + "      USEMAP=\"#graph\" border =\"1\">"
-                    + "     <br>"
-                    + "  </center>"
-                    + "</p>";
+                         + "  <center>"
+                         + "     <img src=\"" + contextPath + "/image?" + cvName + "\""
+                         + "      USEMAP=\"#graph\" border =\"0\">"
+                         + "     <br>"
+                         + "  </center>"
+                         + "</p>";
 
-            pageContext.getOut().write(msg);
+            pageContext.getOut().write( msg );
 
-        } catch (IOException e) {
+        } catch ( IOException e ) {
             e.printStackTrace();
         }
 
         return EVAL_PAGE;
     }
-
 }
