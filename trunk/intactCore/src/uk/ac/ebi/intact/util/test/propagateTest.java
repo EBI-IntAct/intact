b29de41aeb4f936f807428508e0133c8950a44c2
clieftink <clieftink@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2005-12-21 14:43:22 +0000

    * delete test-data also from audit table * renamed procedures, now starting with test to fit into the JUnit framework

--- trunk/intactCore/src/uk/ac/ebi/intact/util/test/propagateTest.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/test/propagateTest.java
@@ -10,27 +10,53 @@ import java.sql.*;
  * User: clief
  * Date: 07-Dec-2005
  * Time: 12:37:12
- * To change this template use File | Settings | File Templates.
+ * @author Cor Lieftink
+ * @version $Id: PropagateTest.java
  */
 public class propagateTest extends TestCase {
-        IntactHelper helper = new IntactHelper();
-        Connection conn = helper.getJDBCConnection();
-        Statement st = conn.createStatement();
+        private IntactHelper helper;
+        private Statement st;
 
+    protected void setUp() throws Exception {
+        super.setUp();
+        helper = new IntactHelper();
+        Connection conn = helper.getJDBCConnection();
+        st = conn.createStatement();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+        if (helper != null) {
+            helper.closeStore();
+        }
+    }
+
+    ///////////////////
+    // Utility methods
 
     public propagateTest( String name ) throws IntactException, SQLException{
         super( name );
     }
     private void cleanupTestdata() throws SQLException{
-        st.execute("DELETE IA_EXPERIMENT      WHERE ac IN ('TST-1','TST-24')");
-        st.execute("DELETE IA_INTERACTOR      WHERE ac IN ('TST-2','TST-104', 'TST-22','TST-3','TST-107','TST-109') ");
-        st.execute("DELETE IA_COMPONENT       WHERE ac IN ('TST-4','TST-108','TST-110','TST-23')");
-        st.execute("DELETE IA_ALIAS           WHERE ac IN ('TST-5','TST-10','TST-14','TST-18','TST-101','TST-105','TST-111','TST-18','TST-107') ");
-        st.execute("DELETE IA_ANNOTATION      WHERE ac IN ('TST-7','TST-102','TST-15','TST-9','TST-104','TST-19','TST-108')  ");
-        st.execute("DELETE IA_XREF            WHERE ac IN ('TST-8','TST-11','TST-20','TST-16','TST-103','TST-106','TST-109') ");
-        st.execute("DELETE IA_controlledvocab WHERE ac IN ('TST-6','TST-12') ");
-        st.execute("DELETE IA_feature         WHERE ac IN ('TST-13','TST-107','TST-109') ");
-        st.execute("DELETE IA_range           WHERE ac IN ('TST-21','TST-110') ");
+        st.execute("DELETE IA_EXPERIMENT            WHERE ac IN ('TST-1','TST-24')");
+        st.execute("DELETE IA_EXPERIMENT_AUDIT      WHERE ac IN ('TST-1','TST-24')");
+        st.execute("DELETE IA_INTERACTOR            WHERE ac IN ('TST-2','TST-104', 'TST-22','TST-3','TST-107','TST-109') ");
+        st.execute("DELETE IA_INTERACTOR_AUDIT      WHERE ac IN ('TST-2','TST-104', 'TST-22','TST-3','TST-107','TST-109') ");
+        st.execute("DELETE IA_COMPONENT             WHERE ac IN ('TST-4','TST-108','TST-110','TST-23')");
+        st.execute("DELETE IA_COMPONENT_AUDIT       WHERE ac IN ('TST-4','TST-108','TST-110','TST-23')");
+        st.execute("DELETE IA_ALIAS                 WHERE ac IN ('TST-5','TST-10','TST-14','TST-18','TST-101','TST-105','TST-111','TST-18','TST-107') ");
+        st.execute("DELETE IA_ALIAS_AUDIT           WHERE ac IN ('TST-5','TST-10','TST-14','TST-18','TST-101','TST-105','TST-111','TST-18','TST-107') ");
+        st.execute("DELETE IA_ANNOTATION            WHERE ac IN ('TST-7','TST-102','TST-15','TST-9','TST-104','TST-19','TST-108')  ");
+        st.execute("DELETE IA_ANNOTATION_AUDIT      WHERE ac IN ('TST-7','TST-102','TST-15','TST-9','TST-104','TST-19','TST-108')  ");
+        st.execute("DELETE IA_XREF                  WHERE ac IN ('TST-8','TST-11','TST-20','TST-16','TST-103','TST-106','TST-109') ");
+        st.execute("DELETE IA_XREF_AUDIT            WHERE ac IN ('TST-8','TST-11','TST-20','TST-16','TST-103','TST-106','TST-109') ");
+        st.execute("DELETE IA_controlledvocab       WHERE ac IN ('TST-6','TST-12') ");
+        st.execute("DELETE IA_controlledvocab_audit WHERE ac IN ('TST-6','TST-12') ");
+        st.execute("DELETE IA_feature               WHERE ac IN ('TST-13','TST-107','TST-109') ");
+        st.execute("DELETE IA_feature_audit         WHERE ac IN ('TST-13','TST-107','TST-109') ");
+        st.execute("DELETE IA_range                 WHERE ac IN ('TST-21','TST-110') ");
+        st.execute("DELETE IA_range_audit           WHERE ac IN ('TST-21','TST-110') ");
 
     }
     private void insertTestdata () throws SQLException{
@@ -92,7 +118,11 @@ public class propagateTest extends TestCase {
 
 
     }
-    public void experimentAlias()       throws IntactException, SQLException,InterruptedException {
+
+    /////////////////////
+    // Tests
+
+    public void testExperimentAlias()       throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO IA_ALIAS (ac, parent_ac) VALUES ('TST-101','TST-1') ", false );
@@ -100,7 +130,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      IA_ALIAS WHERE AC='TST-5' ", false);
         cleanupTestdata();
     }
-    public void experimentAnnotation()  throws IntactException, SQLException,InterruptedException {
+    public void testExperimentAnnotation()  throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO ia_annotation (ac, topic_ac) VALUES ('TST-102','TST-6') "); /*annotation seq not leading to propagate*/
@@ -112,7 +142,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_exp2annot WHERE experiment_ac='TST-1' and annotation_ac='TST-7' ", false );    /*delete test 2 */
         cleanupTestdata();
     }
-    public void experimentXref()        throws IntactException, SQLException,InterruptedException {
+    public void testExperimentXref()        throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_xref (ac, parent_ac) VALUES ('TST-103','TST-1') ", false );
@@ -120,7 +150,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_xref WHERE AC='TST-8' ", false );
         cleanupTestdata();
     }
-    public void experimentInteraction() throws IntactException, SQLException,InterruptedException {
+    public void testExperimentInteraction() throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO IA_INTERACTOR ( ac,objclass ) VALUES ('TST-104','uk.ac.ebi.intact.model.InteractionImpl') ");
@@ -131,13 +161,13 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE ia_interactor WHERE ac='TST-2'", false );
         cleanupTestdata();
     }
-    public void interactionAttributes() throws IntactException, SQLException,InterruptedException {
+    public void testInteractionAttributes() throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" UPDATE ia_interactor SET shortlabel='x' WHERE ac='TST-2'", false );
         cleanupTestdata();
     }
-    public void interactionAlias()      throws IntactException, SQLException,InterruptedException {
+    public void testInteractionAlias()      throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO IA_ALIAS (ac, parent_ac) VALUES ('TST-105','TST-2') ", false );
@@ -145,7 +175,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      IA_ALIAS WHERE AC='TST-10' ", false );
         cleanupTestdata();
     }
-    public void interactionAnnotation() throws IntactException, SQLException,InterruptedException {
+    public void testInteractionAnnotation() throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO ia_annotation (ac, topic_ac) VALUES ('TST-104','TST-6') "); /*annotation seq not leading to propagate*/
@@ -157,7 +187,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_int2annot WHERE interactor_ac='TST-2' and annotation_ac='TST-9' ", false );
         cleanupTestdata();
     }
-    public void interactionXref()       throws IntactException, SQLException,InterruptedException {
+    public void testInteractionXref()       throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_xref (ac, parent_ac) VALUES ('TST-106','TST-2') ", false );
@@ -165,7 +195,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_xref WHERE AC='TST-11' ", false );
         cleanupTestdata();
     }
-    public void interactionComponent()  throws IntactException, SQLException,InterruptedException {
+    public void testInteractionComponent()  throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO IA_INTERACTOR ( ac, objclass ) VALUES ('TST-107','uk.ac.ebi.intact.model.proteinimpl') ");
@@ -175,7 +205,7 @@ public class propagateTest extends TestCase {
         cleanupTestdata();
     }
     /* ComponentAtttributes() see interactionComponent  update*/
-    public void componentFeature()  throws IntactException, SQLException,InterruptedException {
+    public void testComponentFeature()  throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_feature (ac, component_ac) VALUES ('TST-108','TST-4') ", false );
@@ -183,7 +213,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_feature where ac= 'TST-13'", false );
         cleanupTestdata();
     }
-    public void componentProtein()  throws IntactException, SQLException,InterruptedException {
+    public void testComponentProtein()  throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         /* is actually an update of the field interactor_ac of the component record */
@@ -192,14 +222,15 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" UPDATE ia_component set interactor_ac = 'TST-109' where ac='TST-110'  ", false ); /* updates and deletes tested on full tree*/
         cleanupTestdata();
     }
-    public void proteinCRC64()    throws IntactException, SQLException,InterruptedException {
+     public void testProtein()    throws IntactException, SQLException,InterruptedException {
 
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
-        evaluateTestStatement(" UPDATE IA_INTERACTOR SET CRC64='X' WHERE AC='TST-3' ", true );
+        evaluateTestStatement(" UPDATE IA_INTERACTOR SET CRC64='X' WHERE AC='TST-3' ",false);
+        evaluateTestStatement(" DELETE IA_INTERACTOR WHERE AC='TST-3' ",false);
         cleanupTestdata();
     }
-    public void proteinAlias()      throws IntactException, SQLException,InterruptedException {
+    public void testProteinAlias()      throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO IA_ALIAS (ac, parent_ac) VALUES ('TST-111','TST-3') ", true );
@@ -207,7 +238,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      IA_ALIAS WHERE AC='TST-14' ", false );
         cleanupTestdata();
     }
-    public void proteinAnnotation() throws IntactException, SQLException,InterruptedException {
+    public void testProteinAnnotation() throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO ia_annotation (ac, topic_ac) VALUES ('TST-104','TST-6') "); /*annotation seq not leading to propagate*/
@@ -219,7 +250,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_int2annot WHERE interactor_ac='TST-3' and annotation_ac='TST-15' ", false );
         cleanupTestdata(); /* clean up possible still existing testdata */
     }
-    public void proteinXref()       throws IntactException, SQLException,InterruptedException {
+    public void testProteinXref()       throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_xref (ac, parent_ac) VALUES ('TST-106','TST-3') ", false );
@@ -227,7 +258,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_xref WHERE AC='TST-16' ", false );
         cleanupTestdata();
     }
-    public void featureAlias()      throws IntactException, SQLException,InterruptedException {
+    public void testFeatureAlias()      throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO IA_ALIAS (ac, parent_ac) VALUES ('TST-107','TST-13') ", false );
@@ -235,7 +266,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      IA_ALIAS WHERE AC='TST-18' ", false );
         cleanupTestdata();
     }
-    public void featureAnnotation() throws IntactException, SQLException,InterruptedException {
+    public void testFeatureAnnotation() throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         st.execute(" INSERT INTO ia_annotation (ac, topic_ac) VALUES ('TST-108','TST-6') "); /*annotation seq not leading to propagate*/
@@ -247,7 +278,7 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_feature2annot WHERE feature_ac='TST-13' and annotation_ac='TST-19' ", false );
         cleanupTestdata();
     }
-    public void featureXref()       throws IntactException, SQLException,InterruptedException {
+    public void testFeatureXref()       throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_xref (ac, parent_ac) VALUES ('TST-109','TST-13') ", false );
@@ -255,36 +286,13 @@ public class propagateTest extends TestCase {
         evaluateTestStatement(" DELETE      ia_xref WHERE AC='TST-20' ", false );
         cleanupTestdata();
     }
-    public void featureRange()       throws IntactException, SQLException,InterruptedException {
+    public void testFeatureRange()       throws IntactException, SQLException,InterruptedException {
         cleanupTestdata(); /* clean up possible still existing testdata */
         insertTestdata ();
         evaluateTestStatement(" INSERT INTO ia_range (ac, feature_ac) VALUES ('TST-110','TST-13') ", false );
         evaluateTestStatement(" UPDATE      ia_range SET fromintervalstart = '1' WHERE AC='TST-21' ", false ); /* updates and deletes tested on full tree*/
         evaluateTestStatement(" DELETE      ia_range WHERE AC='TST-21' ", false );
         cleanupTestdata();
-    }
-
-
-    public void allTests ()           throws IntactException, SQLException,InterruptedException{
-        experimentAlias();
-        experimentAnnotation();
-        experimentXref();
-        experimentInteraction ();
-        interactionAttributes();
-        interactionAlias();
-        interactionAnnotation();
-        interactionXref();
-        interactionComponent();
-        componentFeature();
-        componentProtein();
-        proteinCRC64();
-        proteinAlias();
-        proteinAnnotation();
-        proteinXref();
-        featureAlias();
-        featureAnnotation();
-        featureXref();
-        featureRange();
     }
 
 
