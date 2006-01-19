7a6d639e63ac9c27da611325d19b3577a40edfbd
ckohler <ckohler@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-01-19 17:15:54 +0000

    Added new forward name as well as merged constants with search3.struts.framework.util

--- trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/util/SearchConstants.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/search3/struts/util/SearchConstants.java
@@ -19,26 +19,49 @@ public interface SearchConstants {
     ////////////////////////////////////////////////////
 
     /**
-     * The key to access configuration directory.
-     */
-    public static final String CONFIG_DIR = "configdir";
-
-    /**
      * The name of the hierarchView properties file.
      */
     public static final String HV_PROPS = "HierarchView";
 
     /**
+     * The key to access configuration directory.
+     */
+    String CONFIG_DIR = "configdir";
+    /**
      * Used as a key to identify a datasource class - its value is deifned in the web.xml file as a
      * servlet context parameter
      */
-    public static final String DATA_SOURCE = "datasource";
+    String DATA_SOURCE = "datasource";
 
+    //////////////////////////
+    // Session/Request keys
+    //////////////////////////
 
+    /**
+     * URL to which the waiting page should redirect.
+     */
+    public static final String WAITING_URL = "waiting_url";
 
-    ///////////////////////
-    // Session keys
-    ///////////////////////
+    /**
+     * Message to display on the waiting page.
+     */
+    public static final String WAITING_MSG = "waiting_message";
+
+    /**
+     * This value indicates how long should the waiting page wait before to fire redirect.
+     */
+    public static final Integer DEFAULT_WAITING_TIME = new Integer(1);
+
+    /**
+     * This value indicates how long should the waiting page wait before to fire redirect.
+     */
+    public static final String WAITING_TIME = "waiting_time";
+
+    /**
+     * When using the waiting page, if that key is bound in the session and the values if Boolean.TRUE,
+     * then there is no forward.
+     */
+    public static final String DO_NOT_FORWARD = "waiting_page_do_not_forward";
 
     /**
      * The key to store an Intact Service object.
@@ -48,7 +71,7 @@ public interface SearchConstants {
     /**
      * The key to access a user session object.
      */
-    public static final String INTACT_USER = "user";
+    String INTACT_USER = "user";
 
     /**
      * The search criteria key to display in the results JSP.
@@ -71,7 +94,6 @@ public interface SearchConstants {
      */
     public static final String LARGE_EXPERIMENT_BEAN = "largeExp";
 
-
     /**
      * The most recent user-defined search (ie not from a CvObject link).
      */
@@ -83,18 +105,64 @@ public interface SearchConstants {
     public static final String SEARCH_RESULTS = "searchResults";
 
     /**
-      * The label used to identify the search results Map (often saved in a request)
+     * The label used to identify the search results Map (often saved in a request)
      */
     public static final String SEARCH_RESULTS_MAP = "searchResultsMap";
 
-     /**
-      * class to be searched for, needed for the advanced search
-      */
-    public static final String SEARCH_CLASS = "search_class" ;
+    /**
+     * class to be searched for, needed for the advanced search
+     */
+    public static final String SEARCH_CLASS = "search_class";
+
+    /**
+     * Identifies the map (stored in the servlet context) which contains the maximum display size
+     * values for various intact types.
+     */
+    public static final String MAX_ITEMS_MAP = "maxMap";
+
+    /**
+     * Indetifies the map (stored in the servlet context) which contains the the count and classname
+     * from the initial search request for displaying on the the tooLarge jsp
+     */
+    public static final String RESULT_INFO = "resultInfo";
+
+    /**
+     * Maximum size for the initial request of objects
+     */
+    public static final int MAXIMUM_RESULT_SIZE = 50;
+
+    /////////////////////
+    // Misc constants
+    /////////////////////
+
+    /**
+     * Forward to the too large page if the dataset is too large
+     */
+    public static final String TOO_LARGE_DATASET = "tooLarge";
+
+    /**
+     * Contains a list of the shortlabels of the items found in a search result. Used by JSPs to
+     * decide what shortlabels should be highlighted.
+     */
+    public static final String HIGHLIGHT_LABELS_LIST = "labelList";
 
     ///////////////////////
     // STRUTS forwards
     ///////////////////////
+
+    /**
+     * Used to identify which page a request came from. Mainly needed by Action classes to decide
+     * what to do for a given request when the same request may originate from different sources and
+     * then require different processing (eg for Protein it should give a partners view if from the
+     * main 'simple' page, or the Protein beans if from another link).
+     */
+    public static final String PAGE_SOURCE = "view";
+
+    public static final String ERROR_MESSAGE = "error_message";
+
+    public static final String IMAGE_BEAN = "imageBean";
+
+    public static final String SEARCH_OBJECT = "searchObject";
 
     /**
      * The key to success action.
@@ -106,16 +174,6 @@ public interface SearchConstants {
      * the struts-config.xml file to see where the page that is using this forwards to.
      */
     public static final String FORWARD_FAILURE = "failure";
-
-    /**
-     * Forward to the results page.
-     */
-    public static final String FORWARD_SESSION_LOST = "sessionLost";
-
-    /**
-     * Forward to the too large page if the dataset is too large
-     */
-    public static final String TOO_LARGE_DATASET = "tooLarge";
 
     /**
      * Key to the too large action
@@ -155,7 +213,7 @@ public interface SearchConstants {
      */
     public static final String FORWARD_SIMPLE_ACTION = "simple";
 
- /**
+    /**
      * Forward to the Action responsible for setting up the 'simple' initial view of simple text
      * search results
      */
@@ -171,26 +229,13 @@ public interface SearchConstants {
      */
     public static final String FORWARD_RESULTS = "results";
 
-    /**
-     * Used as a key to identify a page to display when matches are found from a search.
-     */
-    public static final String FORWARD_MATCHES = "match";
+    public static final String FORWARD_DETAIL_PAGE = "detailPage";
 
     /**
      * Used as a key to identify a page to display when no matches are found from a search.
      */
     public static final String FORWARD_NO_MATCHES = "noMatch";
 
-    /**
-     *
-     */
-    public static final String FORWARD_TOO_MANY_PROTEINS = "tooManyProteins";
-
-    public static final String FORWARD_NO_INTERACTIONS = "noInteractions";
-
-    /**
-     * Used as a key to identify a page to display when warning are raised from a search.
-     */
     public static final String FORWARD_WARNING = "warning";
 
     /**
@@ -198,78 +243,48 @@ public interface SearchConstants {
      */
     public static final String FORWARD_BINARYPROTEIN_ACTION = "binaryProtein";
 
+
     /**
-     *  Used to forward to a syntax error page, if the search string has an incorrect syntax
+     * If we got more than 2 proteins forward to errorpage.
+     */
+    public static final String FORWARD_TOO_MANY_PROTEINS = "tooManyProteins";
+
+    /**
+     * forward when no interaction found in BinaryProteinAction.
+     */
+    public static final String FORWARD_NO_INTERACTIONS = "noInteractions";
+
+    /**
+     * forward to a graph representation of IntAct CVs.
+     */
+    public static final String FORWARD_SHOW_CV_DAG = "showCvDag";
+
+    /**
+     * forward to a waiting page.
+     */
+    public static final String FORWARD_WAITING_PAGE = "waiting_page";
+
+
+    /**
+     * Used to forward to a syntax error page, if the search string has an incorrect syntax
      */
     public static final String FORWARD_ERROR = "error";
-
-
-    /**
-     * Used to identify which page a request came from. Mainly needed by Action classes to decide
-     * what to do for a given request when the same request may originate from different sources and
-     * then require different processing (eg for Protein it should give a partners view if from the
-     * main 'simple' page, or the Protein beans if from another link).
-     */
-    public static final String PAGE_SOURCE = "view";
+    public static final String FORWARD_BIOSOURCE = "bioSource";
+    public static final String FORWARD_CVOBJECT = "cvObject";
+    public static final String FORWARD_PROTEIN = "singleProtein";
+    public static final String FORWARD_NO_RESOURCE = "noResource";
+    public static final String FORWARD_SHOW_INTERACTION_TYPE_GRAPH = "showIntTypeGraph";
+    public static final String FORWARD_INDEX_ERROR = "indexError";
 
     /**
-     * Identifies the map (stored in the servlet context) which contains the maximum display size
-     * values for various intact types.
+     * Used as a key to identify a page to display when matches are found from a search.
      */
-    public static final String MAX_ITEMS_MAP = "maxMap";
-
-    /**
-     * Indetifies the map (stored in the servlet context) which contains the the count and classname
-     * from the initial search request for displaying on the the tooLarge jsp
-     */
-    public static final String RESULT_INFO = "resultInfo";
-    /**
-     * Maximum size for the initial request of objects
-     */
-    public static final int MAXIMUM_RESULT_SIZE = 50;
-
-    /**
-     * This can go here later
-     */
-    public static final String SEARCH_URL = "searchUrl";
-
-    /**
-     * Contains a list of the shortlabels of the items found in a search result. Used by JSPs to
-     * decide what shortlabels should be highlighted.
-     */
-    public static final String HIGHLIGHT_LABELS_LIST = "labelList";
+    String FORWARD_MATCHES = "match";
 
     //Maximum display sizes allowable for each relevant type. makes sense to
     //keep these in code as a simple config change will not be able to affect these values
-    public static final int MAXIMUM_DISPLAYABLE_INTERACTION = 50;
-    public static final int MAXIMUM_DISPLAYABLE_PROTEIN = 30;
-    public static final int MAXIMUM_DISPLAYABLE_CVOBJECTS = 30;
-    public static final int MAXIMUM_DISPLAYABLE_EXPERIMENTS = 30;
-
-    public static final String FORWARD_BIOSOURCE = "bioSource";
-
-    public static final String FORWARD_CVOBJECT = "cvObject";
-
-    public static final String FORWARD_PROTEIN = "singleProtein";
-
-    public static final String FORWARD_NO_RESOURCE = "noResource";
-
-
-    public static final String FORWARD_SHOW_IDENTIFICATION_GRAPH = "showIdGraph";
-
-    public static final String FORWARD_SHOW_INTERACTION_GRAPH = "showIntGraph";
-
-    public static final String FORWARD_SHOW_INTERACTION_TYPE_GRAPH = "showIntTypeGraph";
-
-
-    // string to hold the shortened iql statement
-    public static final String IQL_DISPLAY = "iqlDisplay";
-
-    public static final String FORWARD_INDEX_ERROR = "indexError";
-
-    public static final String ERROR_MESSAGE = "error_message";
-
-    public static final String IMAGE_BEAN = "imageBean";
-
-    public static final  String SEARCH_OBJECT = "searchObject" ;
+    int MAXIMUM_DISPLAYABLE_INTERACTION = 50;
+    int MAXIMUM_DISPLAYABLE_PROTEIN = 30;
+    int MAXIMUM_DISPLAYABLE_CVOBJECTS = 30;
+    int MAXIMUM_DISPLAYABLE_EXPERIMENTS = 30;
 }
