f09989b18c2314139ddd80ec2f1a6df63b72b06a
catherineleroy <catherineleroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-02-17 10:25:23 +0000

    Changes in argument

--- trunk/intactCore/scripts/psiXmlDownload.sh
+++ trunk/intactCore/scripts/psiXmlDownload.sh
@@ -4,11 +4,11 @@
 #
 # Purpose: Generate PSI MI XML files from prepared input file
 #
-# Usage example:
+# Usage:
 # psiXmlDownload.sh file_prefix 2.5 cv.map< sections_file >& output.log
 # *******************************************************************
 
-# declare default CV mapping from PSI 2.5 to PSI 1.0
+
 CV_MAPPING_FILENAME=application/dataConversion/controlledvocab/reverseMapping.txt
 
 
@@ -17,49 +17,50 @@ CV_MAPPING_FILENAME=application/dataConversion/controlledvocab/reverseMapping.tx
 #
 processLine()
 {
-     # get parameters
-     filePrefix=$1
-     shift
-     psiVersion=$1
-     shift
-     filename=$1
-     shift
-     searchPattern=$@
+  # get parameters
+  filePrefix=$1
+  shift
+  psiVersion=$1
+  shift
+  filename=$1
+  shift
+  searchPattern=$@
 
-     optionalParam=""
-     if [ $psiVersion == "1" ]; then
-        optionalParam="-cvMapping $CV_MAPPING_FILENAME"
-     fi
+  optionalParam=""
+  if [ $psiVersion == "1" ]; then
+     optionalParam="-cvMapping $CV_MAPPING_FILENAME"
+  fi
 
-     # Run the script
-     outputFile=$filePrefix$filename
-     scripts/psiRun.sh FileGenerator -pattern $searchPattern \
-                                     -output $outputFile \
-                                     -psiVersion $psiVersion \
-                                     $optionalParam
+  # Run the script
+  outputFile=$filePrefix$filename
+  echo $outputFile  
+  scripts/psiRun.sh FileGenerator -pattern $searchPattern \
+                                  -output $outputFile \
+                                  -psiVersion $psiVersion \
+                                  $optionalParam
 }
 
 
 # the two first params are mandatory
-if [ $# -ne 2 ]; then
-      echo ""
-      echo "ERROR: wrong number of parameters."
-      echo "usage: psiXmlDownload.sh <file_prefix> <psiVersion(either '1', '2' or '2.5')> [mapping.file] < sections_file"
-      echo ""
-      exit 1
+if [ $# -ne 2 && $# -ne 3 ]; then
+   echo ""
+   echo "ERROR: wrong number of parameters."
+   echo "usage: psiXmlDownload.sh <file_prefix> <psiVersion(1, 2, 25)> [mapping.file] < sections_file"
+   echo ""
+   exit 1
 fi
 
 filePrefix=$1
 psiVersion=$2
 
 if [ $# -e 3 ]; then
-      CV_MAPPING_FILENAME=$3
+   CV_MAPPING_FILENAME=$3
 fi
 
 echo "filePrefix:    $filePrefix"
 comment=""
 if [ $psiVersion == "1" ]; then
-    comment="( using CV mapping file: $CV_MAPPING_FILENAME )"
+     "( using CV mapping file: $CV_MAPPING_FILENAME )"
 fi
 echo "PSI version:   $psiVersion $comment"
 echo "filename:      $filename"
@@ -68,9 +69,11 @@ echo "searchPattern: $searchPattern"
 # read line by line the input file
 while read line
 do
-   echo "Processing $line"
-   processLine $filePrefix $psiVersion $line
+    echo "Processing $line"
+    processLine $filePrefix $psiVersion $line
 done
 
 echo ""
 echo "done."
+
+
