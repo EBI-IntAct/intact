8cfcb6ea24fadd056f8b5ba13f300e905fbc56ee
skerrien <samuel.kerrien@gmail.com>
2006-02-22 10:18:24 +0000

    add constants

--- trunk/intactCore/src/uk/ac/ebi/intact/model/CvFeatureType.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/CvFeatureType.java
@@ -6,7 +6,6 @@ in the root directory of this distribution.
 package uk.ac.ebi.intact.model;
 
 
-
 /**
  * TODO comments
  *
@@ -15,10 +14,17 @@ package uk.ac.ebi.intact.model;
  */
 public class CvFeatureType extends CvDagObject {
 
+    public static final String EXPERIMENTAL_FEATURE = "experimental feature";
+    public static final String EXPERIMENTAL_FEATURE_MI_REF = "MI:0505";
+
     /**
-     * Cache a Vector of all shortLabels of the class, e.g. for menus.
-     * This should not be here as it has no model functionality but is
-     * related to eg user interfaces.
+     * Cache a Vector of all shortLabels of the class, e.g. for menus. This should not be here as it has no model
+     * functionality but is related to eg user interfaces.
+     * <p/>
+     * This constructor should <b>not</b> be used as it could result in objects with invalid state. It is here for
+     * object mapping purposes only and if possible will be made private.
+     *
+     * @deprecated Use the full constructor instead
      */
 //    protected static Vector menuList = null;
 
@@ -34,16 +40,17 @@ public class CvFeatureType extends CvDagObject {
     }
 
     /**
-     * Creates a valid CvFeatureType instance. Requires at least a shortLabel and an
-     * owner to be specified.
+     * Creates a valid CvFeatureType instance. Requires at least a shortLabel and an owner to be specified.
+     *
      * @param shortLabel The memorable label to identify this CvFeatureType
-     * @param owner The Institution which owns this CvFeatureType
-     * @exception NullPointerException thrown if either parameters are not specified
+     * @param owner      The Institution which owns this CvFeatureType
+     *
+     * @throws NullPointerException thrown if either parameters are not specified
      */
-    public CvFeatureType(Institution owner, String shortLabel) {
+    public CvFeatureType( Institution owner, String shortLabel ) {
 
         //super call sets up a valid CvObject
-        super(owner, shortLabel);
+        super( owner, shortLabel );
     }
 
 } // end CvFeatureType
