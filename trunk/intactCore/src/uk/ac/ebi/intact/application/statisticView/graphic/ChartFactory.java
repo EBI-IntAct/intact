8aa52d2a81a0535bcd8514ab8c8040eef5f6d78a
skerrien <samuel.kerrien@gmail.com>
2006-02-28 09:10:21 +0000

    update method call to suit the latest jfreechart library

--- trunk/intactCore/src/uk/ac/ebi/intact/application/statisticView/graphic/ChartFactory.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/statisticView/graphic/ChartFactory.java
@@ -39,34 +39,35 @@ public class ChartFactory {
      *
      * @param dataset a DefaultPieDataset which holds the datavalues for the specific PieChart
      * @param title   a String which represents the title of the specific PieChart
+     *
      * @return a JfreeChart PieChart Diagramm
      */
-    public static JFreeChart getPieChart(final DefaultPieDataset dataset, final String title) {
+    public static JFreeChart getPieChart( final DefaultPieDataset dataset, final String title ) {
 
         final DefaultPieDataset pieDataSet = dataset;
 
-        final JFreeChart chart = org.jfree.chart.ChartFactory.createPieChart(title, // chart title
-                                                                             pieDataSet, // data
-                                                                             false, // exclude legend
-                                                                             true,
-                                                                             true);
+        final JFreeChart chart = org.jfree.chart.ChartFactory.createPieChart( title, // chart title
+                                                                              pieDataSet, // data
+                                                                              false, // exclude legend
+                                                                              true,
+                                                                              true );
 
         final PiePlot plot = (PiePlot) chart.getPlot();
 
         // set a new font for the label
 
-        plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 8));
-        plot.setNoDataMessage("No data available");
+        plot.setLabelFont( new Font( "SansSerif", Font.PLAIN, 8 ) );
+        plot.setNoDataMessage( "No data available" );
 
         // rotate the plot
-        plot.setStartAngle(180);
+        plot.setStartAngle( 180 );
 
 
-        plot.setCircular(true);
-        plot.setLabelGap(0.1);
+        plot.setCircular( true );
+        plot.setLabelGap( 0.1 );
 
         // set a background color
-        chart.setBackgroundPaint(new Color(237, 237, 237));
+        chart.setBackgroundPaint( new Color( 237, 237, 237 ) );
 
         return chart;
 
@@ -80,57 +81,58 @@ public class ChartFactory {
      * @param title       a String which represents the title of the specific BarChart
      * @param xAxisTitle  a String which represent the x-axis label of the specific BarChart
      * @param yAxisTitle  a String which represent the y-axis label of the specific BarChart
-     * @param rotateLabel a boolean which descripes if the labels should rotate which 3 degree used for long names in the Dataset
-     *                    true means rotate the label. false means not to rotate the label
+     * @param rotateLabel a boolean which descripes if the labels should rotate which 3 degree used for long names in
+     *                    the Dataset true means rotate the label. false means not to rotate the label
+     *
      * @return a JfreeChart BarChart Diagramm
      */
-    public static JFreeChart getBarChart(final DefaultCategoryDataset dataset,
-                                         final String title,
-                                         final String xAxisTitle,
-                                         final String yAxisTitle,
-                                         final boolean rotateLabel) {
+    public static JFreeChart getBarChart( final DefaultCategoryDataset dataset,
+                                          final String title,
+                                          final String xAxisTitle,
+                                          final String yAxisTitle,
+                                          final boolean rotateLabel ) {
 
         // cast to dataset
         DefaultCategoryDataset barDataset = dataset;
 
-        JFreeChart chart = org.jfree.chart.ChartFactory.createBarChart(title, // chart title
-                                                                       xAxisTitle, // domain axis label
-                                                                       yAxisTitle, // range axis label
-                                                                       barDataset, // data
-                                                                       PlotOrientation.VERTICAL, // orientation
-                                                                       true, // include legend
-                                                                       true, // tooltips
-                                                                       true // no URLs
+        JFreeChart chart = org.jfree.chart.ChartFactory.createBarChart( title, // chart title
+                                                                        xAxisTitle, // domain axis label
+                                                                        yAxisTitle, // range axis label
+                                                                        barDataset, // data
+                                                                        PlotOrientation.VERTICAL, // orientation
+                                                                        true, // include legend
+                                                                        true, // tooltips
+                                                                        true // no URLs
         );
 
         // modify the chart and setting nice colour
         CategoryPlot plot = chart.getCategoryPlot();
-        chart.setBackgroundPaint(new Color(237, 237, 237));
+        chart.setBackgroundPaint( new Color( 237, 237, 237 ) );
 
-        if (rotateLabel == true) {
+        if ( rotateLabel == true ) {
             CategoryAxis domainAxis = plot.getDomainAxis();
-            domainAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0));
+            domainAxis.setCategoryLabelPositions( CategoryLabelPositions.createUpRotationLabelPositions( Math.PI / 6.0 ) );
         }
 
         // set the range axis to display integers only
         final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setStandardTickUnits( NumberAxis.createIntegerTickUnits() );
 
         // disable bar outlines
         BarRenderer renderer = (BarRenderer) plot.getRenderer();
-        renderer.setDrawBarOutline(false);
+        renderer.setDrawBarOutline( false );
         // renderer.setMaxBarWidth(0.8);
-        renderer.setMaxBarWidth(0.4);
+        renderer.setMaximumBarWidth( 0.4 );
 
         // set up gradient paints for series
-        GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.red,
-                                              0.0f, 0.0f, new Color(64, 0, 0));
-        GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.blue,
-                                              0.0f, 0.0f, new Color(0, 0, 64));
+        GradientPaint gp0 = new GradientPaint( 0.0f, 0.0f, Color.red,
+                                               0.0f, 0.0f, new Color( 64, 0, 0 ) );
+        GradientPaint gp1 = new GradientPaint( 0.0f, 0.0f, Color.blue,
+                                               0.0f, 0.0f, new Color( 0, 0, 64 ) );
 
         // renderer.
-        renderer.setSeriesPaint(0, gp0);
-        renderer.setSeriesPaint(1, gp1);
+        renderer.setSeriesPaint( 0, gp0 );
+        renderer.setSeriesPaint( 1, gp1 );
 
         return chart;
     }
@@ -140,36 +142,37 @@ public class ChartFactory {
      * @param title      a String which represents the title of the specific BarChart
      * @param xAxisTitle a String which represent the x-axis label of the specific XYChart
      * @param yAxisTitle a String which represent the y-axis label of the specific XYChart
+     *
      * @return a JfreeChart XYChart Diagramm
      */
-    public static JFreeChart getXYChart(final XYDataset dataset, final String title, final String xAxisTitle,
-                                        final String yAxisTitle) {
+    public static JFreeChart getXYChart( final XYDataset dataset, final String title, final String xAxisTitle,
+                                         final String yAxisTitle ) {
 
         XYDataset xyDataset = dataset;
 
-        final ValueAxis timeAxis = new DateAxis(xAxisTitle);
-        final NumberAxis valueAxis = new NumberAxis(yAxisTitle);
+        final ValueAxis timeAxis = new DateAxis( xAxisTitle );
+        final NumberAxis valueAxis = new NumberAxis( yAxisTitle );
 
-        valueAxis.setAutoRangeIncludesZero(false);  // override default
+        valueAxis.setAutoRangeIncludesZero( false );  // override default
         final StandardXYItemRenderer renderer =
-                new StandardXYItemRenderer(StandardXYItemRenderer.LINES + StandardXYItemRenderer.SHAPES,
-                                           null, // ToolTip
-                                           null); // URLs
-        renderer.setShapesFilled(true);
+                new StandardXYItemRenderer( StandardXYItemRenderer.LINES + StandardXYItemRenderer.SHAPES,
+                                            null, // ToolTip
+                                            null ); // URLs
+        renderer.setShapesFilled( true );
 
-        final XYPlot plot = new XYPlot(xyDataset, timeAxis, valueAxis, renderer);
-        final JFreeChart xyChart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot,
-                                                  false);
-        xyChart.setBackgroundPaint(new Color(237, 237, 237));
+        final XYPlot plot = new XYPlot( xyDataset, timeAxis, valueAxis, renderer );
+        final JFreeChart xyChart = new JFreeChart( title, JFreeChart.DEFAULT_TITLE_FONT, plot,
+                                                   false );
+        xyChart.setBackgroundPaint( new Color( 237, 237, 237 ) );
 
-        JFreeChart chart = org.jfree.chart.ChartFactory.createXYLineChart(title, // chart title
-                                                                          xAxisTitle, // domain axis label
-                                                                          yAxisTitle, // range axis label
-                                                                          xyDataset, // data
-                                                                          PlotOrientation.VERTICAL, // orientation
-                                                                          true, // include legend
-                                                                          true, // tooltips
-                                                                          true // no URLs
+        JFreeChart chart = org.jfree.chart.ChartFactory.createXYLineChart( title, // chart title
+                                                                           xAxisTitle, // domain axis label
+                                                                           yAxisTitle, // range axis label
+                                                                           xyDataset, // data
+                                                                           PlotOrientation.VERTICAL, // orientation
+                                                                           true, // include legend
+                                                                           true, // tooltips
+                                                                           true // no URLs
         );
         return xyChart;
     }
