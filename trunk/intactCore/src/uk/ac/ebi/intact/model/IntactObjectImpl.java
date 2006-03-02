2974f67ec7abe7935eecea80d4c0798bcf6e99d8
skerrien <samuel.kerrien@gmail.com>
2006-03-02 13:50:49 +0000

    removed useless imports

--- trunk/intactCore/src/uk/ac/ebi/intact/model/IntactObjectImpl.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/IntactObjectImpl.java
@@ -7,8 +7,6 @@ package uk.ac.ebi.intact.model;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
 
 /**
  * This is the top level class for all intact model object.
@@ -17,14 +15,12 @@ import java.util.Date;
  * @version $Id$
  */
 public abstract class IntactObjectImpl implements IntactObject, Serializable,
-        Cloneable {
+                                                  Cloneable {
 
     /**
      * This is used in toString() in order to be platform compatible.
      */
-    protected final String NEW_LINE = System.getProperty("line.separator");
-
-
+    protected final String NEW_LINE = System.getProperty( "line.separator" );
 
     ///////////////////////////////////////
     //attributes
@@ -36,34 +32,27 @@ public abstract class IntactObjectImpl implements IntactObject, Serializable,
 
 
     /**
-     * The unique accession number of an object. This is
-     * defined as protected to allow concrete subclasses to generate
+     * The unique accession number of an object. This is defined as protected to allow concrete subclasses to generate
      * copies if required.
      */
     protected String ac;
 
     /**
-     * Creation date of an object.
-     * The type is java.sql.Date, not java.util.Data,
-     * for database compatibility.
+     * Creation date of an object. The type is java.sql.Date, not java.util.Data, for database compatibility.
      */
     private Timestamp created;
 
     /**
-     * The last update of the object.
-     * The type is java.sql.Date, not java.util.Data,
-     * for database compatibility.
+     * The last update of the object. The type is java.sql.Date, not java.util.Data, for database compatibility.
      */
     private Timestamp updated;
 
     /**
-     * Protected constructor for use by subclasses - used to set
-     * the creation data for instances
+     * Protected constructor for use by subclasses - used to set the creation data for instances
      */
     protected IntactObjectImpl() {
         ojbConcreteClass = this.getClass().getName();
     }
-
 
     ///////////////////////////////////////
     //access methods for attributes
@@ -73,13 +62,14 @@ public abstract class IntactObjectImpl implements IntactObject, Serializable,
     }
 
     /**
-     * This method should not be used by applications, as the AC
-     * is a primary key which is auto-generated. If we move
-     * to an application server it may then be needed.
+     * This method should not be used by applications, as the AC is a primary key which is auto-generated. If we move to
+     * an application server it may then be needed.
+     *
      * @param ac
+     *
      * @deprecated No replacement - should not be used by applications
      */
-    public void setAc(String ac) {
+    public void setAc( String ac ) {
         this.ac = ac;
     }
 
@@ -88,15 +78,14 @@ public abstract class IntactObjectImpl implements IntactObject, Serializable,
     }
 
     /**
-     * <b>Avoid calling this method as this field is set by the DB and it can't
-     * be modifiable via OJB because 'created' field is declared as read-only</b>
+     * <b>Avoid calling this method as this field is set by the DB and it can't be modifiable via OJB because 'created'
+     * field is declared as read-only</b>
      */
-    public void setCreated(java.util.Date created) {
-        if(created != null) {
+    public void setCreated( java.util.Date created ) {
+        if ( created != null ) {
 
-            this.created = new Timestamp(created.getTime());
-        }
-        else {
+            this.created = new Timestamp( created.getTime() );
+        } else {
 
             //needed for object-based search
             this.created = null;
@@ -108,16 +97,15 @@ public abstract class IntactObjectImpl implements IntactObject, Serializable,
     }
 
     /**
-     * <b>Avoid calling this method as this field is set by the DB and it can't
-     * be modifiable via OJB because 'updated' field is declared as read-only</b>
+     * <b>Avoid calling this method as this field is set by the DB and it can't be modifiable via OJB because 'updated'
+     * field is declared as read-only</b>
      */
-    public void setUpdated(java.util.Date updated) {
+    public void setUpdated( java.util.Date updated ) {
 
-        if(updated != null) {
+        if ( updated != null ) {
 
-            this.updated = new Timestamp(updated.getTime());
-        }
-        else {
+            this.updated = new Timestamp( updated.getTime() );
+        } else {
 
             //needed for object-based search
             this.updated = null;
@@ -126,12 +114,13 @@ public abstract class IntactObjectImpl implements IntactObject, Serializable,
 
     /**
      * Makes a clone of this intact object.
+     *
      * @return a cloned version of the current instance.
-     * @throws CloneNotSupportedException to indicate that an instance cannot be
-     * cloned.
+     *
+     * @throws CloneNotSupportedException to indicate that an instance cannot be cloned.
      */
     public Object clone() throws CloneNotSupportedException {
-        IntactObjectImpl copy =  (IntactObjectImpl) super.clone();
+        IntactObjectImpl copy = (IntactObjectImpl) super.clone();
         // Reset the AC.
         copy.ac = null;
 
