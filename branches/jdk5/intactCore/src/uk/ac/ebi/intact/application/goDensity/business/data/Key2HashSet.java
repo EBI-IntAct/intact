4a600a6eb1aad1183c3233f0ef1d14fc7361e2a1
baranda <brunoaranda@gmail.com>
2006-03-06 14:45:24 +0000

    enum is a keyword in JDK5.0

--- branches/jdk5/intactCore/src/uk/ac/ebi/intact/application/goDensity/business/data/Key2HashSet.java
+++ branches/jdk5/intactCore/src/uk/ac/ebi/intact/application/goDensity/business/data/Key2HashSet.java
@@ -135,11 +135,11 @@ public class Key2HashSet {
     public String toString() {
         StringBuffer result = new StringBuffer();
         Hashtable ht = this.getAll();
-        Enumeration enum = ht.keys();
+        Enumeration enumer = ht.keys();
         Collection values = ht.values();
         for (Iterator iterator = values.iterator(); iterator.hasNext();) {
             HashSet o = (HashSet) iterator.next();
-            result.append("\n\nkey: " + enum.nextElement().toString());
+            result.append("\n\nkey: " + enumer.nextElement().toString());
             Iterator col = o.iterator();
             while (col.hasNext()) {
                 Object obj = col.next();
--- branches/jdk5/intactCore/src/uk/ac/ebi/intact/application/goDensity/business/data/MyHashtable.java
+++ branches/jdk5/intactCore/src/uk/ac/ebi/intact/application/goDensity/business/data/MyHashtable.java
@@ -22,11 +22,11 @@ public class MyHashtable extends Hashtable {
 
     public String toString() {
         StringBuffer result = new StringBuffer();
-        Enumeration enum = this.keys();
+        Enumeration enumer = this.keys();
         Collection values = this.values();
         for (Iterator iterator = values.iterator(); iterator.hasNext();) {
             Object o = iterator.next();
-            result.append("key: " + enum.nextElement().toString());
+            result.append("key: " + enumer.nextElement().toString());
             result.append("  ->  value: " + o.toString() + "\n");
         }
         return result.toString();
