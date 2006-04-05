bd66c5db0a48ad21fcc22f6afc0147571dccfe56
skerrien <samuel.kerrien@gmail.com>
2006-04-05 13:18:20 +0000

    more comments, add stats on protein's biosource found per experiment.

--- trunk/intactCore/src/uk/ac/ebi/intact/util/UpdateTargetSpecies.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/UpdateTargetSpecies.java
@@ -5,20 +5,26 @@
  */
 package uk.ac.ebi.intact.util;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.ojb.broker.accesslayer.LookupException;
 import uk.ac.ebi.intact.business.IntactException;
 import uk.ac.ebi.intact.business.IntactHelper;
 import uk.ac.ebi.intact.model.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.*;
 
 /**
- * Update all experiment's target-species Xrefs. <br> Given the fact that an Experiment relates to a set of interaction,
- * and each interaction having a set of Protein, we build a distinct list of interacting Protein's BioSource and for
- * each BioSource, we create at the Experiment level an Xref( primaryId=biosource.taxid,
- * secondaryId=biosource.shortlabel, qualifier=target-species ). <br> That set of Xref is kept up-to-date, by adding
- * missing Xref, and removing those that should no longer exists.
+ * Update all experiment's target-species Xrefs and show stats.
+ * <p/>
+ * Given the fact that an Experiment relates to a set of interaction, and each interaction having a set of Protein, we
+ * build a distinct list of interacting Protein's BioSource and for each BioSource, we create at the Experiment level an
+ * Xref( primaryId=biosource.taxid, secondaryId=biosource.shortlabel, qualifier=target-species ).
+ * <p/>
+ * That set of Xref is kept up-to-date, by adding missing Xref, and removing those that should no longer exists.
  *
  * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
@@ -26,12 +32,61 @@ import java.util.*;
  */
 public class UpdateTargetSpecies {
 
+    //////////////////////////
+    // Inner class
+
+    /**
+     * Contains stats about the count of protein's biosources per experiment.
+     */
+    private static class BioSourceStat {
+        //////////////////////////
+        // Instance variable
+
+        private String name;
+        private String taxid;
+        private int count = 0;
+
+        //////////////////////////
+        // Constructor
+
+        public BioSourceStat( String name, String taxid ) {
+            this.name = name;
+            this.taxid = taxid;
+        }
+
+        ////////////////////
+        // Getters
+        public String getName() {
+            return name;
+        }
+
+        public String getTaxid() {
+            return taxid;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        ///////////////////////
+        // Update stats
+        public void increment() {
+            count++;
+        }
+    }
+
+    //////////////////////////
+    // Instance variable
+
     public static CvTopic noUniprotUpdate = null;
     public static CvDatabase newt = null;
     public static CvXrefQualifier targetSpeciesQualifier = null;
+    private static final String NEW_LINE = System.getProperty( "line.separator" );
 
-    private UpdateTargetSpecies()
-    {
+    ///////////////////////////
+    // Constructor
+
+    private UpdateTargetSpecies() {
         // no instantiable
     }
 
@@ -51,16 +106,15 @@ public class UpdateTargetSpecies {
             throw new IllegalStateException( "The IntAct database should contain a CvTopic( " +
                                              CvTopic.NON_UNIPROT + " ). abort." );
         } else {
-            System.out.println( "CvTopic( " + CvTopic.NON_UNIPROT + " )." );
+            System.out.println( "CvTopic( " + CvTopic.NON_UNIPROT + " ) found." );
         }
 
-
-        newt =  helper.getObjectByPrimaryId( CvDatabase.class, CvDatabase.NEWT_MI_REF );
+        newt = helper.getObjectByPrimaryId( CvDatabase.class, CvDatabase.NEWT_MI_REF );
         if ( newt == null ) {
             throw new IllegalStateException( "The IntAct database should contain a CvDatabase( " + CvDatabase.NEWT +
                                              " ) having an Xref( " + CvDatabase.NEWT_MI_REF + " ). abort." );
         } else {
-            System.out.println( "CvDatabase( " + CvDatabase.NEWT + " )." );
+            System.out.println( "CvDatabase( " + CvDatabase.NEWT + " ) found." );
         }
 
         targetSpeciesQualifier = helper.getObjectByLabel( CvXrefQualifier.class, CvXrefQualifier.TARGET_SPECIES );
@@ -113,11 +167,9 @@ public class UpdateTargetSpecies {
     public static Collection getTargetSpeciesXrefs( Experiment experiment ) {
 
         Collection<Xref> targets = new ArrayList<Xref>();
-        for (Xref xref : experiment.getXrefs())
-        {
-            if (targetSpeciesQualifier.equals(xref.getCvXrefQualifier()))
-            {
-                targets.add(xref);
+        for ( Xref xref : experiment.getXrefs() ) {
+            if ( targetSpeciesQualifier.equals( xref.getCvXrefQualifier() ) ) {
+                targets.add( xref );
             }
         }
         return targets;
@@ -125,7 +177,7 @@ public class UpdateTargetSpecies {
 
 
     /**
-     * D E M O
+     * M A I N
      */
     public static void main( String[] args ) throws IntactException, SQLException, LookupException {
 
@@ -137,16 +189,24 @@ public class UpdateTargetSpecies {
 
             init( helper );
 
+            // get all experiments
             Collection experiments = helper.search( Experiment.class, "shortlabel", "*" );
+
+            Map biosource2count = new HashMap( 4 );
             Set biosources = new HashSet( 4 );
             Set biosourcesTaxid = new HashSet( 4 );
+
+            ArrayList stats = new ArrayList( experiments.size() );
+
             for ( Iterator iterator = experiments.iterator(); iterator.hasNext(); ) {
                 Experiment experiment = (Experiment) iterator.next();
 
                 System.out.println( "Updating " + experiment.getShortLabel() + " (" + experiment.getAc() + ")" );
+
                 // empty collections
                 biosources.clear();
                 biosourcesTaxid.clear();
+                biosource2count.clear();
 
                 // 1. look for distinct list of Protein's biosource
                 for ( Iterator iterator1 = experiment.getInteractions().iterator(); iterator1.hasNext(); ) {
@@ -161,13 +221,24 @@ public class UpdateTargetSpecies {
 
                             // we only take into account UniProt Proteins
                             if ( isFromUniprot( protein ) ) {
-                                biosources.add( protein.getBioSource() );
-                                biosourcesTaxid.add( protein.getBioSource().getTaxId() );
+                                BioSource bioSource = protein.getBioSource();
+
+                                biosources.add( bioSource );
+                                biosourcesTaxid.add( bioSource.getTaxId() );
+
+                                // update stats for each new proteins
+                                BioSourceStat count = (BioSourceStat) biosource2count.get( bioSource );
+
+                                if ( count == null ) {
+                                    count = new BioSourceStat( bioSource.getShortLabel(), bioSource.getTaxId() );
+                                    biosource2count.put( bioSource, count );
+                                }
+
+                                count.increment();
                             }
                         }
                     } // components
                 } // interactions
-
 
                 // 2. process the list of BioSource.
                 Collection existingTargetXrefs = getTargetSpeciesXrefs( experiment );
@@ -181,7 +252,7 @@ public class UpdateTargetSpecies {
                                           targetSpeciesQualifier );
 
                     // add it only if not already there
-                    if ( false == experiment.getXrefs().contains( xref ) ) {
+                    if ( ! experiment.getXrefs().contains( xref ) ) {
                         System.out.println( "\tAdding Xref(" + xref.getPrimaryId() + ", " + xref.getSecondaryId() + ")" );
                         experiment.addXref( xref );
                         helper.create( xref );
@@ -192,7 +263,6 @@ public class UpdateTargetSpecies {
 
                 } // biosources
 
-
                 // 3. remove Xref( target-species ) that should not be there
                 for ( Iterator iterator1 = existingTargetXrefs.iterator(); iterator1.hasNext(); ) {
                     Xref xref = (Xref) iterator1.next();
@@ -202,11 +272,43 @@ public class UpdateTargetSpecies {
                     helper.delete( xref );
                 }
 
-
                 // try to free up some resource.
                 iterator.remove();
 
+                // show stats
+                StringBuffer sb = new StringBuffer( 500 );
+                sb.append( StringUtils.rightPad( experiment.getShortLabel(), 25 ) );
+                for ( Iterator iterator1 = biosource2count.keySet().iterator(); iterator1.hasNext(); ) {
+                    BioSource bioSource = (BioSource) iterator1.next();
+
+                    BioSourceStat stat = (BioSourceStat) biosource2count.get( bioSource );
+                    sb.append( stat.getName() ).append( "(" ).append( stat.getTaxid() ).append( "):" );
+                    sb.append( stat.getCount() );
+
+                    if ( iterator1.hasNext() ) {
+                        sb.append( "  " );
+                    }
+                }
+
+                stats.add( sb.toString() );
+                System.out.println( sb.toString() );
+
+
             } // experiments
+
+            // write stats in a file.
+            try {
+                BufferedWriter out = new BufferedWriter( new FileWriter( "target-species.stat" ) );
+                for ( Iterator iterator = stats.iterator(); iterator.hasNext(); ) {
+                    String line = (String) iterator.next();
+
+                    out.write( line );
+                    out.write( NEW_LINE );
+                }
+
+                out.close();
+            } catch ( IOException e ) {
+            }
 
         } finally {
             if ( helper != null ) {
