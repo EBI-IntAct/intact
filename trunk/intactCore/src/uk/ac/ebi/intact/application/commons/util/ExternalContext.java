75558cca1bd7454825d15ef87ef02ebd379222b8
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-04-11 14:58:17 +0000

    Use log4J instead of commons logging

--- trunk/intactCore/src/uk/ac/ebi/intact/application/commons/util/ExternalContext.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/commons/util/ExternalContext.java
@@ -5,67 +5,60 @@
  */
 package uk.ac.ebi.intact.application.commons.util;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 
 import javax.servlet.ServletContext;
 
 /**
- * This "singleton" (with ThreadLocal pattern) contains information about the application context. It is
- * a convenience class to get the ServletContext from any class, without having to use the session or passing
- * the class around
+ * This "singleton" (with ThreadLocal pattern) contains information about the application context. It is a convenience
+ * class to get the ServletContext from any class, without having to use the session or passing the class around
  *
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  * @since <pre>28-Mar-2006</pre>
  */
-public class ExternalContext
-{
+public class ExternalContext {
 
-    private static final Log log = LogFactory.getLog(ExternalContext.class);
+    public static final Logger log = Logger.getLogger( ExternalContext.class );
+
 
     private ServletContext servletContext;
 
-    protected ExternalContext(ServletContext servletContext)
-    {
+    protected ExternalContext( ServletContext servletContext ) {
         this.servletContext = servletContext;
     }
 
-    private static ThreadLocal<ExternalContext> currentInstance = new ThreadLocal()
-    {
+    private static ThreadLocal<ExternalContext> currentInstance = new ThreadLocal() {
         // the initial value of the CvContext is null
-        protected ExternalContext initialValue()
-        {
+        protected ExternalContext initialValue() {
             return null;
         }
 
     };
 
-    public static ExternalContext newInstance(ServletContext servletContext)
-    {
-        ExternalContext externalContext = new ExternalContext(servletContext);
-        currentInstance.set(externalContext);
+    public static ExternalContext newInstance( ServletContext servletContext ) {
+        ExternalContext externalContext = new ExternalContext( servletContext );
+        currentInstance.set( externalContext );
 
         return externalContext;
     }
 
-    public static ExternalContext getCurrentInstance()
-    {
+    public static ExternalContext getCurrentInstance() {
         ExternalContext context = currentInstance.get();
 
-        if (context == null)
-        {
-            context = new ExternalContext(new DummyServletContext());
-            currentInstance.set(context);
+        if ( context == null ) {
+            context = new ExternalContext( new DummyServletContext() );
+            currentInstance.set( context );
 
-            if (log.isWarnEnabled())
-                log.warn("Created ExternalContext not using ServletContext");
+            if ( log.isEnabledFor( Priority.WARN ) ) {
+                log.warn( "Created ExternalContext not using ServletContext" );
+            }
         }
         return context;
     }
 
-    public ServletContext getServletContext()
-    {
+    public ServletContext getServletContext() {
         return servletContext;
     }
 }
