2f899f3e0eda952b16bd1300d50c42fdd761c24e
skerrien <samuel.kerrien@gmail.com>
2006-04-11 15:16:37 +0000

    Added @Deprecated on deprecated method to avoid warning at compile time

--- trunk/intactCore/src/uk/ac/ebi/intact/application/commons/util/DummyServletContext.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/application/commons/util/DummyServletContext.java
@@ -5,143 +5,126 @@
  */
 package uk.ac.ebi.intact.application.commons.util;
 
-import javax.servlet.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import java.io.InputStream;
-import java.util.*;
-import java.net.URL;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * Dummy Servlet Context to be able to run the testfill.sh script without using a web app.
- * Only setAttribute and getAttribute are implemented. The other methods throw an
- * <code>UnsupportedOperationException</code>
+ * Dummy Servlet Context to be able to run the testfill.sh script without using a web app. Only setAttribute and
+ * getAttribute are implemented. The other methods throw an <code>UnsupportedOperationException</code>
  *
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  * @since <pre>03-Apr-2006</pre>
  */
-public class DummyServletContext implements javax.servlet.ServletContext
-{
-    private Map<String,Object> attMap = new HashMap<String,Object>();
+public class DummyServletContext implements javax.servlet.ServletContext {
+    private Map<String, Object> attMap = new HashMap<String, Object>();
 
-    public ServletContext getContext(String string)
-    {
+    public ServletContext getContext( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public int getMajorVersion()
-    {
+    public int getMajorVersion() {
         throw new UnsupportedOperationException();
     }
 
-    public int getMinorVersion()
-    {
+    public int getMinorVersion() {
         throw new UnsupportedOperationException();
     }
 
-    public String getMimeType(String string)
-    {
+    public String getMimeType( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public Set getResourcePaths(String string)
-    {
+    public Set getResourcePaths( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public URL getResource(String string) throws MalformedURLException
-    {
+    public URL getResource( String string ) throws MalformedURLException {
         throw new UnsupportedOperationException();
     }
 
-    public InputStream getResourceAsStream(String string)
-    {
+    public InputStream getResourceAsStream( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public RequestDispatcher getRequestDispatcher(String string)
-    {
+    public RequestDispatcher getRequestDispatcher( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public RequestDispatcher getNamedDispatcher(String string)
-    {
+    public RequestDispatcher getNamedDispatcher( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public Servlet getServlet(String string) throws ServletException
-    {
+    @Deprecated
+    public Servlet getServlet( String string ) throws ServletException {
         throw new UnsupportedOperationException();
     }
 
-    public Enumeration getServlets()
-    {
+    @Deprecated
+    public Enumeration getServlets() {
         throw new UnsupportedOperationException();
     }
 
-    public Enumeration getServletNames()
-    {
+    @Deprecated
+    public Enumeration getServletNames() {
         throw new UnsupportedOperationException();
     }
 
-    public void log(String string)
-    {
+    public void log( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public void log(Exception exception, String string)
-    {
+    @Deprecated
+    public void log( Exception exception, String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public void log(String string, Throwable throwable)
-    {
+    public void log( String string, Throwable throwable ) {
         throw new UnsupportedOperationException();
     }
 
-    public String getRealPath(String string)
-    {
+    public String getRealPath( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public String getServerInfo()
-    {
+    public String getServerInfo() {
         throw new UnsupportedOperationException();
     }
 
-    public String getInitParameter(String string)
-    {
+    public String getInitParameter( String string ) {
         throw new UnsupportedOperationException();
     }
 
-    public Enumeration getInitParameterNames()
-    {
+    public Enumeration getInitParameterNames() {
         throw new UnsupportedOperationException();
     }
 
-    public Object getAttribute(String string)
-    {
-        return attMap.get(string);
+    public Object getAttribute( String string ) {
+        return attMap.get( string );
     }
 
-    public Enumeration getAttributeNames()
-    {
+    public Enumeration getAttributeNames() {
         throw new UnsupportedOperationException();
     }
 
-    public void setAttribute(String string, Object object)
-    {
-         attMap.put(string,object);
+    public void setAttribute( String string, Object object ) {
+        attMap.put( string, object );
     }
 
-    public void removeAttribute(String string)
-    {
-        attMap.remove(string);
+    public void removeAttribute( String string ) {
+        attMap.remove( string );
     }
 
-    public String getServletContextName()
-    {
+    public String getServletContextName() {
         throw new UnsupportedOperationException();
     }
 }
