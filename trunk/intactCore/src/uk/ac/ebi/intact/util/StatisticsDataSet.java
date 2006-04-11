ee630d7e43890c785b482fc6ee7fb39c68c75741
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-04-11 15:20:44 +0000

    Reformating + added documentation

--- trunk/intactCore/src/uk/ac/ebi/intact/util/StatisticsDataSet.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/StatisticsDataSet.java
@@ -14,20 +14,26 @@ in the root directory of this distribution.
  */
 package uk.ac.ebi.intact.util;
 
-import uk.ac.ebi.intact.business.IntactHelper;
-import uk.ac.ebi.intact.business.IntactException;
-import uk.ac.ebi.intact.application.statisticView.business.data.StatisticsBean;
-import uk.ac.ebi.intact.application.statisticView.business.data.NoDataException;
-
 import org.apache.log4j.Logger;
 import org.apache.ojb.broker.accesslayer.LookupException;
+import uk.ac.ebi.intact.application.statisticView.business.data.NoDataException;
+import uk.ac.ebi.intact.application.statisticView.business.data.StatisticsBean;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.business.IntactHelper;
 
-import java.util.*;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-
+/**
+ * Holds statistics data.
+ *
+ * @author Samuel Kerrien
+ */
 public final class StatisticsDataSet {
 
     ////////////////////////////////
@@ -39,9 +45,7 @@ public final class StatisticsDataSet {
 
     public static final long MAXIMUM_DATA_AVAILABILITY = 1000 * 60 * 60 * 24; // one days in ms.
 
-    public static final SimpleDateFormat dateFormater = new SimpleDateFormat ( "dd-MMM-yyyy" );
-
-
+    public static final SimpleDateFormat dateFormater = new SimpleDateFormat( "dd-MMM-yyyy" );
 
     ///////////////////////////////
     // Instance data
@@ -73,8 +77,6 @@ public final class StatisticsDataSet {
      */
     private static long timestamp = DATA_NOT_LOADED;
 
-
-
     ///////////////////////////////
     // Instanciation methods
     ///////////////////////////////
@@ -82,13 +84,13 @@ public final class StatisticsDataSet {
     public synchronized static StatisticsDataSet getInstance( final String loggerName ) {
 
         if ( ourInstance == null ) {
-            ourInstance = new StatisticsDataSet ( loggerName);
+            ourInstance = new StatisticsDataSet( loggerName );
         } else {
-            if (dataOutDated()) {
+            if ( dataOutDated() ) {
                 try {
-                    collectStatistics( );
+                    collectStatistics();
                 } catch ( IntactException e ) {
-                    logger.error ( "Error when trying to refresh the existing data.", e );
+                    logger.error( "Error when trying to refresh the existing data.", e );
                 }
             } else {
                 logger.info( "Data are not out dated, reuse them." );
@@ -98,25 +100,23 @@ public final class StatisticsDataSet {
         return ourInstance;
     }
 
-    public synchronized static StatisticsDataSet getInstance () {
+    public synchronized static StatisticsDataSet getInstance() {
 
         return getInstance( StatisticsDataSet.class.getName() );
     }
 
-    private StatisticsDataSet ( final String loggerName ) {
+    private StatisticsDataSet( final String loggerName ) {
 
         // get the logger
         logger = Logger.getLogger( loggerName );
 
         // collect all statistic data from IntAct
         try {
-            collectStatistics( );
+            collectStatistics();
         } catch ( IntactException e ) {
-            logger.error ( "Error when trying to collect the data.", e );
+            logger.error( "Error when trying to collect the data.", e );
         }
     }
-
-
 
     ///////////////////////////////
     // Data collection
@@ -126,21 +126,20 @@ public final class StatisticsDataSet {
 
         final long currentTime = System.currentTimeMillis();
         final boolean outdated = ( ( timestamp + MAXIMUM_DATA_AVAILABILITY ) < currentTime );
-        logger.info ( "Data out dated: " + outdated );
+        logger.info( "Data out dated: " + outdated );
         return outdated;
     }
 
     /**
      * This private method is called by the others public method of this class.
-     *
-     * Get all data of the IA_Statistics table in the IntAct database, thanks to the search method
-     * managing an IntactHelper object.
-     * The null parameter in the search method means to retrieve all the data from a table via OJB
+     * <p/>
+     * Get all data of the IA_Statistics table in the IntAct database, thanks to the search method managing an
+     * IntactHelper object. The null parameter in the search method means to retrieve all the data from a table via OJB
      * and JDBC.
      */
-    private static void collectStatistics ( ) throws IntactException {
+    private static void collectStatistics() throws IntactException {
 
-        logger.info("retreiving all statistics...");
+        logger.info( "retreiving all statistics..." );
         try {
             logger.info( "creating IntactHelper..." );
             final IntactHelper helper = new IntactHelper();
@@ -148,11 +147,11 @@ public final class StatisticsDataSet {
             try {
                 userName = helper.getDbUserName();
                 databaseName = helper.getDbName();
-                logger.info("Helper created - access to database " + databaseName + " as " + userName );
+                logger.info( "Helper created - access to database " + databaseName + " as " + userName );
             } catch ( LookupException e ) {
-                logger.error("Error when trying to get the database and username.", e);
+                logger.error( "Error when trying to get the database and username.", e );
             } catch ( SQLException e ) {
-                logger.error("Error when trying to get the database and username.", e);
+                logger.error( "Error when trying to get the database and username.", e );
             }
 
             // search method to get the IntactStatistics object and all statistics in IntAct
@@ -167,36 +166,33 @@ public final class StatisticsDataSet {
             // keep track of the time.
             timestamp = System.currentTimeMillis();
 
-            logger.info ("Content of the table before sorting");
-            for (IntactStatistics singleItem : intactStatistics)
-            {
-                logger.info(singleItem);
+            logger.info( "Content of the table before sorting" );
+            for ( IntactStatistics singleItem : intactStatistics ) {
+                logger.info( singleItem );
             }
 
 
-            if (intactStatistics.size() > 0) {
+            if ( intactStatistics.size() > 0 ) {
                 statistics = new ArrayList<IntactStatistics>( intactStatistics );
-                Collections.sort(statistics);
+                Collections.sort( statistics );
 
             } else {
                 // empty collection.
                 statistics = Collections.EMPTY_LIST;
             }
 
-            logger.info ("Content of the table after sorting");
-            for (IntactStatistics singleItem : statistics)
-            {
-                logger.info(singleItem);
+            logger.info( "Content of the table after sorting" );
+            for ( IntactStatistics singleItem : statistics ) {
+                logger.info( singleItem );
             }
         }
-        catch (IntactException ie) {
-            if (null != logger)
-                logger.error ("when trying to get all statistics, cause: " + ie.getRootCause(), ie);
+        catch ( IntactException ie ) {
+            if ( null != logger ) {
+                logger.error( "when trying to get all statistics, cause: " + ie.getRootCause(), ie );
+            }
             throw ie;
         }
     }
-
-
 
     //////////////////////////////////
     // Data access
@@ -209,18 +205,16 @@ public final class StatisticsDataSet {
      */
     public final synchronized StatisticsBean getStatisticBean() throws NoDataException {
 
-        return getStatisticBean (null, null);
+        return getStatisticBean( null, null );
     }
 
     /**
      * Return the data collected, this is the only public method.
      *
-     * @param startDate the date from which we want to display the statistics.
-     *        null indicated that there is no start date.
-     *        That date has to respect the format defined by the SimpleDateFormater (DD-MMM-YYYY)
-     * @param stopDate the date until which we want to display the statistics.
-     *        null indicated that there is no end date.
-     *        That date has to respect the format defined by the SimpleDateFormater (DD-MMM-YYYY)
+     * @param startDate the date from which we want to display the statistics. null indicated that there is no start
+     *                  date. That date has to respect the format defined by the SimpleDateFormater (DD-MMM-YYYY)
+     * @param stopDate  the date until which we want to display the statistics. null indicated that there is no end
+     *                  date. That date has to respect the format defined by the SimpleDateFormater (DD-MMM-YYYY)
      *
      * @return a bean containing the statistics and the database source details.
      */
@@ -228,62 +222,56 @@ public final class StatisticsDataSet {
                                                                final String stopDate )
             throws NoDataException {
 
-        if (statistics == null || statistics.size() == 0)
+        if ( statistics == null || statistics.size() == 0 ) {
             throw new NoDataException();
+        }
 
         long start = NONE;
         if ( startDate != null ) {
             try {
                 start = dateFormater.parse( startDate ).getTime();
             } catch ( ParseException e ) {
-                e.printStackTrace ();
+                e.printStackTrace();
             }
         }
 
         long stop = NONE;
         if ( stopDate != null ) {
             try {
-                stop = dateFormater.parse(stopDate).getTime();
+                stop = dateFormater.parse( stopDate ).getTime();
             } catch ( ParseException e ) {
-                e.printStackTrace ();
+                e.printStackTrace();
             }
         }
 
         if ( start != NONE || stop != NONE ) {
             // filter
             final List<IntactStatistics> filteredStatistics = new ArrayList<IntactStatistics>();
-            for (IntactStatistics statistic : statistics)
-            {
+            for ( IntactStatistics statistic : statistics ) {
                 final long timestamp = statistic.getTimestamp().getTime();
                 boolean keepIt = true;
 
-                if (start != NONE)
-                {
-                    if (timestamp < start)
-                    {
+                if ( start != NONE ) {
+                    if ( timestamp < start ) {
                         keepIt = false;
                     }
                 }
 
-                if (stop != NONE)
-                {
-                    if (timestamp > stop)
-                    {
+                if ( stop != NONE ) {
+                    if ( timestamp > stop ) {
                         keepIt = false;
                     }
                 }
 
-                if (keepIt)
-                {
-                    filteredStatistics.add(statistic);
+                if ( keepIt ) {
+                    filteredStatistics.add( statistic );
                 }
             } // for
 
-        return new StatisticsBean( filteredStatistics, databaseName, userName );
+            return new StatisticsBean( filteredStatistics, databaseName, userName );
+        }
+
+        // full dataset
+        return new StatisticsBean( statistics, databaseName, userName );
     }
-
-    // full dataset
-    return new StatisticsBean( statistics, databaseName, userName );
 }
-}
-
