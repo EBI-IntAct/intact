7184956b5f330abb94aa899a6a68fc291e5982cc
skerrien <samuel.kerrien@gmail.com>
2006-05-18 08:16:25 +0000

    Migration scripts to schema version 1.2.0

---
+++ trunk/intactCore/sql/oracle/version_1_2_0/setup_tablespaces.sql
@@ -0,0 +1,8 @@
+------------------------------------
+-- Variables used across DDL files
+------------------------------------
+
+DEFINE intactMainTablespace         = INTACT_TAB
+DEFINE intactIndexTablespace        = INTACT_IDX
+DEFINE intactAuditTablespace        = INTACT_TAB
+DEFINE spoolFile                    = ddl.sql
