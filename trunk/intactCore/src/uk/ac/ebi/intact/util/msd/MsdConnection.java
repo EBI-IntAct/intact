0eb6e49862fe0629dc98aeedd3654d97da70b708
catherineleroy <catherineleroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-05-18 10:48:55 +0000

    Modif of the connection management

--- trunk/intactCore/src/uk/ac/ebi/intact/util/msd/MsdConnection.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/util/msd/MsdConnection.java
@@ -15,8 +15,16 @@ import java.sql.SQLException;
  */
 final class MsdConnection  {
 
+    private static Connection conn;
+
+    /**
+     * String containing the driver class name.
+     */
     private static  String DRIVER_NAME;
-    private static  String DB_NAME;
+    /**
+     * String
+     */
+    private static  String DB_URL;
     private static  String USER_NAME;
     private static  String USER_PASSWORD;
 
@@ -26,28 +34,44 @@ final class MsdConnection  {
      * @throws IntactException  if the config file is not found  or if one of the properties
      * is not found by MsdPropertyLoader.
      */
-    static Connection getMsdConnection() throws IntactException {
+    public static Connection getMsdConnection() throws IntactException {
 
-        MsdPropertyLoader msdPropertyLoader = new MsdPropertyLoader();
-        DRIVER_NAME =msdPropertyLoader.getDriverName();
-        DB_NAME =msdPropertyLoader.getDbName();
-        USER_NAME =msdPropertyLoader.getUserName();
-        USER_PASSWORD =msdPropertyLoader.getUserPassword();
+        if(conn == null) {
 
-        Connection result = null;
-        try{
-            Class.forName(DRIVER_NAME).newInstance();
+            MsdPropertyLoader msdPropertyLoader = new MsdPropertyLoader();
+            DRIVER_NAME =msdPropertyLoader.getDriverName();
+            DB_URL =msdPropertyLoader.getDbUrl();
+            USER_NAME =msdPropertyLoader.getUserName();
+            USER_PASSWORD =msdPropertyLoader.getUserPassword();
+
+
+            try{
+                Class.forName(DRIVER_NAME).newInstance();
+            }
+            catch (Exception ex){
+                // We don't want to continue, if the connection to the database can't work so just throw an Exception.
+                throw new IntactException("Problem instanciating " + DRIVER_NAME + " the msd Connection can be created : ", ex);
+            }
+            try{
+                conn = DriverManager.getConnection(DB_URL, USER_NAME, USER_PASSWORD);
+            }
+            catch (SQLException ex){
+                // We don't want to continue, if the connection to the database can't work so just throw an Exception.
+                throw new IntactException ("Problem getting the connection to: " + DB_URL, ex);
+            }
         }
-        catch (Exception ex){
-            System.out.println("Check claspath. Cannot load db driver: "+ DRIVER_NAME);
+        return conn;
+    }
+
+    public static void closeConnection() throws IntactException {
+        if( conn != null ){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new IntactException("Problem trying to close the connection to " + DB_URL + " : ", e);
+            }
+            conn = null;
         }
-        try{
-            result = DriverManager.getConnection(DB_NAME, USER_NAME, USER_PASSWORD);
-        }
-        catch (SQLException ex){
-            System.err.println("Driver loaded, but cannot connect to db:" + DB_NAME);
-        }
-        return result;
-}
+    }
 
 }
