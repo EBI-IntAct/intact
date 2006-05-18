e500cea4b8db5b287fea784d2128352bf04f7ad0
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-05-18 11:15:00 +0000

    table ia_pub2exp wasn't necessary, adding a extra column in ia_experiment is enough.

--- trunk/intactCore/sql/oracle/version_1_2_0/update_privileges.sql
+++ trunk/intactCore/sql/oracle/version_1_2_0/update_privileges.sql
@@ -7,11 +7,6 @@ PROMPT Creating table "Updating rights on IA_Publication"
 grant select on IA_Publication to INTACT_SELECT;
 grant select, insert,update,delete on IA_Publication to INTACT_CURATOR;
 
--- IA_Pub2Exp
-PROMPT Creating table "Updating rights on IA_Pub2Exp"
-grant select on IA_Pub2Exp to INTACT_SELECT;
-grant select, insert,update,delete on IA_Pub2Exp to INTACT_CURATOR;
-
 -- IA_Pub2Annot
 PROMPT Creating table "Updating rights on IA_Pub2Annot"
 grant select on IA_Pub2Annot to INTACT_SELECT;
@@ -32,14 +27,12 @@ PROMPT Creating table "Updating rights on IA_Publication_audit"
 grant select on IA_Publication_audit to INTACT_SELECT;
 grant select, insert,update,delete on IA_Publication_audit to INTACT_CURATOR;
 
+-- IA_Pub2Annot_audit
 PROMPT Creating table "Updating rights on IA_Pub2Annot_audit"
 grant select on IA_Pub2Annot_audit to INTACT_SELECT;
 grant select, insert,update,delete on IA_Pub2Annot_audit to INTACT_CURATOR;
 
-PROMPT Creating table "Updating rights on IA_Pub2Exp_audit"
-grant select on IA_Pub2Exp_audit to INTACT_SELECT;
-grant select, insert,update,delete on IA_Pub2Exp_audit to INTACT_CURATOR;
-
+-- IA_Key_Assigner_Request_audit
 PROMPT Creating table "Updating rights on IA_Key_Assigner_Request_audit"
 grant select on IA_Key_Assigner_Request_audit to INTACT_SELECT;
 grant select, insert,update,delete on IA_Key_Assigner_Request_audit to INTACT_CURATOR;
