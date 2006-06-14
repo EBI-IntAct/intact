e42824fb2b9b09e47da836b21385a66511d03f1d
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-06-14 09:21:42 +0000

    Initialise collection of features.

--- trunk/intactCore/src/uk/ac/ebi/intact/model/Component.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/Component.java
@@ -5,28 +5,17 @@ in the root directory of this distribution.
 */
 package uk.ac.ebi.intact.model;
 
-import uk.ac.ebi.intact.model.proxy.InteractorProxy;
 import uk.ac.ebi.intact.model.proxy.InteractionProxy;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Column;
-import javax.persistence.FetchType;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 /**
- * The specific instance of an interactor which
- * participates in an interaction.
- *
- * The same interactor may participate more than once,
- * for example to describe different roles of the Interactors.
- * However, simple multimers should be expressed by
- * the relativeQuantity attribute.
+ * The specific instance of an interactor which participates in an interaction.
+ * <p/>
+ * The same interactor may participate more than once, for example to describe different roles of the Interactors.
+ * However, simple multimers should be expressed by the relativeQuantity attribute.
  *
  * @author hhe
  * @version $Id$
@@ -47,10 +36,9 @@ public class Component extends BasicObjectImpl {
 
 
     /**
-     * Represents the relative quantitity of the interactor
-     * participating in the interaction. Default is one.
-     * To describe for example a homodimer, an interaction might have
-     * only one substrate, but the relative quantity would be 2.
+     * Represents the relative quantitity of the interactor participating in the interaction. Default is one. To
+     * describe for example a homodimer, an interaction might have only one substrate, but the relative quantity would
+     * be 2.
      */
     private float stoichiometry = 1;
 
@@ -75,7 +63,7 @@ public class Component extends BasicObjectImpl {
     /**
      * The domain a Substrate binds by.
      */
-    private Collection<Feature> bindingDomains;
+    private Collection<Feature> bindingDomains = new ArrayList<Feature>();
 
     /**
      * TODO comments
@@ -83,10 +71,8 @@ public class Component extends BasicObjectImpl {
     private CvComponentRole cvComponentRole;
 
     /**
-     * This constructor should <b>not</b> be used as it could
-     * result in objects with invalid state. It is here for object mapping
-     * purposes only.
-     * <p/>Made the constructor protected to allow access for subclasses.
+     * This constructor should <b>not</b> be used as it could result in objects with invalid state. It is here for
+     * object mapping purposes only. <p/>Made the constructor protected to allow access for subclasses.
      */
     public Component() {
         //super call sets creation time data
@@ -94,35 +80,38 @@ public class Component extends BasicObjectImpl {
     }
 
     /**
-     * Creates a valid Component instance. To be valid, a Component must have at least:
-     * <ul>
-     * <li>An onwer (Institution)</li>
-     * <li>a biological source that the interaaction was expressed in</li>
-     * <li>an Interaction that this instance is a Component of</li>
-     * <li>an Interactor which defines the entity (eg Protein) which takes part in the
-     * Interaction and is therefore the 'core' of this Component</li>
-     * <li>the biological role that this Component plays in the Interaction (eg bait/prey etc)</li>
-     * </ul>
-     * <p>
-     * A side-effect of this constructor is to
-     * set the <code>created</code> and <code>updated</code> fields of the instance
-     * to the current time.
-     * @param owner The Institution owner of this Component (non-null)
+     * Creates a valid Component instance. To be valid, a Component must have at least: <ul> <li>An onwer
+     * (Institution)</li> <li>a biological source that the interaaction was expressed in</li> <li>an Interaction that
+     * this instance is a Component of</li> <li>an Interactor which defines the entity (eg Protein) which takes part in
+     * the Interaction and is therefore the 'core' of this Component</li> <li>the biological role that this Component
+     * plays in the Interaction (eg bait/prey etc)</li> </ul>
+     * <p/>
+     * A side-effect of this constructor is to set the <code>created</code> and <code>updated</code> fields of the
+     * instance to the current time.
+     *
+     * @param owner       The Institution owner of this Component (non-null)
      * @param interaction The Interaction this Component is a part of (non-null)
-     * @param interactor The 'wrapped active entity' (eg a Protein) that this Component represents
-     * in the Interaction (non-null)
-     * @param role The biological/experimental role played by this Component in the Interaction
-     * experiment (eg bait/prey). This is a controlled vocabulary term (non-null)
-     * @exception NullPointerException thrown if any of the parameters are not specified.
+     * @param interactor  The 'wrapped active entity' (eg a Protein) that this Component represents in the Interaction
+     *                    (non-null)
+     * @param role        The biological/experimental role played by this Component in the Interaction experiment (eg
+     *                    bait/prey). This is a controlled vocabulary term (non-null)
+     *
+     * @throws NullPointerException thrown if any of the parameters are not specified.
      */
-    public Component(Institution owner, Interaction interaction,
-                     Interactor interactor, CvComponentRole role) {
+    public Component( Institution owner, Interaction interaction,
+                      Interactor interactor, CvComponentRole role ) {
 
         //super call sets creation time data
-        super (owner);
-        if(interaction == null) throw new NullPointerException("valid Component must have an Interaction set!");
-        if(interactor == null) throw new NullPointerException("valid Component must have an Interactor (eg Protein) set!");
-        if(role == null) throw new NullPointerException("valid Component must have a role set (ie a CvComponentRole)!");
+        super( owner );
+        if ( interaction == null ) {
+            throw new NullPointerException( "valid Component must have an Interaction set!" );
+        }
+        if ( interactor == null ) {
+            throw new NullPointerException( "valid Component must have an Interactor (eg Protein) set!" );
+        }
+        if ( role == null ) {
+            throw new NullPointerException( "valid Component must have a role set (ie a CvComponentRole)!" );
+        }
 
 //        this.interaction = interaction;
 //        this.interactor = interactor;
@@ -133,31 +122,32 @@ public class Component extends BasicObjectImpl {
         this.cvComponentRole = role;
     }
 
-
     ///////////////////////////////////////
     //access methods for attributes
 
     public float getStoichiometry() {
         return stoichiometry;
     }
-    public void setStoichiometry(float stoichiometry) {
+
+    public void setStoichiometry( float stoichiometry ) {
         this.stoichiometry = stoichiometry;
     }
 
-    @ManyToOne (fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "expressedin_ac")
     public BioSource getExpressedIn() {
         return expressedIn;
     }
-    public void setExpressedIn(BioSource expressedIn) {
+
+    public void setExpressedIn( BioSource expressedIn ) {
         this.expressedIn = expressedIn;
     }
 
     ///////////////////////////////////////
     // access methods for associations
 
-    @ManyToOne (targetEntity = InteractorImpl.class)
-    @JoinColumn (name = "interactor_ac")
+    @ManyToOne(targetEntity = InteractorImpl.class)
+    @JoinColumn(name = "interactor_ac")
     public Interactor getInteractor() {
 
         //Need to check for Proxies - callers should
@@ -174,9 +164,10 @@ public class Component extends BasicObjectImpl {
 
     /**
      * TODO document that non obvious method
+     *
      * @param interactor
      */
-    public void setInteractor(Interactor interactor) {
+    public void setInteractor( Interactor interactor ) {
         // TODO (BA) IMPORTANT: I have just commented this method at seems to provoke an illegal access
         // exception to the list of components
         /*
@@ -188,22 +179,22 @@ public class Component extends BasicObjectImpl {
         this.interactor = interactor;
     }
 
-    @ManyToOne (targetEntity = InteractionImpl.class, fetch = FetchType.LAZY)
-    @JoinColumn (name = "interaction_ac")
+    @ManyToOne(targetEntity = InteractionImpl.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "interaction_ac")
     public Interaction getInteraction() {
 
         //Need to check for Proxies - callers should
         //not expect a Proxy back since Component only holds
         //a single interaction, hence no need for a Proxy
-        if((interaction !=  null) & (interaction instanceof InteractionProxy)){
-            InteractionProxy proxy = (InteractionProxy)interaction;
-            return (Interaction)proxy.getRealSubject();
+        if ( ( interaction != null ) & ( interaction instanceof InteractionProxy ) ) {
+            InteractionProxy proxy = (InteractionProxy) interaction;
+            return (Interaction) proxy.getRealSubject();
         }
         return interaction;
     }
 
     // TODO document that non obvious method
-    public void setInteraction(Interaction interaction) {
+    public void setInteraction( Interaction interaction ) {
         // TODO (BA) IMPORTANT: I have just commented this method at seems to provoke an illegal access
         // exception to the list of components
         /*
@@ -215,48 +206,61 @@ public class Component extends BasicObjectImpl {
         this.interaction = interaction;
     }
 
-    public void setBindingDomains(Collection someBindingDomain) {
+    public void setBindingDomains( Collection<Feature> someBindingDomain ) {
+        if ( someBindingDomain == null ) {
+            throw new IllegalArgumentException( "features cannot be null." );
+        }
         this.bindingDomains = someBindingDomain;
     }
 
-    @OneToMany (mappedBy = "component")
+    @OneToMany(mappedBy = "component")
     public Collection<Feature> getBindingDomains() {
         return bindingDomains;
     }
-    public void addBindingDomain(Feature feature) {
-        if (! this.bindingDomains.contains(feature)) {
-            this.bindingDomains.add(feature);
-            feature.setComponent(this);
+
+    public void addBindingDomain( Feature feature ) {
+        if ( ! this.bindingDomains.contains( feature ) ) {
+            this.bindingDomains.add( feature );
+            feature.setComponent( this );
         }
     }
-    public void removeBindingDomain(Feature feature) {
-        boolean removed = this.bindingDomains.remove(feature);
-        if (removed) feature.setComponent(null);
+
+    public void removeBindingDomain( Feature feature ) {
+        boolean removed = this.bindingDomains.remove( feature );
+        if ( removed ) {
+            feature.setComponent( null );
+        }
     }
 
-    @ManyToOne (fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role")
     public CvComponentRole getCvComponentRole() {
         return cvComponentRole;
     }
-    public void setCvComponentRole(CvComponentRole cvComponentRole) {
+
+    public void setCvComponentRole( CvComponentRole cvComponentRole ) {
         this.cvComponentRole = cvComponentRole;
     }
 
     // instance methods
 
     /**
-     * Equality for Components is currently based on <b>object identity</b>
-     * (i.e. the references point to the same objects) for
-     * Interactors, Interactions and CvComponentRoles.
+     * Equality for Components is currently based on <b>object identity</b> (i.e. the references point to the same
+     * objects) for Interactors, Interactions and CvComponentRoles.
+     *
      * @param o The object to check
+     *
      * @return true if the parameter equals this object, false otherwise
      */
     @Override
-    public boolean equals(Object o){
+    public boolean equals( Object o ) {
         /* TODO: Take features into account when they are implemented. */
-        if (this == o) return true;
-        if (!(o instanceof Component)) return false;
+        if ( this == o ) {
+            return true;
+        }
+        if ( !( o instanceof Component ) ) {
+            return false;
+        }
         // don't call super because that's a BasicObject !
 
         final Component component = (Component) o;
@@ -269,28 +273,37 @@ public class Component extends BasicObjectImpl {
                this.cvComponentRole == component.cvComponentRole;
     }
 
-    /** This class overwrites equals. To ensure proper functioning of HashTable,
-     * hashCode must be overwritten, too.
-     * @return  hash code of the object.
+    /**
+     * This class overwrites equals. To ensure proper functioning of HashTable, hashCode must be overwritten, too.
+     *
+     * @return hash code of the object.
      */
     @Override
-    public int hashCode(){
+    public int hashCode() {
         /* TODO: Take features into account when they are implemented. */
         int code = 29;
 
         //need these checks because we still have a no-arg
         //constructor at the moment.....
-        if(interactor != null) code = code * 29 + getInteractor().hashCode();
-        if(interaction != null) code = code * 29 +  getInteractor().hashCode();
-        if(cvComponentRole != null) code = code * 29 +  cvComponentRole.hashCode();
+        if ( interactor != null ) {
+            code = code * 29 + getInteractor().hashCode();
+        }
+        if ( interaction != null ) {
+            code = code * 29 + getInteractor().hashCode();
+        }
+        if ( cvComponentRole != null ) {
+            code = code * 29 + cvComponentRole.hashCode();
+        }
 
         return code;
     }
 
     /**
      * Returns a cloned version of the current Component.
-     * @return a cloned version of the current Component. References to
-     * interactor and interaction are set to null. Features are deep cloned.
+     *
+     * @return a cloned version of the current Component. References to interactor and interaction are set to null.
+     *         Features are deep cloned.
+     *
      * @throws CloneNotSupportedException
      */
     @Override
@@ -302,14 +315,13 @@ public class Component extends BasicObjectImpl {
         copy.interactor = null;
 
         // Make deep copies of Features.
-        copy.bindingDomains = new ArrayList<Feature>(bindingDomains.size());
-        for (Feature feature : bindingDomains)
-        {
+        copy.bindingDomains = new ArrayList<Feature>( bindingDomains.size() );
+        for ( Feature feature : bindingDomains ) {
             Feature copyFeature = (Feature) feature.clone();
             // Set the copy component as the component for feature copy.
-            copyFeature.setComponentForClone(copy);
+            copyFeature.setComponentForClone( copy );
             // Add the cloned feature to the binding domains.
-            copy.bindingDomains.add(copyFeature);
+            copy.bindingDomains.add( copyFeature );
         }
         return copy;
     }
@@ -317,55 +329,57 @@ public class Component extends BasicObjectImpl {
     //attributes used for mapping BasicObjects - project synchron
     // TODO: should be move out of the model.
     @Column(name = "interactor_ac", insertable = false, updatable = false)
-    public String getInteractorAc(){
+    public String getInteractorAc() {
         return this.interactorAc;
     }
-    public void setInteractorAc(String ac){
+
+    public void setInteractorAc( String ac ) {
         this.interactorAc = ac;
     }
 
     @Column(name = "interaction_ac", insertable = false, updatable = false)
-    public String getInteractionAc(){
+    public String getInteractionAc() {
         return this.interactionAc;
     }
-    public void setInteractionAc(String ac){
+
+    public void setInteractionAc( String ac ) {
         this.interactionAc = ac;
     }
 
     @Column(name = "role", insertable = false, updatable = false)
-    public String getCvComponentRoleAc(){
+    public String getCvComponentRoleAc() {
         return this.cvComponentRoleAc;
     }
-    public void setCvComponentRoleAc(String ac){
+
+    public void setCvComponentRoleAc( String ac ) {
         this.cvComponentRoleAc = ac;
     }
 
     @Column(name = "expressedin_ac", insertable = false, updatable = false)
-    public String getExpressedInAc(){
+    public String getExpressedInAc() {
         return this.expressedInAc;
     }
-    public void setExpressedInAc(String ac){
+
+    public void setExpressedInAc( String ac ) {
         this.expressedInAc = ac;
     }
 
     /**
-     * This method is specifically for the clone method
-     * of InteractionImpl class. The present setInteraction method changes
-     * the argument passed, thus causing changes to the source of the
-     * clone.
+     * This method is specifically for the clone method of InteractionImpl class. The present setInteraction method
+     * changes the argument passed, thus causing changes to the source of the clone.
      *
-     * @param interaction the interaction to set. This simply replaces
-     * the existing interaction.
+     * @param interaction the interaction to set. This simply replaces the existing interaction.
      */
-    protected void setInteractionForClone(Interaction interaction) {
+    protected void setInteractionForClone( Interaction interaction ) {
         this.interaction = interaction;
     }
 
     /**
      * See the comments for {@link #setInteractionForClone(Interaction)} method.
+     *
      * @param interactor
      */
-    protected void setInteractorForClone(Interactor interactor) {
+    protected void setInteractorForClone( Interactor interactor ) {
         this.interactor = interactor;
     }
 } // end Component
