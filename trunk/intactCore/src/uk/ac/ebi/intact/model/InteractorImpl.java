fbd66904403d187d87b8cd10de9cf2cbf8b8f8f5
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-06-14 09:33:20 +0000

    updated access to products Updated equals/hashCode so it doesn't use product. both methods are now in synch.

--- trunk/intactCore/src/uk/ac/ebi/intact/model/InteractorImpl.java
+++ trunk/intactCore/src/uk/ac/ebi/intact/model/InteractorImpl.java
@@ -5,19 +5,7 @@ in the root directory of this distribution.
 */
 package uk.ac.ebi.intact.model;
 
-import org.apache.commons.collections.CollectionUtils;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Transient;
-import javax.persistence.ManyToMany;
-import javax.persistence.JoinTable;
-import javax.persistence.FetchType;
-import javax.persistence.Column;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -30,7 +18,7 @@ import java.util.Iterator;
  */
 @Entity
 @Table(name = "ia_interactor")
-@DiscriminatorColumn(name="objclass")
+@DiscriminatorColumn(name = "objclass")
 public abstract class InteractorImpl extends AnnotatedObjectImpl implements Interactor {
 
     ///////////////////////////////////////
@@ -68,12 +56,11 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     /**
      * TODO comments
      */
-    private Collection<Product> products = new ArrayList<Product>();
+    private Collection<Product> products; // not implemented yet.
 
 
     /**
-     * no-arg constructor provided for compatibility with subclasses
-     * that have no-arg constructors.
+     * no-arg constructor provided for compatibility with subclasses that have no-arg constructors.
      */
     public InteractorImpl() {
         //super call sets creation time data
@@ -81,15 +68,14 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     }
 
     /**
-     * Constructor for subclass use only. Ensures that Interactors cannot be
-     * created without at least a shortLabel and an owner specified. NOTE: It is
-     * assumed that subclasses of Interactor will supply a valid BioSource; this
-     * is initially set to null but <b>other classes may expect it to be non-null</b>.
+     * Constructor for subclass use only. Ensures that Interactors cannot be created without at least a shortLabel and
+     * an owner specified. NOTE: It is assumed that subclasses of Interactor will supply a valid BioSource; this is
+     * initially set to null but <b>other classes may expect it to be non-null</b>.
      *
      * @param shortLabel The memorable label to identify this Interactor
      * @param owner      The Institution which owns this Interactor
-     * @throws NullPointerException thrown if either parameters are not specified
      *
+     * @throws NullPointerException thrown if either parameters are not specified
      * @deprecated Use {@link #InteractorImpl(String, Institution, CvInteractorType)} instead
      */
     @Deprecated
@@ -99,19 +85,19 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
 
 
     /**
-     * Constructor for subclass use only. Ensures that Interactors cannot be
-     * created without at least a shortLabel, an owner and type specified. NOTE: It is
-     * assumed that subclasses of Interactor will supply a valid BioSource; this
+     * Constructor for subclass use only. Ensures that Interactors cannot be created without at least a shortLabel, an
+     * owner and type specified. NOTE: It is assumed that subclasses of Interactor will supply a valid BioSource; this
      * is initially set to null but <b>other classes may expect it to be non-null</b>.
      *
      * @param shortLabel The memorable label to identify this Interactor
      * @param owner      The Institution which owns this Interactor
      * @param type       The Interactor type
+     *
      * @throws NullPointerException thrown if either parameters are not specified
      */
     protected InteractorImpl( String shortLabel, Institution owner, CvInteractorType type ) {
         super( shortLabel, owner );
-        setCvInteractorType(type);
+        setCvInteractorType( type );
     }
 
     ///////////////////////////////////////
@@ -133,16 +119,19 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
     ///////////////////////////////////////
     // access methods for associations
     public void setActiveInstances( Collection<Component> someActiveInstance ) {
+        if ( someActiveInstance == null ) {
+            throw new IllegalArgumentException( "Active instances cannot be null." );
+        }
         this.activeInstances = someActiveInstance;
     }
 
-    @OneToMany (mappedBy = "interactor")
+    @OneToMany(mappedBy = "interactor")
     public Collection<Component> getActiveInstances() {
         return activeInstances;
     }
 
     public void addActiveInstance( Component component ) {
-        if( !this.activeInstances.contains( component ) ) {
+        if ( !this.activeInstances.contains( component ) ) {
             this.activeInstances.add( component );
             component.setInteractor( this );
         }
@@ -150,37 +139,41 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
 
     public void removeActiveInstance( Component component ) {
         boolean removed = this.activeInstances.remove( component );
-        if (removed)
-        {
-            component.setInteractor(null);
+        if ( removed ) {
+            component.setInteractor( null );
         }
     }
 
     public void setProducts( Collection<Product> someProduct ) {
         this.products = someProduct;
+        throw new UnsupportedOperationException();
     }
 
     @Transient
     public Collection<Product> getProducts() {
-        return products;
+        // return products;
+        throw new UnsupportedOperationException();
     }
 
     public void addProduct( Product product ) {
-        if( !this.products.contains( product ) ) {
+        if ( !this.products.contains( product ) ) {
             this.products.add( product );
             product.setInteractor( this );
         }
+
+        throw new UnsupportedOperationException();
     }
 
     public void removeProduct( Product product ) {
         boolean removed = this.products.remove( product );
-        if (removed)
-        {
-            product.setInteractor(null);
+        if ( removed ) {
+            product.setInteractor( null );
         }
+
+        throw new UnsupportedOperationException();
     }
 
-    public void setCvInteractorType(CvInteractorType type) {
+    public void setCvInteractorType( CvInteractorType type ) {
         interactorType = type;
     }
 
@@ -192,24 +185,21 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
 
     @ManyToMany
     @JoinTable(
-        name="ia_int2annot",
-        joinColumns={@JoinColumn(name="interactor_ac")},
-        inverseJoinColumns={@JoinColumn(name="annotation_ac")}
+            name = "ia_int2annot",
+            joinColumns = { @JoinColumn(name = "interactor_ac") },
+            inverseJoinColumns = { @JoinColumn(name = "annotation_ac") }
     )
     @Override
-    public Collection<Annotation> getAnnotations()
-    {
+    public Collection<Annotation> getAnnotations() {
         return super.getAnnotations();
     }
 
     @Column(insertable = false, updatable = false)
-    public String getObjClass()
-    {
+    public String getObjClass() {
         return objClass;
     }
 
-    public void setObjClass(String objClass)
-    {
+    public void setObjClass( String objClass ) {
         this.objClass = objClass;
     }
 
@@ -223,9 +213,9 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
         result = "AC: " + this.getAc() + " Owner: " + this.getOwner().getShortLabel()
                  + " Label: " + this.getShortLabel() + "[";
 
-        if( null != this.getXrefs() ) {
+        if ( null != this.getXrefs() ) {
             i = this.getXrefs().iterator();
-            while( i.hasNext() ) {
+            while ( i.hasNext() ) {
                 result = result + i.next();
             }
         }
@@ -233,71 +223,41 @@ public abstract class InteractorImpl extends AnnotatedObjectImpl implements Inte
         return result + "]";
     }
 
-    /**
-     * Equality for Interactors is currently based on equality for
-     * <code>AnnotatedObjects</code>, BioSources and Products.
-     *
-     * @param o The object to check
-     * @return true if the parameter equlas this object, false otherwise
-     * @see uk.ac.ebi.intact.model.AnnotatedObject
-     */
     @Override
     public boolean equals( Object o ) {
-        if (this == o)
-        {
+        if ( this == o ) {
             return true;
         }
-        if (!(o instanceof Interactor))
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        if ( !super.equals( o ) ) {
+            return false;
+        }
+
+        final InteractorImpl that = (InteractorImpl) o;
+
+        if ( activeInstances != null ? !activeInstances.equals( that.activeInstances ) : that.activeInstances != null )
         {
             return false;
         }
-        if (!super.equals(o))
-        {
+        if ( bioSource != null ? !bioSource.equals( that.bioSource ) : that.bioSource != null ) {
+            return false;
+        }
+        if ( interactorType != null ? !interactorType.equals( that.interactorType ) : that.interactorType != null ) {
             return false;
         }
 
-        final Interactor interactor = (Interactor) o;
-
-        // possible cycle ...
-        // if (!activeInstances.equals(interactor.activeInstances)) return false;
-
-        if (bioSource != null) {
-            if( !bioSource.equals( interactor.getBioSource() ) ) {
-                return false;
-            }
-        }
-        // We should remove this check later as interactor type is mandatory.
-        // This is here till we migrated all our code to accept the InteractorType.
-        if (interactorType != null) {
-            if (!interactorType.equals(interactor.getCvInteractorType())) {
-                return false;
-            }
-        }
-        return CollectionUtils.isEqualCollection( interactor.getProducts(), products );
+        return true;
     }
 
     @Override
     public int hashCode() {
-
-        int code = 29 * super.hashCode();
-
-        if (bioSource != null) {
-            code = 29 * code + bioSource.hashCode();
-        }
-
-        // Can't take into account activeInstances since it happens that an object
-        // references himself into that collection ... it would never ends.
-        if( !products.isEmpty() ) {
-            //add the codes for those too....
-            int productsCode = 17;
-            for( Iterator it = products.iterator(); it.hasNext(); ) {
-                Product prod = (Product) it.next(); //controlled here - must be valid cast
-                productsCode = 29 * productsCode + prod.hashCode();
-            }
-            code = 29 * code + productsCode;
-        }
-
-        return code;
+        int result = super.hashCode();
+        result = 29 * result + ( bioSource != null ? bioSource.hashCode() : 0 );
+        result = 29 * result + ( interactorType != null ? interactorType.hashCode() : 0 );
+        result = 29 * result + ( activeInstances != null ? activeInstances.hashCode() : 0 );
+        return result;
     }
 
 } // end Interactor
