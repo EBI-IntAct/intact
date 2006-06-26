ede014e562fca5c4b7fb7c0223253b533386bb06
baranda <brunoaranda@gmail.com>
2006-06-26 13:05:38 +0000

    Updated poms and reorg script

--- branches/intact-reorg/reorg.sh
+++ branches/intact-reorg/reorg.sh
@@ -3,20 +3,79 @@
 SOURCE_BASE=~/projects/intact
 DEST_BASE=~/projects/intact-reorg
 
+#When using subversion the value of this variable has to be the client path ('svn')
 SVN=
 
-INTACT_PKG=/uk/ac/ebi/intact
+# Set when not using SVN
+if [ "$SVN" = "" ]; then
+  MKDIR="mkdir -p"
+  CP="cp -r"
+  RM="rm -rf"
+else
+  COMMIT_MSG=-m "Done automatically by the reorganization script"
+  MKDIR=$SVN mkdir $COMMIT_MSG
+  CP=$SVN cp $COMMIT_MSG
+  RM=$SVN rm $COMMIT_MSG
+fi
+
+INTACT_PKG=uk/ac/ebi/intact
 
 
 # INTACT CORE
-# -------------
-IC_SOURCE_SRC=$SOURCE_BASE/intactCore/src/$INTACT_PKG
-IC_DEST_SRC=$DEST_BASE/intact-core/trunk/src/$INTACT_PKG
+# -----------
+echo INTACT CORE
+IC_SOURCE_SRC=$SOURCE_BASE/src/$INTACT_PKG
+IC_DEST_SRC=$DEST_BASE/intact-core/trunk/src/main/java/$INTACT_PKG
 
-$SVN mkdir -p $IC_DEST_SRC
-$SVN cp $IC_SOURCE_SRC/business $IC_DEST_SRC/business
-$SVN cp $IC_SOURCE_SRC/model $IC_DEST_SRC/model
-$SVN cp $IC_SOURCE_SRC/persistence $IC_DEST_SRC/persistence
-$SVN cp $IC_SOURCE_SRC/simpleGraph $IC_DEST_SRC/simpleGraph
-$SVN cp $IC_SOURCE_SRC/util $IC_DEST_SRC/util
+$RM $IC_DEST_SRC
+$MKDIR $IC_DEST_SRC
+$CP $IC_SOURCE_SRC/business $IC_DEST_SRC/business
+$CP $IC_SOURCE_SRC/model $IC_DEST_SRC/model
+$CP $IC_SOURCE_SRC/persistence $IC_DEST_SRC/persistence
+$CP $IC_SOURCE_SRC/simpleGraph $IC_DEST_SRC/simpleGraph
+$CP $IC_SOURCE_SRC/util $IC_DEST_SRC/util
+$CP $IC_SOURCE_SRC/core $IC_DEST_SRC/core
 
+# removes the sanity checker classes from core
+echo   removing sanityChecker packages
+$RM $IC_DEST_SRC/util/sanityChecker
+$RM $IC_DEST_SRC/util/rangeChecker
+$RM $IC_DEST_SRC/util/correctionAssigner
+$RM $IC_DEST_SRC/util/test
+
+if [ "$SVN" = "" ]; then
+  echo " " Removing .svn
+  find $IC_DEST_SRC -name ".svn" | xargs rm -rf
+fi
+
+# APP COMMONS
+# -----------
+echo APP COMMONS
+IC_SOURCE_SRC=$SOURCE_BASE/src/$INTACT_PKG/application/commons
+IC_DEST_SRC=$DEST_BASE/app-commons/trunk/src/main/java/$INTACT_PKG/application/commons
+
+$RM $IC_DEST_SRC
+$CP $IC_SOURCE_SRC $IC_DEST_SRC
+
+if [ "$SVN" = "" ]; then
+  echo " " Removing .svn
+  find $IC_DEST_SRC -name ".svn" | xargs rm -rf
+fi
+
+# APP COMMONS
+# -----------
+echo SEARCH ENGINE
+IC_SOURCE_SRC=$SOURCE_BASE/src/$INTACT_PKG/application/search3
+IC_DEST_SRC=$DEST_BASE/search/search-engine/trunk/src/main/java/$INTACT_PKG/application/search3
+
+$RM $IC_DEST_SRC
+$MKDIR $IC_DEST_SRC
+#$CP $IC_SOURCE_SRC/advancedSearch $IC_DEST_SRC/advancedSearch
+$CP $IC_SOURCE_SRC/business $IC_DEST_SRC/business
+$CP $IC_SOURCE_SRC/searchEngine $IC_DEST_SRC/searchEngine
+$CP $IC_SOURCE_SRC/util $IC_DEST_SRC/util
+
+if [ "$SVN" = "" ]; then
+  echo " " Removing .svn
+  find $IC_DEST_SRC -name ".svn" | xargs rm -rf
+fi
