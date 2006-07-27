a2a100805dd5f1954fa067611ed55417c415580f
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-07-27 09:39:14 +0000

    Released 1.0

---
+++ repo/maven-plugins/maven-hibernateconfig-plugin/tags/1_0/src/main/java/uk/ac/ebi/intact/hibernate/HibernateEvent.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.hibernate;
+
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>21-Jul-2006</pre>
+ */
+public class HibernateEvent
+{
+
+    private String type;
+    private String className;
+
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public String getClassName()
+    {
+        return className;
+    }
+
+    public void setClassName(String className)
+    {
+        this.className = className;
+    }
+}
