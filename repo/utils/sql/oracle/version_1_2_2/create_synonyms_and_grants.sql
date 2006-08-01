787e30a3457c4e11719da001c03ec1f0bc3e6301
baranda <brunoaranda@gmail.com>
2006-08-01 12:02:12 +0000

    Changed ordering of statements to improve clearance

--- repo/utils/sql/oracle/version_1_2_2/create_synonyms_and_grants.sql
+++ repo/utils/sql/oracle/version_1_2_2/create_synonyms_and_grants.sql
@@ -1,30 +1,26 @@
 -- creates synonims and grants permissions for the xref tables
 
-create public synonym IA_BioSource_Xref for INTACT.IA_BioSource_Xref;
 grant select on IA_BioSource_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_BioSource_Xref to INTACT_CURATOR ;
 
-
-create public synonym IA_Experiment_Xref for INTACT.IA_Experiment_Xref;
 grant select on IA_Experiment_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_Experiment_Xref to INTACT_CURATOR ;
 
-
-create public synonym IA_ControlledVocab_Xref for INTACT.IA_ControlledVocab_Xref;
 grant select on IA_ControlledVocab_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_ControlledVocab_Xref to INTACT_CURATOR ;
 
-
-create public synonym IA_Feature_Xref for INTACT.IA_Feature_Xref;
 grant select on IA_Feature_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_Feature_Xref to INTACT_CURATOR ;
 
-
-create public synonym IA_Interactor_Xref for INTACT.IA_Interactor_Xref;
 grant select on IA_Interactor_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_Interactor_Xref to INTACT_CURATOR ;
 
-
-create public synonym IA_Publication_Xref for INTACT.IA_Publication_Xref;
 grant select on IA_Publication_Xref to INTACT_SELECT ;
 grant select, insert,update,delete on IA_Publication_Xref to INTACT_CURATOR ;
+
+create public synonym IA_BioSource_Xref for INTACT.IA_BioSource_Xref;
+create public synonym IA_Experiment_Xref for INTACT.IA_Experiment_Xref;
+create public synonym IA_ControlledVocab_Xref for INTACT.IA_ControlledVocab_Xref;
+create public synonym IA_Feature_Xref for INTACT.IA_Feature_Xref;
+create public synonym IA_Interactor_Xref for INTACT.IA_Interactor_Xref;
+create public synonym IA_Publication_Xref for INTACT.IA_Publication_Xref;
