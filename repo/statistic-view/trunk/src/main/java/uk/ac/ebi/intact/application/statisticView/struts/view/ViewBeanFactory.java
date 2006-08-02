bd9d53381a74771f66f4ffee070b2c6af5948ef2
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-02 14:50:24 +0000

    Released 1.1.0. This commit also contains changes to the way the charts are generated, so the application can work in a cluster.

--- repo/statistic-view/trunk/src/main/java/uk/ac/ebi/intact/application/statisticView/struts/view/ViewBeanFactory.java
+++ repo/statistic-view/trunk/src/main/java/uk/ac/ebi/intact/application/statisticView/struts/view/ViewBeanFactory.java
@@ -6,8 +6,11 @@ in the root directory of this distribution.
 package uk.ac.ebi.intact.application.statisticView.struts.view;
 
 import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.entity.StandardEntityCollection;
 import org.jfree.chart.servlet.ServletUtilities;
 import uk.ac.ebi.intact.application.statisticView.business.data.StatisticHelper;
@@ -15,14 +18,17 @@ import uk.ac.ebi.intact.application.statisticView.business.util.Constants;
 import uk.ac.ebi.intact.business.IntactException;
 
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletContext;
 import java.io.IOException;
+import java.io.File;
 
 /**
  * User: Michael Kleen mkleen@ebi.ac.uk Date: Mar 22, 2005 Time: 4:24:29 PM
  */
 public class ViewBeanFactory {
 
-    private static Logger logger = Logger.getLogger( Constants.LOGGER_NAME );
+    private static final Log logger = LogFactory.getLog(ViewBeanFactory.class);
 
     private StatisticHelper helper;
     private String contextPath;
@@ -50,20 +56,42 @@ public class ViewBeanFactory {
 
         final ChartRenderingInfo info = new ChartRenderingInfo( new StandardEntityCollection() );
         IntactStatisticsBean intactBean = new IntactStatisticsBean( contextPath );
-        intactBean.setCvTermChartName( ServletUtilities.saveChartAsPNG( cvChart, 600, 400, info, session ) );
-        intactBean.setExperimentChartName( ServletUtilities.saveChartAsPNG( experimentChart, 600, 400, info, session ) );
-        intactBean.setInteractionChartName( ServletUtilities.saveChartAsPNG( interactionChart, 600, 400, info, session ) );
-        intactBean.setProteinChartName( ServletUtilities.saveChartAsPNG( proteinChart, 600, 400, info, session ) );
-        intactBean.setBinaryChartName( ServletUtilities.saveChartAsPNG( binaryChart, 600, 400, info, session ) );
+        intactBean.setCvTermChartUrl(writeChart(cvChart, 600, 400, info, session));
+        intactBean.setExperimentChartUrl ( writeChart( experimentChart, 600, 400, info, session ) );
+        intactBean.setInteractionChartUrl ( writeChart( interactionChart, 600, 400, info, session ) );
+        intactBean.setProteinChartUrl ( writeChart( proteinChart, 600, 400, info, session ) );
+        intactBean.setBinaryChartUrl ( writeChart( binaryChart, 600, 400, info, session ) );
 
         intactBean.setCvTermCount( helper.getCvCount() );
         intactBean.setExperimentCount( helper.getExperimentCount() );
         intactBean.setInteractionCount( helper.getInteractionCount() );
         intactBean.setProteinCount( helper.getProteinCount() );
         intactBean.setBinaryInteractionCount( helper.getBinaryInteractionCount() );
-        intactBean.setBioSourceChartName( ServletUtilities.saveChartAsPNG( bioSourceChart, 600, 400, info, session ) );
-        intactBean.setDetectionChartName( ServletUtilities.saveChartAsPNG( identificationChart, 600, 400, info, session ) );
+        intactBean.setBioSourceChartUrl ( writeChart( bioSourceChart, 600, 400, info, session ) );
+        intactBean.setDetectionChartUrl ( writeChart( identificationChart, 600, 400, info, session ) );
 
         return intactBean;
+    }
+
+    private static String writeChart(JFreeChart chart, int length, int width, ChartRenderingInfo info, HttpSession session)
+            throws IOException
+    {
+        ServletContext context = session.getServletContext();
+
+        String chartFilename = session.getId()+"_"+chart.hashCode()+".png";
+
+        String strTempDir = "/temp";
+        String chartContextPath = strTempDir+"/"+chartFilename;
+
+        File realChartfile = new File(context.getRealPath(chartContextPath));
+
+        if (realChartfile.exists())
+        {
+            return chartContextPath;
+        }
+
+        ChartUtilities.saveChartAsPNG(realChartfile, chart, length, width, info);
+
+        return chartContextPath;
     }
 }
