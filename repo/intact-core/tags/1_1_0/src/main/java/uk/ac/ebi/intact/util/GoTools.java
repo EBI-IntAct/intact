c231fc0856943cac5193efbc0022d368d1324f8a
baranda <brunoaranda@gmail.com>
2006-08-02 13:53:08 +0000

    released 1.1.0

---
+++ repo/intact-core/tags/1_1_0/src/main/java/uk/ac/ebi/intact/util/GoTools.java
@@ -0,0 +1,345 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.persistence.dao.CvObjectDao;
+import uk.ac.ebi.intact.persistence.dao.DaoFactory;
+import uk.ac.ebi.intact.util.go.GoUtils;
+
+import java.io.File;
+import java.util.Iterator;
+
+/**
+ * Utilities to read and write files in GO format
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk), Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class GoTools {
+
+    private static final Log log = LogFactory.getLog(GoTools.class);
+
+    public static final String NEW_LINE = System.getProperty( "line.separator" );
+
+    private GoTools()
+    {
+        // this is a util class, so it can never be instantiated
+    }
+
+    /**
+     * Load or unload Controlled Vocabularies in GO format. Usage: GoTools upload   IntAct_classname goid_db
+     * Go_DefinitionFile [Go_DagFile] | GoTools download[v14] IntAct_classname goid_db Go_DefinitionFile [Go_DagFile]
+     * <p/>
+     * goid_db is the shortLabel of the database which is to be used to establish object identity by mapping it to goid:
+     * in the GO flat file. Example: If goid_db is psi-mi, an CvObject with an xref "psi-mi; MI:123" is considered to be
+     * the same object as an object from the flat file with goid MI:123. If goid_db is '-', the shortLabel will be used
+     * if present.
+     *
+     * @param args
+     *
+     * @throws Exception
+     */
+    public static void main( String[] args ) throws Exception {
+
+        String usage = "Usage: GoTools upload IntAct_classname goid_db Go_DefinitionFile [Go_DagFile] " + NEW_LINE +
+                       "OR" + NEW_LINE +
+                       "GoTools download IntAct_classname goid_db Go_DefinitionFile [Go_DagFile]" + NEW_LINE +
+                       "goid_db is the shortLabel of the database which is to be used to establish" + NEW_LINE +
+                       "object identity by mapping it to goid: in the GO flat file." + NEW_LINE +
+                       "Example: If goid_db is psi-mi, an CvObject with an xref psi-mi; MI:123 is" + NEW_LINE +
+                       "considered to be the same object as an object from the flat file with goid MI:123." + NEW_LINE +
+                       "If goid_db is '-', the short label will be used if present.";
+
+        // Check parameters
+        if ( ( args.length < 4 ) || ( args.length > 5 ) ) {
+            throw new IllegalArgumentException( "Invalid number of arguments." + NEW_LINE + usage );
+        }
+
+        // retreive parameters
+        String mode = args[ 0 ];
+        String cvClazz = args[ 1 ];
+        String id_db = args[ 2 ];
+        String defFilename = args[ 3 ];
+
+        String dagFile = null;
+        if ( args.length == 5 ) {
+            dagFile = args[ 4 ];
+        }
+
+        // The first argument must be either upload or download
+        if ( mode.equals( "upload" ) || mode.equals( "download" ) ) {
+            // The target as a Class object
+            Class targetClass = Class.forName( cvClazz );
+
+            createNecessaryCvTerms( );
+
+                // args[2] is the go id database.
+                GoUtils goUtils = new GoUtils( id_db, targetClass );
+
+                if ( mode.equals( "upload" ) ) {
+                    // Insert definitions
+                    File defFile = new File( defFilename );
+                    goUtils.insertGoDefinitions( defFile );
+
+                    // Insert DAG
+                    if ( args.length == 5 ) {
+                        goUtils.insertGoDag( dagFile );
+                    }
+                } else if ( mode.equals( "download" ) ) {
+                    // Write definitions
+                    System.out.println( "Writing GO definitons to " + defFilename + " ..." );
+                    goUtils.writeGoDefinitions( defFilename );
+
+                    // Write go dag format
+                    if ( args.length == 5 ) {
+                        System.out.println( "Writing GO DAG to " + dagFile + " ..." );
+                        goUtils.writeGoDag( dagFile );
+                        System.out.println( "Done." );
+                    }
+                }
+        } else {
+            throw new IllegalArgumentException( "Invalid argument " + mode + NEW_LINE + usage );
+        }
+    }
+
+    //////////////////////////////////////////////////////
+    // Constants related to the identity and psi-mi CVs
+
+    public static final String IDENTITY_MI_REFERENCE = "MI:0356";
+    public static final String IDENTITY_SHORTLABEL = "identity";
+
+    public static final String PSI_MI_REFERENCE = "MI:0488";
+    public static final String PSI_SHORTLABEL = "psi-mi";
+
+
+    /**
+     * Assures that necessary Controlled vocabulary terms are present prior to manipulation of other terms.
+     *
+     * @throws IntactException
+     */
+    private static void createNecessaryCvTerms() throws IntactException {
+
+        Institution institution = DaoFactory.getInstitutionDao().getInstitution();
+
+        CvObjectDao<CvXrefQualifier> cvXrefQualifierDao = DaoFactory.getCvObjectDao(CvXrefQualifier.class);
+
+        // 1. check that the CvXrefQualifier 'identity'/'MI:0356' is present...
+        boolean identityCreated = false;
+        CvXrefQualifier identity = cvXrefQualifierDao.getByXref(IDENTITY_MI_REFERENCE );
+        if ( identity == null ) {
+            // then look for shortlabel
+            identity = cvXrefQualifierDao.getByShortLabel(IDENTITY_SHORTLABEL);
+
+            if ( identity == null ) {
+
+                log.info( "CvXrefQualifier( " + IDENTITY_SHORTLABEL + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                identity = new CvXrefQualifier( institution, IDENTITY_SHORTLABEL );
+                cvXrefQualifierDao.persist( identity );
+
+                // That flag will allow the creation of the PSI MI Xref after checking that the CvDatabase exists.
+                identityCreated = true;
+                if (identityCreated){
+                    log.debug("identity has been created");
+                }
+            }
+        }
+
+
+        // 2. check that the CvDatabase 'psi-mi'/'MI:0356' is present...
+        CvObjectDao<CvDatabase> cvDatabaseDao = DaoFactory.getCvObjectDao(CvDatabase.class);
+
+        boolean psiAlreadyExists = false;
+        CvDatabase psi = cvDatabaseDao.getByXref(PSI_MI_REFERENCE );
+        if ( psi == null ) {
+            // then look for shortlabel
+            psi = cvDatabaseDao.getByShortLabel( PSI_SHORTLABEL );
+
+            if ( psi == null ) {
+
+                log.info( "CvDatabase( " + PSI_SHORTLABEL + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                psi = new CvDatabase( institution, PSI_SHORTLABEL );
+                cvDatabaseDao.persist( psi );
+
+                CvObjectXref xref = new CvObjectXref( institution, psi, PSI_MI_REFERENCE, null, null, identity );
+                psi.addXref( xref );
+
+                DaoFactory.getXrefDao().persist( xref );
+            } else {
+                psiAlreadyExists = true;
+            }
+        } else {
+            psiAlreadyExists = true;
+        }
+
+        if ( psiAlreadyExists ) {
+            // check that it has the right MI reference.
+            updatePsiXref( psi, PSI_MI_REFERENCE, psi, identity );
+        }
+
+        if ( identityCreated ) {
+
+            // add the psi reference to the identity term.
+            CvObjectXref xref = new CvObjectXref( institution, psi, IDENTITY_MI_REFERENCE, null, null, identity );
+            identity.addXref( xref );
+
+            DaoFactory.getXrefDao().persist( xref );
+        } else {
+            // identity already existed, check that it has the right PSI Xref.
+            updatePsiXref( identity, IDENTITY_MI_REFERENCE, psi, identity );
+        }
+
+
+        // 3. check that the CvDatabase 'pubmed'/'MI:0446' is present...
+        boolean pubmedAlreadyExists = false;
+        CvDatabase pubmed = cvDatabaseDao.getByXref(CvDatabase.PUBMED_MI_REF );
+        if ( pubmed == null ) {
+            // then look for shortlabel
+            pubmed = cvDatabaseDao.getByShortLabel(CvDatabase.PUBMED );
+
+            if ( pubmed == null ) {
+
+                log.info( "CvDatabase( " + CvDatabase.PUBMED + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                pubmed = new CvDatabase( institution, CvDatabase.PUBMED );
+                cvDatabaseDao.persist( pubmed );
+
+                CvObjectXref xref = new CvObjectXref( institution, psi, CvDatabase.PUBMED_MI_REF, null, null, identity );
+                pubmed.addXref( xref );
+
+                DaoFactory.getXrefDao().persist( xref );
+            } else {
+                pubmedAlreadyExists = true;
+            }
+        } else {
+            pubmedAlreadyExists = true;
+        }
+
+        if ( pubmedAlreadyExists ) {
+            // check that it has the right MI reference.
+            updatePsiXref( pubmed, CvDatabase.PUBMED_MI_REF, psi, identity );
+        }
+
+
+
+        // 4. check that the CvDatabase 'go-definition-ref'/'MI:0242' is present...
+        boolean goDefRefAlreadyExists = false;
+        CvXrefQualifier goDefRef = cvXrefQualifierDao.getByXref(CvXrefQualifier.GO_DEFINITION_REF_MI_REF );
+        if ( goDefRef == null ) {
+            // then look for shortlabel
+            goDefRef = cvXrefQualifierDao.getByShortLabel( CvXrefQualifier.GO_DEFINITION_REF );
+
+            if ( goDefRef == null ) {
+
+                log.info( "CvXrefQualifier( " + CvXrefQualifier.GO_DEFINITION_REF + " ) doesn't exists, create it." );
+
+                // not there, then create it manually
+                goDefRef = new CvXrefQualifier( institution, CvXrefQualifier.GO_DEFINITION_REF );
+                cvXrefQualifierDao.persist( goDefRef );
+
+                CvObjectXref xref = new CvObjectXref( institution, psi, CvXrefQualifier.GO_DEFINITION_REF_MI_REF, null, null, identity );
+                goDefRef.addXref( xref );
+
+                DaoFactory.getXrefDao().persist( xref );
+            } else {
+                goDefRefAlreadyExists = true;
+            }
+        } else {
+            goDefRefAlreadyExists = true;
+        }
+
+        if ( goDefRefAlreadyExists ) {
+            // check that it has the right MI reference.
+            updatePsiXref( goDefRef, CvXrefQualifier.GO_DEFINITION_REF_MI_REF, psi, identity );
+        }
+        
+
+//        if ( pubmedCreated ) {
+//
+//            // add the psi reference to the identity term.
+//            Xref xref = new Xref( institution, psi, CvDatabase.PUBMED_MI_REF, null, null, identity );
+//            pubmed.addXref( xref );
+//
+//            helper.create( xref );
+//        } else {
+//            // identity already existed, check that it has the right PSI Xref.
+//            updatePsiXref( pubmed, CvDatabase.PUBMED_MI_REF, psi, identity );
+//        }
+//
+//        if ( goDefRefCreated ) {
+//
+//            // add the psi reference to the identity term.
+//            Xref xref = new Xref( institution, psi, CvXrefQualifier.GO_DEFINITION_REF_MI_REF, null, null, identity );
+//            goDefRef.addXref( xref );
+//
+//            helper.create( xref );
+//        } else {
+//            // identity already existed, check that it has the right PSI Xref.
+//            updatePsiXref( goDefRef, CvXrefQualifier.GO_DEFINITION_REF_MI_REF, psi, identity );
+//        }
+
+    }
+
+    /**
+     * Update the given CvObject with the given PSI MI reference. update existing Xref if necessary, or create a new one
+     * if none exists.
+     *
+     * @param cv       the Controlled Vocabulary term to update.
+     * @param mi       the MI reference to be found.
+     * @param psi      the CvDatabase( psi-mi )
+     * @param identity the CvXrefQualifier( identity )
+     *
+     * @throws IntactException
+     */
+    private static void updatePsiXref( CvObject cv, String mi,
+                                       CvDatabase psi,
+                                       CvXrefQualifier identity ) throws IntactException {
+
+        // Note: we assume that there is at most one psi ref per term.
+        boolean psiRefFound = false;
+
+        for ( Iterator iterator = cv.getXrefs().iterator(); iterator.hasNext() && false == psiRefFound; ) {
+            Xref xref = (Xref) iterator.next();
+
+            if ( psi.equals( xref.getCvDatabase() ) ) {
+                if ( false == mi.equals( xref.getPrimaryId() ) ) {
+
+                    log.debug( "Updating " + cv.getShortLabel() + "'s MI reference (" +
+                                        xref.getPrimaryId() + " becomes " + mi + ")" );
+                    xref.setPrimaryId( mi );
+                    DaoFactory.getXrefDao().update( xref );
+                }
+
+                if ( false == identity.equals( xref.getCvXrefQualifier() ) ) {
+                    log.debug( "Updating " + cv.getShortLabel() + "'s CvXrefQualifier to identity." );
+
+                    xref.setCvXrefQualifier( identity );
+                    DaoFactory.getXrefDao().update( xref );
+                }
+
+                psiRefFound = true;
+            }
+        } // for
+
+        if ( !psiRefFound ) {
+            // then create the PSI ref
+            CvObjectXref xref = new CvObjectXref( DaoFactory.getInstitutionDao().getInstitution(), psi, mi, null, null, identity );
+            psi.addXref( xref );
+
+            DaoFactory.getXrefDao().persist( xref );
+        }
+    }
+}
+
---
+++ repo/intact-core/tags/1_1_0/src/main/java/uk/ac/ebi/intact/util/GoaTools.java
@@ -0,0 +1,387 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.persistence.dao.DaoFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class GoaTools {
+
+    private static final Log log = LogFactory.getLog(GoaTools.class);
+
+    //////////////////////
+    // Inner class
+    //////////////////////
+
+    private class GoaItem {
+
+        private String database;
+        private String ac;
+        private String symbol;
+        private String goId;
+        private String goCategory;
+
+        public GoaItem ( String database, String ac, String symbol, String goId, String goCategory ) {
+            this.database = database;
+            this.ac = ac;
+            this.symbol = symbol;
+            this.goId = goId;
+            this.goCategory = goCategory;
+        }
+
+        public String getDatabase () {
+            return database;
+        }
+
+        public String getAc () {
+            return ac;
+        }
+
+        public String getSymbol () {
+            return symbol;
+        }
+
+        public String getGoId () {
+            return goId;
+        }
+
+        public String getGoCategory () {
+            return goCategory;
+        }
+
+        @Override
+        public String toString() {
+            StringBuffer sb = new StringBuffer(256);
+
+            sb.append( "\n Database: " + database );
+            sb.append( "\n ac: " + ac );
+            sb.append( "\n symbol: " + symbol );
+            sb.append( "\n goId: " + goId );
+            sb.append( "\n goCategory: " + goCategory );
+
+            return sb.toString();
+        }
+    }
+
+
+
+    /**
+     *
+     */
+    private class GoaCollection {
+
+        private URL mySourceURL;
+
+        private BufferedReader goaBufferedReader;
+
+        private GoaIterator currentIterator;
+
+        public GoaCollection( String sourceUrl ) throws MalformedURLException {
+
+            try {
+                mySourceURL = new URL( sourceUrl );
+            } catch ( MalformedURLException e ) {
+                throw e;
+            }
+        }
+
+        public Iterator iterator () {
+
+            final String isProxySet = System.getProperty( "proxySet" );
+            final String proxyHost  = System.getProperty( "proxyHost" );
+            final String proxyPort  = System.getProperty( "proxyPort" );
+            log.debug( "Uses: proxySet="+ isProxySet +", proxyHost="+ proxyHost +", proxyPort="+ proxyPort );
+
+            try {
+                InputStream in = mySourceURL.openStream();
+                InputStreamReader isr = new InputStreamReader( in );
+                goaBufferedReader = new BufferedReader( isr );
+
+                GoaIterator goaIterator = new GoaIterator( goaBufferedReader );
+                currentIterator = goaIterator;
+                return goaIterator;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        public long getLineProcessedCount() {
+            return currentIterator.getLineProcessedCount();
+        }
+    } // GoaCollection
+
+
+
+    private class GoaIterator implements Iterator {
+
+        private BufferedReader goaBufferedReader;
+        private String currentLine;
+        private long goaLineCount = 0;
+
+
+        public GoaIterator ( BufferedReader goaBufferedReader ) {
+            this.goaBufferedReader = goaBufferedReader;
+            try {
+                currentLine = goaBufferedReader.readLine();
+                goaLineCount++;
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+        public boolean hasNext(){
+
+            return (currentLine != null);
+        }
+
+        public Object next(){
+
+            if ( ! hasNext() ) {
+                throw new NoSuchElementException();
+            }
+
+            // build a GoaItem
+            GoaItem goaItem = null;
+            StringTokenizer st = new StringTokenizer( currentLine, "\t" );
+//            log.debug ( currentLine );
+            /* 01 */ String database = st.nextToken();
+            /* 02 */ String ac = st.nextToken();
+            /* 03 */ String symbol = st.nextToken();
+            /* TODO: find out why the empty column 04 (human file) is not found */
+            /* 04 */ String goId = st.nextToken();
+            /* 05 */ st.nextToken();
+            /* 06 */ st.nextToken();
+            /* 07 */ String goCategory = st.nextToken();
+
+            goaItem = new GoaItem(database, ac, symbol, goId, goCategory);
+
+            // Read the next item
+            try {
+                currentLine = goaBufferedReader.readLine();
+                goaLineCount++;
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            return goaItem;
+        }
+
+        public void remove(){
+            // do nothing ... can't remove element from a readonly stream.
+            // TODO: pass the current item ?
+        }
+
+        public long getLineProcessedCount() {
+            return goaLineCount;
+        }
+
+    } // GoaIterator
+
+
+
+
+    ////////////////////////
+    // Instance variables
+    ////////////////////////
+
+    private GoaCollection goaBrowser;
+
+    private GoServerProxy goServerProxy;
+
+    private long newAnnotationCount = 0;
+
+
+
+    //////////////////////////
+    // Instance constructors
+    //////////////////////////
+
+    public GoaTools ( ) {
+        goServerProxy = new GoServerProxy();
+    }
+
+    public GoaTools ( String url ) throws MalformedURLException, IntactException {
+        this();
+        goaBrowser = new GoaCollection( url );
+    }
+
+
+    ///////////////////////
+    // Instance methods
+    ///////////////////////
+
+    public GoaCollection getGoaBrowser () {
+        return goaBrowser;
+    }
+
+    private boolean isXrefAlreadyExisting( Collection<InteractorXref> xrefs, String primaryId, CvDatabase database) {
+
+        for (Xref xref : xrefs)
+        {
+            if (xref != null && xref.getPrimaryId().equals(primaryId) &&
+                    database != null && database.equals(xref.getCvDatabase()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addNewXref (AnnotatedObject current, final Xref xref)  {
+        // Make sure the xref does not yet exist in the object
+        Collection<Xref> xrefs = current.getXrefs();
+        for (Xref anXref : xrefs)
+        {
+            if (anXref.equals(xref))
+            {
+                return; // already in, exit
+            }
+        }
+
+        // add the xref to the AnnotatedObject
+        current.addXref (xref);
+
+        // That test is done to avoid to record in the database an Xref
+        // which is already linked to that AnnotatedObject.
+        if (xref.getParentAc() == current.getAc()) {
+            try {
+                DaoFactory.getXrefDao().persist(xref);
+                newAnnotationCount++;
+            } catch (Exception e_xref) {
+                log.error ( "Could not create the Xref: " + xref.getPrimaryId() +
+                                     " for the AnnotatedObject: " + current.getShortLabel() );
+            }
+        }
+    }
+
+    private void updateGoXref ( Institution institution, Protein protein, GoaItem goaItem, CvDatabase database ) {
+
+        Collection<InteractorXref> xrefs = protein.getXrefs();
+        if ( ! isXrefAlreadyExisting( xrefs, goaItem.getGoId(), database ) ) {
+            // add a new Xref
+            String goId = goaItem.getGoId();
+
+            GoServerProxy.GoResponse goResponse;
+            try {
+                goResponse = goServerProxy.query( goId );
+            } catch ( IOException e ) {
+                log.error ( "Could not find GO term: " + goId + ". abort creation." );
+                e.printStackTrace ();
+                return;
+            } catch ( GoServerProxy.GoIdNotFoundException e ) {
+                log.error ( "Could not find GO term: " + goId + ". abort creation." );
+                e.printStackTrace ();
+                return;
+            }
+
+            InteractorXref xref = new InteractorXref( institution,
+                    database,
+                    goId,
+                    goResponse.getName(),
+                    null, null );
+
+            addNewXref( protein, xref );
+            log.debug ( "Update protein " + protein.getShortLabel() + " with Xref: " + goId );
+        }
+    } // updateGoXref
+
+    public void displayStatistics() {
+        log.debug ( "#GOA line processed: " + getGoaBrowser().getLineProcessedCount());
+        log.debug ( "#GO added: " + newAnnotationCount );
+    }
+
+
+
+
+
+
+    /**
+     * D E M O
+     *
+     * @param args [0] goa URL from which we'll read the data
+     */
+    public static void main ( String[] args ) throws Exception {
+        // Check parameters
+        if ( args.length == 0 ) {
+            // usage
+            log.error ( "Usage: GoaTools <GOA source URL>" );
+            System.exit( 1 );
+        }
+
+        CvDatabase goDatabase = DaoFactory.getCvObjectDao(CvDatabase.class).getByShortLabel("go");
+
+        if ( goDatabase == null ){
+            throw new IntactException ( "Could not find the CvDatabase: go. Stop processing." );
+        }
+
+        Institution institution = DaoFactory.getInstitutionDao().getInstitution();
+
+        if ( institution == null ){
+            throw new IntactException ( "Could not find the Institution: EBI. Stop processing." );
+        }
+
+
+        GoaTools goaTools = new GoaTools( args[0] );
+        GoaCollection goaCollection = goaTools.getGoaBrowser();
+        Collection<ProteinImpl> proteins;
+        Protein protein;
+        long count = 0;
+        for ( Iterator goaIterator = goaCollection.iterator (); goaIterator.hasNext () ; ) {
+            GoaItem goaItem = (GoaItem) goaIterator.next();
+
+            count++;
+            if ((count % 500) == 0) goaTools.displayStatistics();
+
+            proteins = DaoFactory.getProteinDao().getByXrefLike(goaItem.getAc());
+            if (proteins.size() != 0) {
+                log.debug ( proteins.size() + " Protein found by Xref: " + goaItem.getAc() );
+                for (Protein prot : proteins)
+                {
+                    goaTools.updateGoXref(institution, prot, goaItem, goDatabase);
+                }
+                continue;
+            } else {
+                // TODO: we should not fall in taht case ... kind of inconstistancY.
+
+                String symbol = goaItem.getSymbol();
+                if ( ! goaItem.getAc().equals( symbol ) ) {
+                    protein = DaoFactory.getProteinDao().getByShortLabel(symbol);
+
+                    if (protein != null) {
+                        log.debug ( "Protein found by Label: " + symbol );
+                        goaTools.updateGoXref( institution, protein, goaItem, goDatabase );
+                        continue;
+                    }
+                }
+            }
+
+            System.out.print ( "." ); // displayed only if the Goa line hasn't been usedfull
+        }
+    } // main
+
+} // GoaTools
