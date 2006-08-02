d6bef5d1aef864cb63629cd2c043dff07ca782a7
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-02 14:08:51 +0000

    Released 1.1.0

---
+++ repo/data-conversion/tags/1_1_0/src/main/java/uk/ac/ebi/intact/application/dataConversion/XsdEntityResolver.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.application.dataConversion;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.EntityResolver;
+
+/**
+ * Entity resolver for XSD files to avoid connecting to the internet
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>01-Aug-2006</pre>
+ */
+public class XsdEntityResolver implements EntityResolver
+{
+    private static final Log log = LogFactory.getLog(XsdEntityResolver.class);
+
+    public InputSource resolveEntity(String publicId, String systemId)
+    {
+
+        log.debug("File to validate systemId="+systemId+" ; publicId="+publicId);
+
+        InputSource inputSource = null;
+
+        if (systemId != null &&
+                systemId.equals("http://psidev.sourceforge.net/mi/rel25/src/MIF25.xsd"))
+        {
+            log.debug("Entity resolved using XsdEntityResolver");
+
+            inputSource = new InputSource(XsdEntityResolver.class.getResourceAsStream("/META-INF/MIF25.xsd"));
+            inputSource.setPublicId(publicId);
+            inputSource.setSystemId(systemId);
+        }
+
+        // If nothing found, null is returned, for normal processing
+        return inputSource;
+
+    }
+
+}
