b0cafa1c7d8ba9d990d966ed700d44956454d4fe
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-04 02:31:58 +0000

    Bugfix release 1.1.2 - NodeObject must implement Serializable, as it is stored in the session

--- repo/mine-app/trunk/src/main/java/uk/ac/ebi/intact/application/mine/business/graph/model/NodeObject.java
+++ repo/mine-app/trunk/src/main/java/uk/ac/ebi/intact/application/mine/business/graph/model/NodeObject.java
@@ -3,13 +3,16 @@
  */
 package uk.ac.ebi.intact.application.mine.business.graph.model;
 
+import java.io.Serializable;
+
 /**
  * A node object is attached to the graph used in the MiNe application. <br>
  * It stores the accession number and the shortlable for a protein.
  * 
  * @author Andreas Groscurth
  */
-public class NodeObject {
+public class NodeObject implements Serializable
+{
     private String acNr;
     private String shortLabel;
 
