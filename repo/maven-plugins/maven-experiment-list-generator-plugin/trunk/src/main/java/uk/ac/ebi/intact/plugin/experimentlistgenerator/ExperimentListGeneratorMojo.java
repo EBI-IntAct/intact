0b5653ca4c9e0d420301e42ca5c093ff2cbf393a
baranda <brunoaranda@gmail.com>
2006-08-09 16:14:35 +0000

    Implemented mojo using the new ExperimentListGenerator from data-conversion 1.3-SNAPSHOT

--- repo/maven-plugins/maven-experiment-list-generator-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/experimentlistgenerator/ExperimentListGeneratorMojo.java
+++ repo/maven-plugins/maven-experiment-list-generator-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/experimentlistgenerator/ExperimentListGeneratorMojo.java
@@ -7,10 +7,18 @@ package uk.ac.ebi.intact.plugin.experimentlistgenerator;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import uk.ac.ebi.intact.application.dataConversion.ExperimentListGenerator;
+import uk.ac.ebi.intact.application.dataConversion.ExperimentListItem;
 import uk.ac.ebi.intact.config.impl.CustomCoreDataConfig;
 import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.model.Experiment;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Generates list of experiments
@@ -24,6 +32,8 @@ import java.io.File;
  */
 public class ExperimentListGeneratorMojo extends ExperimentListGeneratorAbstractMojo
 {
+
+    private static final String NEW_LINE = System.getProperty("line.separator");
 
     /**
     * File containing the publications
@@ -91,13 +101,72 @@ public class ExperimentListGeneratorMojo extends ExperimentListGeneratorAbstract
         IntactContext.getCurrentInstance().getConfig().addDataConfig(testConfig, true);
 
         ExperimentListGenerator gen = new ExperimentListGenerator();
-        gen.setSpeciesFile(speciesFile);
-        gen.setPublicationsFile(publicationsFile);
-        gen.setSearchPattern(searchPattern);
-        gen.setOverwrite(overwrite);
         gen.setOnlyWithPmid(onlyWithPmid);
-        
-        gen.execute();
 
+        getLog().info("Starting to classify by species and publications");
+
+        List<ExperimentListItem> eliSpecies = gen.generateClassificationBySpecies();
+        getLog().info("Files by species: "+eliSpecies.size());
+
+        try
+        {
+            writeItems(speciesFile, eliSpecies);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Problem creating the species file", e);
+        }
+
+        List<ExperimentListItem> eliPublications = gen.generateClassificationByPublications();
+        getLog().info("Files by publications: "+eliPublications.size());
+
+        try
+        {
+            writeItems(publicationsFile, eliPublications);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Problem creating the publications file", e);
+        }
+
+        Set<Experiment> negativeExps = gen.getNegativeExperiments();
+        getLog().info("Negative experiments: "+negativeExps.size());
+
+        Map<String,String> experimentsWithErrors = gen.getExperimentWithErrors();
+        getLog().info("Experiments with errors: "+experimentsWithErrors.size());
+
+        try
+        {
+            writeErrorFile(experimentErrorFile, experimentsWithErrors);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Problem creating the experiment with errors file", e);
+        }
+
+    }
+
+    private static void writeItems(File itemsFile, List<ExperimentListItem> items) throws IOException
+    {
+        Writer writer = new FileWriter(itemsFile);
+
+        for (ExperimentListItem item : items)
+        {
+            writer.write(item.getFilename()+" "+item.getPattern()+NEW_LINE);
+        }
+
+        writer.close();
+    }
+
+    private static void writeErrorFile(File errorFile, Map<String,String> experimentsWithErrors) throws IOException
+    {
+        Writer writer = new FileWriter(errorFile);
+
+        for (Map.Entry<String,String> error : experimentsWithErrors.entrySet())
+        {
+            writer.write(error.getKey()+" "+error.getValue()+NEW_LINE);
+        }
+
+        writer.close();
     }
 }
