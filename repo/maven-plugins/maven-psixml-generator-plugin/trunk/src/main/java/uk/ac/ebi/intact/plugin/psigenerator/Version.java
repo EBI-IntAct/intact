5b007c7afa1dd7f626f1f29612c140decf4a9638
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-11 15:08:37 +0000

    Implemented PsiXml generator Mojo, which is able to create the psi xml files, put the files in the user defined structure, zip and cluster xmls

---
+++ repo/maven-plugins/maven-psixml-generator-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/psigenerator/Version.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.plugin.psigenerator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>11-Aug-2006</pre>
+ */
+public class Version
+{
+
+    private static final Log log = LogFactory.getLog(Version.class);
+
+    private String number;
+    private String folderName;
+
+
+    public Version()
+    {
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+
+    public String getFolderName()
+    {
+        return folderName;
+    }
+
+    public void setFolderName(String folderName)
+    {
+        this.folderName = folderName;
+    }
+}
--- repo/maven-plugins/maven-psixml-generator-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugin/psigenerator/PsiXmlGeneratorTest.java
+++ repo/maven-plugins/maven-psixml-generator-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugin/psigenerator/PsiXmlGeneratorTest.java
@@ -3,11 +3,9 @@
  * All rights reserved. Please see the file LICENSE
  * in the root directory of this distribution.
  */
-package uk.ac.ebi.intact.plugin.experimentlistgenerator;
+package uk.ac.ebi.intact.plugin.psigenerator;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-import org.apache.maven.plugin.Mojo;
-import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
 
@@ -15,17 +13,17 @@ import java.io.File;
  * TODO: comment this!
  *
  * @author Bruno Aranda (baranda@ebi.ac.uk)
- * @version $Id:$
+ * @version $Id$
  * @since <pre>04/08/2006</pre>
  */
-public class ExperimentListGeneratorTest extends AbstractMojoTestCase
+public class PsiXmlGeneratorTest extends AbstractMojoTestCase
 {
         public void testSimpleGeneration()
             throws Exception
         {
             File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/simple-config.xml" );
 
-            ExperimentListGeneratorMojo mojo = (ExperimentListGeneratorMojo) lookupMojo( "generate-list", pluginXmlFile );
+            PsiXmlGeneratorMojo mojo = (PsiXmlGeneratorMojo) lookupMojo( "psi", pluginXmlFile );
 
             mojo.execute();
         }
