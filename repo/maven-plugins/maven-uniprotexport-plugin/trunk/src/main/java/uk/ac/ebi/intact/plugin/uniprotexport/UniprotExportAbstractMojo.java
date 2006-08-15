59144ff31abd835662ba3f5d9a6bd66c68dc143c
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-15 16:23:05 +0000

    Created uniprot export maven plugin, based on intact-core 1.3-SNAPSHOT, which is able to create the unprot export files (DR, CC and GOA lines)

---
+++ repo/maven-plugins/maven-uniprotexport-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/uniprotexport/UniprotExportAbstractMojo.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.plugin.uniprotexport;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import uk.ac.ebi.intact.config.impl.CustomCoreDataConfig;
+import uk.ac.ebi.intact.context.IntactContext;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>14-Aug-2006</pre>
+ */
+public abstract class UniprotExportAbstractMojo extends AbstractMojo
+{
+
+    /**
+    * Project instance
+    * @parameter default-value="${project}"
+    * @readonly
+    */
+    protected MavenProject project;
+
+    /**
+    * File containing the species
+    * @parameter default-value="target/uniprot-export"
+    * @required
+    */
+    protected File targetPath;
+
+    /**
+     * @parameter default-value="target/hibernate/config/hibernate.cfg.xml"
+     * @required
+     */
+    protected File hibernateConfig;
+
+    /**
+     * Whether to update the existing project files or overwrite them.
+     *
+     * @parameter expression="${overwrite}" default-value="false"
+     */
+    protected boolean overwrite;
+
+    /**
+     * @parameter default-value="uniprotlinks.dat"
+     * @required
+     */
+    private String uniprotLinksFilename;
+
+    /**
+     * @parameter default-value="target/uniprot-export/uniprotlinks.dat"
+     */
+    private File uniprotLinksFile;
+
+    private boolean initialized;
+
+    protected void initialize() throws MojoExecutionException
+    {
+        if (initialized)
+        {
+            return;
+        }
+
+        getLog().info("Using hibernate cfg file: "+hibernateConfig);
+
+        if (!hibernateConfig.exists())
+        {
+            throw new MojoExecutionException("No hibernate config file found: "+hibernateConfig);
+        }
+
+        if (!targetPath.exists())
+        {
+            targetPath.mkdirs();
+        }
+
+        // configure the context
+        CustomCoreDataConfig testConfig = new CustomCoreDataConfig("PsiXmlGeneratorTest", hibernateConfig);
+        testConfig.initialize();
+        IntactContext.initStandaloneContext(testConfig);
+
+        try
+        {
+            getLog().info( "Database instance: " + IntactContext.getCurrentInstance().getDataContext().getDaoFactory().getBaseDao().getDbName() );
+        }
+        catch (SQLException e)
+        {
+            throw new MojoExecutionException("Error loading database name", e);
+        }
+
+        getLog().info( "User: " + IntactContext.getCurrentInstance().getUserContext().getUserId() );
+
+        initialized = true;
+    }
+
+    protected File getUniprotLinksFile()
+    {
+        if (uniprotLinksFile != null && uniprotLinksFile.exists())
+        {
+            return uniprotLinksFile;
+        }
+
+        return new File(targetPath,uniprotLinksFilename);
+    }
+
+
+    public void setUniprotLinksFile(File uniprotLinksFile)
+    {
+        this.uniprotLinksFile = uniprotLinksFile;
+    }
+
+    public void setUniprotLinksFilename(String uniprotLinksFilename)
+    {
+        this.uniprotLinksFilename = uniprotLinksFilename;
+    }
+}
