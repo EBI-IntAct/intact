b69256a2e9bd2c7b16bfd2d0a51b29c489c36d25
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-17 14:49:41 +0000

    Add the triggers that delete any xref for the new xref tables before deleting a cross referenced object so that it does not cause problem for th e key constraint on parent_ac of the xref table.

--- repo/utils/sql/oracle/create_tables.sql
+++ repo/utils/sql/oracle/create_tables.sql
@@ -1116,9 +1116,63 @@ BEGIN
 END;
 /
 
+-- Those triggers are to allow to delete cross referenced object.  Before deleting a 
+-- cross referenced object you need first to delete the xreference that are attached to it. That
+-- what those triggers do.
 
+PROMPT Creating trigger "TRG_IA_BIOSOURCE_XREF_DELETE"
+create or replace TRIGGER TRG_IA_BIOSOURCE_XREF_DELETE BEFORE DELETE ON IA_BIOSOURCE
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_biosource_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
 
+PROMPT Creating trigger "TRG_IA_CVOBJECT_XREF_DELETE"
+create or replace TRIGGER TRG_IA_CVOBJECT_XREF_DELETE BEFORE DELETE ON IA_CONTROLLEDVOCAB
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_controlledvocab_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
 
+PROMPT Creating trigger "TRG_IA_EXPERIMENT_XREF_DELETE"
+create or replace TRIGGER TRG_IA_EXPERIMENT_XREF_DELETE BEFORE DELETE ON IA_EXPERIMENT
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_experiment_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
+
+PROMPT Creating trigger "TRG_IA_FEATURE_XREF_DELETE"
+create or replace TRIGGER TRG_IA_FEATURE_XREF_DELETE BEFORE DELETE ON IA_FEATURE
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_feature_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
+
+PROMPT Creating trigger "TRG_IA_PUBLICATION_XREF_DELETE"
+create or replace TRIGGER TRG_IA_PUBLICATION_XREF_DELETE BEFORE DELETE ON IA_PUBLICATION
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_publication_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
+
+PROMPT Creating trigger "TRG_IA_INTERACTOR_XREF_DELETE"
+create or replace TRIGGER TRG_IA_INTERACTOR_XREF_DELETE BEFORE DELETE ON IA_INTERACTOR
+FOR EACH ROW
+BEGIN
+  DELETE FROM ia_interactor_xref
+  WHERE parent_ac = :OLD.ac;
+END;
+/
 ----------------------------------
 -- Create tables for GO Density --
 ----------------------------------
