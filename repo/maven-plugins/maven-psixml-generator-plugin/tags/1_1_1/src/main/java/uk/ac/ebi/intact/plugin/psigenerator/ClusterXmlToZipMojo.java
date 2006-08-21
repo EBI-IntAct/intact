eb599ab7d0d80ae7ab85d36cc10b9322b431bc94
baranda <brunoaranda@gmail.com>
2006-08-21 13:03:15 +0000

    Tagged 1.1.1

---
+++ repo/maven-plugins/maven-psixml-generator-plugin/tags/1_1_1/src/main/java/uk/ac/ebi/intact/plugin/psigenerator/ClusterXmlToZipMojo.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.plugin.psigenerator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import uk.ac.ebi.intact.application.dataConversion.ZipFileGenerator;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>11-Aug-2006</pre>
+ *
+ * @goal zip-xml
+ * @phase process-resources
+ */
+public class ClusterXmlToZipMojo extends PsiXmlGeneratorAbstractMojo
+{
+
+    private static final Log log = LogFactory.getLog(ClusterXmlToZipMojo.class);
+
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info("ClusterXmlToZipMojo in action"); 
+        ZipFileGenerator.clusterAllXmlFilesFromDirectory( targetPath, true );
+    }
+}
