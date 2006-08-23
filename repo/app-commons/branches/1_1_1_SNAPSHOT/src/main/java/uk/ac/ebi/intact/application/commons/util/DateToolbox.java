a40a05a1d6a9927992c9adb6f23ae8800b0369b4
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-23 10:37:36 +0000

    Change in the DateToolbox to make it use the Calendar object.

--- repo/app-commons/branches/1_1_1_SNAPSHOT/src/main/java/uk/ac/ebi/intact/application/commons/util/DateToolbox.java
+++ repo/app-commons/branches/1_1_1_SNAPSHOT/src/main/java/uk/ac/ebi/intact/application/commons/util/DateToolbox.java
@@ -19,18 +19,18 @@ public class DateToolbox {
     public static String getMonth(int monthNumber){
         String monthName = new String();
         switch (monthNumber) {
-            case 1:  monthName = "JAN"; break;
-            case 2:  monthName = "FEB"; break;
-            case 3:  monthName = "MAR"; break;
-            case 4:  monthName = "APR"; break;
-            case 5:  monthName = "MAY"; break;
-            case 6:  monthName = "JUN"; break;
-            case 7:  monthName = "JUL"; break;
-            case 8:  monthName = "AUG"; break;
-            case 9:  monthName = "SEP"; break;
-            case 10: monthName = "OCT"; break;
-            case 11: monthName = "NOV"; break;
-            case 12: monthName = "DEC"; break;
+            case 0:  monthName = "JAN"; break;
+            case 1:  monthName = "FEB"; break;
+            case 2:  monthName = "MAR"; break;
+            case 3:  monthName = "APR"; break;
+            case 4:  monthName = "MAY"; break;
+            case 5:  monthName = "JUN"; break;
+            case 6:  monthName = "JUL"; break;
+            case 7:  monthName = "AUG"; break;
+            case 8:  monthName = "SEP"; break;
+            case 9: monthName = "OCT"; break;
+            case 10: monthName = "NOV"; break;
+            case 11: monthName = "DEC"; break;
             default: monthName = "Not a month!";break;
         }
 
