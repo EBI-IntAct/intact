183f2da56e1c1e4a5d4a140c9ad73fd05411fb30
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-08-24 16:00:20 +0000

    Prepared release 1.3.5 - Fix to the InteractionUtils.isBinary() method, that didn't return true when there was only one component with stoichiometry 2. - Tried to improve the Uniprot export classes

--- repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/model/util/InteractionUtils.java
+++ repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/model/util/InteractionUtils.java
@@ -30,41 +30,44 @@ public class InteractionUtils
      * @return
      */
     public static boolean isBinaryInteraction(Interaction interaction) {
-        boolean isBinaryInteraction = false;
-
-        int stoichiometrySum = 0;
         Collection<Component> components = interaction.getComponents();
         int componentCount = components.size();
 
-        for ( Component component : components ) {
-            stoichiometrySum += component.getStoichiometry();
-        }
+       if (componentCount == 1)
+       {
+           Component component1 = components.iterator().next();
+           if (component1.getStoichiometry() == 2)
+           {
+               log.debug("Binary interaction "+interaction.getAc()+". Stoichiometry 2, each component with stoichiometry 1");
+               return true;
+           }
+       }
+       else if (componentCount == 2)
+       {
+           Iterator<Component> iterator1 = components.iterator();
 
-        if (stoichiometrySum == 0 && componentCount == 2) {
-            log.debug("Binary interaction. Stoichiometry 0, components 2");
-            isBinaryInteraction = true;
-        } else if (stoichiometrySum == 2 && componentCount == 1) {
-            log.debug("Binary interaction. Stoichiometry 2, components 1");
-        } else {
-
-             if ( componentCount == 2 ) {
-                // check that the stochiometry is 1 for each component
-                Iterator<Component> iterator1 = components.iterator();
-
-                Component component1 = iterator1.next();
-                float stochio1 = component1.getStoichiometry();
-
-                Component component2 = iterator1.next();
-                float stochio2 = component2.getStoichiometry();
-
-                if ( stochio1 == 1 && stochio2 == 1 ) {
-                    log.debug("Binary interaction. Stoichiometry 2, each component with stoichiometry 1");
-                    isBinaryInteraction = true;
-                }
-            }
-        }
-
-        return isBinaryInteraction;
+           Component component1 = iterator1.next();
+           float stochio1 = component1.getStoichiometry();
+           if (stochio1 == 1)
+           {
+               Component component2 = iterator1.next();
+               if (component2.getStoichiometry() == 1)
+               {
+                   log.debug("Binary interaction "+interaction.getAc()+". Stoichiometry 2, each component with stoichiometry 1");
+                   return true;
+               }
+           }
+           else if (stochio1 == 0)
+           {
+               Component component2 = iterator1.next();
+               if (component2.getStoichiometry() == 0)
+               {
+                   log.debug("Binary interaction "+interaction.getAc()+". Stoichiometry 0, components 2");
+                   return true;
+               }
+           }
+       }
+        return false;
     }
 
     /**
