8b4a9ff9ab76cca797ce93b4c529c5ab29e9e386
baranda <brunoaranda@gmail.com>
2006-09-05 08:48:12 +0000

    Tagged 1.4.0

---
+++ repo/intact-core/tags/1_4_0/src/main/java/uk/ac/ebi/intact/context/IntactConfigurator.java
@@ -0,0 +1,370 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.context;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.config.DataConfig;
+import uk.ac.ebi.intact.config.SchemaVersion;
+import uk.ac.ebi.intact.config.impl.StandardCoreDataConfig;
+import uk.ac.ebi.intact.context.impl.IntactContextWrapper;
+import uk.ac.ebi.intact.model.Institution;
+import uk.ac.ebi.intact.model.meta.DbInfo;
+import uk.ac.ebi.intact.persistence.dao.DaoFactory;
+import uk.ac.ebi.intact.persistence.dao.IntactTransaction;
+
+import java.sql.SQLException;
+
+/**
+ * Class to initialize IntAct, and to initialize IntactContexts
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>04/08/2006</pre>
+ */
+public class IntactConfigurator
+{
+    private static final Log log = LogFactory.getLog(IntactConfigurator.class);
+
+    private static final String DEFAULT_INSTITUTION_LABEL = "ebi";
+    private static final String DEFAULT_INSTITUTION_FULL_NAME = "European Bioinformatics Institute";
+    private static final String DEFAULT_INSTITUTION_POSTAL_ADDRESS = "European Bioinformatics Institute; " +
+                                                                    "Wellcome Trust Genome Campus; " +
+                                                                    "Hinxton, Cambridge; " +
+                                                                    "CB10 1SD; " +
+                                                                    "United Kingdom";
+    private static final String DEFAULT_INSTITUTION_URL = "http://www.ebi.ac.uk/";
+
+    private static final String DEFAULT_AC_PREFIX = "UNK";
+    
+    private static final String DEFAULT_READ_ONLY_APP = Boolean.TRUE.toString();
+
+    private static final String INSTITUTION_TO_BE_PERSISTED_FLAG = "uk.ac.ebi.intact.internal.INSTITUTION_TO_BE_PERSISTED";
+    private static final String SCHEMA_VERSION_TO_BE_PERSISTED_FLAG = "uk.ac.ebi.intact.internal.SCHEMA_VERSION_TO_BE_PERSISTED";
+
+
+    /**
+     * Initializes the fundamental intact parameters, such as data configs, the default prefix,  and default data
+     * from the database. Note that it only performs only database read-only, as to write to the database
+     * is not allowed at this point if using the IntactIdGenerator.
+     * @param session the IntactSession to use
+     * @throws IntactInitializationError if something unexpected happends during loading or a check fails
+     */
+    public static void initIntact(IntactSession session) throws IntactInitializationError
+    {
+        log.info("Initializing intact-core...");
+
+        RuntimeConfig config = RuntimeConfig.getCurrentInstance(session);
+
+        if (config.getDefaultDataConfig() == null)
+        {
+            // add the core model data config
+            log.info("Registering standard data-config");
+            StandardCoreDataConfig stdDataConfig = new StandardCoreDataConfig(session);
+            stdDataConfig.initialize();
+            config.addDataConfig(stdDataConfig, true);
+        }
+
+        // load the data configs
+        if (session.containsInitParam(IntactEnvironment.DATA_CONFIG_PARAM_NAME))
+        {
+            String dataConfigValue = session.getInitParam(IntactEnvironment.DATA_CONFIG_PARAM_NAME);
+
+            String[] dataConfigs = dataConfigValue.split(",");
+
+            for (String dataConfigClass : dataConfigs)
+            {
+                dataConfigClass = dataConfigClass.trim();
+                log.info("Registering data-config: " + dataConfigClass);
+
+                try
+                {
+                    DataConfig dataConfig = (DataConfig) (Class.forName(dataConfigClass).newInstance());
+                    dataConfig.initialize();
+                    config.addDataConfig(dataConfig);
+                }
+                catch (Exception e)
+                {
+                    throw new IntactInitializationError("Error initializing data configs", e);
+                }
+            }
+        }
+
+        // load the default prefix for generated ACs
+        String prefix = getInitParamValue(session, IntactEnvironment.AC_PREFIX_PARAM_NAME, DEFAULT_AC_PREFIX);
+        config.setAcPrefix(prefix);
+
+        if (prefix.equals(DEFAULT_AC_PREFIX))
+        {
+            log.warn("The default prefix '"+DEFAULT_AC_PREFIX+"' will be used when persisting data. Usually." +
+                    "this is not the desired functionality if the application is persisting data.");
+        }
+
+        // check the schema Version
+        checkSchemaCompatibility(session);
+
+        // read only
+        String strReadOnly = getInitParamValue(session, IntactEnvironment.READ_ONLY_APP, DEFAULT_READ_ONLY_APP);
+        boolean readOnly = Boolean.parseBoolean(strReadOnly);
+        config.setReadOnlyApp(readOnly);
+        log.debug("Application is read-only: "+readOnly);
+
+        // load the institution
+        loadInstitution(session);
+
+        // preload the most common CvObjects
+        boolean preloadCommonCvs = Boolean.valueOf(getInitParamValue(session, IntactEnvironment.PRELOAD_COMMON_CVS_PARAM_NAME, String.valueOf(Boolean.FALSE)));
+        if (preloadCommonCvs)
+        {
+            log.info("Preloading common CvObjects");
+            CvContext.getCurrentInstance(session).loadCommonCvObjects();
+        }
+    }
+
+    public static IntactContext createIntactContext(IntactSession session)
+    {
+        if (RuntimeConfig.getCurrentInstance(session).getDataConfigs().isEmpty())
+        {
+            log.warn("No data configs found. Re-initializing IntAct");
+            initIntact(session);
+        }
+
+        String defaultUser = null;
+
+        if (log.isDebugEnabled())
+        {
+            log.debug("Data Configs registered: "+RuntimeConfig.getCurrentInstance(session).getDataConfigs());
+        }
+
+        DaoFactory daoFactory = DaoFactory.getCurrentInstance(session,
+                RuntimeConfig.getCurrentInstance(session).getDefaultDataConfig());
+
+        try
+        {
+            IntactTransaction tx = daoFactory.beginTransaction();
+            defaultUser = daoFactory.getBaseDao().getDbUserName();
+            tx.commit();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        log.debug("Creating user context, for user: "+defaultUser);
+        UserContext userContext = new UserContext(defaultUser);
+
+        log.debug("Creating data context...");
+        DataContext dataContext = new DataContext(session);
+
+        // start a context
+        log.info("Creating IntactContext...");
+        IntactContext context = new IntactContextWrapper(userContext, dataContext, session);
+        persistInstitutionIfNecessary(context);
+        persistSchemaVersionIfNecessary(context);
+
+        return context;
+    }
+
+    private static void checkSchemaCompatibility(IntactSession session)
+    {
+        SchemaVersion requiredVersion = SchemaVersion.minimumVersion();
+
+        DaoFactory daoFactory = DaoFactory.getCurrentInstance(session, RuntimeConfig.getCurrentInstance(session).getDefaultDataConfig());
+
+        IntactTransaction tx = daoFactory.beginTransaction();
+        DbInfo dbInfoSchemaVersion = daoFactory.getDbInfoDao().get(DbInfo.SCHEMA_VERSION);
+        tx.commit();
+
+        SchemaVersion schemaVersion;
+
+        if (dbInfoSchemaVersion == null)
+        {
+            log.warn("Schema version does not exist. Will be created");
+            setSchemaVersionToBePersisted(session);
+            return;
+        }
+
+        try
+        {
+            schemaVersion = SchemaVersion.parse(dbInfoSchemaVersion.getValue());
+        }
+        catch (Exception e)
+        {
+            throw new IntactInitializationError("Error getting schema version", e);
+        }
+
+        log.info("Schema Version: "+schemaVersion);
+
+        if (!schemaVersion.isCompatibleWith(requiredVersion))
+        {
+            throw new IntactInitializationError("Database schema version "+requiredVersion+" is required" +
+                    " to use this version of intact-core. Schema version found: "+schemaVersion);
+        }
+    }
+
+    private static String getInitParamValue(IntactSession session, String initParamName, String defaultValue )
+    {
+        return getInitParamValue(session,initParamName,defaultValue,null);
+    }
+
+    private static String getInitParamValue(IntactSession session, String initParamName, String defaultValue, String systemPropertyDefault )
+    {
+        String initParamValue;
+
+        if (session.containsInitParam(initParamName))
+        {
+            initParamValue = session.getInitParam(initParamName);
+            log.debug(initParamName+": "+initParamValue);
+        }
+        else
+        {
+            if (session.isWebapp())
+            {
+                log.warn("Init-Param missing in web.xml: "+initParamName);
+            }
+            else
+            {
+                if (systemPropertyDefault != null)
+                {
+                    String propValue = System.getProperty(systemPropertyDefault);
+
+                    if (propValue != null)
+                    {
+                        log.debug("Found environment property for default value: "+propValue);
+                        defaultValue = propValue;
+                    }
+                }
+            }
+            log.debug("Using default value for param "+initParamName+": "+defaultValue);
+
+            initParamValue = defaultValue;
+        }
+
+        return initParamValue;
+    }
+
+    private static void loadInstitution(IntactSession session)
+    {
+        String institutionLabel = getInitParamValue(session, IntactEnvironment.INSTITUTION_LABEL, null, "institution");
+
+        if (institutionLabel == null)
+        {
+            if (session.isWebapp())
+            {
+                throw new IntactException("A institution label is mandatory. " +
+                        "Provide it by setting the init parameter "+IntactEnvironment.INSTITUTION_LABEL+" " +
+                        "in the web.xml file");
+            }
+            else
+            {
+                throw new IntactException("A institution label is mandatory. " +
+                        "Provide it by setting the environment variable 'institution'" +
+                        " when executing the java command. (e.g. java ... -Dinstitution=yourInstitution)." +
+                        " You can also set the init parameter "+IntactEnvironment.INSTITUTION_LABEL+" in the IntactSession Object. ");
+            }
+        }
+
+        DaoFactory daoFactory = DaoFactory.getCurrentInstance(session, RuntimeConfig.getCurrentInstance(session).getDefaultDataConfig());
+
+        IntactTransaction tx = daoFactory.beginTransaction();
+        Institution institution = daoFactory
+                .getInstitutionDao().getByShortLabel( institutionLabel );
+        tx.commit();
+
+        if (institution == null)
+        {
+            // doesn't exist, create it
+            institution = new Institution( institutionLabel );
+
+            String fullName;
+            String postalAddress;
+            String url;
+
+            if (institutionLabel.equalsIgnoreCase(DEFAULT_INSTITUTION_LABEL))
+            {
+                fullName = getInitParamValue(session, IntactEnvironment.INSTITUTION_FULL_NAME, DEFAULT_INSTITUTION_FULL_NAME);
+                postalAddress = getInitParamValue(session, IntactEnvironment.INSTITUTION_POSTAL_ADDRESS, DEFAULT_INSTITUTION_POSTAL_ADDRESS);
+                url = getInitParamValue(session, IntactEnvironment.INSTITUTION_URL, DEFAULT_INSTITUTION_URL);
+            }
+            else
+            {
+                fullName = getInitParamValue(session, IntactEnvironment.INSTITUTION_FULL_NAME, "");
+                postalAddress = getInitParamValue(session, IntactEnvironment.INSTITUTION_POSTAL_ADDRESS, "");
+                url = getInitParamValue(session, IntactEnvironment.INSTITUTION_URL, "");
+            }
+
+            institution.setFullName(fullName);
+            institution.setPostalAddress(postalAddress);
+            institution.setUrl(url);
+
+            log.warn("Institution does not exist. Will be created, overriding any read-only attribute");
+            setInstitutionToBePersisted(session);
+        }
+
+        RuntimeConfig.getCurrentInstance(session).setInstitution(institution);
+        log.debug("Institution: "+institution.getShortLabel());
+    }
+
+    private static void persistInstitutionIfNecessary(IntactContext context)
+    {
+        IntactSession session = context.getSession();
+
+        Object obj = session.getApplicationAttribute(INSTITUTION_TO_BE_PERSISTED_FLAG);
+
+        if (obj == null)
+        {
+            return;
+        }
+
+        boolean needsToBePersisted = (Boolean) obj;
+
+        if (needsToBePersisted)
+        {
+            Institution institution = context.getConfig().getInstitution();
+
+            log.debug("Persisting institution");
+            context.getDataContext().getDaoFactory().getInstitutionDao().persist( institution );
+
+            session.setApplicationAttribute(INSTITUTION_TO_BE_PERSISTED_FLAG, Boolean.FALSE);
+        }
+    }
+
+    private static void persistSchemaVersionIfNecessary(IntactContext context)
+    {
+        IntactSession session = context.getSession();
+
+        Object obj = session.getApplicationAttribute(SCHEMA_VERSION_TO_BE_PERSISTED_FLAG);
+
+        if (obj == null)
+        {
+            return;
+        }
+
+        boolean needsToBePersisted = (Boolean) obj;
+
+        if (needsToBePersisted)
+        {
+            DbInfo dbInfo = new DbInfo(DbInfo.SCHEMA_VERSION, SchemaVersion.minimumVersion().toString());
+
+            log.debug("Persisting schema version");
+            context.getDataContext().getDaoFactory().getDbInfoDao().persist( dbInfo );
+
+            session.setApplicationAttribute(SCHEMA_VERSION_TO_BE_PERSISTED_FLAG, Boolean.FALSE);
+        }
+    }
+
+    private static void setInstitutionToBePersisted(IntactSession session)
+    {
+        session.setApplicationAttribute(INSTITUTION_TO_BE_PERSISTED_FLAG, Boolean.TRUE);
+    }
+
+    private static void setSchemaVersionToBePersisted(IntactSession session)
+    {
+        session.setApplicationAttribute(SCHEMA_VERSION_TO_BE_PERSISTED_FLAG, Boolean.TRUE);
+    }
+
+}
---
+++ repo/intact-core/tags/1_4_0/src/main/java/uk/ac/ebi/intact/context/impl/WebappSession.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.context.impl;
+
+import uk.ac.ebi.intact.context.IntactSession;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * Webapp session, that uses the session and the request to store attributes
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>04/08/2006</pre>
+ */
+public class WebappSession extends IntactSession
+{
+
+    private HttpSession session;
+    private ServletContext servletContext;
+    private HttpServletRequest request;
+
+    public WebappSession(ServletContext servletContext, HttpSession session, HttpServletRequest request)
+    {
+        this.session = session;
+        this.servletContext = servletContext;
+        this.request = request;
+
+        readDefaultProperties();
+    }
+
+    public WebappSession(ServletContext servletContext, HttpSession session, HttpServletRequest request, Properties properties)
+    {
+        this(servletContext, session, request);
+
+        Enumeration<String> propNames = (Enumeration<String>) properties.propertyNames();
+
+        while (propNames.hasMoreElements())
+        {
+           String propName = propNames.nextElement();
+           setInitParam(propName, properties.getProperty(propName));
+        }
+    }
+
+    public Object getApplicationAttribute(String name)
+    {
+        return servletContext.getAttribute(name);
+    }
+
+    public void setApplicationAttribute(String name, Object attribute)
+    {
+        servletContext.setAttribute(name, attribute);
+    }
+
+    public Serializable getAttribute(String name)
+    {
+        return (Serializable) session.getAttribute(name);
+    }
+
+    public void setAttribute(String name, Serializable attribute)
+    {
+        session.setAttribute(name, attribute);
+    }
+
+    public Object getRequestAttribute(String name)
+    {
+        return request.getAttribute(name);
+    }
+
+    public void setRequestAttribute(String name, Object value)
+    {
+       request.setAttribute(name,value);
+    }
+
+    public boolean containsInitParam(String name)
+    {
+        return (servletContext.getInitParameter(name) != null);
+    }
+
+    public String getInitParam(String name)
+    {
+        return servletContext.getInitParameter(name);
+    }
+
+    public void setInitParam(String name, String value)
+    {
+        throw new UnsupportedOperationException("It is not possible to add init-params to a webapp session.");
+    }
+
+    public boolean isWebapp()
+    {
+        return true;
+    }
+}
