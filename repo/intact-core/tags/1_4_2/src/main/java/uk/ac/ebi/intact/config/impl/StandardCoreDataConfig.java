691823128d60a885685be0b71c98828cfeb64d3c
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-09-15 13:26:43 +0000

    Tagged 1.4.2

---
+++ repo/intact-core/tags/1_4_2/src/main/java/uk/ac/ebi/intact/config/impl/StandardCoreDataConfig.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.config.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+
+import uk.ac.ebi.intact.model.Interactor;
+import uk.ac.ebi.intact.model.meta.DbInfo;
+import uk.ac.ebi.intact.context.IntactSession;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>07-Aug-2006</pre>
+ */
+public class StandardCoreDataConfig extends AbstractHibernateDataConfig
+{
+
+    private static final Log log = LogFactory.getLog(StandardCoreDataConfig.class);
+
+    public static final String NAME = "uk.ac.ebi.intact.config.STANDARD_CORE";
+
+    public StandardCoreDataConfig(IntactSession session)
+    {
+        super(session);
+    }
+
+    public String getName()
+    {
+        return NAME;
+    }
+
+    protected List<String> getPackagesWithEntities()
+    {
+        List<String> packages = new ArrayList<String>(1);
+
+        // /uk/ac/ebi/intact/model
+        packages.add(Interactor.class.getPackage().getName());
+        packages.add(DbInfo.class.getPackage().getName());
+
+        return packages;
+    }
+
+    protected File getConfigFile()
+    {
+        // uses the default file in the classpath (/hibernate.cfg.xml)
+        return null;
+    }
+}
