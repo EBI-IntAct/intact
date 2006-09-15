23227cc676c3e0dd7b295f15f6b6f25a4c7d3e73
baranda <brunoaranda@gmail.com>
2006-09-15 16:14:08 +0000

    - Added methods needed by the Uniprot Project, to get the interactions using a pair of proteins

--- repo/search/search-ws/trunk/src/main/java/uk/ac/ebi/intact/search/ws/SearchService.java
+++ repo/search/search-ws/trunk/src/main/java/uk/ac/ebi/intact/search/ws/SearchService.java
@@ -7,23 +7,17 @@ package uk.ac.ebi.intact.search.ws;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.cfg.Environment;
-import org.hibernate.SessionFactory;
-
-import java.util.*;
-import java.io.IOException;
-
-import uk.ac.ebi.intact.model.ProteinImpl;
-import uk.ac.ebi.intact.persistence.dao.DaoFactory;
+import uk.ac.ebi.intact.context.IntactConfigurator;
 import uk.ac.ebi.intact.context.IntactContext;
 import uk.ac.ebi.intact.context.IntactSession;
-import uk.ac.ebi.intact.context.IntactConfigurator;
 import uk.ac.ebi.intact.context.impl.WebServiceSession;
+import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.persistence.dao.DaoFactory;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.naming.NamingEnumeration;
-import javax.naming.NameClassPair;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * TODO comment this!
@@ -46,6 +40,63 @@ public class SearchService
         IntactSession intactSession = new WebServiceSession();
         IntactConfigurator.initIntact(intactSession);
         daoFactory = IntactContext.getCurrentInstance().getDataContext().getDaoFactory();
+    }
+
+    public InteractionInfo[] getInteractionInfoUsingUniprotIds(String uniprotId1, String uniprotId2)
+    {
+        List<ProteinImpl> protsForId1 = daoFactory.getProteinDao().getByUniprotId(uniprotId1);
+        List<ProteinImpl> protsForId2 = daoFactory.getProteinDao().getByUniprotId(uniprotId2);
+
+        List<InteractionInfo> interInfos = new ArrayList<InteractionInfo>();
+
+        for (Protein prot1 : protsForId1)
+        {
+             for (Protein prot2 : protsForId2)
+             {
+                 InteractionInfo[] results = getInteractionInfoUsingIntactIds(prot1.getAc(), prot2.getAc());
+                 for (InteractionInfo result : results)
+                 {
+                     interInfos.add(result);
+                 }
+             }
+        }
+
+        return interInfos.toArray(new InteractionInfo[interInfos.size()]);
+    }
+
+    public InteractionInfo[] getInteractionInfoUsingIntactIds(String id1, String id2)
+    {
+        List<Interaction> interactions = daoFactory.getInteractionDao().getInteractionsForProtPair(id1,id2);
+
+        List<InteractionInfo> interInfos = new ArrayList<InteractionInfo>();
+
+        for (Interaction inter : interactions)
+        {
+            String intactAc = inter.getAc();
+            String shortName = inter.getShortLabel();
+            String fullName = inter.getFullName();
+
+            CvInteractionType type = inter.getCvInteractionType();
+            String interactionType = type.getShortLabel();
+            String description = type.getFullName();
+
+            // annotation definition
+            String definition = null;
+
+            for (Annotation annotation : type.getAnnotations())
+            {
+                if (annotation.getCvTopic().getShortLabel().equals(CvTopic.DEFINITION))
+                {
+                    definition = annotation.getAnnotationText();
+                    break;
+                }
+            }
+
+            InteractionInfo interInfo = new InteractionInfo(intactAc, shortName, fullName, interactionType, description, definition);
+            interInfos.add(interInfo);
+        }
+
+        return interInfos.toArray(new InteractionInfo[interInfos.size()]);
     }
 
     /**
