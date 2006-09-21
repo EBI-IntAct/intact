1121517f48f4aa3abe0fa1c42b382019d8903d6e
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-09-21 15:21:59 +0000

    Tagged 1.2.0

---
+++ repo/search/search-wsclient/tags/1_2_0/src/test/java/uk/ac/ebi/intact/search/wsclient/SearchClientTest.java
@@ -0,0 +1,55 @@
+package uk.ac.ebi.intact.search.wsclient;
+
+import junit.framework.TestCase;
+import uk.ac.ebi.intact.search.wsclient.generated.InteractionInfo;
+
+import java.util.List;
+
+
+public class SearchClientTest extends TestCase
+{
+
+    private static final String LOCALHOST_URL = "http://localhost:8080/search-ws/search?wsdl";
+
+    public void testFindPartnersUsingUniprotIds() throws Exception
+    {
+        SearchServiceClient client = new SearchServiceClient();
+        List<String> uniprotIds = client.findPartnersUsingUniprotIds("Q9VZ59");
+
+        for (String id : uniprotIds)
+        {
+            System.out.println(id);
+        }
+    }
+
+    public void testGetInteractionInfoUsingIntactIds() throws Exception
+    {
+        SearchServiceClient client = new SearchServiceClient();
+        List<InteractionInfo> interInfos = client.getInteractionInfoUsingIntactIds("EBI-1004115","EBI-710997");
+
+        for (InteractionInfo interInfo : interInfos)
+        {
+            System.out.println(interInfo.getIntactAc());
+        }
+
+    }
+
+    public void testGetInteractionInfoUsingUniprotIds() throws Exception
+    {
+        SearchServiceClient client = new SearchServiceClient();
+        List<InteractionInfo> interInfos = client.getInteractionInfoUsingUniprotIds("Q15691","P54274");
+
+        for (InteractionInfo interInfo : interInfos)
+        {
+            System.out.println(interInfo.getIntactAc());
+        }
+
+    }
+     
+
+    public void testServiceVersion() throws Exception
+    {
+        SearchServiceClient client = new SearchServiceClient();
+        System.out.println(client.getServiceVersion());
+    }
+}
