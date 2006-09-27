236bf30a26ede4a3642359ec7d0bc17094a117d0
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-09-27 15:37:06 +0000

    Tagged 1.4.5

---
+++ repo/intact-core/tags/1_4_5/src/main/java/uk/ac/ebi/intact/context/UserContext.java
@@ -0,0 +1,115 @@
+package uk.ac.ebi.intact.context;
+
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.config.impl.StandardCoreDataConfig;
+import uk.ac.ebi.intact.config.impl.AbstractHibernateDataConfig;
+import uk.ac.ebi.intact.config.DataConfig;
+import uk.ac.ebi.intact.context.impl.StandaloneSession;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.io.Serializable;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+/**
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class UserContext implements Serializable
+{
+
+    private static final String SESSION_ATT_NAME = UserContext.class.getName();
+
+    private String userId = null;
+    private String userMail;
+    private String userPassword;
+
+    private Connection connection;
+
+    public UserContext(String userId, String userPassword)
+    {
+        this.userId = userId;
+        this.userPassword = userPassword;
+    }
+
+    public static UserContext getCurrentInstance(IntactSession session)
+    {
+        if (!session.isRequestAvailable())
+        {
+            throw new IntactException("Cannot get a UserContext if the IntactSession does not have request and session scope");
+        }
+
+        Object obj = session.getAttribute(SESSION_ATT_NAME);
+
+        if (obj != null)
+        {
+            return (UserContext)obj;
+        }
+
+        UserContext userContext = createDefaultUserContext(session);
+        session.setAttribute(SESSION_ATT_NAME, userContext);
+
+        return userContext;
+    }
+
+    private static UserContext createDefaultUserContext(IntactSession session)
+    {
+        AbstractHibernateDataConfig defaultDataConfig = (AbstractHibernateDataConfig) RuntimeConfig.getCurrentInstance(session).getDefaultDataConfig();
+
+        Configuration configuration = defaultDataConfig.getConfiguration();
+        //configuration.configure();
+
+        String currentUser = configuration.getProperty(Environment.USER);
+        String password = configuration.getProperty(Environment.PASS);
+
+        return new UserContext(currentUser, password);
+    }
+
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getUserMail()
+    {
+        if (userMail == null)
+        {
+            return userId+"@ebi.ac.uk";
+
+        }
+        return userMail;
+    }
+
+    public void setUserMail(String userMail)
+    {
+        this.userMail = userMail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Connection getConnection()
+    {
+        return connection;
+    }
+
+    public void setConnection(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+}
