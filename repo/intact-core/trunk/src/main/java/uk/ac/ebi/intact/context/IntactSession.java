e342492908eac90daba8e8f00a1b66ddd2715515
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-10-16 11:32:01 +0000

    Search API: - Fixed a couple of issues with the DAO, that returned duplicated rows in some cases - SearchableQuery can be created using a String

--- repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/context/IntactSession.java
+++ repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/context/IntactSession.java
@@ -22,7 +22,7 @@ import java.util.Properties;
  * @version $Id$
  * @since <pre>04/08/2006</pre>
  */
-public abstract class IntactSession implements Serializable
+public abstract class IntactSession
 {
     private static final String DEFAULT_PROP_FILE = "/intact.properties";
 
