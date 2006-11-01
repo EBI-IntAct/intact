2df26b6efc2dd0bfe3e453ea5f8860473db58993
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-01 10:54:12 +0000

    Fix javascript to point out at not allowed caracteres.

--- repo/editor/editor-app/trunk/src/main/webapp/js/annotation.js
+++ repo/editor/editor-app/trunk/src/main/webapp/js/annotation.js
@@ -6,19 +6,41 @@
  * Version: $Id$
  */
 function validateComment(element, evt) {
+    frm=document.forms[0];
     var keyCode = evt.which ? evt.which : evt.keyCode;
     //window.alert(keyCode);
     // Allow backspace or else a user can't delete his/own text!!
     if ((keyCode > 31 && keyCode < 127) || keyCode == 08) {
         var desc = element.value;//document.forms[0].elements['newAnnotation.description'].value;
-        if (desc.charAt(desc.length - 1) == ' ' && keyCode == 32) {
+        if (desc.charAt(desc.length - 1) == ' ' && desc.charAt(desc.length - 2) == ' ') {
+            //keyCode == 32) {
             window.alert("Multiple spaces are not allowed");
+            frm.dispatch.disabled = true;
             return false;
         }
+        frm.dispatch.disabled = false;
+
         return true;
     }
+
+    var s = element.value;
+    var o="";
+    for( m=0;s.charAt(m);++m ) {
+		if ( (c=s.charCodeAt(m)) < 128 && c != 38 ) {
+			o+=s.charAt(m);
+  	        } else if (c==38) {
+			o+="&";
+		} else {
+			o+="&#"+c+";";
+		}
+
+	}
+
+
     msg = "The character you entered is not allowed. Only Unicode characters from 0020";
-    msg += "(space) to 007E(~) are allowed";
+    msg += "(space) to 007E(~) are allowed : '"+o+"`'" ;
+    o="";
     window.alert(msg);
+    document.anchors('annotation').dispatch.disabled= true;
     return false;
 }
