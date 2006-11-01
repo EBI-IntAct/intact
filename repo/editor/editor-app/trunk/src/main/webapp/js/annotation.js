b7486a05d162f071ca66fe509408f1dfdf98e9a4
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-01 11:14:47 +0000

    Removing the document.forms[0].dispatch=true or false as they are not working.

--- repo/editor/editor-app/trunk/src/main/webapp/js/annotation.js
+++ repo/editor/editor-app/trunk/src/main/webapp/js/annotation.js
@@ -6,7 +6,6 @@
  * Version: $Id$
  */
 function validateComment(element, evt) {
-    frm=document.forms[0];
     var keyCode = evt.which ? evt.which : evt.keyCode;
     //window.alert(keyCode);
     // Allow backspace or else a user can't delete his/own text!!
@@ -15,10 +14,9 @@ function validateComment(element, evt) {
         if (desc.charAt(desc.length - 1) == ' ' && desc.charAt(desc.length - 2) == ' ') {
             //keyCode == 32) {
             window.alert("Multiple spaces are not allowed");
-            frm.dispatch.disabled = true;
+            form.dispatch.disabled = true;
             return false;
         }
-        frm.dispatch.disabled = false;
 
         return true;
     }
@@ -33,14 +31,11 @@ function validateComment(element, evt) {
 		} else {
 			o+="&#"+c+";";
 		}
-
 	}
-
 
     msg = "The character you entered is not allowed. Only Unicode characters from 0020";
     msg += "(space) to 007E(~) are allowed : '"+o+"`'" ;
     o="";
     window.alert(msg);
-    document.anchors('annotation').dispatch.disabled= true;
     return false;
 }
