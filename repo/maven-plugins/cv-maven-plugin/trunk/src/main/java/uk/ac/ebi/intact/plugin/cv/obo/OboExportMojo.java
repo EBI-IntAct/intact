267d1b7a4c3da53cf8423ed2493ce073435d4fed
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-08 15:53:23 +0000

    Plugin created. The release DownloadCVs tasks will be replaced by this plugin

---
+++ repo/maven-plugins/cv-maven-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/cv/obo/OboExportMojo.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2006 The European Bioinformatics Institute.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.plugin.cv.obo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import uk.ac.ebi.intact.plugin.IntactHibernateMojo;
+import uk.ac.ebi.intact.plugin.MojoUtils;
+import uk.ac.ebi.intact.dbutil.cv.DownloadCVs;
+
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Export an OBO file from the provided database in the hibernateConfig file
+ *
+ * @goal obo-exp
+ * 
+ * @phase process-resources
+ */
+public class OboExportMojo
+        extends IntactHibernateMojo
+{
+
+    /**
+     * @parameter expression="${project.build.directory}/intact-exported.obo"
+     * @required
+     */
+    private File exportedOboFile;
+
+    /**
+     * Main execution method, which is called after hibernate has been initialized
+     */
+    public void executeIntactMojo()
+        throws MojoExecutionException, MojoFailureException, IOException
+    {
+        MojoUtils.prepareFile(exportedOboFile, true);
+
+        BufferedWriter out = new BufferedWriter(new FileWriter(exportedOboFile));
+
+        DownloadCVs downloadCVs = new DownloadCVs();
+        downloadCVs.download(out);
+
+
+        out.flush();
+        out.close();
+
+        getLog().info("Closed " + exportedOboFile);
+    }
+}
