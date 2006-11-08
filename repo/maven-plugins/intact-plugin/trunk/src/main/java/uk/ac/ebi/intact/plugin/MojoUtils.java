d1ae6a08c894f3ea8a52777c11eb1da0ef5818dc
baranda <brunoaranda@gmail.com>
2006-11-08 10:04:29 +0000

    Added base library for intact maven plugins

---
+++ repo/maven-plugins/intact-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugin/MojoUtils.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2006 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package uk.ac.ebi.intact.plugin;
+
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Utilities for intact mojos
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MojoUtils
+{
+    private MojoUtils(){}
+
+    /**
+     * Prepares a file to be used, checking if it exists and creating the parent folder if necessary
+     * @param file the file to check
+     * @param createParentFolder if the parent of the file does not exist, it will create it
+     * @throws IOException
+     */
+    public static void prepareFile(File file, boolean createParentFolder) throws IOException
+    {
+        if (file.exists())
+        {
+            return;
+        }
+
+        if (createParentFolder)
+        {
+            if (file.isDirectory())
+            {
+                file.mkdirs();
+            }
+            else
+            {
+                file.getParentFile().mkdirs();
+            }
+        }
+    }
+}
