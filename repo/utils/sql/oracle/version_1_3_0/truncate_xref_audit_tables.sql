b38b5100e638a032733e8d63940fddcd9b27f2b5
skerrien <samuel.kerrien@gmail.com>
2006-11-13 13:51:05 +0000

    Initial checkin

---
+++ repo/utils/sql/oracle/version_1_3_0/truncate_xref_audit_tables.sql
@@ -0,0 +1,12 @@
+--
+-- Delete the content of all new xrefs tables
+--
+
+TRUNCATE TABLE IA_biosource_xref_audit;
+TRUNCATE TABLE IA_controlledvocab_xref_audit;
+TRUNCATE TABLE IA_feature_xref_audit;
+TRUNCATE TABLE IA_interactor_xref_audit;
+TRUNCATE TABLE IA_experiment_xref_audit;
+TRUNCATE TABLE IA_publication_xref_audit;
+TRUNCATE TABLE IA_component_xref_audit;
+
