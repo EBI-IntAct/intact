48b6cfc45b27aa202591da6385d90285c5edc3c6
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-13 14:20:11 +0000

    Added test for isUnaryInteraction.

--- repo/intact-core/trunk/src/test/java/uk/ac/ebi/intact/modelt/util/InteractionUtilsTest.java
+++ repo/intact-core/trunk/src/test/java/uk/ac/ebi/intact/modelt/util/InteractionUtilsTest.java
@@ -23,8 +23,8 @@ import java.util.List;
  * @since 08/22/2006
  */
 public class InteractionUtilsTest extends TestCase {
-    public InteractionUtilsTest(String name) {
-        super(name);
+    public InteractionUtilsTest( String name ) {
+        super( name );
     }
 
     public void setUp() throws Exception {
@@ -35,94 +35,120 @@ public class InteractionUtilsTest extends TestCase {
         super.tearDown();
     }
 
-    public void testIsBinaryInteraction() throws Exception
-    {
-        assertTrue(InteractionUtils.isBinaryInteraction(createBinaryInteraction()));
-        assertTrue(InteractionUtils.isBinaryInteraction(createSelfBinaryInteraction()));
+    public void testIsBinaryInteraction() throws Exception {
+        assertTrue( InteractionUtils.isBinaryInteraction( createBinaryInteraction() ) );
+        assertTrue( InteractionUtils.isBinaryInteraction( createSelfBinaryInteraction() ) );
     }
 
-    public void testIsSelfInteraction() throws Exception
-    {
-        assertTrue(InteractionUtils.isSelfBinaryInteraction(createSelfBinaryInteraction()));
-        assertFalse(InteractionUtils.isSelfBinaryInteraction(createBinaryInteraction()));
+    public void testIsSelfInteraction() throws Exception {
+        assertTrue( InteractionUtils.isSelfBinaryInteraction( createSelfBinaryInteraction() ) );
+        assertFalse( InteractionUtils.isSelfBinaryInteraction( createBinaryInteraction() ) );
     }
 
-    public void testIsSelfInteraction2() throws Exception
-    {
-        assertTrue(InteractionUtils.isSelfBinaryInteraction(createSelfInteraction2()));
+    public void testIsSelfInteraction2() throws Exception {
+        assertTrue( InteractionUtils.isSelfBinaryInteraction( createSelfInteraction2() ) );
     }
 
-    public void testContainsNonProteinInteractors() throws Exception
-    {
-        assertTrue(InteractionUtils.containsNonProteinInteractors(createBinaryInteractionWithNonProtein()));
-        assertFalse(InteractionUtils.containsNonProteinInteractors(createBinaryInteraction()));
+    public void testContainsNonProteinInteractors() throws Exception {
+        assertTrue( InteractionUtils.containsNonProteinInteractors( createBinaryInteractionWithNonProtein() ) );
+        assertFalse( InteractionUtils.containsNonProteinInteractors( createBinaryInteraction() ) );
+    }
+
+    public void testIsUnaryInteraction() {
+
+        // true unary
+        assertTrue( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 0f ) ) );
+        assertTrue( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 0.5f ) ) );
+        assertTrue( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 1.0f ) ) );
+
+        // false unary
+        assertFalse( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 1.0001f ) ) );
+        assertFalse( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 1.5f ) ) );
+        assertFalse( InteractionUtils.isUnaryInteraction( createUnaryInteraction( 99f ) ) );
+
+        // true binary and self
+        assertFalse( InteractionUtils.isUnaryInteraction( createBinaryInteraction( ) ) );
+        assertFalse( InteractionUtils.isUnaryInteraction( createBinaryInteractionWithNonProtein( ) ) );
+        assertFalse( InteractionUtils.isUnaryInteraction( createSelfBinaryInteraction( ) ) );
+        assertFalse( InteractionUtils.isUnaryInteraction( createSelfInteraction2( ) ) );
     }
 
     public static Test suite() {
-        return new TestSuite(InteractionUtilsTest.class);
+        return new TestSuite( InteractionUtilsTest.class );
     }
 
-    private InteractionImpl createBinaryInteraction()
-    {
+    private InteractionImpl createBinaryInteraction() {
         List<Component> comps = new ArrayList<Component>();
-        comps.add(createProteinComponent("EBI-1"));
-        comps.add(createProteinComponent("EBI-2"));
+        comps.add( createProteinComponent( "EBI-1" ) );
+        comps.add( createProteinComponent( "EBI-2" ) );
 
-        return new InteractionImpl(new ArrayList(), comps, null, null, "Int-"+System.currentTimeMillis(), null);
+        return new InteractionImpl( new ArrayList(), comps, null, null, "Int-" + System.currentTimeMillis(), null );
     }
 
-    private InteractionImpl createBinaryInteractionWithNonProtein()
-    {
+    private InteractionImpl createBinaryInteractionWithNonProtein() {
         List<Component> comps = new ArrayList<Component>();
-        comps.add(createProteinComponent("EBI-1"));
-        comps.add(createNonProteinComponent());
+        comps.add( createProteinComponent( "EBI-1" ) );
+        comps.add( createNonProteinComponent() );
 
-        return new InteractionImpl(new ArrayList(), comps, null, null, "Int-"+System.currentTimeMillis(), null);
+        return new InteractionImpl( new ArrayList(), comps, null, null, "Int-" + System.currentTimeMillis(), null );
     }
 
-    private InteractionImpl createSelfBinaryInteraction()
-    {
+    private InteractionImpl createSelfBinaryInteraction() {
         List<Component> comps = new ArrayList<Component>();
 
-        Component c = createProteinComponent("EBI-1");
-        c.setStoichiometry(2.0f);
+        Component c = createProteinComponent( "EBI-1" );
+        c.setStoichiometry( 2.0f );
         CvComponentRole role = new CvComponentRole();
-        role.setShortLabel(CvComponentRole.SELF);
-        role.addXref(new CvObjectXref(null, null, CvComponentRole.SELF_PSI_REF, null));
-        c.setCvComponentRole(role);
-        comps.add(c);
+        role.setShortLabel( CvComponentRole.SELF );
+        role.addXref( new CvObjectXref( null, null, CvComponentRole.SELF_PSI_REF, null ) );
+        c.setCvComponentRole( role );
+        comps.add( c );
 
-        return new InteractionImpl(new ArrayList(), comps, null, null, "Int-"+System.currentTimeMillis(), null);
+        return new InteractionImpl( new ArrayList(), comps, null, null, "Int-" + System.currentTimeMillis(), null );
     }
 
-    private InteractionImpl createSelfInteraction2()
-    {
+    private InteractionImpl createSelfInteraction2() {
         List<Component> comps = new ArrayList<Component>();
 
-        Component c = createProteinComponent("EBI-1");
+        Component c = createProteinComponent( "EBI-1" );
         CvComponentRole role = new CvComponentRole();
         //role.setShortLabel(CvComponentRole.SELF);
         //role.addXref(new CvObjectXref(null, new CvDatabase(), CvComponentRole.SELF_PSI_REF, new CvXrefQualifier()));
-        c.setCvComponentRole(role);
-        comps.add(c);
-        comps.add(c);
+        c.setCvComponentRole( role );
+        comps.add( c );
+        comps.add( c );
 
-        return new InteractionImpl(new ArrayList(), comps, null, null, "Int-"+System.currentTimeMillis(), null);
+        return new InteractionImpl( new ArrayList(), comps, null, null, "Int-" + System.currentTimeMillis(), null );
     }
 
-     private Component createProteinComponent(String ac)
-    {
-        ProteinImpl prot = new ProteinImpl(null, null, "Prot-"+System.currentTimeMillis(), null);
-        prot.setAc(ac);
-        Component c = new Component(null, new InteractionImpl(), prot, new CvComponentRole());
-        c.setInteractorAc(ac);
+    private Component createProteinComponent( String ac ) {
+        ProteinImpl prot = new ProteinImpl( null, null, "Prot-" + System.currentTimeMillis(), null );
+        prot.setAc( ac );
+        Component c = new Component( null, new InteractionImpl(), prot, new CvComponentRole() );
+        c.setInteractorAc( ac );
 
         return c;
     }
 
-    private Component createNonProteinComponent()
-    {
-       SmallMoleculeImpl sm = new SmallMoleculeImpl("SmallMol-"+System.currentTimeMillis(), null, null);
-       return new Component(null, new InteractionImpl(), sm, new CvComponentRole());
+    private Component createNonProteinComponent() {
+        SmallMoleculeImpl sm = new SmallMoleculeImpl( "SmallMol-" + System.currentTimeMillis(), null, null );
+        return new Component( null, new InteractionImpl(), sm, new CvComponentRole() );
+    }
+
+    private InteractionImpl createUnaryInteraction( float stoichiometry ) {
+        List<Component> comps = new ArrayList<Component>();
+        InteractionImpl interaction = new InteractionImpl( new ArrayList(), comps, null, null, "Int-" + System.currentTimeMillis(), null );
+
+        Component c = createUnaryComponent( stoichiometry, interaction );
+        interaction.addComponent( c );
+
+        return interaction;
+    }
+
+    private Component createUnaryComponent( float stoichiometry, Interaction interaction ) {
+        SmallMoleculeImpl sm = new SmallMoleculeImpl( "SmallMol-" + System.currentTimeMillis(), null, null );
+        Component component = new Component( null, interaction, sm, new CvComponentRole() );
+        component.setStoichiometry( stoichiometry );
+        return component;
     }
 }
