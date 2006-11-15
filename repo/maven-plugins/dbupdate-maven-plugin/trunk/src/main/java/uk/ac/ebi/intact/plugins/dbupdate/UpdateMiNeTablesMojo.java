ba11a94cab8725531f67c16a153b85192d15e3be
baranda <brunoaranda@gmail.com>
2006-11-15 09:31:51 +0000

    Mine UpdateMineTablesMojo working for the first time

--- repo/maven-plugins/dbupdate-maven-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugins/dbupdate/UpdateMiNeTablesMojo.java
+++ repo/maven-plugins/dbupdate-maven-plugin/trunk/src/main/java/uk/ac/ebi/intact/plugins/dbupdate/UpdateMiNeTablesMojo.java
@@ -5,13 +5,17 @@
  */
 package uk.ac.ebi.intact.plugins.dbupdate;
 
-import static uk.ac.ebi.intact.dbutil.mine.MineDatabaseFill.buildDatabase;
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import uk.ac.ebi.intact.plugin.IntactHibernateMojo;
+import uk.ac.ebi.intact.plugin.MojoUtils;
+import uk.ac.ebi.intact.dbutil.mine.MineDatabaseFill;
+import uk.ac.ebi.intact.dbutil.mine.MineDatabaseFillReport;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.sql.SQLException;
 
 /**
@@ -23,13 +27,36 @@ import java.sql.SQLException;
 public class UpdateMiNeTablesMojo extends IntactHibernateMojo {
 
     /**
+     * @parameter default-value="${project.build.directory}/MiNe-ecluded-interactors.txt"
+     * @required
+     */
+    private File nullTaxIdFile;
+
+    /**
      * Main execution method, which is called after hibernate has been initialized
      */
     public void executeIntactMojo() throws MojoExecutionException, MojoFailureException, IOException {
+        PrintStream ps = new PrintStream(getOutputFile());
+
+        MineDatabaseFillReport report = null;
+
         try {
-            buildDatabase();
+            report = MineDatabaseFill.buildDatabase(ps);
         } catch ( SQLException e ) {
+            e.printStackTrace();
             throw new MojoExecutionException( "SQL error while building the MiNe table. cf. nested Exception !", e );
         }
+
+        // excluded
+        MojoUtils.prepareFile(nullTaxIdFile, true);
+        MojoUtils.writeStandardHeaderToFile("Null taxID interactors", "Interactors with null taxID",
+                getProject(), nullTaxIdFile);
+
+        FileWriter writer = new FileWriter(nullTaxIdFile);
+        for (String interactorAc : report.getNullTaxidInteractors())
+        {
+            writer.write(interactorAc+NEW_LINE);
+        }
+        writer.close();
     }
 }
