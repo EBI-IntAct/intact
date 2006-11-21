008c50733b5f8d73a37e70ef97ca13c375999fb5
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-21 16:37:14 +0000

    Test on interaction (clone...)

---
+++ repo/intact-core/trunk/src/test/java/uk/ac/ebi/intact/modelt/InteractionTest.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.intact.modelt;
+
+import junit.framework.TestCase;
+import uk.ac.ebi.intact.persistence.dao.InteractionDao;
+import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.model.Interaction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: CatherineLeroy
+ * Date: 21-Nov-2006
+ * Time: 15:49:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class InteractionTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testClone() throws Exception{
+        InteractionDao interactionDao = IntactContext.getCurrentInstance().getDataContext().getDaoFactory().getInteractionDao();
+        Interaction interaction = interactionDao.getByShortLabel("cara-7");
+        System.out.println(interaction.getAc());
+    }
+}
