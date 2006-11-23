a669349f7815a300c19067f78fb307a70540079a
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-11-23 17:08:01 +0000

    Initial checkin

---
+++ repo/current/sandbox/intact-ebi-search-engine-export/trunk/src/test/java/uk/ac/ebi/intact/externalservices/searchengine/InteractorIndexExporterTest.java
@@ -0,0 +1,54 @@
+package uk.ac.ebi.intact.externalservices.searchengine;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.File;
+
+/**
+ * InteractorIndexExporter Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>11/23/2006</pre>
+ */
+public class InteractorIndexExporterTest extends TestCase {
+    public InteractorIndexExporterTest( String name ) {
+        super( name );
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public static Test suite() {
+        return new TestSuite( InteractorIndexExporterTest.class );
+    }
+
+    ////////////////////
+    // Tests
+
+    public void testBuildIndex() throws Exception {
+        File aFile = new File( InteractorIndexExporterTest.class.getResource( "/hibernate.cfg.xml" ).getFile() );
+        File outputDir = aFile.getParentFile();
+        System.out.println( "Directory: " + outputDir.getAbsolutePath() );
+        if ( !outputDir.exists() ) {
+            outputDir.mkdirs();
+        }
+
+        File f = new File( outputDir.getAbsolutePath() + File.separator + "interactor.xml" );
+
+        if ( f.exists() ) {
+            f.delete();
+        }
+
+        InteractorIndexExporter exporter = new InteractorIndexExporter( f, "1" );
+        exporter.buildIndex();
+        exporter.closeIndex();
+    }
+}
