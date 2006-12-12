1cd9b1353f2628c4714ef98ff2f93362cb9b6dec
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-12-12 15:32:13 +0000

    Change in the pattern : so that : species/yast_small-11.xml can be parse properly and not become species/yast_small-1-01.xml

--- repo/data-conversion/trunk/src/test/java/uk/ac/ebi/intact/application/dataConversion/ExperimentListItemTest.java
+++ repo/data-conversion/trunk/src/test/java/uk/ac/ebi/intact/application/dataConversion/ExperimentListItemTest.java
@@ -55,8 +55,26 @@ public class ExperimentListItemTest extends TestCase {
 
     public void testStaticParseString() throws Exception {
         ExperimentListItem experimentListItem = ExperimentListItem.parseString("species/humt-_small.xml kanamori-2003-4");
-        System.out.println("experimentListItem.getFilename() = " + experimentListItem.getFilename());
         assertEquals("species/\\humt-_small.xml",experimentListItem.getFilename());
+
+        experimentListItem = ExperimentListItem.parseString("species/yeast_small-11.xml suvorova-2002-4,metz-2006-4,huang-1998b-1,skibbens-2004-1,chilkova-2003-1,hall-2003-2,vandemark-20\\\n" +
+                "06-1,millson-2005-1,cairns-1994-2,szerlong-2003-2,bardwell-1996-3,mcnew-1998-1,gadbois-1997-1,treich-1995-2,howe-2002-2,bardw\\\n" +
+                "ell-1996-2,guo-1999-2,matsuura-2004-1,yao-2000-2,millson-2005-3,agarwal-2000-3,matsuura-2003-1,sabbioneda-2005-3,slessareva-2\\\n" +
+                "006-1,cote-1994-1,li-1995-2,singh-2006-2,gillingham-2004-2,skibbens-2004-2,zachariae-1998-2,measday-2002-1,suvorova-2002-5,mi\\\n" +
+                "zumura-1999-2,conibear-2000-3,costa-2005-1,slessareva-2006-3,wilkinson-2006-1,branzei-2006-1,kulp-2006-1,rea-2000-2,wang-2005\\\n" +
+                "b-1,armache-2005-1,measday-1997-1,warren-2002-1,xu-2003b-1,whitby-2000-1,ohdate-2003-2,tong-2002a-4,brown-2000-1,park-2002-2,\\\n" +
+                "jonsson-2001-1,kofler-2005-5,eisen-2001-4,downey-2006-4,qi-2000-4,tsubouchi-2002-1,hershey-1999-4,madania-1999-2,pike-2004-2,\\\n" +
+                "costa-2005-3,rudge-2004-1,wang-1999c-1,dziembowski-2004-1,basmaji-2006-2,kenna-2003-2,pike-2004-3,flores_roza-1998-1,hermjako\\\n" +
+                "b-2004-5,karpichev-1997-1,kobor-2004-1,fiedler-2002-1,seol-2001-1,downey-2006-3,chin-2006-2,lalioti-2002a-2,blumer-1988-1,gal\\\n" +
+                "arneau-2000-2,chilkova-2003-3,milkereit-2001-2,seol-2001-2,eugster-2000-2,chavan-2003-1,gonzales-2005-3,hershey-1999-1,vignol\\\n" +
+                "s-2005-1,matern-2000-1,rodriguez_n-2002-1,english-2006-1,lalo-1996-1,allard-1999-1,mizuguchi-2004-2,singh-2006-3,eugster-2000\\\n" +
+                "-3,fiedler-2002-2,chavan-2005-1,kim-2005a-1,zhao-2005-2");
+        assertEquals("species/\\yeast_small-11.xml",experimentListItem.getFilename());
+
+        experimentListItem = ExperimentListItem.parseString("species/drome_giot-2003-1_01.xml giot-2003-1 [1,2000]");
+        assertEquals("species/\\drome_giot-2003-1_01.xml",experimentListItem.getFilename());
+
+
     }
 
 
