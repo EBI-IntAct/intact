0c472926e707e1548793b144a0f64a34189264f3
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-12-14 11:00:56 +0000

    Release candidate 5

---
+++ repo/editor/editor-app/branches/1_5-rc/src/main/java/uk/ac/ebi/intact/application/editor/struts/security/UserAuthenticator.java
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2002-2005 The European Bioinformatics Institute, and others.
+All rights reserved. Please see the file LICENSE
+in the root directory of this distribution.
+*/
+
+package uk.ac.ebi.intact.application.editor.struts.security;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.application.editor.business.EditUser;
+import uk.ac.ebi.intact.application.editor.business.EditUserI;
+import uk.ac.ebi.intact.application.editor.exception.AuthenticateException;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.context.UserContext;
+
+/**
+ * The custom authenticator for Intact editor.
+ *
+ * @author Sugath Mudali (smudali@ebi.ac.uk)
+ * @version $Id$
+ */
+public class UserAuthenticator {
+
+    private static final Log log = LogFactory.getLog(UserAuthenticator.class);
+
+    /**
+     * Authenticate a user by accessing a persistent system using given
+     * user name and password
+     * @param username the user name
+     * @param password the password
+     * @return a user object if a user exists for <code>username</code> and
+     * <code>password</code>.
+     * @exception AuthenticateException invalid user for given
+     * <code>username</code> and <code>password</code>.
+     */
+    public static EditUserI authenticate(String username, String password)
+            throws AuthenticateException {
+        try {
+           IntactContext intactContext = IntactContext.getCurrentInstance();
+            UserContext userContext = intactContext.getUserContext();
+            userContext.setUserId(username.toUpperCase());
+            userContext.setUserPassword(password);
+            return new EditUser(username, password);
+        }
+        catch (AuthenticateException ie) {
+            log.error(username + " disallowed", ie);
+            throw new AuthenticateException(username + " disallowed");
+        }
+    }
+}
