00a63e5a4be563824ab7e1d00faa340c1f1cf811
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-12-18 16:32:27 +0000

    branche to fix a bug in release

---
+++ repo/intact-core/branches/1_3_6_x/src/main/java/uk/ac/ebi/intact/context/impl/IntactContextWrapper.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.context.impl;
+
+import uk.ac.ebi.intact.context.*;
+
+/**
+ * TODO: comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>04/08/2006</pre>
+ */
+public class IntactContextWrapper extends IntactContext
+{
+        public IntactContextWrapper(UserContext userContext, DataContext dataContext, IntactSession session)
+        {
+            super(userContext, dataContext, session);
+            IntactContext.setCurrentInstance(this);
+        }
+
+}
