8f949d0a4094a6740ff5a2ee27e9537a45265f22
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2006-12-18 16:32:27 +0000

    branche to fix a bug in release

---
+++ repo/intact-core/branches/1_3_6_x/src/main/java/uk/ac/ebi/intact/context/RuntimeConfig.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.context;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.config.DataConfig;
+import uk.ac.ebi.intact.config.impl.StandardCoreDataConfig;
+import uk.ac.ebi.intact.model.Institution;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO: comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>04/08/2006</pre>
+ */
+public final class RuntimeConfig implements Serializable
+{
+    private static final Log log = LogFactory.getLog(RuntimeConfig.class);
+
+    private static final String APPLICATION_PARAM_NAME = RuntimeConfig.class.getName()+".CONFIG";
+
+    private Institution institution;
+    private String acPrefix;
+    private final Map<String, DataConfig> dataConfigs;
+    private DataConfig defaultDataConfig;
+
+    private RuntimeConfig()
+    {
+        this.dataConfigs = new HashMap<String,DataConfig>();
+    }
+
+    public static RuntimeConfig getCurrentInstance(IntactSession session)
+    {
+        RuntimeConfig runtimeConfig
+                = (RuntimeConfig)session.getApplicationAttribute(APPLICATION_PARAM_NAME);
+        if (runtimeConfig == null)
+        {
+            log.debug("Creating new RuntimeConfig");
+            runtimeConfig = new RuntimeConfig();
+            session.setApplicationAttribute(APPLICATION_PARAM_NAME, runtimeConfig);
+        }
+        return runtimeConfig;
+    }
+
+     public Institution getInstitution() throws IntactException
+     {
+        return institution;
+     }
+
+    public void setInstitution(Institution institution)
+    {
+        this.institution = institution;
+    }
+
+    public String getAcPrefix()
+    {
+        return acPrefix;
+    }
+
+    public void setAcPrefix(String acPrefix)
+    {
+        this.acPrefix = acPrefix;
+    }
+
+    public Collection<DataConfig> getDataConfigs()
+    {
+        return dataConfigs.values();
+    }
+
+    public DataConfig getDataConfig(String name)
+    {
+        return dataConfigs.get(name);
+    }
+
+    public void addDataConfig(DataConfig dataConfig)
+    {
+        addDataConfig(dataConfig, false);
+    }
+
+    public void addDataConfig(DataConfig dataConfig, boolean isTheDefaultOne)
+    {
+        if (!dataConfig.isInitialized())
+        {
+            throw new IllegalArgumentException("DataConfig added to RuntimeConfig must be already initialized: "+dataConfig.getName());
+        }
+
+        this.dataConfigs.put(dataConfig.getName(), dataConfig);
+
+        if (isTheDefaultOne)
+        {
+            setDefaultDataConfig(dataConfig);
+        }
+    }
+
+    public void setDefaultDataConfig(DataConfig dataConfig)
+    {
+        if (!dataConfigs.containsKey(dataConfig.getName()))
+        {
+            addDataConfig(dataConfig);
+        }
+
+        defaultDataConfig = dataConfig;
+    }
+
+    public DataConfig getDefaultDataConfig()
+    {
+        if (defaultDataConfig == null)
+        {
+            log.warn("No default data config configured. Using: "+ StandardCoreDataConfig.NAME);
+            defaultDataConfig = getDataConfig(StandardCoreDataConfig.NAME);
+        }
+
+        return defaultDataConfig;
+    }
+}
---
+++ repo/intact-core/branches/1_3_6_x/src/main/java/uk/ac/ebi/intact/persistence/dao/impl/IntactObjectDaoImpl.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2002-2006 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.persistence.dao.impl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import uk.ac.ebi.intact.model.IntactObject;
+import uk.ac.ebi.intact.persistence.dao.IntactObjectDao;
+import uk.ac.ebi.intact.persistence.dao.IntactObjectIterator;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Basic queries for IntactObjects
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>24-Apr-2006</pre>
+ */
+@SuppressWarnings({ "unchecked" })
+public class IntactObjectDaoImpl<T extends IntactObject> extends HibernateBaseDaoImpl<T> implements IntactObjectDao<T> {
+
+    public IntactObjectDaoImpl( Class<T> entityClass, Session session ) {
+        super( entityClass, session );
+    }
+
+    /**
+     * Get an item using its AC
+     *
+     * @param ac the identifier
+     *
+     * @return the object
+     */
+    public T getByAc( String ac ) {
+        return (T) getSession().get( getEntityClass(), ac );
+    }
+
+    public Collection<T> getByAcLike( String ac ) {
+        return getByPropertyNameLike( "ac", ac );
+    }
+
+    public Collection<T> getByAcLike(String ac, boolean ignoreCase){
+        return getByPropertyNameLike("ac", ac, ignoreCase);
+    }
+
+
+
+    /**
+     * Performs a unique query for an array of ACs. Beware that depending on the database used this query has limitation
+     * (for instance, in Oracle it is limited to 1000 items)
+     *
+     * @param acs The acs to look for
+     *
+     * @return the collection of entities with those ACs
+     */
+    public List<T> getByAc( String[] acs ) {
+        if ( acs.length == 0 ) {
+            throw new HibernateException( "At least one AC is needed to query by AC." );
+        }
+
+        return getSession().createCriteria( getEntityClass() )
+                .add( Restrictions.in( "ac", acs ) )
+                .addOrder( Order.asc( "ac" ) ).list();
+    }
+
+    public List<T> getByAc( Collection<String> acs ) {
+        return getByAc( acs.toArray( new String[acs.size()] ) );
+    }
+
+    public List<T> getAll() {
+        return getSession().createCriteria( getEntityClass() ).list();
+    }
+
+    public List<T> getAll( int firstResult, int maxResults ) {
+        return getSession().createCriteria( getEntityClass() )
+                .setFirstResult( firstResult )
+                .setMaxResults( maxResults ).list();
+    }
+
+    public int countAll() {
+        return (Integer) getSession()
+                .createCriteria( getEntityClass() )
+                .setProjection( Projections.rowCount() )
+                .uniqueResult();
+    }
+
+    public Iterator<T> iterator() {
+        return new IntactObjectIterator<T>( getEntityClass() );
+    }
+
+    public Iterator<T> iterator( int batchSize ) {
+        return new IntactObjectIterator<T>( getEntityClass(), batchSize );
+    }
+
+    public void update( T objToUpdate ) {
+        getSession().update( objToUpdate );
+    }
+
+    public void persist( T objToPersist ) {
+        getSession().persist( objToPersist );
+    }
+
+    public void persistAll( Collection<T> objsToPersist ) {
+        for ( T objToPersist : objsToPersist ) {
+            persist( objToPersist );
+        }
+    }
+
+    public void delete( T objToDelete ) {
+        getSession().delete( objToDelete );
+    }
+
+    public void deleteAll( Collection<T> objsToDelete ) {
+        for ( T objToDelete : objsToDelete ) {
+            delete( objToDelete );
+        }
+    }
+
+    public void saveOrUpdate( T objToPersist ) {
+        getSession().saveOrUpdate( objToPersist );
+    }
+
+    public boolean exists( T obj ) {
+        return ( getSession().get( getEntityClass(), obj.getAc() ) != null );
+    }
+
+    public void refresh( T objToRefresh ) {
+        getSession().refresh( objToRefresh );
+    }
+
+}
