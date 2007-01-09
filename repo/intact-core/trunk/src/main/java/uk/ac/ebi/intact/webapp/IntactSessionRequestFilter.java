b59297ac3bf239396fee449164a5158901cf432e
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-01-09 13:25:47 +0000

    Change to : 	close the hibernate session in a finally after commiting the transaction		wrap the http response so that if the commit fail we can still send a warning to the user.

--- repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/webapp/IntactSessionRequestFilter.java
+++ repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/webapp/IntactSessionRequestFilter.java
@@ -7,15 +7,22 @@ package uk.ac.ebi.intact.webapp;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import uk.ac.ebi.intact.context.IntactConfigurator;
 import uk.ac.ebi.intact.context.IntactContext;
 import uk.ac.ebi.intact.context.IntactSession;
 import uk.ac.ebi.intact.context.impl.WebappSession;
+import uk.ac.ebi.intact.business.IntactException;
+import uk.ac.ebi.intact.business.IntactTransactionException;
+import uk.ac.ebi.intact.config.impl.AbstractHibernateDataConfig;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +36,7 @@ import java.util.List;
  * @since <pre>24-Apr-2006</pre>
  */
 public class IntactSessionRequestFilter implements Filter {
+    private FilterConfig myFilterConfig;
 
     private static final Log log = LogFactory.getLog(IntactSessionRequestFilter.class);
 
@@ -49,10 +57,8 @@ public class IntactSessionRequestFilter implements Filter {
         String requestUrl = req.getRequestURL().toString();
 
         // if the the url end matches with a filtered extensions do not start IntactContext
-        for (String ext : excludedExtensions)
-        {
-            if (requestUrl.toLowerCase().endsWith(ext.toLowerCase()))
-            {
+        for (String ext : excludedExtensions){
+            if (requestUrl.toLowerCase().endsWith(ext.toLowerCase())){
                 log.debug("Context not created for (excluded): "+requestUrl);
                 chain.doFilter(request, response);
                 return;
@@ -63,40 +69,59 @@ public class IntactSessionRequestFilter implements Filter {
         IntactSession intactSession = new WebappSession(session.getServletContext(), session, req);
         IntactContext context = IntactConfigurator.createIntactContext(intactSession);
 
-        try
-        {
-            // Call the next filter (continue request processing)
-            chain.doFilter(request, response);
-        }
-        catch (IOException e)
-        {
-//           e.printStackTrace();
-        	// use the lograther than standard output or some errors could be raised 
-        	log.error("intact session: ", e);
-        }
-        catch (ServletException e)
-        {
-//           e.printStackTrace();
-        	log.error("intact session: ", e);
-        }
-        finally
-        {
-            log.debug("Committing active transactions");
-            try
-            {
-                context.getDataContext().commitAllActiveTransactions();
-            }
-            catch (Exception e)
-            {
-//                e.printStackTrace();
-            	log.error("intact session: ", e);
-            }
-        }
+        PrintWriter out = response.getWriter();
+        StringResponseWrapper responseWrapper = new StringResponseWrapper((HttpServletResponse) response);
 
+        try{
+            // Call the next filter (continue request processing)
+            chain.doFilter(request, responseWrapper);
+        }
+        catch (IOException e){
+        	// use the lograther than standard output or some errors could be raised
+        	log.error("intact session: ", e);
+        }
+        catch (ServletException e){
+        	log.error("intact session: ", e);
+        }
+        finally {
+            log.debug("Committing active transactions");
+            try {
+                context.getDataContext().commitTransaction();
+                out.write(responseWrapper.toString());
+            }
+            catch (Exception e){
+                log.error("Exception commiting trying to commit : ", e);
+                try{
+                    context.getDataContext().getDaoFactory().getCurrentTransaction().rollback();
+                }catch(Exception ie){
+                    log.error("Couldn't rollback." + ie);
+                    throw new ServletException("Couldn't rollback." + ie);
+                }
+                throw new ServletException("Exception commiting, rollback sucessfull" + e);
+
+            }finally{
+                Session hibernateSession = getSession();
+                if(hibernateSession.isOpen()){
+                    hibernateSession.close();
+                }
+            }
+
+        }
+        out.close();
+
+    }
+
+    private Session getSession(){
+        AbstractHibernateDataConfig abstractHibernateDataConfig = (AbstractHibernateDataConfig) IntactContext.getCurrentInstance().getConfig().getDefaultDataConfig();
+        SessionFactory factory = abstractHibernateDataConfig.getSessionFactory();
+        Session session = factory.getCurrentSession();
+        return session;
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {
         log.debug("Initializing filter...");
+
+        myFilterConfig = filterConfig;
 
         excludedExtensions = new ArrayList<String>();
 
