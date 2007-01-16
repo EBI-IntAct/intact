f344de712e775dbf926d301edd630697104dd76f
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-01-16 10:49:55 +0000

    add necessary parameters

--- repo/maven-plugins/psimitab-maven-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugins/ConvertXmlPublicationToTabMojoTest.java
+++ repo/maven-plugins/psimitab-maven-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugins/ConvertXmlPublicationToTabMojoTest.java
@@ -22,10 +22,16 @@ public class ConvertXmlPublicationToTabMojoTest extends AbstractMojoTestCase {
         ConvertXmlPublicationToTabMojo mojo =
                 ( ConvertXmlPublicationToTabMojo ) lookupEmptyMojo( "pub2tab", pluginXmlFile );
 
-        File rootDir =
+        File srcDir =
                 new File( getBasedir(), "target" + File.separator + "test-classes" + File.separator + "xml-samples" );
 
-        setVariableValueToObject( mojo, "directoryPath", rootDir.getAbsolutePath() );
+        File targetDir =
+                new File( getBasedir(), "target" + File.separator + "test-classes" + File.separator + "tab-output" );
+        targetDir.mkdirs();
+
+        setVariableValueToObject( mojo, "sourceDirectoryPath", srcDir.getAbsolutePath() );
+        setVariableValueToObject( mojo, "targetDirectoryPath", targetDir.getAbsolutePath() );
+        setVariableValueToObject( mojo, "logFilePath", targetDir.getAbsolutePath() + File.separator + "mitab.log" );
 
         mojo.setLog( new SystemStreamLog() );
 
@@ -38,11 +44,11 @@ public class ConvertXmlPublicationToTabMojoTest extends AbstractMojoTestCase {
 
         // now check if all files were created sucessfuly
 
-        assertTrue( new File( rootDir, "11283351.xls" ).exists() );
-        assertTrue( new File( rootDir, "7568142.xls" ).exists() );
-        assertTrue( new File( rootDir, "9070862.xls" ).exists() );
+        assertTrue( new File( targetDir, "11283351.xls" ).exists() );
+        assertTrue( new File( targetDir, "7568142.xls" ).exists() );
+        assertTrue( new File( targetDir, "9070862.xls" ).exists() );
 
-        File subDir = new File( rootDir, "sub-dir" );
+        File subDir = new File( targetDir, "sub-dir" );
         assertTrue( new File( subDir, "10366597.xls" ).exists() );
         assertTrue( new File( subDir, "9686597.xls" ).exists() );
     }
