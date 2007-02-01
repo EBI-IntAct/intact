c290de06ef40694f879972765ebce6a706490f73
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-02-01 12:28:00 +0000

    Added ways to count and retreive interactors that are known to interact.

--- repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/persistence/dao/impl/InteractorDaoImpl.java
+++ repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/persistence/dao/impl/InteractorDaoImpl.java
@@ -25,9 +25,8 @@ import java.util.List;
  * @version $Id$
  * @since <pre>27-Apr-2006</pre>
  */
-@SuppressWarnings("unchecked")
-public class InteractorDaoImpl<T extends InteractorImpl> extends AnnotatedObjectDaoImpl<T> implements InteractorDao<T>
-{
+@SuppressWarnings( "unchecked" )
+public class InteractorDaoImpl<T extends InteractorImpl> extends AnnotatedObjectDaoImpl<T> implements InteractorDao<T> {
     /**
      * Filter to provide filtering on GeneNames
      */
@@ -42,39 +41,35 @@ public class InteractorDaoImpl<T extends InteractorImpl> extends AnnotatedObject
         geneNameFilter.add( "locus name" );
     }
 
-    public InteractorDaoImpl(Class<T> entityClass, Session session, IntactSession intactSession)
-    {
-        super(entityClass, session, intactSession);
+    public InteractorDaoImpl( Class<T> entityClass, Session session, IntactSession intactSession ) {
+        super( entityClass, session, intactSession );
     }
 
-    public Integer countInteractionsForInteractorWithAc(String ac)
-    {
-        return (Integer) getSession().createCriteria(Component.class)
-                    .createAlias("interactor", "interactor")
-                    .createAlias("interaction", "interaction")
-                    .add(Restrictions.eq("interactor.ac", ac))
-                    .setProjection(Projections.countDistinct("interaction.ac")).uniqueResult();
+    public Integer countInteractionsForInteractorWithAc( String ac ) {
+        return ( Integer ) getSession().createCriteria( Component.class )
+                .createAlias( "interactor", "interactor" )
+                .createAlias( "interaction", "interaction" )
+                .add( Restrictions.eq( "interactor.ac", ac ) )
+                .setProjection( Projections.countDistinct( "interaction.ac" ) ).uniqueResult();
     }
 
-    public Integer countComponentsForInteractorWithAc(String ac)
-    {
-        return (Integer) getSession().createCriteria(Component.class)
-                    .createAlias("interactor", "interactor")
-                    .add(Restrictions.eq("interactor.ac", ac))
-                    .setProjection(Projections.countDistinct("ac")).uniqueResult();
+    public Integer countComponentsForInteractorWithAc( String ac ) {
+        return ( Integer ) getSession().createCriteria( Component.class )
+                .createAlias( "interactor", "interactor" )
+                .add( Restrictions.eq( "interactor.ac", ac ) )
+                .setProjection( Projections.countDistinct( "ac" ) ).uniqueResult();
     }
 
 
-    public List<String> getGeneNamesByInteractorAc(String proteinAc)
-    {
+    public List<String> getGeneNamesByInteractorAc( String proteinAc ) {
         //the gene names are obtained from the Aliases for the Protein
         //which are of type 'gene name'...
-        Criteria crit = getSession().createCriteria(getEntityClass())
-                .add(Restrictions.idEq(proteinAc))
-                .createAlias("aliases", "alias")
-                .createAlias("alias.cvAliasType", "aliasType")
-                .add(Restrictions.in("aliasType.shortLabel", geneNameFilter))
-                .setProjection(Property.forName("alias.name"));
+        Criteria crit = getSession().createCriteria( getEntityClass() )
+                .add( Restrictions.idEq( proteinAc ) )
+                .createAlias( "aliases", "alias" )
+                .createAlias( "alias.cvAliasType", "aliasType" )
+                .add( Restrictions.in( "aliasType.shortLabel", geneNameFilter ) )
+                .setProjection( Property.forName( "alias.name" ) );
 
         List<String> geneNames = crit.list();
 
@@ -85,10 +80,35 @@ public class InteractorDaoImpl<T extends InteractorImpl> extends AnnotatedObject
         return geneNames;
     }
 
-    public List<T> getByBioSourceAc(String ac)
-    {
-        return getSession().createCriteria(getEntityClass())
-                .createCriteria("bioSource")
-                .add(Restrictions.idEq("ac")).list();
+    public List<T> getByBioSourceAc( String ac ) {
+        return getSession().createCriteria( getEntityClass() )
+                .createCriteria( "bioSource" )
+                .add( Restrictions.idEq( "ac" ) ).list();
+    }
+
+    public int countInteractorInvolvedInInteraction() {
+        return ( Integer ) getSession().createCriteria( InteractorImpl.class )
+                .add( Restrictions.isNotEmpty( "activeInstances" ) )
+                .setProjection( Projections.rowCount() ).uniqueResult();
+    }
+
+    public List<T> getInteractorInvolvedInInteraction( Integer firstResult, Integer maxResults ) {
+        {
+            Criteria crit = getSession().createCriteria( InteractorImpl.class )
+//                    .createAlias("xrefs", "xref")
+                    .add( Restrictions.isNotEmpty( "activeInstances" ) )
+//                    .addOrder( Order.asc( "xref.primaryId" ) )
+                    ;
+
+            if ( firstResult != null && firstResult >= 0 ) {
+                crit.setFirstResult( firstResult );
+            }
+
+            if ( maxResults != null && maxResults > 0 ) {
+                crit.setMaxResults( maxResults );
+            }
+
+            return crit.list();
+        }
     }
 }
