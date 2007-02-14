15530d0b9d2ad8231a49b26d008126ef9136a0f4
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-02-14 14:41:41 +0000

    Initial checkin

---
+++ repo/intact-newt/trunk/src/main/java/uk/ac/ebi/intact/util/taxonomy/DummyTaxonomyService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.util.taxonomy;
+
+import java.util.List;
+
+/**
+ * Taxonomy Bridge to be used for testing purpose as it creates Taxonomy terms in a predictible manner without relying
+ * on the network.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since 1.0
+ */
+public class DummyTaxonomyService implements TaxonomyService {
+
+    public TaxonomyTerm getTaxonomyTerm( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException();
+    }
+
+    public void retreiveChildren( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+    }
+
+    public void retreiveParents( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+    }
+
+    public List<TaxonomyTerm> getTermChildren( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<TaxonomyTerm> getTermParent( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getSourceDatabaseMiRef() throws TaxonomyServiceException {
+        return "MI:9999";
+    }
+}
---
+++ repo/intact-newt/trunk/src/main/java/uk/ac/ebi/intact/util/taxonomy/OLSTaxonomyService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.util.taxonomy;
+
+import java.util.List;
+
+/**
+ * OLS integration for accession the NCBI Taxonomy.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since 1.0
+ */
+public class OLSTaxonomyService implements TaxonomyService {
+
+    public TaxonomyTerm getTaxonomyTerm( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException( );
+    }
+
+    public void retreiveChildren( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException( );
+    }
+
+    public void retreiveParents( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException( );
+    }
+
+    public List<TaxonomyTerm> getTermChildren( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException( );
+    }
+
+    public List<TaxonomyTerm> getTermParent( int taxid ) throws TaxonomyServiceException {
+        throw new UnsupportedOperationException( );
+    }
+
+    public String getSourceDatabaseMiRef() throws TaxonomyServiceException {
+        throw new UnsupportedOperationException();
+    }
+}
