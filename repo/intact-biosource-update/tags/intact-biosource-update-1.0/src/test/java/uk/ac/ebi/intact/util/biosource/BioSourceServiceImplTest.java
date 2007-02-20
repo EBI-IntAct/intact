0243a758d9939fd878df513284b79e4f7bb8ecb0
skerrien <samuel.kerrien@gmail.com>
2007-02-20 17:45:16 +0000

    [maven-scm] copy for tag intact-biosource-update-1.0

---
+++ repo/intact-biosource-update/tags/intact-biosource-update-1.0/src/test/java/uk/ac/ebi/intact/util/biosource/BioSourceServiceImplTest.java
@@ -0,0 +1,55 @@
+package uk.ac.ebi.intact.util.biosource;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import uk.ac.ebi.intact.model.BioSource;
+import uk.ac.ebi.intact.util.taxonomy.DummyTaxonomyService;
+import uk.ac.ebi.intact.util.taxonomy.TaxonomyService;
+
+/**
+ * BioSourceServiceImpl Tester.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @since 1.0
+ */
+public class BioSourceServiceImplTest extends TestCase {
+
+    public BioSourceServiceImplTest( String name ) {
+        super( name );
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public static Test suite() {
+        return new TestSuite( BioSourceServiceImplTest.class );
+    }
+
+    ////////////////////
+    // Tests
+
+    public void testGetBiosource_existingOne() throws Exception {
+        TaxonomyService taxService = new DummyTaxonomyService();
+        BioSourceService service = new BioSourceServiceImpl( taxService );
+        BioSource bs = service.getBiosourceByTaxid( String.valueOf( 9606 ) );
+        assertNotNull( bs );
+    }
+
+    public void testGetBiosource_newBioSource() throws Exception {
+        TaxonomyService taxService = new DummyTaxonomyService();
+        BioSourceService service = new BioSourceServiceImpl( taxService );
+        BioSource bs = service.getBiosourceByTaxid( String.valueOf( 9999999 ) );
+        assertNotNull( bs );
+
+        BioSource bs2 = service.getBiosourceByTaxid( String.valueOf( 9999999 ) );
+        assertNotNull( bs2 );
+
+        assertEquals( bs, bs2 );
+    }
+}
