50b1798ac95811ebd8734ec1f55d5a2d6cb2a1a8
skerrien <samuel.kerrien@gmail.com>
2007-02-20 16:16:37 +0000

    Implementation of a dummy service that provide functionalities to be used in tests. It creates TaxonomyTerm in a predictible manner without relying on the network.

--- repo/intact-taxonomy/trunk/src/main/java/uk/ac/ebi/intact/util/taxonomy/DummyTaxonomyService.java
+++ repo/intact-taxonomy/trunk/src/main/java/uk/ac/ebi/intact/util/taxonomy/DummyTaxonomyService.java
@@ -5,6 +5,10 @@
  */
 package uk.ac.ebi.intact.util.taxonomy;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,25 +21,50 @@ import java.util.List;
  */
 public class DummyTaxonomyService implements TaxonomyService {
 
+    public static final Log log = LogFactory.getLog( DummyTaxonomyService.class );
+
     public TaxonomyTerm getTaxonomyTerm( int taxid ) throws TaxonomyServiceException {
-        throw new UnsupportedOperationException();
+        log.debug( "Building Taxonomy term with taxid: " + taxid );
+        TaxonomyTerm term = new TaxonomyTerm( taxid );
+        term.setCommonName( String.valueOf( taxid ) );
+        term.setScientificName( String.valueOf( taxid ) );
+        return term;
     }
 
     public void retreiveChildren( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+        log.debug( "Retreiving children taxonomy term(s) of: " + term.getTaxid() );
     }
 
     public void retreiveParents( TaxonomyTerm term, boolean recursively ) throws TaxonomyServiceException {
+        log.debug( "Retreiving parent taxonomy term of: " + term.getTaxid() );
+        if( term.getTaxid() != 1 ) {
+            // add the root: 1
+            term.addParent( buildRoot() );
+        }
+    }
+
+    private TaxonomyTerm buildRoot() {
+
+        TaxonomyTerm term = new TaxonomyTerm( 1 );
+
+        term.setCommonName( "root" );
+        term.setScientificName( "root" );
+
+        return term;
     }
 
     public List<TaxonomyTerm> getTermChildren( int taxid ) throws TaxonomyServiceException {
-        throw new UnsupportedOperationException();
+        log.debug( "Collecting all children of Taxonomy term: " + taxid );
+        return Collections.EMPTY_LIST;
     }
 
     public List<TaxonomyTerm> getTermParent( int taxid ) throws TaxonomyServiceException {
-        throw new UnsupportedOperationException();
+        log.debug( "Collecting parent of Taxonomy term: " + taxid );
+        return Collections.EMPTY_LIST;
     }
 
     public String getSourceDatabaseMiRef() {
-        return "MI:9999";
+        // return IntAct
+        return "MI:0469";
     }
 }
