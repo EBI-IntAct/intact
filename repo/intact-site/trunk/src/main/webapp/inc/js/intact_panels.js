6409937cbb82dde036ae9c3b1be7de6716420ad5
baranda <brunoaranda@gmail.com>
2007-03-08 15:49:39 +0000

    Site now using the jsf components from the ebifaces module.

--- repo/intact-site/trunk/src/main/webapp/inc/js/intact_panels.js
+++ repo/intact-site/trunk/src/main/webapp/inc/js/intact_panels.js
@@ -1,18 +1,3 @@
-function intact_toggleVisibility(panelId)
-{
-    var panel = document.getElementById(panelId);
-    var currentVisibility = panel.style.visibility;
-
-    if (currentVisibility == 'hidden')
-    {
-        panel.style.visibility = 'visible';
-    }
-    else
-    {
-        panel.style.visibility = 'hidden';
-    }
-}
-
 function intact_clearAndFocusField(fieldId)
 {
     var field = document.getElementById(fieldId);
