bb41ec6562694fc82dd2c2d5d414c57a6826d94c
baranda <brunoaranda@gmail.com>
2007-03-14 16:43:01 +0000

    now using latest intact-core (1.5.0-rc2-SNAPSHOT)

--- repo/graph2mif/server/trunk/src/main/java/uk/ac/ebi/intact/application/graph2MIF/IntactGraphHelper.java
+++ repo/graph2mif/server/trunk/src/main/java/uk/ac/ebi/intact/application/graph2MIF/IntactGraphHelper.java
@@ -1,13 +1,12 @@
 package uk.ac.ebi.intact.application.graph2MIF;
 
-import uk.ac.ebi.intact.model.*;
-import uk.ac.ebi.intact.simpleGraph.BasicGraphI;
-import uk.ac.ebi.intact.simpleGraph.Edge;
-import uk.ac.ebi.intact.simpleGraph.EdgeI;
-import uk.ac.ebi.intact.simpleGraph.Graph;
-import uk.ac.ebi.intact.persistence.dao.DaoFactory;
 import uk.ac.ebi.intact.business.IntactException;
 import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.util.simplegraph.BasicGraphI;
+import uk.ac.ebi.intact.util.simplegraph.Edge;
+import uk.ac.ebi.intact.util.simplegraph.EdgeI;
+import uk.ac.ebi.intact.util.simplegraph.Graph;
 
 import java.util.ArrayList;
 import java.util.Collection;
