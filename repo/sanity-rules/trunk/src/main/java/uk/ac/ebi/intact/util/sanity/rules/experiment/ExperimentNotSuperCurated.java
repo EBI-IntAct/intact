a17d629c898836f28c6d84a0c9dadd2090c22343
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-03-16 15:11:20 +0000

    New rules, new tests

---
+++ repo/sanity-rules/trunk/src/main/java/uk/ac/ebi/intact/util/sanity/rules/experiment/ExperimentNotSuperCurated.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.util.sanity.rules.experiment;
+
+import uk.ac.ebi.intact.util.sanity.rules.Rule;
+import uk.ac.ebi.intact.util.sanity.rules.util.MethodArgumentValidator;
+import uk.ac.ebi.intact.util.sanity.rules.util.CommonMethods;
+import uk.ac.ebi.intact.util.sanity.rules.messages.GeneralMessage;
+import uk.ac.ebi.intact.util.sanity.exception.SanityCheckerException;
+import uk.ac.ebi.intact.model.IntactObject;
+import uk.ac.ebi.intact.model.Experiment;
+
+import java.util.*;
+
+/**
+ * TODO comment this
+ *
+ * @author Catherine Leroy (cleroy@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO
+ */
+public class ExperimentNotSuperCurated  implements Rule {
+    private static final String DESCRIPTION = "This/those experiments have not been super curated";
+    private static final String SUGGESTION = "Ask a super-creator to add and accepted or to-be-reviewed stamp on it.";
+    private static Date startingDateSuperCuration;
+
+    static{
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(2005, Calendar.SEPTEMBER, 1);
+        startingDateSuperCuration = calendar.getTime();
+
+    }
+
+    public Collection<GeneralMessage> check(IntactObject intactObject) throws SanityCheckerException {
+        MethodArgumentValidator.isValidArgument(intactObject, Experiment.class);
+        Collection<GeneralMessage> messages = new ArrayList<GeneralMessage>();
+        Experiment experiment = (Experiment) intactObject;
+
+        if(startingDateSuperCuration.before(experiment.getCreated())){
+            if(!CommonMethods.isAccepted(experiment) && !CommonMethods.isToBeReviewed(experiment)){
+                messages.add(new GeneralMessage(DESCRIPTION,GeneralMessage.AVERAGE_LEVEL,SUGGESTION, experiment));    
+            }
+        }
+
+        return messages;
+    }
+
+
+    public static String getDescription() {
+        return DESCRIPTION;
+    }
+
+    public static String getSuggestion() {
+        return SUGGESTION;
+    }
+}
