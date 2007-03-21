f84ad48fb5c4027150b069fee4868cceda1fd445
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-03-21 10:20:34 +0000

    Mock for tests

---
+++ repo/sanity-rules/trunk/src/test/java/uk/ac/ebi/intact/mocks/cvComponentRoles/EnzymeMock.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.mocks.cvComponentRoles;
+
+import uk.ac.ebi.intact.model.CvComponentRole;
+import uk.ac.ebi.intact.model.CvObjectXref;
+import uk.ac.ebi.intact.mocks.CvObjectMock;
+import uk.ac.ebi.intact.mocks.IntactObjectSetter;
+import uk.ac.ebi.intact.mocks.XrefMock;
+import uk.ac.ebi.intact.mocks.cvXrefQualifiers.IdentityMock;
+import uk.ac.ebi.intact.mocks.cvDatabases.PsiMiMock;
+
+/**
+ * TODO comment this
+ *
+ * @author Catherine Leroy (cleroy@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO
+ */
+public class EnzymeMock {
+    public static CvComponentRole getMock(){
+        CvComponentRole enzyme = CvObjectMock.getMock(CvComponentRole.class,CvComponentRole.ENZYME, CvComponentRole.ENZYME);
+
+        enzyme = (CvComponentRole) IntactObjectSetter.setBasicObject(enzyme);
+
+        CvObjectXref xref = XrefMock.getMock(CvObjectXref.class, PsiMiMock.getMock(), IdentityMock.getMock(),CvComponentRole.ENZYME_PSI_REF);
+        enzyme.addXref(xref);
+
+        return enzyme;
+    }
+}
