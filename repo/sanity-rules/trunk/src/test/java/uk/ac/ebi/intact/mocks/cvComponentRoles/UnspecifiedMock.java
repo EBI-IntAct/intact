c7f2c987325cfa0d30e51c4b7d8248f49eac6aef
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-03-21 10:27:45 +0000

    Mock for tests

---
+++ repo/sanity-rules/trunk/src/test/java/uk/ac/ebi/intact/mocks/cvComponentRoles/UnspecifiedMock.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
+package uk.ac.ebi.intact.mocks.cvComponentRoles;
+
+import uk.ac.ebi.intact.model.CvComponentRole;
+import uk.ac.ebi.intact.model.CvObjectXref;
+import uk.ac.ebi.intact.mocks.CvObjectMock;
+import uk.ac.ebi.intact.mocks.IntactObjectSetter;
+import uk.ac.ebi.intact.mocks.XrefMock;
+import uk.ac.ebi.intact.mocks.cvXrefQualifiers.IdentityMock;
+import uk.ac.ebi.intact.mocks.cvDatabases.PsiMiMock;
+
+/**
+ * TODO comment this
+ *
+ * @author Catherine Leroy (cleroy@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO
+ */
+public class UnspecifiedMock {
+    public static CvComponentRole getMock(){
+        CvComponentRole unspecified = CvObjectMock.getMock(CvComponentRole.class,CvComponentRole.UNSPECIFIED, CvComponentRole.UNSPECIFIED);
+        unspecified = (CvComponentRole) IntactObjectSetter.setBasicObject(unspecified);
+        CvObjectXref xref = XrefMock.getMock(CvObjectXref.class, PsiMiMock.getMock(), IdentityMock.getMock(),CvComponentRole.UNSPECIFIED_PSI_REF);
+        unspecified.addXref(xref);
+
+        return unspecified;
+    }
+}
