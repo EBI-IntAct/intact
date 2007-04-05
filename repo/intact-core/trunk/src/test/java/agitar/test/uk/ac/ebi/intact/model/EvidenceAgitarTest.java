48db1e92d2dc37dbf432ffc211eb2517adcbe521
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-04-05 13:11:05 +0000

    Fixed compilation errors

--- repo/intact-core/trunk/src/test/java/agitar/test/uk/ac/ebi/intact/model/EvidenceAgitarTest.java
+++ repo/intact-core/trunk/src/test/java/agitar/test/uk/ac/ebi/intact/model/EvidenceAgitarTest.java
@@ -9,7 +9,7 @@
 
 package uk.ac.ebi.intact.model;
 
-import com.agitar.lib.junit.AgitarTestCase;
+import agitar.test.uk.ac.ebi.intact.model.AgitarTestCase;
 
 public class EvidenceAgitarTest extends AgitarTestCase {
 
