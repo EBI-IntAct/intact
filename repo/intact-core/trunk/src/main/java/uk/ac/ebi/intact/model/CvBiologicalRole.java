3874a5fb32ad350fe7b10ec0e2e2753f03dbbfab
skerrien <samuel.kerrien@gmail.com>
2007-04-30 15:50:46 +0000

    Split component role into biological and experimental role

--- repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/model/CvBiologicalRole.java
+++ repo/intact-core/trunk/src/main/java/uk/ac/ebi/intact/model/CvBiologicalRole.java
@@ -1,57 +1,40 @@
 /*
-Copyright (c) 2002 The European Bioinformatics Institute, and others.
-All rights reserved. Please see the file LICENSE
-in the root directory of this distribution.
-*/
+ * Copyright (c) 2002 The European Bioinformatics Institute, and others.
+ * All rights reserved. Please see the file LICENSE
+ * in the root directory of this distribution.
+ */
 package uk.ac.ebi.intact.model;
 
 import uk.ac.ebi.intact.annotation.EditorTopic;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.DiscriminatorValue;
 
 /**
- * The experimental role of the specific substrate in the interaction.
+ * The biological role of the specific substrate in the interaction.
  * <p/>
- * example bait example prey
+ * example enzyme, target, electron donor...
  *
- * @author hhe, Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
+ * @since 1.6
  */
 @Entity
-@DiscriminatorValue( "uk.ac.ebi.intact.model.CvComponentRole" )
+@DiscriminatorValue( "uk.ac.ebi.intact.model.CvBiologicalRole" )
 @EditorTopic
-public class CvComponentRole extends CvObject implements Editable {
+public class CvBiologicalRole extends CvObject implements Editable {
 
     //////////////////////
     // Constants
 
-    public static final String INHIBITOR = "inhibitor";
-    public static final String INHIBITOR_PSI_REF = "MI:0586";
-
-    public static final String INHIBITED = "inhibited";
-    public static final String INHIBITED_PSI_REF = "MI:0587";
-
-    public static final String BAIT = "bait";
-    public static final String BAIT_PSI_REF = "MI:0496";
-
-    public static final String PREY = "prey";
-    public static final String PREY_PSI_REF = "MI:0498";
-
-    public static final String TARGET = "target";
-    public static final String TARGET_PSI_REF = "MI:0502";
-
-    public static final String NEUTRAL = "neutral component";
-    public static final String NEUTRAL_PSI_REF = "MI:0497";
+    public static final String UNSPECIFIED = "unspecified";
+    public static final String UNSPECIFIED_PSI_REF = "MI:0499";    
 
     public static final String ENZYME = "enzyme";
     public static final String ENZYME_PSI_REF = "MI:0501";
 
     public static final String ENZYME_TARGET = "enzyme target";
-    public static final String ENZYME_TARGET_PSI_REF = "MI:0502";
-
-    public static final String UNSPECIFIED = "unspecified";
-    public static final String UNSPECIFIED_PSI_REF = "MI:0499";
+    public static final String ENZYME_TARGET_PSI_REF = "MI:0502";    
 
     public static final String SELF = "self";
     public static final String SELF_PSI_REF = "MI:0503";
@@ -62,23 +45,29 @@ public class CvComponentRole extends CvObject implements Editable {
     public static final String ELECTRON_ACCEPTOR = "electron acceptor";
     public static final String ELECTRON_ACCEPTOR_MI_REF = "MI:0580";
 
+    public static final String INHIBITOR = "inhibitor";
+    public static final String INHIBITOR_MI_REF = "MI:0586";
+
+    public static final String COFACTOR = "cofactor";
+    public static final String COFACTOR_MI_REF = "MI:0682";
+
+    public static final String STIMULATOR = "stimulator";
+    public static final String STIMULATOR_MI_REF = "MI:0840";
+
     public static final String FLUROPHORE_DONOR = "donor fluorophore";
     public static final String FLUROPHORE_DONOR_MI_REF = "MI:0583";
 
     public static final String FLUROPHORE_ACCEPTOR = "acceptor fluorophore";
     public static final String FLUROPHORE_ACCEPTOR_MI_REF = "MI:0584";
 
-    public static final String ANCILLARY = "ancillary";
-    public static final String ANCILLARY_MI_REF = "MI:0684";
 
-    public static final String COFACTOR = "cofactor";
-    public static final String COFACTOR_MI_REF = "MI:0682";
-
+    //////////////////////
+    // Constructor
 
     /**
      * Necessary for hibernate, yet set to private as it should not be used for any other purpose.
      */
-    private CvComponentRole() {
+    private CvBiologicalRole() {
         //super call sets creation time data
         super();
     }
@@ -91,7 +80,7 @@ public class CvComponentRole extends CvObject implements Editable {
      *
      * @throws NullPointerException thrown if either parameters are not specified
      */
-    public CvComponentRole( Institution owner, String shortLabel ) {
+    public CvBiologicalRole( Institution owner, String shortLabel ) {
 
         //super call sets up a valid CvObject
         super( owner, shortLabel );
