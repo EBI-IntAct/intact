f144bc81e15a3893fa7ec0ebf25a45214ae01866
skerrien <samuel.kerrien@gmail.com>
2007-05-01 16:47:58 +0000

    update of the IA_COMPOENNT table to split the role

---
+++ repo/utils/sql/oracle/version_1_4_0/update_component.sql
@@ -0,0 +1,5 @@
+ALTER TABLE IA_Component ADD(biologicalrole_ac  VARCHAR2(30)  CONSTRAINT fk_component$biologicalrole  REFERENCES IA_ControlledVocab(ac));
+ALTER TABLE IA_Component ADD(identmethod_ac     VARCHAR2(30)  CONSTRAINT fk_component$identmethod     REFERENCES IA_ControlledVocab(ac));
+
+
+
