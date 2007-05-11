7c0cb3aa9fb00ba85f36d72eb9fa839f892d502b
catherineleroy <catherineleroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-05-11 10:11:04 +0000

    script adding 2 fieds in ia_component

---
+++ repo/utils/sql/oracle/version_1_6_0/130_update_component.sql
@@ -0,0 +1,2 @@
+ALTER TABLE IA_Component ADD(biologicalrole_ac  VARCHAR2(30)  CONSTRAINT fk_component$biologicalrole  REFERENCES IA_ControlledVocab(ac));
+ALTER TABLE IA_Component ADD(identmethod_ac     VARCHAR2(30)  CONSTRAINT fk_component$identmethod     REFERENCES IA_ControlledVocab(ac));
