842c7db57cec53c857772f563e6cab5519bd2c89
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-06-12 22:37:09 +0000

    Created base TestDataset class

---
+++ repo/commons/trunk/intact-commons/src/main/java/uk/ac/ebi/intact/commons/util/TestDataset.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.intact.commons.util;
+
+import java.net.URL;
+
+/**
+ * Implemented for those classes that are datasets (for testing)
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public interface TestDataset
+{
+    String getId();
+
+    URL getResource();
+
+    boolean containsAllCVs();
+}
