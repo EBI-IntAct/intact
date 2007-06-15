3cf6eeda7e9533e9202b9a21b4fc759f2c0cf774
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-06-15 20:13:22 +0000

    Refactored packages

--- repo/unit-datasets/trunk/dbunit-dataset-maven-plugin/src/main/java/uk/ac/ebi/intact/unitdataset/plugin/CvConfiguration.java
+++ repo/unit-datasets/trunk/dbunit-dataset-maven-plugin/src/main/java/uk/ac/ebi/intact/unitdataset/plugin/CvConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.ebi.intact.dataexchange.psimi.xml.unitdataset;
+package uk.ac.ebi.intact.unitdataset.plugin;
 
 import java.io.File;
 
