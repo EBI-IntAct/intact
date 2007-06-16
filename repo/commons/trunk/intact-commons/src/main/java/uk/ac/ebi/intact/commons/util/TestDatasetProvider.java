3f4dbea2e58e96e4e8d3b12f01b467883cf6f4b2
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-06-16 23:30:41 +0000

    Changed TestDataSet interface and created provider interface, that creates TestDataSets

---
+++ repo/commons/trunk/intact-commons/src/main/java/uk/ac/ebi/intact/commons/util/TestDatasetProvider.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.intact.commons.util;
+
+/**
+ * TODO comment this
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public interface TestDatasetProvider
+{
+    TestDataset getTestDataset(String id);
+}
