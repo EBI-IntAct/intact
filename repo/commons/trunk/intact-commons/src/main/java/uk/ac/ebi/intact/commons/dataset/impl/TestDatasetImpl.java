340624f56554e76bef6c19096cd5b3d635b81d5f
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-06-18 11:31:44 +0000

    Created equals and haschode for implementation of TestDataset (and test)

--- repo/commons/trunk/intact-commons/src/main/java/uk/ac/ebi/intact/commons/dataset/impl/TestDatasetImpl.java
+++ repo/commons/trunk/intact-commons/src/main/java/uk/ac/ebi/intact/commons/dataset/impl/TestDatasetImpl.java
@@ -43,4 +43,23 @@ public class TestDatasetImpl implements TestDataset
     {
         return containsAllCVs;
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestDatasetImpl that = (TestDatasetImpl) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (id != null ? id.hashCode() : 0);
+    }
 }
---
+++ repo/commons/trunk/intact-commons/src/test/java/uk/ac/ebi/intact/commons/dataset/impl/TestDatasetImplTest.java
@@ -0,0 +1,48 @@
+package uk.ac.ebi.intact.commons.dataset.impl;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.intact.commons.dataset.TestDataset;
+import uk.ac.ebi.intact.commons.dataset.TestDatasetFactory;
+
+import java.net.URL;
+
+/**
+ * TODO comment this
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class TestDatasetImplTest
+{
+    private URL dummyURL;
+
+    @Before
+    public void before() throws Exception {
+        dummyURL = TestDatasetImplTest.class.getResource(getClass().getSimpleName()+".class");
+    }
+
+    @After
+    public void after() throws Exception {
+        dummyURL = null;
+    }
+
+    @Test
+    public void testEquals_sameId() throws Exception {
+        final String id = "id1";
+        TestDataset ds1 = TestDatasetFactory.createTestDataset(id, dummyURL, false);
+        TestDataset ds2 = TestDatasetFactory.createTestDataset(id, dummyURL, false);
+
+        Assert.assertEquals(ds1, ds2);
+    }
+
+    @Test
+    public void testEquals_differentId() throws Exception {
+        TestDataset ds1 = TestDatasetFactory.createTestDataset("id1", dummyURL, false);
+        TestDataset ds2 = TestDatasetFactory.createTestDataset("id2", dummyURL, false);
+
+        Assert.assertNotSame(ds1, ds2);
+    }
+}
