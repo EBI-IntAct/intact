f1938618219ce6cde87cd0b55e028a3a63bbfdb9
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-08-01 16:39:52 +0000

    script to add unspecified role as experimental role

---
+++ repo/utils/sql/oracle/version_1_6_0/185_insert_unspecified_as_experimentalRole.sql
@@ -0,0 +1,122 @@
+declare
+	identity_ac ia_controlledvocab.ac%TYPE;
+	psimi_ac ia_controlledvocab.ac%TYPE;
+	unspecified_biorole_ac ia_controlledvocab.ac%TYPE;
+	owner_ac ia_controlledvocab.ac%TYPE;
+	pubmed_ac ia_controlledvocab.ac%TYPE;
+	primaryref_ac ia_controlledvocab.ac%TYPE;
+	
+begin
+	dbms_output.enable(1000000000000);
+
+	------------------
+	-- get psimi_ac --
+	------------------
+	select ac
+	into psimi_ac
+	from ia_controlledvocab
+	where shortlabel = 'psi-mi';
+	
+	------------------
+	-- get owner_ac --
+	------------------
+	select owner_ac
+	into owner_ac
+	from ia_controlledvocab
+	where shortlabel = 'psi-mi';
+	
+	---------------------
+	-- get identity_ac --
+	---------------------
+	select ac
+	into identity_ac
+	from ia_controlledvocab
+	where shortlabel = 'identity';
+
+		
+	-------------------
+	-- get pubmed_ac --
+	-------------------
+	select cv.ac
+	into pubmed_ac
+	from ia_controlledvocab cv, ia_controlledvocab_xref x
+	where cv.ac = x.parent_ac
+	and x.primaryId = 'MI:0446'
+	and x.database_ac = psimi_ac
+	and x.qualifier_ac = identity_ac;
+
+	 -------------------
+	 -- get pubmed_ac --
+	 -------------------
+	 select cv.ac
+	 into primaryref_ac
+	 from ia_controlledvocab cv, ia_controlledvocab_xref x
+	 where cv.ac = x.parent_ac
+	 and x.primaryId = 'MI:0358'
+	 and x.database_ac = psimi_ac
+	 and x.qualifier_ac = identity_ac;
+
+	-------------------------------------------------------------
+	-- Insert "unspecified role" biorole in ia_controlledvocab --
+	-------------------------------------------------------------	
+	insert into ia_controlledvocab (ac, 
+					owner_ac, 
+					objclass, 
+					shortlabel, 
+					fullname)
+	values ('EBI-' || Intact_ac.nextval, 
+		owner_ac, 
+		'uk.ac.ebi.intact.model.CvExperimentalRole', 
+		'unspecified role',
+		'unspecified role');
+
+	------------------------------------
+	-- Get the unspecified_biorole_ac --
+	------------------------------------
+	select ac
+	into unspecified_biorole_ac
+	from ia_controlledvocab 
+	where shortlabel = 'unspecified role'
+	and objclass = 'uk.ac.ebi.intact.model.CvExperimentalRole';
+
+	---------------------------------------------------------------
+	-- Add the psi-mi identity xref to the "unspecified role" cv --
+	---------------------------------------------------------------
+	insert into ia_controlledvocab_xref (parent_ac, 
+					     ac, 
+					     qualifier_ac, 
+					     database_ac, 
+					     owner_ac, 
+					     primaryId)
+	values (unspecified_biorole_ac, 
+		'EBI-' || Intact_ac.nextval, 
+		identity_ac, 
+		psimi_ac, 
+		owner_ac, 
+		'MI:0499');
+
+        ------------------------------------------------------------------
+        -- Add the psi-mi primary-ref xref to the "unspecified role" cv --
+        ------------------------------------------------------------------
+	insert into ia_controlledvocab_xref (parent_ac,
+                                     ac,
+                                     qualifier_ac,
+                                     database_ac,
+                                     owner_ac,
+                                     primaryId)
+	values (unspecified_biorole_ac,
+        	'EBI-' || Intact_ac.nextval,
+        	primaryref_ac,
+        	pubmed_ac,
+        	owner_ac, 
+		'14755292');
+
+	commit;
+
+end;
+/
+
+exit
+
+
+
