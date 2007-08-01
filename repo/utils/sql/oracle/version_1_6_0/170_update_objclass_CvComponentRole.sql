d7328d6659b7b1210e4025193ac30f916318ce1c
skerrien <samuel.kerrien@gmail.com>
2007-08-01 15:56:07 +0000

    removed line break in the middle of the query

--- repo/utils/sql/oracle/version_1_6_0/170_update_objclass_CvComponentRole.sql
+++ repo/utils/sql/oracle/version_1_6_0/170_update_objclass_CvComponentRole.sql
@@ -6,7 +6,9 @@ from ia_controlledvocab cv, ia_controlledvocab_xref x
 where cv.ac = x.parent_ac
 and x.database_ac = 'EBI-77142'
 and x.qualifier_ac = 'EBI-28'
-and x.primaryId in ('MI:0496', 'MI:0497', 'MI:0498', 'MI:0499', 'MI:0503', 'MI:0581', 'MI:0582', 'MI:0583', 'MI:0584', 'MI:0684','MI:0495' ));
+and x.primaryId in ('MI:0496', 'MI:0497', 'MI:0498', 'MI:0499', 
+                    'MI:0503', 'MI:0581', 'MI:0582', 'MI:0583', 
+                    'MI:0584', 'MI:0684', 'MI:0495' ));
 
 update ia_controlledvocab
 set objclass='uk.ac.ebi.intact.model.CvBiologicalRole'
@@ -16,8 +18,10 @@ from ia_controlledvocab cv, ia_controlledvocab_xref x
 where cv.ac = x.parent_ac
 and x.database_ac = 'EBI-77142'
 and x.qualifier_ac = 'EBI-28'
-and x.primaryId in ('MI:0499', 'MI:0501', 'MI:0502', 'MI:0503', 'MI:0579', 'MI:0580', 'MI:0586', 'MI:0682', '
-MI:0840', 'MI:0842', 'MI:0843','MI:0840','MI:0500' ));
+and x.primaryId in ('MI:0499', 'MI:0501', 'MI:0502', 'MI:0503', 
+                    'MI:0579', 'MI:0580', 'MI:0586', 'MI:0682', 
+                    'MI:0840', 'MI:0842', 'MI:0843', 'MI:0840',
+                    'MI:0500' ));
 
 commit;
 
