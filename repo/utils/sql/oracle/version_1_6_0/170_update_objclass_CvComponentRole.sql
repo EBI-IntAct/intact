c397b43d042b6a41385e0a6539992068ce87c8d0
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-08-02 08:52:03 +0000

    added missing CVs 'fret pair' and 'inhibited'

--- repo/utils/sql/oracle/version_1_6_0/170_update_objclass_CvComponentRole.sql
+++ repo/utils/sql/oracle/version_1_6_0/170_update_objclass_CvComponentRole.sql
@@ -8,7 +8,7 @@ and x.database_ac = 'EBI-77142'
 and x.qualifier_ac = 'EBI-28'
 and x.primaryId in ('MI:0496', 'MI:0497', 'MI:0498', 'MI:0499', 
                     'MI:0503', 'MI:0581', 'MI:0582', 'MI:0583', 
-                    'MI:0584', 'MI:0684', 'MI:0495' ));
+                    'MI:0584', 'MI:0684', 'MI:0495', 'MI:0865' ));
 
 update ia_controlledvocab
 set objclass='uk.ac.ebi.intact.model.CvBiologicalRole'
@@ -21,7 +21,7 @@ and x.qualifier_ac = 'EBI-28'
 and x.primaryId in ('MI:0499', 'MI:0501', 'MI:0502', 'MI:0503', 
                     'MI:0579', 'MI:0580', 'MI:0586', 'MI:0682', 
                     'MI:0840', 'MI:0842', 'MI:0843', 'MI:0840',
-                    'MI:0500' ));
+                    'MI:0500', 'MI:0587' ));
 
 commit;
 
