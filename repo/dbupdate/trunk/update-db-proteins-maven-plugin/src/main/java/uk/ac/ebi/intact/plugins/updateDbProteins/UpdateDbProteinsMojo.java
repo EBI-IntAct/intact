356eca7b33712e33d29f645ee777b57cf508c4e6
CatherineLeroy <CatherineLeroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-08-14 12:23:15 +0000

    remove the return

--- repo/dbupdate/trunk/update-db-proteins-maven-plugin/src/main/java/uk/ac/ebi/intact/plugins/updateDbProteins/UpdateDbProteinsMojo.java
+++ repo/dbupdate/trunk/update-db-proteins-maven-plugin/src/main/java/uk/ac/ebi/intact/plugins/updateDbProteins/UpdateDbProteinsMojo.java
@@ -142,10 +142,10 @@ public class UpdateDbProteinsMojo
 
         System.out.println("P88946 UPDATED\n\n\n\n\n\n\n\n");
         System.out.println("BEFORE ASSERT");
-        int x = 1;
-        if(x == 1){
-            return;
-        }
+//        int x = 1;
+//        if(x == 1){
+//            return;
+//        }
 
         commitTransaction();
 
