f603625a6f8d5467a728188f20152c6ee8c02cdc
baranda <brunoaranda@gmail.com>
2007-08-25 14:17:31 +0000

    - Fixed gzip option in mojo. - Added test for mojo

--- repo/maven-plugins/fasta-maven-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugins/fasta/FastaExportMojoTest.java
+++ repo/maven-plugins/fasta-maven-plugin/trunk/src/test/java/uk/ac/ebi/intact/plugins/fasta/FastaExportMojoTest.java
@@ -17,18 +17,44 @@ package uk.ac.ebi.intact.plugins.fasta;
 
 import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Test;
+import uk.ac.ebi.intact.config.impl.CustomCoreDataConfig;
+import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.context.IntactSession;
+import uk.ac.ebi.intact.context.impl.StandaloneSession;
+import uk.ac.ebi.intact.core.persister.standard.InteractionPersister;
+import uk.ac.ebi.intact.core.unit.IntactMockBuilder;
+import uk.ac.ebi.intact.core.unit.IntactUnit;
 
 import java.io.File;
-
-import uk.ac.ebi.intact.plugins.fasta.FastaExportMojo;
 
 public class FastaExportMojoTest extends AbstractMojoTestCase
 {
 
+    @Test
     public void testSimpleGeneration() throws Exception {
+        File hibernateConfig = new File(FastaExportMojoTest.class.getResource("/test-hibernate.cfg.xml").getFile());
+
+        IntactSession session = new StandaloneSession();
+        CustomCoreDataConfig dataConfig = new CustomCoreDataConfig("custom", hibernateConfig, session);
+        IntactContext.initContext(dataConfig, session);
+
+        IntactUnit iu = new IntactUnit();
+        iu.createSchema(true);
+
+        IntactMockBuilder mockBuilder = new IntactMockBuilder(IntactContext.getCurrentInstance().getInstitution());
+
+        IntactContext.getCurrentInstance().getDataContext().beginTransaction();
+        InteractionPersister.getInstance().saveOrUpdate(mockBuilder.createInteractionRandomBinary());
+        InteractionPersister.getInstance().commit();
+        IntactContext.getCurrentInstance().getDataContext().commitTransaction();
+
+        IntactContext.getCurrentInstance().getConfig().setReadOnlyApp(true);
+
         File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/fasta-export.xml" );
 
         FastaExportMojo mojo = (FastaExportMojo) lookupMojo( "export", pluginXmlFile );
+        mojo.setHibernateConfig(hibernateConfig);
         mojo.setLog( new SystemStreamLog() );
 
         mojo.execute();
