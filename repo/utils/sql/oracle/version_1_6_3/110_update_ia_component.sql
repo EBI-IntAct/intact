8fe15b53fad793d106a427c264d9c173e15c9026
skerrien <samuel.kerrien@gmail.com>
2007-08-31 14:34:30 +0000

    do not remove column from ia_component as this would not be backward compatible

--- repo/utils/sql/oracle/version_1_6_3/110_update_ia_component.sql
+++ repo/utils/sql/oracle/version_1_6_3/110_update_ia_component.sql
@@ -1,10 +1,10 @@
 
-PROMPT Remove column handling single participant identification method from IA_COMPONENT.
-ALTER TABLE ia_component DROP COLUMN IDENTMETHOD_AC;
+-- PROMPT Remove column handling single participant identification method from IA_COMPONENT.
+-- ALTER TABLE ia_component DROP COLUMN IDENTMETHOD_AC;
 
 PROMPT Add missing columns (shortLabel, fullName) in the ia_component_audit table
 ALTER TABLE ia_component_audit  ADD(shortLabel VARCHAR2(20)  NULL);
 ALTER TABLE ia_component_audit  ADD(fullName   VARCHAR2(250) NULL);
 
-PROMPT Remove column handling single participant identification method from IA_COMPONENT_AUDIT.
-ALTER TABLE ia_component_audit DROP COLUMN IDENTMETHOD_AC;
+-- PROMPT Remove column handling single participant identification method from IA_COMPONENT_AUDIT.
+-- ALTER TABLE ia_component_audit DROP COLUMN IDENTMETHOD_AC;
