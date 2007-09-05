34801bac0d016da9f0363cbed2f60acc87351e1b
catherineleroy <catherineleroy@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-09-05 13:53:30 +0000

    releasing version 6

---
+++ repo/bridges/tags/bridges-master-2.0-beta-6/intact-taxonomy/src/test/java/uk/ac/ebi/intact/bridges/taxonomy/NewtTermTest.java
@@ -0,0 +1,48 @@
+package uk.ac.ebi.intact.bridges.taxonomy;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * NewtTerm Tester.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version 1.0
+ * @since <pre>01/17/2007</pre>
+ */
+public class NewtTermTest extends TestCase {
+
+    public NewtTermTest( String name ) {
+        super( name );
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public static Test suite() {
+        return new TestSuite( NewtTermTest.class );
+    }
+
+    ////////////////////
+    // Tests
+
+    public void testNewtTerm() {
+        NewtTerm t = new NewtTerm( "123|common|sci|blabla" );
+        assertNotNull( t );
+        assertEquals( 123, t.getTaxid() );
+        assertNotNull( t.getScientificName() );
+        assertEquals( "sci", t.getScientificName() );
+
+        assertNotNull( t.getCommonName() );
+        assertEquals( "common", t.getCommonName() );
+
+        assertTrue( t.getChildren().isEmpty() );
+        assertTrue( t.getParents().isEmpty() );
+    }
+}
