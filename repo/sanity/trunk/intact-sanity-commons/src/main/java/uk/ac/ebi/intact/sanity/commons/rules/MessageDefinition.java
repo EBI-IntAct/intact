99b0eaeea50744dfa2471058b1063b98af330f50
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-18 14:02:23 +0000

    Added constructors and instance variable

--- repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
@@ -1,14 +1,50 @@
 package uk.ac.ebi.intact.sanity.commons.rules;
 
+import static uk.ac.ebi.intact.sanity.commons.rules.KeyPrefix.*;
+
 /**
- * TODO comment that class header
+ * Definition of the messages.
  *
  * @author Samuel Kerrien
  * @version $Id$
- * @since TODO specify the maven artifact version
+ * @since 2.0.0
  */
 public enum MessageDefinition {
 
-    
+    BLA( ANNOTATED_OBJECT, "1", "", "");
 
+    ////////////////////////
+    // Instance variable
+
+    String key;
+    String description;
+    String suggestion;
+
+    //////////////////
+    // Constructors
+
+    MessageDefinition(String keyPrefix, String value, String description, String suggestion) {
+        this( keyPrefix, value, description );
+        this.suggestion = suggestion;
+    }
+
+    MessageDefinition(String keyPrefix, String value, String description ) {
+        this.key = keyPrefix + value;
+        this.description = description;
+    }
+
+    ///////////////
+    // Getters
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSuggestion() {
+        return suggestion;
+    }
 }
