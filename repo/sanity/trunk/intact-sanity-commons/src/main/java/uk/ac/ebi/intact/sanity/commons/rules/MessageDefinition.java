5f988c2bdd859c17ef000f156002f789b1d65df2
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-18 13:44:36 +0000

    Class that will hold the check key, description and suggestion.

---
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.intact.sanity.commons.rules;
+
+/**
+ * TODO comment that class header
+ *
+ * @author Samuel Kerrien
+ * @version $Id$
+ * @since TODO specify the maven artifact version
+ */
+public enum MessageDefinition {
+
+    
+
+}
