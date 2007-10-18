2603974e858e6d727df90c6fe4dfa36b8325abfb
skerrien <samuel.kerrien@gmail.com>
2007-10-18 14:30:10 +0000

    more definitions

--- repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
@@ -11,11 +11,61 @@ import static uk.ac.ebi.intact.sanity.commons.rules.KeyPrefix.*;
  */
 public enum MessageDefinition {
 
-    ANNOTATION_WITH_WRONG_TOPIC( ANNOTATED_OBJECT, 1, "Objects with annotation using hidden or obsolete CvTopic",
-                                 "");
+    /////////////////////////
+    // Annotated Objects
+
+    ANNOTATION_WITH_WRONG_TOPIC( ANNOTATED_OBJECT, 1, "Objects with annotation using hidden or obsolete CvTopic" ),
+
+    BROKEN_URL( ANNOTATED_OBJECT, 2, "Invalid URLs" ),
+
+    XREF_INVALID_PRIMARYID( ANNOTATED_OBJECT, 3, "Xref primary ID not matching CvDatabase regular expression" ),
+
+    ////////////////////////
+    // BioSource
+
+    BIOSOURCE_WITHOUT_NEWT_XREF( BIOSOURCE, 1, "BioSource without Newt Xref" ),
+
+    ////////////////////////
+    // CvObject
+
+    INTERACTION_DETECTION( CV, 1, "Interaction detection method without annotation unniprot-de-export" ),
+
+    ////////////////////////
+    // Experiment
+
+    EXPERIMENT_NOT_SUPER_CURATED(  EXPERIMENT, 1, "" ),
+
+    EXPERIMENT_ON_HOLD( EXPERIMENT, 2, "" ),
+
+    EXPERIMENT_WITHOUT_BIOSOURCE( EXPERIMENT, 3, "" ),
+
+    EXPERIMENT_WITHOUT_INTERACTION_DETECT( EXPERIMENT, 4, "" ),
+
+    EXPERIMENT_WITHOUT_PARTICIPANT_DETECT( EXPERIMENT, 5, "" ),
+
+    EXPERIMENT_WITHOUT_INTERACTION( EXPERIMENT, 6, "" ),
+
+    EXPERIMENT_TO_BE_REVIEWED( EXPERIMENT, 7, "" ),
+
+    EXPERIMENT_WITHOUT_PUBMED( EXPERIMENT, 8, "" ),
+
+    ////////////////////////
+    // Feature
+
+    FEATURE_WITHOUT_TYPE( FEATURE, 1, "" ),
+
+    FEATURE_WITHOUT_RANGE( FEATURE, 2, "" ),
+
+    ////////////////////////
+    // Interaction
+
+    INTERACTION_COMPONENT_ROLE( INTERACTION, 1, "" ),
+
+    INTERACTION_WITHOUT_COMPONENT( INTERACTION, 1, "" ),
+
+    INTERACTION_WITHOUT_EXPERIMENT( INTERACTION, 1, "" );
 
     // Bruno
-
 
     ////////////////////////
     // Instance variable
