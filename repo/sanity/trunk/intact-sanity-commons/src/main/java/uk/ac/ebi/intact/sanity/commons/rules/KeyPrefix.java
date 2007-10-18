600972e439c00f5f7e7afac4f5322ee741c86784
skerrien <samuel.kerrien@gmail.com>
2007-10-18 13:50:58 +0000

    Message prefixes

---
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/KeyPrefix.java
@@ -0,0 +1,19 @@
+package uk.ac.ebi.intact.sanity.commons.rules;
+
+/**
+ * Definition of the prefixes for all message types.
+ *
+ * @author Samuel Kerrien
+ * @version $Id$
+ * @since 2.0.0
+ */
+public interface KeyPrefix {
+    static final String EXPERIMENT = "EXP-";
+    static final String INTERACTION = "INT-";
+    static final String PROTEIN = "PROT-";
+    static final String SMALL_MOLECULE = "SM-";
+    static final String NUCLEIC_ACID = "NUC-";
+    static final String CV = "CV-";
+    static final String FEATURE = "FTR-";
+    static final String PUBLICATION = "PUB-";
+}
