16d4906e47366fe1a7bb0a21367719679ef73019
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-18 14:15:25 +0000

    updated constructors

--- repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
@@ -11,7 +11,11 @@ import static uk.ac.ebi.intact.sanity.commons.rules.KeyPrefix.*;
  */
 public enum MessageDefinition {
 
-    BLA( ANNOTATED_OBJECT, "1", "", "");
+    ANNOTATION_WITH_WRONG_TOPIC( ANNOTATED_OBJECT, 1, "Objects with annotation using hidden or obsolete CvTopic",
+                                 "");
+
+    // Bruno
+
 
     ////////////////////////
     // Instance variable
@@ -23,13 +27,22 @@ public enum MessageDefinition {
     //////////////////
     // Constructors
 
-    MessageDefinition(String keyPrefix, String value, String description, String suggestion) {
-        this( keyPrefix, value, description );
+    MessageDefinition(String keyPrefix, int id, String description, String suggestion) {
+        this( keyPrefix, id, description );
         this.suggestion = suggestion;
     }
 
-    MessageDefinition(String keyPrefix, String value, String description ) {
-        this.key = keyPrefix + value;
+    MessageDefinition(String keyPrefix, int id, String description ) {
+        if( keyPrefix == null || keyPrefix.trim().length() == 0 ) {
+            throw new IllegalArgumentException( "You must give a non null key prefix" );
+        }
+        if( id <= 0 ) {
+            throw new IllegalArgumentException( "You must give a positive identifier" );
+        }
+        if( description == null || description.trim().length() == 0 ) {
+            throw new IllegalArgumentException( "You must give a non null description" );
+        }
+        this.key = keyPrefix + String.valueOf( id );
         this.description = description;
     }
 
