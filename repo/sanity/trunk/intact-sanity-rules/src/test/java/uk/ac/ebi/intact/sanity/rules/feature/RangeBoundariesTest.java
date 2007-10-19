17cd50f794194e108d861e294500c3e2524b100e
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-19 16:44:32 +0000

    [ISAN-13] New Sanity Check: ranges with boundaries not matching underlying sequence

---
+++ repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/feature/RangeBoundariesTest.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.intact.sanity.rules.feature;
+
+import org.junit.Test;
+
+/**
+ * RangeBoundaries Tester.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since 2.0.0
+ */
+public class RangeBoundariesTest {
+
+    @Test
+    public void check() throws Exception {
+        // TODO implement me !!
+    }
+}
