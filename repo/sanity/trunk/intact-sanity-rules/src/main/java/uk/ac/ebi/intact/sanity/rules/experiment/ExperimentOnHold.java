29b886332005fc47d6a953e3e5e6567a6fe213eb
baranda <brunoaranda@gmail.com>
2007-10-19 16:31:53 +0000

    ISAN-16 (Check on experiment that haven't been super-curated should not be applied to those having an Annotation( on-hold ))

--- repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentOnHold.java
+++ repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentOnHold.java
@@ -5,6 +5,7 @@
  */
 package uk.ac.ebi.intact.sanity.rules.experiment;
 
+import uk.ac.ebi.intact.context.IntactContext;
 import uk.ac.ebi.intact.model.Experiment;
 import uk.ac.ebi.intact.model.util.ExperimentUtils;
 import uk.ac.ebi.intact.sanity.commons.SanityRuleException;
@@ -16,6 +17,7 @@ import uk.ac.ebi.intact.sanity.rules.RuleGroup;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Check is the experiment is on-hold.
@@ -25,13 +27,27 @@ import java.util.Collection;
  * @since 2.0.0
  */
 
-@SanityRule(target = Experiment.class, group = RuleGroup.INTACT )
+@SanityRule(target = Experiment.class, group = RuleGroup.INTACT_DB )
 public class ExperimentOnHold implements Rule<Experiment> {
 
     public Collection<GeneralMessage> check(Experiment experiment) throws SanityRuleException {
         Collection<GeneralMessage> messages = new ArrayList<GeneralMessage>();
         if(ExperimentUtils.isOnHold(experiment)){
             messages.add(new GeneralMessage( MessageDefinition.EXPERIMENT_ON_HOLD, experiment));
+        } else {
+            // TODO this can be removed when all the experiments have a Publication object in the DB
+            String pubmedId = ExperimentUtils.getPubmedId(experiment);
+
+            if (pubmedId != null) {
+                final List<Experiment> experimentsSamePubmed = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
+                        .getExperimentDao().getByPubId(pubmedId);
+
+                for (Experiment experimentSamePubmed : experimentsSamePubmed) {
+                    if (ExperimentUtils.isOnHold(experimentSamePubmed)) {
+                        messages.add(new GeneralMessage( MessageDefinition.EXPERIMENT_ON_HOLD, experiment));
+                    }
+                }
+            }
         }
         return messages;
     }
