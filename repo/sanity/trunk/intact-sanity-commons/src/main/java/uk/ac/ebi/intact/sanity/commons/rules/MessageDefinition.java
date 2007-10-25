1940feda386bb81b40a762d662ccb330b245bfcf
skerrien <samuel.kerrien@gmail.com>
2007-10-25 17:07:56 +0000

    [ISAN-36] Small Molecule have to have a Chebi identity

--- repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
+++ repo/sanity/trunk/intact-sanity-commons/src/main/java/uk/ac/ebi/intact/sanity/commons/rules/MessageDefinition.java
@@ -44,7 +44,6 @@ public enum MessageDefinition {
     TOPIC_WITHOUT_USED_IN_CLASS( CvTopic.class, CV, 2, "Topic without annotation 'used-in-class'", WARNING,
                                  "Add 'used-in-class' if you wish to restrict which editors the term is visible in" ),
 
-
     ////////////////////////
     // Experiment
 
@@ -59,18 +58,18 @@ public enum MessageDefinition {
     EXPERIMENT_WITHOUT_PARTICIPANT_DETECT( Experiment.class, EXPERIMENT, 5, "Experiment without participant detection method", ERROR ),
 
     EXPERIMENT_WITHOUT_INTERACTION( Experiment.class, EXPERIMENT, 6, "Experiment without interactions", ERROR,
-                                    "Edit the experiment and add at least one interaction"),
+                                    "Edit the experiment and add at least one interaction" ),
 
     EXPERIMENT_TO_BE_REVIEWED( Experiment.class, EXPERIMENT, 7, "Experiment marked as 'To be reviewed'", INFO ),
 
     EXPERIMENT_WITHOUT_PRIMARY_REF( Experiment.class, EXPERIMENT, 8, "No Pubmed ID found for experiment", ERROR,
-                               "Edit the experiment and add the primary-reference to PubMed or a DOI if a PubMed is not available"),
+                                    "Edit the experiment and add the primary-reference to PubMed or a DOI if a PubMed is not available" ),
 
-    EXPERIMENT_WITH_MULTIPLE_PRIMARY_REF ( Experiment.class, EXPERIMENT, 9, "Experiment with more than one primary-reference to PubMed or DOI", ERROR,
-                                           "Remove any redundant or wrong primary reference, giving preference to PubMed"),
+    EXPERIMENT_WITH_MULTIPLE_PRIMARY_REF( Experiment.class, EXPERIMENT, 9, "Experiment with more than one primary-reference to PubMed or DOI", ERROR,
+                                          "Remove any redundant or wrong primary reference, giving preference to PubMed" ),
 
-    EXPERIMENT_WITHOUT_FULLNAME(Experiment.class, EXPERIMENT, 9, "Experiment with no fullname", WARNING,
-                                "Add the corresponding publication title as fullname"),
+    EXPERIMENT_WITHOUT_FULLNAME( Experiment.class, EXPERIMENT, 9, "Experiment with no fullname", WARNING,
+                                 "Add the corresponding publication title as fullname" ),
 
     EXPERIMENT_WITH_PUBMED_TO_BE_ASSIGNED( Experiment.class, EXPERIMENT, 9, "Experiment with no fullname", WARNING,
                                            "Add the corresponding publication title as fullname" ),
@@ -78,30 +77,30 @@ public enum MessageDefinition {
     // Feature
 
     FEATURE_WITHOUT_TYPE( Feature.class, FEATURE, 1, "A feature type is mandatory and was not found", ERROR,
-                          "Edit the feature and add a type"),
+                          "Edit the feature and add a type" ),
 
     FEATURE_WITHOUT_RANGE( Feature.class, FEATURE, 2, "Feature without ranges specified", ERROR ),
 
-    FEATURE_DELETION_FEATURE_TOO_LONG ( Feature.class, FEATURE, 3, "Mutation feature longer than 2 amino-acids",
-                                        WARNING, "Change to binding site (or child term)"),
+    FEATURE_DELETION_FEATURE_TOO_LONG( Feature.class, FEATURE, 3, "Mutation feature longer than 2 amino-acids",
+                                       WARNING, "Change to binding site (or child term)" ),
 
-    FEATURE_WITH_INCOMPATIBLE_INTERACTOR ( Feature.class, FEATURE, 4,
-                                           "Feature on a non polymer interactor having a non undertermined range",
-                                           WARNING, "set the range to undertermined or update the interactor"),
+    FEATURE_WITH_INCOMPATIBLE_INTERACTOR( Feature.class, FEATURE, 4,
+                                          "Feature on a non polymer interactor having a non undertermined range",
+                                          WARNING, "set the range to undertermined or update the interactor" ),
 
     UNDETERMINED_RANGE_WITH_BOUNDARIES( Feature.class, FEATURE, 5,
                                         "Feature range set to undertermined and having to/from boundaries", WARNING ),
 
     DETERMINED_RANGE_WITHOUT_BOUNDARIES( Feature.class, FEATURE, 6,
-                                           "Feature range not set to undertermined and having no to/from boundaries",
-                                           WARNING ),
+                                         "Feature range not set to undertermined and having no to/from boundaries",
+                                         WARNING ),
 
     DETERMINED_RANGE_WITHOUT_SEQUENCE( Feature.class, FEATURE, 7,
                                        "Feature range with to/from boundaries but no underlying interactor's sequence",
                                        WARNING ),
 
-    RANGE_AND_INTERACTOR_BOUNDARIES_MISMATCH ( Feature.class, FEATURE, 8,
-                                               "Range and interactor's boundaries mismatch", WARNING ),
+    RANGE_AND_INTERACTOR_BOUNDARIES_MISMATCH( Feature.class, FEATURE, 8,
+                                              "Range and interactor's boundaries mismatch", WARNING ),
 
     ////////////////////////
     // Interaction
@@ -114,31 +113,31 @@ public enum MessageDefinition {
 
     INTERACTION_WITHOUT_EXPERIMENT( Interaction.class, INTERACTION, 4, "Interaction not associated to an Experiment", ERROR ),
 
-    INTERACTION_WITHOUT_TYPE( Interaction.class, INTERACTION, 5, "Interaction without interaction type", ERROR ),    
+    INTERACTION_WITHOUT_TYPE( Interaction.class, INTERACTION, 5, "Interaction without interaction type", ERROR ),
 
     INTERACTION_EXPERIMENT_COUNT( Interaction.class, INTERACTION, 6, "Interaction having none or more than one experiment", ERROR ),
 
     ////////////////////////
     // Interactor
 
-    INTERACTOR_WITH_TYPE_MISMATCH(  Interactor.class, INTERACTOR, 1,
-                                    "Interactor with mismatching Class and CvInteractorType", ERROR,
-                                    "Ask a developer to fix this"),    
+    INTERACTOR_WITH_TYPE_MISMATCH( Interactor.class, INTERACTOR, 1,
+                                   "Interactor with mismatching Class and CvInteractorType", ERROR,
+                                   "Ask a developer to fix this" ),
 
-    INTERACTOR_WITH_MISSING_TYPE(  Interactor.class, INTERACTOR, 2, "Interactor with missing CvInteractorType", ERROR,
-                                   "Ask a developer to fix this" ),    
+    INTERACTOR_WITH_MISSING_TYPE( Interactor.class, INTERACTOR, 2, "Interactor with missing CvInteractorType", ERROR,
+                                  "Ask a developer to fix this" ),
 
-    INTERACTOR_WITH_UNSUPPORTED_TYPE(  Interactor.class, INTERACTOR, 3,
-                                       "Interactor for which there is no known association between Class and CvinteractorType",
-                                       ERROR, "Ask a developer to fix this" ),
+    INTERACTOR_WITH_UNSUPPORTED_TYPE( Interactor.class, INTERACTOR, 3,
+                                      "Interactor for which there is no known association between Class and CvinteractorType",
+                                      ERROR, "Ask a developer to fix this" ),
 
-    INTERACTOR_WITH_INVALID_TYPE(  Interactor.class, INTERACTOR, 4,
-                                   "Interactor having a CvInteractorType that doesn't have a MI identity", ERROR ),
+    INTERACTOR_WITH_INVALID_TYPE( Interactor.class, INTERACTOR, 4,
+                                  "Interactor having a CvInteractorType that doesn't have a MI identity", ERROR ),
 
     ////////////////////////
     // Protein
 
-    PROTEIN_INCORRECT_CRC64( Protein.class, PROTEIN, 1, "Incorrect CRC64 checksum for the protein sequence", ERROR , "A developer should fix it" ),
+    PROTEIN_INCORRECT_CRC64( Protein.class, PROTEIN, 1, "Incorrect CRC64 checksum for the protein sequence", ERROR, "A developer should fix it" ),
 
     PROTEIN_UNIPROT_NO_XREF( Protein.class, PROTEIN, 2, "Missing Uniprot identity xref", ERROR ),
 
@@ -148,16 +147,29 @@ public enum MessageDefinition {
     // Nucleid Acid
 
     NUC_ACID_IDENTITY_INVALID_DB( NucleicAcid.class, NUCLEIC_ACID, 1, "Nucleic acid with wrong database for identity xref", ERROR,
-                                  "Valid databases: " + CvDatabase.ENTREZ_GENE_MI_REF+" ("+CvDatabase.ENTREZ_GENE+"), "
-                                                        + CvDatabase.DDBG_MI_REF+" ("+CvDatabase.DDBG+"), "
-                                                        + CvDatabase.FLYBASE_MI_REF+" ("+CvDatabase.FLYBASE+"), "
-                                                        + CvDatabase.ENSEMBL_MI_REF+" ("+CvDatabase.ENSEMBL+")"),
+                                  "Valid databases: " + CvDatabase.ENTREZ_GENE_MI_REF + " (" + CvDatabase.ENTREZ_GENE + "), "
+                                  + CvDatabase.DDBG_MI_REF + " (" + CvDatabase.DDBG + "), "
+                                  + CvDatabase.FLYBASE_MI_REF + " (" + CvDatabase.FLYBASE + "), "
+                                  + CvDatabase.ENSEMBL_MI_REF + " (" + CvDatabase.ENSEMBL + ")" ),
 
     NUC_ACID_IDENTITY_MISSING( NucleicAcid.class, NUCLEIC_ACID, 2, "Missing Nucleic Acid identity Xref", ERROR ),
 
     NUC_ACID_IDENTITY_MULTIPLE( NucleicAcid.class, NUCLEIC_ACID, 3, "Multiple identity xrefs for Nucleic Acid", ERROR,
-                                "Correct to one of the following in the given order of preference -> EMBL genome sequence, Ensembl, FlyBase, GeneID")
-    ;
+                                "Correct to one of the following in the given order of preference -> EMBL genome sequence, Ensembl, FlyBase, GeneID" ),
+
+    ////////////////////////
+    // Small Molecule
+
+    SMALL_MOLECULE_IDENTITY_INVALID_DB( SmallMolecule.class, SMALL_MOLECULE, 1,
+                                        "Small molecule with wrong database for identity xref", ERROR,
+                                        "Valid database is : " + CvDatabase.CHEBI + " (" + CvDatabase.CHEBI_MI_REF + ")" ),
+
+    SMALL_MOLECULE_IDENTITY_MISSING( SmallMolecule.class, SMALL_MOLECULE, 2,
+                                     "Missing Small Molecule identity Xref", ERROR ),
+
+    SMALL_MOLECULE_IDENTITY_MULTIPLE( SmallMolecule.class, SMALL_MOLECULE, 3,
+                                      "Multiple identity xrefs for Small Molecule", ERROR,
+                                      "Correct so there's only Chebi as identity" );
 
     ////////////////////////
     // Instance variable
@@ -190,7 +202,7 @@ public enum MessageDefinition {
         if ( description == null || description.trim().length() == 0 ) {
             throw new IllegalArgumentException( "You must give a non null description" );
         }
-        if( level == null ) {
+        if ( level == null ) {
             throw new IllegalArgumentException( "You must give a non null level" );
         }
 
@@ -228,42 +240,42 @@ public enum MessageDefinition {
         return "[" + key + "] " + level + " - " + description + ( ( suggestion == null ) ? "" : " (" + suggestion + ")" );
     }
 
-    public static MessageDefinition[] valuesByTargetClass(Class<? extends IntactObject> clazz) {
+    public static MessageDefinition[] valuesByTargetClass( Class<? extends IntactObject> clazz ) {
         return valuesByTargetClass( clazz, true );
     }
 
-    public static MessageDefinition[] valuesByTargetClass(Class<? extends IntactObject> clazz, boolean excludeSubclasses) {
+    public static MessageDefinition[] valuesByTargetClass( Class<? extends IntactObject> clazz, boolean excludeSubclasses ) {
         Collection<MessageDefinition> messageDefinitions = new ArrayList<MessageDefinition>();
 
-        for (MessageDefinition messageDefinition : values()) {
-            if (excludeSubclasses) {
-                if (clazz.equals(messageDefinition.getTargetClass())) {
-                    messageDefinitions.add(messageDefinition);
+        for ( MessageDefinition messageDefinition : values() ) {
+            if ( excludeSubclasses ) {
+                if ( clazz.equals( messageDefinition.getTargetClass() ) ) {
+                    messageDefinitions.add( messageDefinition );
                 }
-            } else if (clazz.isAssignableFrom(messageDefinition.getTargetClass())) {
-                messageDefinitions.add(messageDefinition);
+            } else if ( clazz.isAssignableFrom( messageDefinition.getTargetClass() ) ) {
+                messageDefinitions.add( messageDefinition );
             }
         }
 
-        return messageDefinitions.toArray(new MessageDefinition[messageDefinitions.size()]);
+        return messageDefinitions.toArray( new MessageDefinition[messageDefinitions.size()] );
     }
 
     public static Set<Class<? extends IntactObject>> allTargetClasses() {
         Set<Class<? extends IntactObject>> classes = new LinkedHashSet<Class<? extends IntactObject>>();
 
-        for (MessageDefinition messageDefinition : values()) {
-            classes.add(messageDefinition.getTargetClass());
+        for ( MessageDefinition messageDefinition : values() ) {
+            classes.add( messageDefinition.getTargetClass() );
         }
 
         return classes;
     }
 
     public static void printAll( PrintStream ps ) {
-        for (Class<? extends IntactObject> targetClass : allTargetClasses()) {
-            ps.println(targetClass.getSimpleName()+":");
+        for ( Class<? extends IntactObject> targetClass : allTargetClasses() ) {
+            ps.println( targetClass.getSimpleName() + ":" );
 
-            for ( MessageDefinition messageDefinition : valuesByTargetClass(targetClass, true) ) {
-                ps.println( "   "+messageDefinition );
+            for ( MessageDefinition messageDefinition : valuesByTargetClass( targetClass, true ) ) {
+                ps.println( "   " + messageDefinition );
             }
 
             ps.println();
