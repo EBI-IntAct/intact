77df48d209b566a0330466b3c156ded98f856154
skerrien <samuel.kerrien@gmail.com>
2007-10-25 16:01:55 +0000

    [ISAN-17] An experiment should have exactly 1 pubmed/doi primary-reference [ISAN-18] When an experiment has a pubmed id like 'to_be_assigned' as identifier, a report should be sent to curator

--- repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoPubmedXref.java
+++ repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoPubmedXref.java
@@ -16,37 +16,69 @@ import uk.ac.ebi.intact.sanity.rules.RuleGroup;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
- * Checks on experiment having no pubmed primary-reference.
+ * Checks on experiment having no pubmed primary-reference or pmid like 'to_be_assigned'.
  *
- * @author Catherine Leroy (cleroy@ebi.ac.uk)
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk), Catherine Leroy (cleroy@ebi.ac.uk)
  * @version $Id$
  * @since 2.0.0
  */
 
-@SanityRule( target = Experiment.class , group = { RuleGroup.INTACT, RuleGroup.IMEX })
+@SanityRule( target = Experiment.class, group = {RuleGroup.INTACT, RuleGroup.IMEX} )
+
 public class ExperimentWithNoPubmedXref implements Rule<Experiment> {
+
+    public static final String TO_BE_ASSIGNED = "to_be_assigned";
 
     public Collection<GeneralMessage> check( Experiment experiment ) throws SanityRuleException {
         Collection<GeneralMessage> messages = new ArrayList<GeneralMessage>();
-        if ( !hasPrimaryRefToPubmed( experiment ) ) {
-            messages.add( new GeneralMessage( MessageDefinition.EXPERIMENT_WITHOUT_PUBMED, experiment ) );
+        Collection<ExperimentXref> xrefs = getAllPrimaryRefToPubmedAndDoi( experiment );
+        if ( xrefs.isEmpty() ) {
+            messages.add( new GeneralMessage( MessageDefinition.EXPERIMENT_WITHOUT_PRIMARY_REF, experiment ) );
+        } else {
+            switch ( xrefs.size() ) {
+                case 1:
+                    final Xref xref = xrefs.iterator().next();
+                    final String id = xref.getPrimaryId();
+                    if ( id != null && id.startsWith( TO_BE_ASSIGNED ) ) {
+                        messages.add( new GeneralMessage( MessageDefinition.EXPERIMENT_WITH_PUBMED_TO_BE_ASSIGNED,
+                                                          experiment ) );
+                    }
+                    break;
+
+                default:
+                    // more than one Xref
+                    messages.add( new GeneralMessage( MessageDefinition.EXPERIMENT_WITH_MULTIPLE_PRIMARY_REF,
+                                                      experiment ) );
+            }
         }
         return messages;
     }
 
-    // TODO should we check that there is only one ?!
-    private boolean hasPrimaryRefToPubmed( Experiment experiment ) {
+    private Collection<ExperimentXref> getAllPrimaryRefToPubmedAndDoi( Experiment experiment ) {
+        Collection<ExperimentXref> xrefs = null;
+
         for ( ExperimentXref xref : experiment.getXrefs() ) {
             CvObjectXref cvDatabaseIdentity = CvObjectUtils.getPsiMiIdentityXref( xref.getCvDatabase() );
-            if ( cvDatabaseIdentity != null && CvDatabase.PUBMED_MI_REF.equals( cvDatabaseIdentity.getPrimaryId() ) ) {
+            if ( cvDatabaseIdentity != null &&
+                 ( CvDatabase.PUBMED_MI_REF.equals( cvDatabaseIdentity.getPrimaryId() )
+                   ||
+                   CvDatabase.DOI_MI_REF.equals( cvDatabaseIdentity.getPrimaryId() )
+                 ) ) {
                 CvObjectXref cvXrerQualifierIdentity = CvObjectUtils.getPsiMiIdentityXref( xref.getCvXrefQualifier() );
                 if ( cvXrerQualifierIdentity != null && CvXrefQualifier.PRIMARY_REFERENCE_MI_REF.equals( cvXrerQualifierIdentity.getPrimaryId() ) ) {
-                    return true;
+                    if ( xrefs == null ) {
+                        xrefs = new ArrayList<ExperimentXref>();
+                    }
+                    xrefs.add( xref );
                 }
             }
         }
-        return false;
+
+        if ( xrefs == null ) return Collections.EMPTY_LIST;
+
+        return xrefs;
     }
 }
--- repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoPubmedXrefTest.java
+++ repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoPubmedXrefTest.java
@@ -5,8 +5,10 @@
  */
 package uk.ac.ebi.intact.sanity.rules.experiment;
 
+import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import uk.ac.ebi.intact.core.unit.IntactMockBuilder;
 import uk.ac.ebi.intact.mocks.experiments.ButkevitchMock;
 import uk.ac.ebi.intact.model.Experiment;
 import uk.ac.ebi.intact.model.ExperimentXref;
@@ -17,29 +19,60 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * TODO comment this
+ * ExperimentWithNoPubmedXref Tester.
  *
- * @author Catherine Leroy (cleroy@ebi.ac.uk)
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk), Catherine Leroy (cleroy@ebi.ac.uk)
  * @version $Id$
- * @since TODO
+ * @since 2.0.0
  */
 public class ExperimentWithNoPubmedXrefTest {
-
 
     @Test
     public void check() throws Exception {
         ExperimentWithNoPubmedXref rule = new ExperimentWithNoPubmedXref();
 
         Experiment experiment = ButkevitchMock.getMock();
-        Collection<GeneralMessage> messages = rule.check(experiment);
-        assertEquals(0, messages.size());
+        Collection<GeneralMessage> messages = rule.check( experiment );
+        assertEquals( 0, messages.size() );
 
         Collection<ExperimentXref> xrefs = new ArrayList<ExperimentXref>();
-        experiment.setXrefs(xrefs);
-        messages = rule.check(experiment);
-        assertEquals(1, messages.size());
-        for(GeneralMessage message : messages){
-            assertEquals(MessageDefinition.EXPERIMENT_WITHOUT_PUBMED, message.getMessageDefinition());
-        }
+        experiment.setXrefs( xrefs );
+        messages = rule.check( experiment );
+        assertEquals( 1, messages.size() );
+        assertEquals( MessageDefinition.EXPERIMENT_WITHOUT_PRIMARY_REF,
+                      messages.iterator().next().getMessageDefinition() );
+    }
+
+    @Test
+    public void check_multiple_primary_ref() throws Exception {
+
+        IntactMockBuilder mockBuilder = new IntactMockBuilder();
+        ExperimentWithNoPubmedXref rule = new ExperimentWithNoPubmedXref();
+
+        Experiment experiment = mockBuilder.createExperimentRandom( 1 );
+        experiment.addXref( mockBuilder.createPrimaryReferenceXref( experiment, "1234566789" ) );
+        Assert.assertEquals( 2, experiment.getXrefs().size() );
+
+        Collection<GeneralMessage> messages = rule.check( experiment );
+        assertEquals( 1, messages.size() );
+        assertEquals( MessageDefinition.EXPERIMENT_WITH_MULTIPLE_PRIMARY_REF,
+                      messages.iterator().next().getMessageDefinition() );
+    }
+
+    @Test
+    public void check_to_be_assigned() throws Exception {
+
+        IntactMockBuilder mockBuilder = new IntactMockBuilder();
+        ExperimentWithNoPubmedXref rule = new ExperimentWithNoPubmedXref();
+
+        Experiment experiment = mockBuilder.createExperimentRandom( 1 );
+        experiment.getXrefs().clear();
+        experiment.addXref( mockBuilder.createPrimaryReferenceXref( experiment, "to_be_assigned_1" ) );
+        Assert.assertEquals( 1, experiment.getXrefs().size() );
+
+        Collection<GeneralMessage> messages = rule.check( experiment );
+        assertEquals( 1, messages.size() );
+        assertEquals( MessageDefinition.EXPERIMENT_WITH_PUBMED_TO_BE_ASSIGNED,
+                      messages.iterator().next().getMessageDefinition() );
     }
 }
