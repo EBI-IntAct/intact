84ef9212f14b83ac5bdf651466b01cb6cca30734
skerrien <samuel.kerrien@gmail.com>
2007-10-27 12:45:45 +0000

    added hasName(String name)

--- repo/sanity/trunk/intact-sanity-check/src/main/java/uk/ac/ebi/intact/sanity/check/config/Curator.java
+++ repo/sanity/trunk/intact-sanity-check/src/main/java/uk/ac/ebi/intact/sanity/check/config/Curator.java
@@ -6,53 +6,56 @@ package uk.ac.ebi.intact.sanity.check.config;
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  */
-public class Curator
-{
+public class Curator {
     private String name;
     private String email;
     private boolean admin;
     private boolean xmlReport;
     private boolean includeAllReports;
 
-    public Curator()
-    {
+    public Curator() {
     }
 
-    public Curator(String name)
-    {
+    public Curator( String name ) {
         this.name = name;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName( String name ) {
         this.name = name;
     }
 
-    public String getEmail()
-    {
-        if (email == null) {
-            return name+"@ebi.ac.uk";
+    public boolean hasName( String name ) {
+        if ( name == null ) {
+            throw new IllegalArgumentException( "you must give a non null name" );
+        }
+
+        if( this.name == null ) {
+            return false;
+        }
+
+        return name.trim().equalsIgnoreCase( this.name.trim() );
+    }
+
+    public String getEmail() {
+        if ( email == null ) {
+            return name + "@ebi.ac.uk";
         }
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail( String email ) {
         this.email = email;
     }
 
-    public boolean isAdmin()
-    {
+    public boolean isAdmin() {
         return admin;
     }
 
-    public void setAdmin(boolean admin)
-    {
+    public void setAdmin( boolean admin ) {
         this.admin = admin;
     }
 
@@ -60,7 +63,7 @@ public class Curator
         return xmlReport;
     }
 
-    public void setXmlReport(boolean xmlReport) {
+    public void setXmlReport( boolean xmlReport ) {
         this.xmlReport = xmlReport;
     }
 
@@ -68,30 +71,30 @@ public class Curator
         return includeAllReports;
     }
 
-    public void setIncludeAllReports(boolean includeAllReports) {
+    public void setIncludeAllReports( boolean includeAllReports ) {
         this.includeAllReports = includeAllReports;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals( Object o ) {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
 
-        Curator curator = (Curator) o;
+        Curator curator = ( Curator ) o;
 
-        if (name != null ? !name.equals(curator.name) : curator.name != null) return false;
+        if ( name != null ? !name.equals( curator.name ) : curator.name != null ) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return (name != null ? name.hashCode() : 0);
+        return ( name != null ? name.hashCode() : 0 );
     }
 
     @Override
     public String toString() {
-        return "Curator{"+getName()+" - "+getEmail()+" - admin:"+admin+
-               " - include All reports: "+includeAllReports+" - include XML report: "+xmlReport+"}";
+        return "Curator{" + getName() + " - " + getEmail() + " - admin:" + admin +
+               " - include All reports: " + includeAllReports + " - include XML report: " + xmlReport + "}";
     }
 }
