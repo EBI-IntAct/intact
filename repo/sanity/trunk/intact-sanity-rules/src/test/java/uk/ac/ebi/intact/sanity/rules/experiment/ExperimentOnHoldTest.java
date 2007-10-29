ccd72a9bee927dd799fce4d3ab2047862ccce4e0
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-29 11:51:38 +0000

    Updated the initialisation of the database so the test do not conflict with each other.

--- repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentOnHoldTest.java
+++ repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentOnHoldTest.java
@@ -20,7 +20,7 @@ import uk.ac.ebi.intact.sanity.commons.rules.GeneralMessage;
 import java.util.Collection;
 
 /**
- * TODO comment this
+ * ExperimentOnHold Tester.
  *
  * @author Bruno Aranda
  * @version $Id$
@@ -29,79 +29,79 @@ public class ExperimentOnHoldTest extends IntactBasicTestCase {
 
     @Before
     public void before() throws Exception {
-        SchemaUtils.createSchema();
+        SchemaUtils.createSchema( true );
     }
 
     @After
     public void after() throws Exception {
         commitTransaction();
     }
-    
+
     @Test
     public void check_notOnHold() throws Exception {
         Experiment experiment = getMockBuilder().createExperimentEmpty();
 
         ExperimentOnHold rule = new ExperimentOnHold();
-        beginTransaction();
-        Collection<GeneralMessage> messages = rule.check(experiment);
-        commitTransaction();
+//        beginTransaction();
+        Collection<GeneralMessage> messages = rule.check( experiment );
+//        commitTransaction();
 
-        assertEquals(0, messages.size());
+        assertEquals( 0, messages.size() );
     }
-    
+
     @Test
     public void check_onHold() throws Exception {
         Experiment experiment = getMockBuilder().createExperimentEmpty();
-        Annotation annotation = getMockBuilder().createAnnotation("", "IA:0", CvTopic.ON_HOLD);
-        experiment.addAnnotation(annotation);
+        Annotation annotation = getMockBuilder().createAnnotation( "", "IA:0", CvTopic.ON_HOLD );
+        experiment.addAnnotation( annotation );
 
         ExperimentOnHold rule = new ExperimentOnHold();
-        beginTransaction();
-        Collection<GeneralMessage> messages = rule.check(experiment);
-        commitTransaction();
+//        beginTransaction();
+        Collection<GeneralMessage> messages = rule.check( experiment );
+//        commitTransaction();
 
-        assertEquals(1, messages.size());
+        assertEquals( 1, messages.size() );
     }
 
     @Test
     public void check_notOnHold_checkingPubmed() throws Exception {
-        Experiment experiment = getMockBuilder().createExperimentEmpty("lala-2005-1", "1234");
+        Experiment experiment = getMockBuilder().createExperimentEmpty( "lala-2005-1", "1234" );
 
-        PersisterHelper.saveOrUpdate(experiment);
+        PersisterHelper.saveOrUpdate( experiment );
 
-        Experiment exp2 = getMockBuilder().createExperimentEmpty("br-2009-1", "4321");
-        Annotation annotation = getMockBuilder().createAnnotation("", "IA:0", CvTopic.ON_HOLD);
-        experiment.addAnnotation(annotation);
+        Experiment exp2 = getMockBuilder().createExperimentEmpty( "br-2009-1", "4321" );
+        Annotation annotation = getMockBuilder().createAnnotation( "", "IA:0", CvTopic.ON_HOLD );
+        experiment.addAnnotation( annotation );
 
-        PersisterHelper.saveOrUpdate(exp2);
+        PersisterHelper.saveOrUpdate( exp2 );
 
         ExperimentOnHold rule = new ExperimentOnHold();
         beginTransaction();
-        Experiment refreshedExp = getDaoFactory().getExperimentDao().getByShortLabel("lala-2005-1");
-        Collection<GeneralMessage> messages = rule.check(refreshedExp);
+        Experiment refreshedExp = getDaoFactory().getExperimentDao().getByShortLabel( "lala-2005-1" );
+        Collection<GeneralMessage> messages = rule.check( refreshedExp );
         commitTransaction();
 
-        assertEquals(0, messages.size());
+        assertEquals( 0, messages.size() );
     }
 
     @Test
     public void check_onHold_checkingPubmed() throws Exception {
-        Experiment experiment = getMockBuilder().createExperimentEmpty("lala-2005-1", "1234");
+        Experiment experiment = getMockBuilder().createExperimentEmpty( "lala-2005-1", "1234" );
 
-        PersisterHelper.saveOrUpdate(experiment);
+        PersisterHelper.saveOrUpdate( experiment );
 
-        Experiment exp2 = getMockBuilder().createExperimentEmpty("lala-2005-4", "1234");
-        Annotation annotation = getMockBuilder().createAnnotation("", "IA:0", CvTopic.ON_HOLD);
-        exp2.addAnnotation(annotation);
+        Experiment exp2 = getMockBuilder().createExperimentEmpty( "lala-2005-4", "1234" );
+        Annotation annotation = getMockBuilder().createAnnotation( "", "IA:0", CvTopic.ON_HOLD );
+        exp2.addAnnotation( annotation );
 
-        PersisterHelper.saveOrUpdate(exp2);
+        PersisterHelper.saveOrUpdate( exp2 );
 
         ExperimentOnHold rule = new ExperimentOnHold();
         beginTransaction();
-        Experiment refreshedExp = getDaoFactory().getExperimentDao().getByShortLabel("lala-2005-1");
-        Collection<GeneralMessage> messages = rule.check(refreshedExp);
+        Experiment refreshedExp = getDaoFactory().getExperimentDao().getByShortLabel( "lala-2005-1" );
+        Collection<GeneralMessage> messages = rule.check( refreshedExp );
         commitTransaction();
 
-        assertEquals(1, messages.size());
+        assertEquals( 1, messages.size() );
     }
 }
--- repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoInteractionTest.java
+++ repo/sanity/trunk/intact-sanity-rules/src/test/java/uk/ac/ebi/intact/sanity/rules/experiment/ExperimentWithNoInteractionTest.java
@@ -5,9 +5,12 @@
  */
 package uk.ac.ebi.intact.sanity.rules.experiment;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import uk.ac.ebi.intact.core.unit.IntactBasicTestCase;
+import uk.ac.ebi.intact.core.util.SchemaUtils;
 import uk.ac.ebi.intact.mocks.experiments.ButkevitchMock;
 import uk.ac.ebi.intact.model.Experiment;
 import uk.ac.ebi.intact.model.Interaction;
@@ -18,35 +21,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * TODO comment this
+ * ExperimentWithNoInteraction Tester.
  *
  * @author Catherine Leroy (cleroy@ebi.ac.uk)
  * @version $Id$
- * @since TODO
+ * @since 2.0
  */
 public class ExperimentWithNoInteractionTest extends IntactBasicTestCase {
 
+    @Before
+    public void before() throws Exception {
+        SchemaUtils.createSchema( true );
+    }
+
+    @After
+    public void after() throws Exception {
+        commitTransaction();
+    }
 
     @Test
     public void check() throws Exception {
         ExperimentWithNoInteraction rule = new ExperimentWithNoInteraction();
 
         Experiment experiment = ButkevitchMock.getMock();
-        beginTransaction();
-        Collection<GeneralMessage> messages = rule.check(experiment);
-        commitTransaction();
-        Assert.assertEquals(0, messages.size());
+        Collection<GeneralMessage> messages = rule.check( experiment );
+        Assert.assertEquals( 0, messages.size() );
 
         Collection<Interaction> interactions = new ArrayList<Interaction>();
-        experiment.setInteractions(interactions);
+        experiment.setInteractions( interactions );
 
-        beginTransaction();
-        messages = rule.check(experiment);
-        commitTransaction();
-        Assert.assertEquals(1, messages.size());
-        for(GeneralMessage message : messages){
-            Assert.assertEquals(MessageDefinition.EXPERIMENT_WITHOUT_INTERACTION, message.getMessageDefinition());
+        messages = rule.check( experiment );
+        Assert.assertEquals( 1, messages.size() );
+        for ( GeneralMessage message : messages ) {
+            Assert.assertEquals( MessageDefinition.EXPERIMENT_WITHOUT_INTERACTION, message.getMessageDefinition() );
         }
     }
-
 }
