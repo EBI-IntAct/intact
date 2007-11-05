9fc8986747af49d37b149e77045d16832420aa7b
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-05 14:28:05 +0000

    Updated rule, the non erroneous cases weren't working.

--- repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/interactor/InteractorClassAndType.java
+++ repo/sanity/trunk/intact-sanity-rules/src/main/java/uk/ac/ebi/intact/sanity/rules/interactor/InteractorClassAndType.java
@@ -26,45 +26,38 @@ import java.util.Collection;
  * @since 2.0.0
  */
 
-@SanityRule(target = Interactor.class, group = { RuleGroup.INTACT, RuleGroup.IMEX })
+@SanityRule( target = Interactor.class, group = {RuleGroup.INTACT, RuleGroup.IMEX} )
 
 public class InteractorClassAndType implements Rule<Interactor> {
 
-    public Collection<GeneralMessage> check(Interactor interactor) throws SanityRuleException {
+    public Collection<GeneralMessage> check( Interactor interactor ) throws SanityRuleException {
         Collection<GeneralMessage> messages = new ArrayList<GeneralMessage>();
-        if(interactor.getCvInteractorType() == null){
+        if ( interactor.getCvInteractorType() == null ) {
 
-            messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_MISSING_TYPE, interactor ));
+            messages.add( new InteractorMessage( MessageDefinition.INTERACTOR_WITH_MISSING_TYPE, interactor ) );
 
         } else {
 
             final CvObjectXref xref = CvObjectUtils.getPsiMiIdentityXref( interactor.getCvInteractorType() );
-            if( xref == null ) {
+            if ( xref == null ) {
 
-                messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_INVALID_TYPE, interactor ));
+                messages.add( new InteractorMessage( MessageDefinition.INTERACTOR_WITH_INVALID_TYPE, interactor ) );
 
             } else {
                 final String mi = xref.getPrimaryId();
 
-                if( (interactor instanceof Interaction) && !(mi.equals( CvInteractorType.INTERACTION_MI_REF) ) ) {
-
-                    messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_TYPE_MISMATCH, interactor ));
-
-                } else if( (interactor instanceof NucleicAcid) && !(mi.equals( CvInteractorType.NUCLEIC_ACID_MI_REF) ) ) {
-
-                    messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_TYPE_MISMATCH, interactor ));
-
-                } else if( (interactor instanceof Protein) && !(mi.equals( CvInteractorType.PROTEIN_MI_REF) ) ) {
-
-                    messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_TYPE_MISMATCH, interactor ));
-
-                } else if( (interactor instanceof SmallMolecule) && !(mi.equals( CvInteractorType.SMALL_MOLECULE_MI_REF) ) ) {
-
-                    messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_TYPE_MISMATCH, interactor ));
-
+                if ( ( interactor instanceof Interaction ) && mi.equals( CvInteractorType.INTERACTION_MI_REF ) ) {
+                    // ok
+                } else if ( ( interactor instanceof Protein ) && mi.equals( CvInteractorType.PROTEIN_MI_REF ) ) {
+                    // ok
+                } else
+                if ( ( interactor instanceof NucleicAcid ) && mi.equals( CvInteractorType.NUCLEIC_ACID_MI_REF ) ) {
+                    // ok
+                } else
+                if ( ( interactor instanceof SmallMolecule ) && mi.equals( CvInteractorType.SMALL_MOLECULE_MI_REF ) ) {
+                    // ok
                 } else {
-
-                    messages.add(new InteractorMessage( MessageDefinition.INTERACTOR_WITH_UNSUPPORTED_TYPE, interactor ));
+                    messages.add( new InteractorMessage( MessageDefinition.INTERACTOR_WITH_TYPE_MISMATCH, interactor ) );
                 }
             }
         }
