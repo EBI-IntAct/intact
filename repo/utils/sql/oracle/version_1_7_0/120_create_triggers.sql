3c01c150016380e4af21237020e8ae3e6013bd1e
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-08 14:47:17 +0000

    IAC-162 (Create/Update SQL ddl/scripts to use the new IMEx Import tables for ORACLE)

--- repo/utils/sql/oracle/version_1_7_0/120_create_triggers.sql
+++ repo/utils/sql/oracle/version_1_7_0/120_create_triggers.sql
@@ -20,28 +20,84 @@ begin
 	insert into ia_imex_import_audit
 		( event
 		, id
-		, original_filename
-		, status
-		, pmid
-		, message
-		, provider_ac
 		, created
 		, created_user
 		, updated
 		, userstamp
+		, activationtype
+		, count_total
+		, count_not_found
+		, count_failed
+		, import_date
+		, repository
 		)
 	values
 		( l_event
 		, :old.id
-		, :old.original_filename
-		, :old.status
-		, :old.pmid
-		, :old.message
-		, :old.provider_ac
 		, :old.created
 		, :old.created_user
 		, :old.updated
 		, :old.userstamp
+		, :old.activationtype
+		, :old.count_total
+		, :old.count_not_found
+		, :old.count_failed
+		, :old.import_date
+		, :old.repository
+		);
+end;
+/
+show error
+
+
+
+
+PROMPT Creating audit trigger for ia_imex_import_pub
+create or replace trigger trgAud_ia_imex_import_pub
+	before update or delete
+	on ia_imex_import_pub
+	for each row
+
+declare
+	l_event char(1);
+begin
+
+	if deleting then
+		l_event := 'D';
+	elsif updating then
+		l_event := 'U';
+		:new.updated := sysdate;
+		:new.userstamp := user;
+	end if ;
+
+
+	insert into ia_imex_import_pub_audit
+		( event
+		, pmid
+		, created
+		, created_user
+		, updated
+		, userstamp
+		, message
+		, original_filename
+		, release_date
+		, status
+		, imeximport_id
+		, provider_ac
+		)
+	values
+		( l_event
+		, :old.pmid
+		, :old.created
+		, :old.created_user
+		, :old.updated
+		, :old.userstamp
+		, :old.message
+		, :old.original_filename
+		, :old.release_date
+		, :old.status
+		, :old.imeximport_id
+		, :old.provider_ac
 		);
 end;
 /
--- repo/utils/sql/oracle/version_1_7_0/140_create_public_synonyms.sql
+++ repo/utils/sql/oracle/version_1_7_0/140_create_public_synonyms.sql
@@ -2,7 +2,11 @@
 PROMPT "Creating public synonym IA_IMEX_IMPORT"
 create or replace public synonym IA_IMEX_IMPORT for INTACT.IA_IMEX_IMPORT;
 
+PROMPT "Creating public synonym IA_IMEX_IMPORT_PUB"
+create or replace public synonym IA_IMEX_IMPORT_PUB for INTACT.IA_IMEX_IMPORT_PUB;
+
 PROMPT "Creating public synonym IA_IMEX_IMPORT_AUDIT"
 create or replace public synonym IA_IMEX_IMPORT_AUDIT for INTACT.IA_IMEX_IMPORT_AUDIT;
 
-
+PROMPT "Creating public synonym IA_IMEX_IMPORT_PUB_AUDIT"
+create or replace public synonym IA_IMEX_IMPORT_PUB_AUDIT for INTACT.IA_IMEX_IMPORT_PUB_AUDIT;
