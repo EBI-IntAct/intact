e1ffd9dee134a395c38c2dc99902e84d2ecd5620
irina-armean <irina-armean@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-09 10:42:21 +0000

    - in case of a ClasCastException there is a warn log and the EBIApplicationResult is null - included the 3 EBIApplicationError -files in the test

--- repo/bridges/trunk/blast-mapping-parser/src/main/java/uk/ac/ebi/intact/confidence/blastmapping/BlastMappingReader.java
+++ repo/bridges/trunk/blast-mapping-parser/src/main/java/uk/ac/ebi/intact/confidence/blastmapping/BlastMappingReader.java
@@ -1,155 +1,164 @@
 /**
- * 
+ *
  */
 package uk.ac.ebi.intact.confidence.blastmapping;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.net.URL;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.confidence.blastmapping.jaxb.EBIApplicationResult;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import uk.ac.ebi.intact.confidence.blastmapping.jaxb.EBIApplicationResult;
+import java.io.*;
+import java.net.URL;
 
 /**
  * @author iarmean
- * 
  */
 public class BlastMappingReader {
-	/**
-	 * Sets up a logger for that class.
-	 */
-	public static final Log	log	= LogFactory.getLog(BlastMappingReader.class);
+    /**
+     * Sets up a logger for that class.
+     */
+    public static final Log log = LogFactory.getLog( BlastMappingReader.class );
 
-	
-	// //////////////////////
-	// Private methods
+    // //////////////////////
+    // Private methods
 
-	private Unmarshaller getUnmarshaller() throws JAXBException {
+    private Unmarshaller getUnmarshaller() throws JAXBException {
 
-		// create a JAXBContext capable of handling classes generated into the
-		// jaxb package
-		JAXBContext jc = JAXBContext.newInstance("uk.ac.ebi.intact.confidence.blastmapping.jaxb");
+        // create a JAXBContext capable of handling classes generated into the
+        // jaxb package
+        JAXBContext jc = JAXBContext.newInstance( "uk.ac.ebi.intact.confidence.blastmapping.jaxb" );
 
-		// create and return Unmarshaller
+        // create and return Unmarshaller
 
-		// TODO enable/disable validation use setSchema( s ) on Marshaller
-		return jc.createUnmarshaller();
-	}
+        // TODO enable/disable validation use setSchema( s ) on Marshaller
+        return jc.createUnmarshaller();
+    }
 
-	private EBIApplicationResult unmarshall(URL url) throws JAXBException, FileNotFoundException {
+    private EBIApplicationResult unmarshall( URL url ) throws JAXBException, FileNotFoundException {
 
-		if (url == null) {
-			throw new IllegalArgumentException("You must give a non null URL.");
-		}
+        if ( url == null ) {
+            throw new IllegalArgumentException( "You must give a non null URL." );
+        }
 
-		// create an Unmarshaller
-		Unmarshaller u = getUnmarshaller();
+        // create an Unmarshaller
+        Unmarshaller u = getUnmarshaller();
 
-		// unmarshal an entrySet instance document into a tree of Java content
-		// objects composed of classes from the jaxb package.
-		return (EBIApplicationResult) u.unmarshal(url);
-	}
+        // unmarshal an entrySet instance document into a tree of Java content
+        // objects composed of classes from the jaxb package.
+        return ( EBIApplicationResult ) u.unmarshal( url );
+    }
 
-	private EBIApplicationResult unmarshall(File file) throws JAXBException, FileNotFoundException {
+    private EBIApplicationResult unmarshall( File file ) throws JAXBException, FileNotFoundException {
 
-		if (file == null) {
-			throw new IllegalArgumentException("You must give a non null file.");
-		}
+        if ( file == null ) {
+            throw new IllegalArgumentException( "You must give a non null file." );
+        }
 
-		if (!file.exists()) {
-			throw new IllegalArgumentException("You must give an existing file. : " + file.getPath());
-		}
+        if ( !file.exists() ) {
+            throw new IllegalArgumentException( "You must give an existing file. : " + file.getPath() );
+        }
 
-		if (!file.canRead()) {
-			throw new IllegalArgumentException("You must give a readable file.");
-		}
+        if ( !file.canRead() ) {
+            throw new IllegalArgumentException( "You must give a readable file." );
+        }
 
-		// create an Unmarshaller
-		Unmarshaller u = getUnmarshaller();
+        // create an Unmarshaller
+        Unmarshaller u = getUnmarshaller();
 
-		// unmarshal an entrySet instance document into a tree of Java content
-		// objects composed of classes from the jaxb package.
-		
-		return (EBIApplicationResult) u.unmarshal(new FileInputStream(file));
-	//	return (EBIApplicationResult) ((JAXBElement)u.unmarshal(new FileInputStream(file))).getValue();
-		//( CVMappingType ) ( ( JAXBElement ) u.unmarshal( new FileInputStream( file ) ) ).getValue();
-	}
+        // unmarshal an entrySet instance document into a tree of Java content
+        // objects composed of classes from the jaxb package.
 
-	private EBIApplicationResult unmarshall(InputStream is) throws JAXBException {
+       // Object o = u.unmarshal( new FileInputStream( file ) );
+        if (log.isDebugEnabled()){
+            log.debug("unmarshal : " + file.getPath());
+        }
+//        if ( EBIApplicationResult.class.isAssignableFrom( o.getClass() ) ) {
+//            return ( EBIApplicationResult ) u.unmarshal( new FileInputStream( file ) );
+//        }
+//        if ( JAXBElement.class.isAssignableFrom( o.getClass() ) ) {
+//            return ( EBIApplicationResult ) ( ( JAXBElement ) u.unmarshal( new FileInputStream( file ) ) ).getValue();
+//        }
 
-		if (is == null) {
-			throw new IllegalArgumentException("You must give a non null input stream.");
-		}
+        try {
+            return ( EBIApplicationResult ) u.unmarshal( new FileInputStream( file ) );
+        } catch (ClassCastException e){
+            if (log.isWarnEnabled()){
+                log.warn("ClassCastException: file: " + file.getPath());
+            }
+        }
+      //  return (EBIApplicationResult) ((JAXBElement)u.unmarshal(new FileInputStream(file))).getValue(); 
+        //( CVMappingType ) ( ( JAXBElement ) u.unmarshal( new FileInputStream( file ) ) ).getValue();
+        return null;
+    }
 
-		// create an Unmarshaller
-		Unmarshaller u = getUnmarshaller();
+    private EBIApplicationResult unmarshall( InputStream is ) throws JAXBException {
 
-		// unmarshal an entrySet instance document into a tree of Java content
-		// objects composed of classes from the jaxb package.
-		return (EBIApplicationResult) u.unmarshal(is);
-	}
+        if ( is == null ) {
+            throw new IllegalArgumentException( "You must give a non null input stream." );
+        }
 
-	private EBIApplicationResult unmarshall(String s) throws JAXBException {
+        // create an Unmarshaller
+        Unmarshaller u = getUnmarshaller();
 
-		if (s == null) {
-			throw new IllegalArgumentException("You must give a non null String.");
-		}
+        // unmarshal an entrySet instance document into a tree of Java content
+        // objects composed of classes from the jaxb package.
+        return ( EBIApplicationResult ) u.unmarshal( is );
+    }
 
-		// create an Unmarshaller
-		Unmarshaller u = getUnmarshaller();
+    private EBIApplicationResult unmarshall( String s ) throws JAXBException {
 
-		// unmarshal an entrySet instance document into a tree of Java content
-		// objects composed of classes from the jaxb package.
-		return (EBIApplicationResult) u.unmarshal(new StringReader(s));
-	}
-	
-	
-	// ////////////////////////
-	// Public methods
+        if ( s == null ) {
+            throw new IllegalArgumentException( "You must give a non null String." );
+        }
 
-	public EBIApplicationResult read(String s) throws BlastMappingException {
-		try {
-			return unmarshall(s);
-		} catch (JAXBException e) {
-			throw new BlastMappingException(e);
-		}
-	}
+        // create an Unmarshaller
+        Unmarshaller u = getUnmarshaller();
 
-	public EBIApplicationResult read(File file) throws BlastMappingException {
-		try {
-			return unmarshall(file);
-		} catch (JAXBException e) {
-			throw new BlastMappingException(e);
-		} catch (FileNotFoundException e) {
-			throw new BlastMappingException(e);
-		}
-	}
+        // unmarshal an entrySet instance document into a tree of Java content
+        // objects composed of classes from the jaxb package.
+        return ( EBIApplicationResult ) u.unmarshal( new StringReader( s ) );
+    }
 
-	public EBIApplicationResult read(InputStream is) throws BlastMappingException {
-		try {
-			return unmarshall(is);
-		} catch (JAXBException e) {
-			throw new BlastMappingException(e);
-		}
-	}
+    // ////////////////////////
+    // Public methods
 
-	public EBIApplicationResult read(URL url) throws BlastMappingException {
-		try {
-			return unmarshall(url);
-		} catch (JAXBException e) {
-			throw new BlastMappingException(e);
-		} catch (FileNotFoundException e) {
-			throw new BlastMappingException(e);
-		}
-	}	
+    public EBIApplicationResult read( String s ) throws BlastMappingException {
+        try {
+            return unmarshall( s );
+        } catch ( JAXBException e ) {
+            throw new BlastMappingException( e );
+        }
+    }
+
+    public EBIApplicationResult read( File file ) throws BlastMappingException {
+        try {
+            return unmarshall( file );
+        } catch ( JAXBException e ) {
+            throw new BlastMappingException( e );
+        } catch ( FileNotFoundException e ) {
+            throw new BlastMappingException( e );
+        }
+    }
+
+    public EBIApplicationResult read( InputStream is ) throws BlastMappingException {
+        try {
+            return unmarshall( is );
+        } catch ( JAXBException e ) {
+            throw new BlastMappingException( e );
+        }
+    }
+
+    public EBIApplicationResult read( URL url ) throws BlastMappingException {
+        try {
+            return unmarshall( url );
+        } catch ( JAXBException e ) {
+            throw new BlastMappingException( e );
+        } catch ( FileNotFoundException e ) {
+            throw new BlastMappingException( e );
+        }
+    }
 }
