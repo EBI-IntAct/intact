b8d3e3a8ba83f47ca888dc951a452ce528d3f3f8
baranda <brunoaranda@gmail.com>
2007-12-07 11:33:53 +0000

    Using CvUpdater

--- repo/site/trunk/intact-kickstart/src/main/java/uk/ac/ebi/intact/kickstart/ImportControlledVocabularies.java
+++ repo/site/trunk/intact-kickstart/src/main/java/uk/ac/ebi/intact/kickstart/ImportControlledVocabularies.java
@@ -15,10 +15,9 @@
  */
 package uk.ac.ebi.intact.kickstart;
 
-import uk.ac.ebi.intact.context.DataContext;
 import uk.ac.ebi.intact.context.IntactContext;
-import uk.ac.ebi.intact.dataexchange.cvutils.UpdateCVs;
-import uk.ac.ebi.intact.dataexchange.cvutils.UpdateCVsConfig;
+import uk.ac.ebi.intact.dataexchange.cvutils.*;
+import uk.ac.ebi.intact.dataexchange.cvutils.model.IntactOntology;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -40,42 +39,16 @@ public class ImportControlledVocabularies {
         File pgConfigFile = new File(ImportPsiData.class.getResource("/postgres-hibernate.cfg.xml").getFile());
         IntactContext.initStandaloneContext(pgConfigFile);
 
-        File oboFile = createFileFromURL(new URL("http://intact.svn.sourceforge.net/viewvc/*checkout*/intact/repo/utils/data/controlledVocab/psi-mi25-4intact.obo"));
+        // load the latest ontology from internet
+        IntactOntology ontology = OboUtils.createOntologyFromOboLatestPsiMi();
 
-        // Once an IntactContext has been initialized, we can access to it by getting the current instance
-        IntactContext intactContext = IntactContext.getCurrentInstance();
+        // Import the ontology into the database, using the CvUpdater
+        CvUpdater updater = new CvUpdater(ontology);
 
-        DataContext dataContext = intactContext.getDataContext();
+        // this starts the create/update
+        CvUpdaterStatistics stats = updater.createOrUpdateCVs();
 
-        // We need to begin a transaction
-        dataContext.beginTransaction();
-
-        // To import controlled vocabularies we need to create a configuration object,
-        // which can be used to tweak the import
-        UpdateCVsConfig config = new UpdateCVsConfig();
-
-        // This is the main method to import the OBO file into the database, printing some debug
-        // information in the console
-        UpdateCVs.load(oboFile, System.out, config);
-
-        // Don't forget to commit the transaction
-        dataContext.commitTransaction();
+        System.out.println("Created terms: "+stats.getCreatedCvs().size());
     }
 
-    private static File createFileFromURL(URL url) throws Exception {
-        File tempFile = File.createTempFile("oboFile", ".obo");
-
-        PrintStream ps = new PrintStream(tempFile);
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            ps.println(line);
-        }
-
-        ps.close();
-
-        return tempFile;
-    }
 }
