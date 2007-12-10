2b90b2fe034b25b69cfa0541fc8d42331ab58846
irina-armean <irina-armean@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-12-10 10:11:02 +0000

    [IAC-220] : DDL: integrating the ia_confidence score into the db schema

---
+++ repo/utils/sql/oracle/version_1_8_0/160_update_privileges.sql
@@ -0,0 +1,17 @@
+
+-------------------------------------
+-- Update right on created objects --
+-------------------------------------
+
+PROMPT Creating table "Updating rights on ia_confidence"
+grant select on ia_confidence to INTACT_SELECT;
+grant select, insert,update,delete on ia_confidence to INTACT_CURATOR;
+
+
+---------------------------------------------
+-- Update right on respective audit tables --
+---------------------------------------------
+
+PROMPT Creating table "Updating rights on ia_confidence_audit"
+grant select on ia_confidence_audit to INTACT_SELECT;
+grant select, insert,update,delete on ia_confidence_audit to INTACT_CURATOR;
