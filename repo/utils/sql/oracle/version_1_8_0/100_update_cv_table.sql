f52ccb9c87eb1b571faddd1eb3636f93d6effd47
baranda <brunoaranda@gmail.com>
2008-01-31 16:30:03 +0000

    Create index for miIdentifier column

--- repo/utils/sql/oracle/version_1_8_0/100_update_cv_table.sql
+++ repo/utils/sql/oracle/version_1_8_0/100_update_cv_table.sql
@@ -1,2 +1,4 @@
 PROMPT Add new column (MI_IDENTIFIER) in the ia_controlledvocab
 ALTER TABLE ia_controlledvocab  ADD(MI_IDENTIFIER VARCHAR2(10)  NULL);
+
+CREATE INDEX i_ControlledVocab$mi on IA_ControlledVocab(mi_identifier) TABLESPACE &&intactMainTablespace;
