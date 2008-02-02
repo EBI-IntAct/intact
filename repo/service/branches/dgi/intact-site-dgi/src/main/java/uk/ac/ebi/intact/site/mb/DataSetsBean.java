c987549983267b5cc54ff1c5f57e144c18ab7f5d
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-02-02 21:45:55 +0000

    Branched for DGI

---
+++ repo/service/branches/dgi/intact-site-dgi/src/main/java/uk/ac/ebi/intact/site/mb/DataSetsBean.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2006 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package uk.ac.ebi.intact.site.mb;
+
+import static uk.ac.ebi.intact.site.items.Datasets.Dataset;
+import uk.ac.ebi.intact.site.util.SiteUtils;
+import uk.ac.ebi.faces.DataLoadingException;
+
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class DataSetsBean implements Serializable {
+
+    public static final String DATASET_OF_THE_MONTH_URL = "uk.ac.ebi.intact.DATASET_OF_THE_MONTH_URL";
+
+    private List<Dataset> dataSets;
+    private Dataset dataSetOfTheMonth;
+
+    public DataSetsBean()
+    {
+        String datasetsXml = FacesContext.getCurrentInstance().getExternalContext().getInitParameter(DATASET_OF_THE_MONTH_URL);
+
+        try
+        {
+            dataSets = SiteUtils.readDatasets(datasetsXml);
+        }
+        catch (DataLoadingException e)
+        {
+            e.printStackTrace();
+            dataSets = new ArrayList<Dataset>();
+        }
+
+        if (!dataSets.isEmpty())
+        {
+            dataSetOfTheMonth = dataSets.iterator().next();
+        }
+    }
+
+    public List<Dataset> getDataSets()
+    {
+        return dataSets;
+    }
+
+    public void setDataSets(List<Dataset> dataSets)
+    {
+        this.dataSets = dataSets;
+    }
+
+    public Dataset getDataSetOfTheMonth()
+    {
+        return dataSetOfTheMonth;
+    }
+
+    public void setDataSetOfTheMonth(Dataset dataSetOfTheMonth)
+    {
+        this.dataSetOfTheMonth = dataSetOfTheMonth;
+    }
+}
---
+++ repo/service/branches/dgi/intact-site-dgi/src/main/java/uk/ac/ebi/intact/site/mb/SearchBean.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2006 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package uk.ac.ebi.intact.site.mb;
+
+import uk.ac.ebi.intact.search.wsclient.SearchServiceClient;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * TODO comment this!
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class SearchBean implements Serializable
+{
+    private static final String SEARCH_QUERY_URL = "uk.ac.ebi.intact.SEARCH_QUERY_URL";
+
+    private String searchQuery;
+
+    public SearchBean()
+    {
+
+    }
+
+    public String doSearch()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        String searchQueryUrl = context.getExternalContext().getInitParameter(SEARCH_QUERY_URL);
+
+        // short-circuit the cycle to redirect to a external page
+        try
+        {
+            context.responseComplete();
+            context.getExternalContext().redirect(searchQueryUrl+searchQuery);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getSearchQuery()
+    {
+        return searchQuery;
+    }
+
+    public void setSearchQuery(String searchQuery)
+    {
+        this.searchQuery = searchQuery;
+    }
+}
