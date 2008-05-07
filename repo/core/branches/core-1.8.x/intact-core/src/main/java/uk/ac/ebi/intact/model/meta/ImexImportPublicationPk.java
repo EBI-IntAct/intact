3f74e17f7522e20a679e7a24b6fa16cc202596fa
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-05-07 10:43:49 +0000

    IAC-278 (equals() and hashcode() missing for ImexImportPublicationPk)

--- repo/core/branches/core-1.8.x/intact-core/src/main/java/uk/ac/ebi/intact/model/meta/ImexImportPublicationPk.java
+++ repo/core/branches/core-1.8.x/intact-core/src/main/java/uk/ac/ebi/intact/model/meta/ImexImportPublicationPk.java
@@ -17,7 +17,10 @@ package uk.ac.ebi.intact.model.meta;
 
 import org.hibernate.validator.NotNull;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
 /**
@@ -57,5 +60,24 @@ public class ImexImportPublicationPk implements Serializable {
 
     public void setPmid(String pmid) {
         this.pmid = pmid;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ImexImportPublicationPk that = (ImexImportPublicationPk) o;
+
+        if (imexImport != null ? !imexImport.equals(that.imexImport) : that.imexImport != null) return false;
+        if (pmid != null ? !pmid.equals(that.pmid) : that.pmid != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = (imexImport != null ? imexImport.hashCode() : 0);
+        result = 31 * result + (pmid != null ? pmid.hashCode() : 0);
+        return result;
     }
 }
