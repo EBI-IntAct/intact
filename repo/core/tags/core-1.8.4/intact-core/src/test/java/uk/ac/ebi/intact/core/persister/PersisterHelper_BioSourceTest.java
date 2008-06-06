918af16fc73ecb4c49a6216a8749d885bc25dbc7
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-06-06 13:35:52 +0000

    [maven-release-plugin]  copy for tag core-1.8.4

---
+++ repo/core/tags/core-1.8.4/intact-core/src/test/java/uk/ac/ebi/intact/core/persister/PersisterHelper_BioSourceTest.java
@@ -0,0 +1,74 @@
+package uk.ac.ebi.intact.core.persister;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.ebi.intact.core.unit.IntactBasicTestCase;
+import uk.ac.ebi.intact.model.BioSource;
+import uk.ac.ebi.intact.model.Component;
+import uk.ac.ebi.intact.model.CvTissue;
+
+/**
+ * TODO comment this
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class PersisterHelper_BioSourceTest extends IntactBasicTestCase
+{
+
+    @Test
+    public void persist_sameBioSource() throws Exception {
+        BioSource bs1 = getMockBuilder().createBioSource( 9606, "human" );
+        PersisterHelper.saveOrUpdate( bs1 );
+
+        Assert.assertEquals(1, getDaoFactory().getBioSourceDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getInstitutionDao().countAll());
+        Assert.assertEquals(3, getDaoFactory().getCvObjectDao().countAll());
+        Assert.assertEquals(5, getDaoFactory().getXrefDao().countAll());
+
+        BioSource bs2 = getMockBuilder().createBioSource( 9606, "human" );
+        PersisterHelper.saveOrUpdate( bs2 );
+
+        Assert.assertEquals(1, getDaoFactory().getBioSourceDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getInstitutionDao().countAll());
+        Assert.assertEquals(3, getDaoFactory().getCvObjectDao().countAll());
+        Assert.assertEquals(5, getDaoFactory().getXrefDao().countAll());
+    }
+
+    @Test
+    public void persist_bioSource_differentTissues() throws Exception {
+        BioSource bs1 = getMockBuilder().createBioSource( 9606, "human" );
+        PersisterHelper.saveOrUpdate( bs1 );
+
+        Assert.assertEquals(1, getDaoFactory().getBioSourceDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getInstitutionDao().countAll());
+        Assert.assertEquals(3, getDaoFactory().getCvObjectDao().countAll());
+        Assert.assertEquals(5, getDaoFactory().getXrefDao().countAll());
+
+        BioSource bs2 = getMockBuilder().createBioSource( 9606, "human" );
+        bs2.setCvTissue(getMockBuilder().createCvObject(CvTissue.class, "IA:xxxx", "blood"));
+        PersisterHelper.saveOrUpdate( bs2 );
+
+        Assert.assertEquals(2, getDaoFactory().getBioSourceDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getInstitutionDao().countAll());
+        Assert.assertEquals(4, getDaoFactory().getCvObjectDao().countAll());
+        Assert.assertEquals(7, getDaoFactory().getXrefDao().countAll());
+    }
+
+    @Test
+    public void persist_alwaysLowerCase() throws Exception {
+        BioSource bs1 = getMockBuilder().createBioSource( 9606, "HUMAN" );
+        PersisterHelper.saveOrUpdate( bs1 );
+
+        Assert.assertEquals("human", getDaoFactory().getBioSourceDao().getByTaxonIdUnique("9606").getShortLabel());
+    }
+
+    private Component reloadByAc(Component interaction) {
+        return getDaoFactory().getComponentDao().getByAc(interaction.getAc());
+    }
+
+    private void refresh(Component interaction) {
+        getDaoFactory().getComponentDao().refresh(interaction);
+    }
+
+}
