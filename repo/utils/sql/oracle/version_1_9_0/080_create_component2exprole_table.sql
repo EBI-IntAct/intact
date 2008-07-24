911d0c1f3be92605e6b04f30b3509befc06c1010
prem_intact <prem_intact@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-07-24 16:49:49 +0000

    comment not accepting ()

--- repo/utils/sql/oracle/version_1_9_0/080_create_component2exprole_table.sql
+++ repo/utils/sql/oracle/version_1_9_0/080_create_component2exprole_table.sql
@@ -2,7 +2,6 @@ PROMPT Creating table "ia_component2exprole"
 CREATE TABLE ia_component2exprole
 (      component_ac            VARCHAR2(30)   NOT NULL   CONSTRAINT fk_comp2exprl$compnt REFERENCES IA_COMPONENT(ac)
     ,  experimentalrole_ac     VARCHAR2(30)   NOT NULL   CONSTRAINT fk_comp2exprl$exprl  REFERENCES IA_CONTROLLEDVOCAB(ac)
-  
 )
 TABLESPACE &&intactMainTablespace;
 
@@ -10,7 +9,8 @@ set term off
     COMMENT ON TABLE ia_component2exprole IS
     'Represents a associative table between the component and experimental role.';
     COMMENT ON COLUMN ia_component2exprole.component_ac IS
-    'Refers to the Unique auto-generated accession number of ia_component(ac).';
+    'Refers to the Unique auto-generated accession number of ia_component-ac.';
     COMMENT ON COLUMN ia_component2exprole.experimentalrole_ac IS
-    'Experimental role of that component (eg. bait, prey, ...).';
+    'Experimental role of that component eg. bait, prey, ...';
 set term on
+
