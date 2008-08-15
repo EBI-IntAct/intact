60e7b328d0913e3df2c1796514fa567ba82fe2c4
prem_intact <prem_intact@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-08-15 10:02:44 +0000

    removed  IA_PAPERSTATUS and IA_DELETED_ACS tables from script

--- repo/utils/sql/oracle/version_1_9_0/200_create_cvlocalseq_grants.sql
+++ repo/utils/sql/oracle/version_1_9_0/200_create_cvlocalseq_grants.sql
@@ -1,4 +1,4 @@
 PROMPT Creating grants for "cv_local_seq"
-grant create,select on INTACT.cv_local_seq to INTACT_SELECT;
-grant create,select on INTACT.cv_local_seq to INTACT_CURATOR;
+grant select on INTACT.cv_local_seq to INTACT_SELECT;
+grant select on INTACT.cv_local_seq to INTACT_CURATOR;
 
