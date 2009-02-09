514a2f99c378667b26a11152c2dcd8bf9db49ad0
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2009-02-09 17:05:36 +0000

    First version of a simple MITAB exporter that writes MITAB content without clustering interactions.

---
+++ repo/data-exchange/trunk/psimi/psimitab/intact-psimitab-converters/src/main/java/uk/ac/ebi/intact/psimitab/converters/util/DatabaseSimpleMitabExporter.java
@@ -0,0 +1,211 @@
+/**
+ * Copyright 2008 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.psimitab.converters.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import psidev.psi.mi.tab.model.CrossReference;
+import psidev.psi.mi.tab.model.builder.Row;
+import uk.ac.ebi.intact.business.IntactTransactionException;
+import uk.ac.ebi.intact.context.DataContext;
+import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.model.Component;
+import uk.ac.ebi.intact.model.Interaction;
+import uk.ac.ebi.intact.model.InteractionImpl;
+import uk.ac.ebi.intact.model.Interactor;
+import uk.ac.ebi.intact.psimitab.IntactBinaryInteraction;
+import uk.ac.ebi.intact.psimitab.IntactDocumentDefinition;
+import uk.ac.ebi.intact.psimitab.PsimitabTools;
+import uk.ac.ebi.intact.psimitab.model.ExtendedInteractor;
+import uk.ac.ebi.intact.psimitab.converters.Intact2BinaryInteractionConverter;
+import uk.ac.ebi.intact.psimitab.converters.expansion.SpokeWithoutBaitExpansion;
+import uk.ac.ebi.intact.persistence.dao.DaoFactory;
+import uk.ac.ebi.intact.commons.util.ETACalculator;
+
+import javax.persistence.Query;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.*;
+
+/**
+ * Creates a MITAB file where each line represents a single interaction. Data is retrieved from the database and
+ * the resulting data file may feature multiple lines involving the same interactors (ie. non clustered interactions).
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class DatabaseSimpleMitabExporter {
+
+    private static final Log log = LogFactory.getLog( DatabaseSimpleMitabExporter.class );
+    private static final String NEW_LINE = System.getProperty("line.separator");
+
+    private static final String SMALLMOLECULE_MI_REF = "MI:0328";
+    
+
+    public DatabaseSimpleMitabExporter() {
+    }
+
+    public void exportAllInteractors(Writer mitabWriter) throws IOException, IntactTransactionException {
+        IntactContext.getCurrentInstance().getDataContext().beginTransaction();
+        DaoFactory daoFactory = IntactContext.getCurrentInstance().getDataContext().getDaoFactory();
+        final int interactionCount = daoFactory.getInteractorDao( InteractionImpl.class ).countAll();
+        final int allinteractorCount = daoFactory.getInteractorDao().countAll();
+        final int interactingMoleculeTotalCount = allinteractorCount - interactionCount;
+        IntactContext.getCurrentInstance().getDataContext().commitTransaction();
+
+        final String allInteractorsHql = "from InteractorImpl where objclass <> '" + InteractionImpl.class.getName()+"'";
+        exportInteractors(allInteractorsHql, interactingMoleculeTotalCount, mitabWriter);
+    }
+
+    public void exportInteractors(String interactorQueryHql,
+                                  int interactorTotalCount,
+                                  Writer mitabWriter) throws IOException, IntactTransactionException {
+
+        if (interactorQueryHql == null) {
+            throw new NullPointerException("Query for interactors is null: interactorQuery");
+        }
+        if (mitabWriter == null) {
+            throw new NullPointerException("mitabWriter is null");
+        }
+
+        if ( log.isDebugEnabled() ) {
+            log.debug( "Preparing to index " + interactorTotalCount + " interactor(s)." );
+        }
+
+        ETACalculator eta = null;
+        if( interactorTotalCount > 0 ) {
+            eta = new ETACalculator( interactorTotalCount );
+        }
+
+        // build the interaction clusters
+
+        final DataContext dataContext = IntactContext.getCurrentInstance().getDataContext();
+        final Set<String> interactionAcProcessed = new HashSet<String>();
+
+        List<? extends Interactor> interactors = null;
+
+        Intact2BinaryInteractionConverter converter =
+                new Intact2BinaryInteractionConverter(new SpokeWithoutBaitExpansion(),
+                                                      null); // no clustering at this stage.
+
+        int firstResult = 0;
+        int maxResults = 1;
+        int interactorCount = 0;
+
+        final IntactDocumentDefinition docDef = new IntactDocumentDefinition();
+
+        do {
+            dataContext.beginTransaction();
+            interactors = findInteractors(interactorQueryHql, firstResult, maxResults);
+
+            firstResult = firstResult + maxResults;
+
+            for (Interactor interactor : interactors) {
+
+                final Collection<Component> components = interactor.getActiveInstances();
+                if( components.isEmpty() ) {
+                    // not need to index
+                    log.info( "Interactor " + interactor.getShortLabel() + " isn't involved in any interactions, skipping." );
+                    continue;
+                }
+
+                if (log.isTraceEnabled()) log.trace("Processing interactor: "+interactor.getShortLabel());
+
+                List<Interaction> interactions = new ArrayList<Interaction>();
+                for (Component comp : components) {
+                    final Interaction interaction = comp.getInteraction();
+
+                    if (!interactionAcProcessed.contains(interaction.getAc())) {
+                        interactions.add(interaction);
+                        interactionAcProcessed.add(interaction.getAc());
+                    }
+                }
+
+                if (log.isTraceEnabled()) log.trace("Starting conversion and property enrichment: "+interactor.getShortLabel());
+
+                Collection<IntactBinaryInteraction> binaryInteractions = converter.convert(interactions);
+
+                if ( log.isTraceEnabled() ) log.trace( "Storing " + binaryInteractions.size() + " interactions..." );
+
+                int count = 0;
+                for (IntactBinaryInteraction bi : binaryInteractions) {
+                    count++;
+                    if ( log.isTraceEnabled() ) {
+                        log.trace( "Processing interaction #" + count );
+                    }
+
+                    // TODO is that still necessary ???
+                    flipInteractorsIfNecessary(bi);
+
+                    // write MITAB line
+                    final Row row = docDef.createInteractionRowConverter().createRow( bi );
+
+                    // here we could save some processing by converting first the line to a Row,
+                    // and then converting tow to string and giving this row to the indexers
+
+                    final String line = row.toString();
+                    mitabWriter.write(line+ NEW_LINE);
+                }
+
+                interactorCount++;
+
+                if( (count % 100) == 0 ) {
+                    if ( log.isDebugEnabled() ) {
+                        log.debug( "Processed " + interactorCount + " of " + interactorTotalCount + " interactor(s)" );
+                    }
+                    mitabWriter.flush();
+                }
+            }
+
+            dataContext.commitTransaction();
+
+        } while (!interactors.isEmpty());
+
+        if ( log.isInfoEnabled() ) {
+            log.info( "Completed export of Interactions as MITAB." );
+        }
+    }
+
+    /**
+     * Flips the interactors if necessary, so the small molecule is always interactor A
+     * @param bi
+     */
+    private void flipInteractorsIfNecessary(IntactBinaryInteraction bi) {
+        PsimitabTools.reorderInteractors(bi, new Comparator<ExtendedInteractor>() {
+
+            public int compare(ExtendedInteractor o1, ExtendedInteractor o2) {
+                final CrossReference type1 = o1.getInteractorType();
+                final CrossReference type2 = o2.getInteractorType();
+
+                if (type1 != null && SMALLMOLECULE_MI_REF.equals(type1.getIdentifier())) {
+                    return 1;
+                } else if (type2 != null && SMALLMOLECULE_MI_REF.equals(type2.getIdentifier())) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+    }
+
+    private static List<? extends Interactor> findInteractors(String hql, int firstResult, int maxResults) {
+        Query q = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
+                .getEntityManager().createQuery(hql);
+        q.setFirstResult(firstResult);
+        q.setMaxResults(maxResults);
+
+        return q.getResultList();
+    }
+}
