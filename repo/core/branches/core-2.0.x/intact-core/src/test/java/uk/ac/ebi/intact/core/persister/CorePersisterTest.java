71c83fb29541b8376f00dec7b226517becfe2511
baranda <brunoaranda@gmail.com>
2009-06-01 13:54:39 +0000

    - Created instance for CorePersister - PersisterStats using HashMultimap to avoid duplicated key-values

--- repo/core/branches/core-2.0.x/intact-core/src/test/java/uk/ac/ebi/intact/core/persister/CorePersisterTest.java
+++ repo/core/branches/core-2.0.x/intact-core/src/test/java/uk/ac/ebi/intact/core/persister/CorePersisterTest.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.intact.core.persister;
 
-import org.junit.*;
+import org.junit.Test;
+import uk.ac.ebi.intact.core.context.IntactContext;
 import uk.ac.ebi.intact.core.persister.finder.DefaultFinder;
 
 /**
@@ -14,37 +15,37 @@ public class CorePersisterTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void setEntityStateCopier_null() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setEntityStateCopier( null );
     }    
 
     @Test
     public void setEntityStateCopier() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setEntityStateCopier( new DefaultEntityStateCopier() );
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void setFinder_null() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setFinder( null );
     }    
 
     @Test
     public void setFinder() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setFinder( new DefaultFinder() );
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void setKeyBuilder_null() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setKeyBuilder( null );
     }
 
     @Test
     public void setKeyBuilder() throws Exception {
-        CorePersister persister = new CorePersister();
+        CorePersisterImpl persister = new CorePersisterImpl(new IntactContext(), new DefaultFinder());
         persister.setKeyBuilder( new KeyBuilder() );
     }
 }
