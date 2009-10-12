5ff815ad46035f27db078c6f7aede420976fa15d
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2009-10-12 16:10:36 +0000

    Updated DDL scripts so that IA_COMPONENT2ANNOT and IA_COMPONENT2EXPROLE are compliant for auditing. BUGFIX in the auditing triggers and added new user tracking triggers

--- repo/utils/sql/oracle/version_1_9_0/080_create_component2exprole_table.sql
+++ repo/utils/sql/oracle/version_1_9_0/080_create_component2exprole_table.sql
@@ -2,6 +2,10 @@ PROMPT Creating table "ia_component2exprole"
 CREATE TABLE ia_component2exprole
 (      component_ac            VARCHAR2(30)   NOT NULL   CONSTRAINT fk_comp2exprl$compnt REFERENCES IA_COMPONENT(ac) ON DELETE CASCADE
     ,  experimentalrole_ac     VARCHAR2(30)   NOT NULL   CONSTRAINT fk_comp2exprl$exprl  REFERENCES IA_CONTROLLEDVOCAB(ac) ON DELETE CASCADE
+    ,  created                 DATE           DEFAULT SYSDATE
+    ,  updated                 DATE           DEFAULT SYSDATE
+    ,  userstamp               VARCHAR2(30)   DEFAULT USER
+    ,  created_user            VARCHAR2(30)   DEFAULT USER NOT NULL ENABLE 
 )
 TABLESPACE &&intactMainTablespace;
 
