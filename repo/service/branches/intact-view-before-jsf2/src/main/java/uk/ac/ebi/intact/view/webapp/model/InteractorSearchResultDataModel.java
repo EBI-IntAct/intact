dad3af435518008c1ad670df968ddaa490ce6e2f
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-04-13 09:19:10 +0000

    Prior to migrating intact-view to JSF2 to resolve performance issues

---
+++ repo/service/branches/intact-view-before-jsf2/src/main/java/uk/ac/ebi/intact/view/webapp/model/InteractorSearchResultDataModel.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright 2009 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.view.webapp.model;
+
+import org.apache.commons.collections.map.LRUMap;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.myfaces.trinidad.model.SortableModel;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.springframework.transaction.TransactionStatus;
+import psidev.psi.mi.search.engine.SearchEngineException;
+import uk.ac.ebi.intact.core.context.DataContext;
+import uk.ac.ebi.intact.core.context.IntactContext;
+import uk.ac.ebi.intact.dataexchange.psimi.solr.IntactSolrSearcher;
+import uk.ac.ebi.intact.dataexchange.psimi.solr.InteractorIdCount;
+import uk.ac.ebi.intact.model.Interactor;
+import uk.ac.ebi.intact.model.InteractorXref;
+
+import javax.faces.model.DataModelEvent;
+import javax.faces.model.DataModelListener;
+import javax.persistence.Query;
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * TODO comment that class header
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class InteractorSearchResultDataModel extends SortableModel implements Serializable {
+
+    private static final Log log = LogFactory.getLog(SolrSearchResultDataModel.class);
+
+    private static String DEFAULT_SORT_COLUMN = "relevancescore";
+
+    private SolrQuery solrQuery;
+    private SolrServer solrServer;
+
+    private String[] interactorTypeMis;
+
+    private List<InteractorIdCount> idCounts;
+
+    private int rowIndex = -1;
+    private int firstResult = 0;
+
+    private String sortColumn = DEFAULT_SORT_COLUMN;
+    private SolrQuery.ORDER sortOrder = SolrQuery.ORDER.asc;
+
+    private Map<String,List<InteractorIdCount>> cache = new LRUMap(5);
+    private Map<String,InteractorWrapper> interactorCache = new LRUMap(200);
+
+    public InteractorSearchResultDataModel(SolrServer solrServer, SolrQuery solrQuery, String interactorTypeMi)  {
+        this(solrServer, solrQuery, new String[] {interactorTypeMi});
+    }
+
+    public InteractorSearchResultDataModel(SolrServer solrServer, SolrQuery solrQuery, String[] interactorTypeMis)  {
+        if (solrQuery == null) {
+            throw new IllegalArgumentException("Trying to create data model with a null SolrQuery");
+        }
+
+        this.solrServer = solrServer;
+        this.solrQuery = solrQuery.getCopy();
+        this.interactorTypeMis = interactorTypeMis;
+
+        setRowIndex(0);
+        fetchResults();
+
+        setWrappedData(idCounts);
+    }
+
+    protected void fetchResults() throws SearchEngineException {
+        if (solrQuery == null) {
+            throw new IllegalStateException("Trying to fetch results for a null SolrQuery");
+        }
+
+        String cacheKey = solrQuery+"_"+ Arrays.toString(interactorTypeMis);
+
+        if (cache.containsKey(cacheKey)) {
+            if (log.isDebugEnabled()) log.debug("Fetching interactors for query (cache hit): "+solrQuery);
+
+            idCounts = cache.get(cacheKey);
+        } else {
+            if (log.isDebugEnabled()) log.debug("Fetching interactors for query: "+solrQuery);
+
+            IntactSolrSearcher searcher = new IntactSolrSearcher(solrServer);
+            idCounts = new ArrayList<InteractorIdCount>(searcher.searchInteractors(solrQuery, interactorTypeMis).values());
+
+            cache.put(cacheKey, idCounts);
+        }
+    }
+
+    public int getRowCount() {
+        return idCounts.size();
+    }
+
+    public Object getRowData() {
+        if (idCounts == null) {
+            return null;
+        }
+
+        if (!isRowAvailable()) {
+            throw new IllegalArgumentException("row is unavailable");
+        }
+
+        InteractorIdCount idCount = idCounts.get(getRowIndex());
+
+        if (interactorCache.containsKey(idCount.getAc())) {
+            return interactorCache.get(idCount.getAc());
+        }
+
+        DataContext dataContext = IntactContext.getCurrentInstance().getDataContext();
+        TransactionStatus transactionStatus = dataContext.beginTransaction();
+
+        Query query = dataContext.getDaoFactory()
+                .getEntityManager().createQuery("select i from InteractorImpl i where i.ac = :ac");
+        query.setParameter("ac", idCount.getAc());
+
+        Interactor interactor = (Interactor) query.getSingleResult();
+        Collection<InteractorXref> xrefs = interactor.getXrefs();
+
+        for (InteractorXref xref : xrefs) {
+            String ac = xref.getAc();
+        }
+
+
+        InteractorWrapper wrapper = new InteractorWrapper(interactor, idCount.getCount());
+
+        dataContext.commitTransaction(transactionStatus);
+
+        interactorCache.put(idCount.getAc(), wrapper);
+        
+        return wrapper;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public Object getWrappedData() {
+        return idCounts;
+    }
+
+    public boolean isRowAvailable() {
+        if (idCounts == null) {
+            return false;
+        }
+
+        return rowIndex >= 0 && rowIndex < getRowCount();
+    }
+
+    public void setRowIndex(int rowIndex) {
+        if (rowIndex < -1) {
+            throw new IllegalArgumentException("illegal rowIndex " + rowIndex);
+        }
+        int oldRowIndex = rowIndex;
+        this.rowIndex = rowIndex;
+        if (idCounts != null && oldRowIndex != this.rowIndex) {
+            Object data = isRowAvailable() ? getRowData() : null;
+            DataModelEvent event = new DataModelEvent(this, this.rowIndex, data);
+            DataModelListener[] listeners = getDataModelListeners();
+            for (int i = 0; i < listeners.length; i++) {
+                listeners[i].rowSelected(event);
+            }
+        }
+    }
+
+    @Override
+    public Object getRowKey() {
+        return isRowAvailable()
+               ? getRowIndex()
+               : null;
+    }
+
+    @Override
+    public void setRowKey(Object key) {
+        if (key == null) {
+            setRowIndex(-1);
+        } else {
+            setRowIndex((Integer) key);
+        }
+    }
+
+
+    public List<InteractorIdCount> getInteractorIdCounts() {
+        return idCounts;
+    }
+
+    public SolrQuery getSearchQuery() {
+        return solrQuery;
+    }
+
+    public String getSortColumn() {
+        return sortColumn;
+    }
+
+    public void setSortColumn(String sortColumn) {
+        this.sortColumn = sortColumn;
+    }
+
+    public int getFirstResult() {
+        return firstResult;
+    }
+
+    public boolean isAscending() {
+        return (sortOrder == SolrQuery.ORDER.asc);
+    }
+
+}
