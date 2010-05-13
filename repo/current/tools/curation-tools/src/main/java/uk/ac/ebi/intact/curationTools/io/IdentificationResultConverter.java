4b7e65e69978afbfd2ce97241e9a2909ca9250f4
marine.dumousseau@wanadoo.fr <marine3@users.noreply.github.com>
2010-05-13 10:19:31 +0000

    Fixed a bug in SwissprotRemappingReport

---
+++ repo/current/tools/curation-tools/src/main/java/uk/ac/ebi/intact/curationTools/io/IdentificationResultConverter.java
@@ -0,0 +1,59 @@
+/*package uk.ac.ebi.intact.curationTools.io;
+
+import uk.ac.ebi.intact.curationTools.model.contexts.IdentificationContext;
+import uk.ac.ebi.intact.curationTools.model.contexts.UpdateContext;
+import uk.ac.ebi.intact.curationTools.model.results.IdentificationResults;
+import uk.ac.ebi.intact.curationTools.proteinIdentification.jaxb.*;
+
+import java.util.List;*/
+
+/**
+ * The IdentificationResults converter into JAXB
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>10-May-2010</pre>
+ */
+
+/*public class IdentificationResultConverter {
+
+    private ObjectFactory objectFactory;
+
+    public IdentificationResultConverter(){
+        objectFactory = new ObjectFactory();
+    }
+
+    private Results convertsResultIntoJAXB (String intActId, IdentificationResults result, IdentificationContext context){
+        Results r = this.objectFactory.createResults();
+        r.setProtein(intActId);
+        r.setUniprotAccession(result.getUniprotId());
+
+        Context c = convertsContextIntoJAXB(context);
+        r.setContext(c);
+
+        return r;
+    }
+
+    private Context convertsContextIntoJAXB(IdentificationContext context){
+        Context c = this.objectFactory.createContext();
+        c.setGeneName(context.getGene_name());
+        c.setGlobalName(context.getGlobalName());
+        c.setProteinName(context.getProtein_name());
+        c.setSequence(context.getSequence());
+        c.setTaxId(context.getOrganism().getTaxId());
+
+        ListOfIdentifiers listOfIdentifiers = this.objectFactory.createListOfIdentifiers();
+
+        if (context instanceof UpdateContext){
+            UpdateContext update = (UpdateContext) context ;
+
+            //for (String id : update.getIdentifiers()){
+                //listOfIdentifiers.getIdentifier().add(id);
+            //}
+        }
+
+        c.setListOfIdentifiers(listOfIdentifiers);
+
+        return c;
+    }
+}*/
---
+++ repo/current/tools/curation-tools/src/main/java/uk/ac/ebi/intact/curationTools/io/IdentificationResultsReadingException.java
@@ -0,0 +1,27 @@
+//package uk.ac.ebi.intact.curationTools.io;
+
+/**
+ * The exception thrown while trying to read identification results
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>10-May-2010</pre>
+ */
+
+/*public class IdentificationResultsReadingException extends Exception{
+    public IdentificationResultsReadingException() {
+        super();
+    }
+
+    public IdentificationResultsReadingException(String message) {
+        super(message);
+    }
+
+    public IdentificationResultsReadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IdentificationResultsReadingException(Throwable cause) {
+        super(cause);
+    }
+} */
