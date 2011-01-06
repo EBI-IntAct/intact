75fc388bd783c981456729bf8d7b34e40e265fa2
marine.dumousseau@wanadoo.fr <marine3@users.noreply.github.com>
2011-01-06 16:54:56 +0000

    Fixed one broken test because of a configuration problem

--- repo/current/tools/protein-mapping/src/test/java/uk/ac/ebi/intact/protein/mapping/update/proteinUpdateManagerTest.java
+++ repo/current/tools/protein-mapping/src/test/java/uk/ac/ebi/intact/protein/mapping/update/proteinUpdateManagerTest.java
@@ -16,7 +16,7 @@ import javax.xml.ws.soap.SOAPFaultException;
  * @version $Id$
  * @since <pre>06-May-2010</pre>
  */
-@ContextConfiguration(locations = {"/META-INF/intact.spring.xml", "/META-INF/standalone/update-jpa.spring.xml"} )
+@ContextConfiguration(locations = {"/META-INF/intact.spring.xml"} )
 public class proteinUpdateManagerTest  extends IntactBasicTestCase {
 
     @Test
