20d4ac2136621ad6f2697f0d83e34231ab471b9d
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2011-01-13 11:52:33 +0000

    Fixed a bug in the strategy for update : when we have two identities, if one doesn't point to a uniprot entry but the other does, we keep the result from the other identifier. We don't accept such cases if : - the two identifiers point to different uniprot entries - the sequence point to a different uniprot entry - the sequence doesn't point to any uniprot entries and at least one identifier point to something

--- repo/current/tools/protein-mapping/src/main/java/uk/ac/ebi/intact/protein/mapping/strategies/StrategyForProteinUpdate.java
+++ repo/current/tools/protein-mapping/src/main/java/uk/ac/ebi/intact/protein/mapping/strategies/StrategyForProteinUpdate.java
@@ -90,57 +90,44 @@ public class StrategyForProteinUpdate extends IdentificationStrategyImpl {
      * Check that we don't have any conflict between the uniprot accession returned by the strategy using the identifier
      * and the uniprot accession returned by the strategy using the sequence
      * @param result : the result
-     * @param context : the current context
      * @param updateReport : the curent update report
      * @return true if the uniprot Id returned by the StrategyWithIdentifier and the uniprot Id returned by the StrategyWithSequence are the same
      * @throws uk.ac.ebi.intact.protein.mapping.actions.exception.ActionProcessingException
      * @throws uk.ac.ebi.intact.protein.mapping.strategies.exceptions.StrategyException
      */
-    private boolean checkIdentifierResults(IdentificationResults result, UpdateContext context, ActionReport updateReport) throws ActionProcessingException, StrategyException {
+    private boolean checkIdentifierResults(IdentificationResults result, ActionReport updateReport, String otherIdentifier) throws ActionProcessingException, StrategyException {
         // The strategy using the sequence found a unique Uniprot accession
         if (result.getFinalUniprotId() != null){
-            // Get the uniprot accession using the strategy with identifier
-            String otherResultFromIdentifier = this.listOfActions.get(1).runAction(context);
-            // process the isoforms
-            otherResultFromIdentifier = processIsoforms(otherResultFromIdentifier);
-            // add the reports to the list of reports of the result
-            result.getListOfActions().addAll(this.listOfActions.get(1).getListOfActionReports());
 
             // the uniprot accession returned by the strategy with the sequence
             String uniprot1 = result.getFinalUniprotId();
 
             // the uniprot accession returned by the strategy using the identifier is successful
-            if (otherResultFromIdentifier != null){
+            if (otherIdentifier != null){
                 // The result is not matching the previous one, there is a conflict
-                if (!uniprot1.equals(otherResultFromIdentifier)){
-                    updateReport.addPossibleAccession(otherResultFromIdentifier);
+                if (!uniprot1.equals(otherIdentifier)){
+                    updateReport.addPossibleAccession(otherIdentifier);
                 }
-                return uniprot1.equals(otherResultFromIdentifier);
+                return uniprot1.equals(otherIdentifier);
             }
-            // the uniprot accession returned by the strategy using the identifier is null, we have a conflict 
+            // the uniprot accession returned by the strategy using the identifier is null, we have a possible conflict but we still process the results later
             else {
                 updateReport.addWarning("We found a unique uniprot AC when we tried to identify the protein using the sequence but we didn't find any uniprot AC" +
                         " when we tried to identify this protein using its identifiers with qualifier set to 'identity'.");
-                return false;
+                return true;
             }
         }
         // The strategy using the sequence couldn't find a unique Uniprot accession
         else {
-            // Get the uniprot accession using the strategy with identifier
-            String otherResultFromIdentifier = this.listOfActions.get(1).runAction(context);
-            // process the isoforms
-            otherResultFromIdentifier = processIsoforms(otherResultFromIdentifier);
-            // add the reports to the list of reports of the result
-            result.getListOfActions().addAll(this.listOfActions.get(1).getListOfActionReports());
 
             // the uniprot accession returned by the strategy using the identifier is not null, we can have a conflict, need to check if the result null from
             // the previous strategy is not because of the featureRangeCheckingProcess
-            if (otherResultFromIdentifier != null){
+            if (otherIdentifier != null){
                 updateReport.addWarning("We didn't find a unique uniprot AC when we tried to identify the protein using the sequence but we found a uniprot AC" +
                         " when we tried to identify this protein using its identifiers with qualifier set to 'identity'.");
 
                 // add the result of the last strategy in the report
-                updateReport.addPossibleAccession(otherResultFromIdentifier);
+                updateReport.addPossibleAccession(otherIdentifier);
             }
             // both uniprot accessions are null, we don't have any conflict
             else {
@@ -281,17 +268,39 @@ public class StrategyForProteinUpdate extends IdentificationStrategyImpl {
                     // boolean value to know if there is a conflict with the previous results
                     boolean isMatchingIdentifierResults = false;
 
+                    String otherIdentifier = null;
                     for (Map.Entry<String, String> entry : identifiers.entrySet()){
                         // set the identifier
                         updateContext.setIdentifier(entry.getValue());
                         // set the database
                         updateContext.setDatabaseForIdentifier(entry.getKey());
+                        // Get the uniprot accession using the strategy with identifier
+                        String otherResultFromIdentifier = this.listOfActions.get(1).runAction(context);
+                        // process the isoforms
+                        otherResultFromIdentifier = processIsoforms(otherResultFromIdentifier);
+
+                        if (otherResultFromIdentifier != null){
+                            if (otherIdentifier != null){
+                                 if (!otherIdentifier.equalsIgnoreCase(otherResultFromIdentifier)){
+                                       isMatchingIdentifierResults = false;
+                                     break;
+                                 }
+                            }
+                             otherIdentifier = otherResultFromIdentifier;
+                        }
+
+                        // add the reports to the list of reports of the result
+                        result.getListOfActions().addAll(this.listOfActions.get(1).getListOfActionReports());
                         // check the possible conflicts with the previous results
-                        isMatchingIdentifierResults = checkIdentifierResults(result, updateContext, report);
+                        isMatchingIdentifierResults = checkIdentifierResults(result, report, otherResultFromIdentifier);
+
+                        if (!isMatchingIdentifierResults){
+                            break;
+                        }
                     }
 
                     // We don't have any conflicts with the previous results
-                    if (isMatchingIdentifierResults){
+                    if (isMatchingIdentifierResults && otherIdentifier == result.getFinalUniprotId() ){
                         Status status = new Status(StatusLabel.COMPLETED, "There is no result conflicts when we try to identify the protein using the sequence then using the identifiers " + identifiers);
                         report.setStatus(status);
                         result.addActionReport(report);
@@ -318,9 +327,6 @@ public class StrategyForProteinUpdate extends IdentificationStrategyImpl {
                 ActionReport report = new ActionReport(ActionName.update_checking);
                 report.addPossibleAccession(result.getFinalUniprotId());
 
-                // boolean value to know if there is a conflict with the previous results
-                boolean isMatchingIdentifierResults = true;
-
                 Set<String> uniprots = new HashSet<String>();
 
                 for (Map.Entry<String, String> entry : identifiers.entrySet()){
@@ -336,18 +342,14 @@ public class StrategyForProteinUpdate extends IdentificationStrategyImpl {
                     // we process the isoforms
                     newUniprot = processIsoforms(newUniprot);
 
-                    if (newUniprot == null){
-                        if (!uniprots.isEmpty()){
-                            isMatchingIdentifierResults = false;
-                        }
-                    }
-                    else {
+                    if (newUniprot != null){
                         uniprots.add(newUniprot);
                     }
                 }
 
-                // We don't have any conflicts with the previous results
-                if (uniprots.isEmpty() || (uniprots.size() == 1 && isMatchingIdentifierResults)){
+                // We don't have any conflicts with the previous results  : if one identifier returns nothing but another allows to identify, we
+                // keep the one which could identify. However, if several different uniprot acs are found, we have a conflict
+                if (uniprots.isEmpty() || (uniprots.size() == 1)){
                     Status status = new Status(StatusLabel.COMPLETED, "There is no conflicts in the results when we tried to identify the protein using the identifiers " + identifiers);
                     report.setStatus(status);
                     result.addActionReport(report);
