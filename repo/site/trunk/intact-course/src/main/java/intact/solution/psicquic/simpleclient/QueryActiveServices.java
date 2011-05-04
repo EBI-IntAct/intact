2d9397f3ea7ed45bead53d7f378f30afc1bf59ef
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2011-05-04 14:17:43 +0000

    Updated licence info

--- repo/site/trunk/intact-course/src/main/java/intact/solution/psicquic/simpleclient/QueryActiveServices.java
+++ repo/site/trunk/intact-course/src/main/java/intact/solution/psicquic/simpleclient/QueryActiveServices.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package intact.solution.psicquic.simpleclient;
 
 import org.hupo.psi.mi.psicquic.registry.ServiceType;
