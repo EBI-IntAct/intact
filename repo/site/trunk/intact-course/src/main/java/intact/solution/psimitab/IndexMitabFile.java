61d765fb5fa125f17dbf1ad73bc801129c68abe7
samuel.kerrien <samuel.kerrien@gmail.com>
2011-05-04 14:17:43 +0000

    Updated licence info

--- repo/site/trunk/intact-course/src/main/java/intact/solution/psimitab/IndexMitabFile.java
+++ repo/site/trunk/intact-course/src/main/java/intact/solution/psimitab/IndexMitabFile.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 The European Bioinformatics Institute, and others.
+ * Copyright 2011 The European Bioinformatics Institute, and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package intact.solution.psimitab;
 
 import psidev.psi.mi.search.index.PsimiIndexWriter;
