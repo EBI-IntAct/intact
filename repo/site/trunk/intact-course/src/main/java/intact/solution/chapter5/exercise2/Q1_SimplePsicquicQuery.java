96b3fcd8011905cd5096868b7dd0b375f0ce9d67
brunoaranda <brunoaranda@gmail.com>
2011-05-05 16:37:43 +0000

    Refactored into chapters and exercises

---
+++ repo/site/trunk/intact-course/src/main/java/intact/solution/chapter5/exercise2/Q1_SimplePsicquicQuery.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2011 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package intact.solution.chapter5.exercise2;
+
+import org.hupo.psi.mi.psicquic.wsclient.PsicquicSimpleClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Download interactions from PSICQUIC, using the simple client.
+ *
+ * @see org.hupo.psi.mi.psicquic.wsclient.PsicquicSimpleClient
+ * @see org.hupo.psi.mi.psicquic.wsclient.PsicquicSimpleClient#getByQuery(String)
+ *
+ */
+public class Q1_SimplePsicquicQuery {
+
+     public static void main(String[] args) throws Exception {
+        // get a REST URl from the registry http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=STATUS
+
+        PsicquicSimpleClient client = new PsicquicSimpleClient("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/");
+
+        // miql query
+        String miqlQuery = "pubid:16189514";
+
+        try {
+            final InputStream result = client.getByQuery(miqlQuery);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(result));
+
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
