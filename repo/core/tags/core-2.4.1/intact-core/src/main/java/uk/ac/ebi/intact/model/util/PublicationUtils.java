4c740d3afd2e95814f3584215611695f61e05a46
brunoaranda <brunoaranda@gmail.com>
2011-07-04 15:20:42 +0000

    [maven-release-plugin]  copy for tag core-2.4.1

---
+++ repo/core/tags/core-2.4.1/intact-core/src/main/java/uk/ac/ebi/intact/model/util/PublicationUtils.java
@@ -0,0 +1,81 @@
+package uk.ac.ebi.intact.model.util;
+
+import uk.ac.ebi.intact.core.config.SequenceManager;
+import uk.ac.ebi.intact.core.context.IntactContext;
+import uk.ac.ebi.intact.model.Annotation;
+import uk.ac.ebi.intact.model.CvTopic;
+import uk.ac.ebi.intact.model.Experiment;
+import uk.ac.ebi.intact.model.Publication;
+
+/**
+ * Misc publication utilities.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since 2.2.1
+ */
+public final class PublicationUtils {
+
+    private PublicationUtils() {
+    }
+
+    public static boolean isAccepted(Publication publication) {
+        if (publication == null) {
+            throw new NullPointerException("You must give a non null publication");
+        }
+
+        for (Annotation a : publication.getAnnotations()) {
+            if (a.getCvTopic() != null && CvTopic.ACCEPTED.equals(a.getCvTopic().getShortLabel())) {
+                return true;
+            }
+        }
+
+        return ExperimentUtils.areAllAccepted(publication.getExperiments());
+    }
+
+    public static boolean isToBeReviewed(Publication publication) {
+        if (publication == null) {
+            throw new NullPointerException("You must give a non null publication");
+        }
+
+        for (Annotation a : publication.getAnnotations()) {
+            if (a.getCvTopic() != null && CvTopic.TO_BE_REVIEWED.equals(a.getCvTopic().getShortLabel())) {
+                return true;
+            }
+        }
+
+        for (Experiment experiment : publication.getExperiments()) {
+            if (ExperimentUtils.isToBeReviewed(experiment)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isOnHold(Publication publication) {
+
+        if (publication == null) {
+            throw new NullPointerException("You must give a non null publication");
+        }
+
+        for (Annotation a : publication.getAnnotations()) {
+            if (a.getCvTopic() != null && CvTopic.ON_HOLD.equals(a.getCvTopic().getShortLabel())) {
+                return true;
+            }
+        }
+
+        for (Experiment experiment : publication.getExperiments()) {
+            if (ExperimentUtils.isOnHold(experiment)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static String nextUnassignedId(IntactContext intactContext) {
+        SequenceManager sequenceManager = (SequenceManager) intactContext.getSpringContext().getBean("sequenceManager");
+        return "unassigned" + sequenceManager.getNextValueForSequence("unassigned_seq");
+    }
+}
