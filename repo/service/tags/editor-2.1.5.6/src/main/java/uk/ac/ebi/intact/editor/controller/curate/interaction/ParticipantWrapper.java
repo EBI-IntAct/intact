e419267cde2906a71418a9d8212a0cc696d6253e
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-03-06 14:36:30 +0000

    [maven-scm] copy for tag editor-2.1.5.6

---
+++ repo/service/tags/editor-2.1.5.6/src/main/java/uk/ac/ebi/intact/editor/controller/curate/interaction/ParticipantWrapper.java
@@ -0,0 +1,129 @@
+package uk.ac.ebi.intact.editor.controller.curate.interaction;
+
+import uk.ac.ebi.intact.core.context.IntactContext;
+import uk.ac.ebi.intact.core.persister.IntactCore;
+import uk.ac.ebi.intact.editor.controller.curate.AnnotatedObjectHelper;
+import uk.ac.ebi.intact.editor.controller.curate.ChangesController;
+import uk.ac.ebi.intact.model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Wrapped participant to allow handling of special fields (eg. author given name) from the interaction view. 
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since 2.0
+ */
+public class ParticipantWrapper {
+
+    private Component participant;
+    private AnnotatedObjectHelper annotatedObjectHelper;
+    private ChangesController changesController;
+    private List<FeatureWrapper> features;
+
+    private boolean deleted;
+
+    public ParticipantWrapper( Component participant, ChangesController changesController ) {
+        this.participant = participant;
+        this.annotatedObjectHelper = newAnnotatedObjectHelper(participant);
+        this.changesController = changesController;
+
+        features = new ArrayList<FeatureWrapper>(participant.getFeatures().size());
+
+        for (Feature feature : participant.getFeatures()) {
+            features.add(new FeatureWrapper(feature));
+        }
+
+    }
+
+    public Component getParticipant() {
+        return participant;
+    }
+
+    public void setParticipant( Component participant ) {
+        this.participant = participant;
+    }
+
+    public String getAuthorGivenName() {
+        return annotatedObjectHelper.findAliasName( CvAliasType.AUTHOR_ASSIGNED_NAME_MI_REF );
+    }
+
+    public void setAuthorGivenName( String name ) {
+        String author = annotatedObjectHelper.findAliasName(CvAliasType.AUTHOR_ASSIGNED_NAME_MI_REF);
+        if ((name == null && author != null) || (name != null && author == null)){
+            changesController.markAsUnsaved(participant);
+            annotatedObjectHelper.setAlias(CvAliasType.AUTHOR_ASSIGNED_NAME_MI_REF, name  );
+        }
+        else if (name != null && !name.equalsIgnoreCase(author)){
+            changesController.markAsUnsaved(participant);
+            annotatedObjectHelper.setAlias(CvAliasType.AUTHOR_ASSIGNED_NAME_MI_REF, name  );
+        }
+    }
+
+    public CvExperimentalRole getFirstExperimentalRole() {
+        if( ! participant.getExperimentalRoles().isEmpty() ) {
+            return participant.getExperimentalRoles().iterator().next();
+        }
+        return null;
+    }
+
+    public void setFirstExperimentalRole(CvExperimentalRole role) {
+        if( ! participant.getExperimentalRoles().contains(role) ) {
+            participant.getExperimentalRoles().clear();
+            participant.getExperimentalRoles().add( role );
+        }
+    }
+
+    public CvExperimentalPreparation getFirstExperimentalPreparation() {
+        if( participant.getInteractor() != null ) {
+            final Collection<CvExperimentalPreparation> expPreparations = IntactCore.ensureInitializedExperimentalPreparations(participant);
+            if( expPreparations != null && ! expPreparations.isEmpty() ) {
+                return expPreparations.iterator().next();
+            }
+        }
+
+        return null;
+    }
+
+    public void setFirstExperimentalPreparation( CvExperimentalPreparation prep ) {
+        if (participant.getExperimentalPreparations() == null){
+            participant.setExperimentalPreparations(new ArrayList<CvExperimentalPreparation>());
+        }
+
+        if( ! participant.getExperimentalPreparations().contains( prep ) ) {
+            participant.getExperimentalPreparations().clear();
+            participant.getExperimentalPreparations().add( prep );
+        }
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+
+        if (participant.getAc() != null) {
+            if (deleted) {
+                changesController.markToDelete(participant, participant.getInteraction());
+            } else {
+                changesController.removeFromDeleted(participant, participant.getInteraction());
+            }
+        }
+    }
+
+    public List<FeatureWrapper> getFeatures() {
+        return features;
+    }
+
+    private AnnotatedObjectHelper newAnnotatedObjectHelper(AnnotatedObject annotatedObject) {
+        AnnotatedObjectHelper helper = (AnnotatedObjectHelper) IntactContext.getCurrentInstance().getSpringContext().getBean("annotatedObjectHelper");
+        helper.setAnnotatedObject(annotatedObject);
+
+        return helper;
+    }
+
+}
