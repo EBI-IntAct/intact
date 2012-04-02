9a268ce9bfe0ceeadad5832dfebb611c26b8fbb0
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-04-02 15:12:49 +0000

    [maven-scm] copy for tag editor-2.1.5.8

---
+++ repo/service/tags/editor-2.1.5.8/src/test/java/uk/ac/ebi/intact/editor/it/ParticipantsIT.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.intact.editor.it;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import uk.ac.ebi.intact.model.Experiment;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ParticipantsIT extends EditorIT {
+
+	@Test
+	public void importProteinFromUniprotSuccessfully() throws Exception {
+        Experiment experiment = getDaoFactory().getExperimentDao().getByShortLabel("bigexp-2012-1");
+        
+        // Given: I want to import a participant from uniprot in the participant page
+        final String participantAc = experiment.getInteractions().iterator().next().getComponents().iterator().next().getAc();
+        goToParticipantPage(participantAc);
+        loginAs("curator");
+
+        // When: I import P12345 using the Import... dialog
+        clickOnInteractorImport();
+        searchParticipantsUsing("P12365");
+        importSelectedByDefault();
+
+        // Then: the shortlabel of the interactor should be cata2_maize
+        assertThat(valueForElement(By.id("interactorTxt")), is(equalTo("P12365")));
+	}
+
+    private void searchParticipantsUsing(String query) {
+        final By queryElement = By.id("ipDialogPanel:searchInteractorTxt");
+        driver.findElement(queryElement).clear();
+        driver.findElement(queryElement).sendKeys(query);
+        driver.findElement(By.id("ipDialogPanel:interactorSearchBtn")).click();
+        waitUntilElementIsPresent(By.id("candidatesDialogContent:importSelected"));
+    }
+
+    private void clickOnInteractorImport() {
+        waitUntilElementIsPresent(By.id("importInteractorBtn"));
+        driver.findElement(By.id("importInteractorBtn")).click();
+        waitUntilElementIsPresent(By.id("ipDialogPanel:searchInteractorTxt"));
+    }
+
+    private void importSelectedByDefault() {
+        driver.findElement(By.id("candidatesDialogContent:importSelected")).click();
+        waitUntilLoadingIsComplete();
+    }
+
+
+}
