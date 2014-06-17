64ebc5e674855b516b9e796693d1793028b4bc90
mdumousseau@yahoo.com <marine3@users.noreply.github.com>
2014-06-17 09:33:57 +0000

    added script for deprecated views

--- repo/utils/sql/oracle/deprecated_views.sql
+++ repo/utils/sql/oracle/deprecated_views.sql
@@ -13,3 +13,35 @@ where e.ac = e2a.experiment_ac
       and x.qualifier_ac = q.ac
       and q.shortlabel = 'primary-reference'
 group by e.ac, e.shortlabel, e.created, a.description, x.primaryid;
+
+  CREATE OR REPLACE FORCE VIEW "INTACT"."V_EXPERIMENT_SUBMITED" ("AC", "SHORTLABEL", "CREATED", "SUBMITTED_BY", "PUBMED", "INTERACTION_COUNT") AS 
+  select e.ac, e.shortlabel, e.created, a.description as submitted_by, x.primaryid as pubmed, count(i2e.interaction_ac) as interaction_count
+from ia_experiment e, ia_exp2annot e2a, ia_controlledvocab topic_i, ia_annotation a,
+     ia_experiment_xref x, ia_controlledvocab db, ia_controlledvocab q, ia_int2exp i2e
+where e.ac = e2a.experiment_ac
+      and e.ac = i2e.experiment_ac
+      and e2a.annotation_ac = a.ac
+      and a.topic_ac = topic_i.ac
+      and topic_i.shortlabel in ('submitted')
+      and e.ac = x.parent_ac
+      and x.database_ac = db.ac
+      and db.shortlabel = 'pubmed'
+      and x.qualifier_ac = q.ac
+      and q.shortlabel = 'primary-reference'
+group by e.ac, e.shortlabel, e.created, a.description, x.primaryid;
+
+  CREATE OR REPLACE FORCE VIEW "INTACT"."V_EXPERIMENT_CURATION_REQUEST" ("AC", "SHORTLABEL", "CREATED", "SUBMITTED_BY", "PUBMED", "INTERACTION_COUNT") AS 
+  select e.ac, e.shortlabel, e.created, a.description as submitted_by, x.primaryid as pubmed, count(i2e.interaction_ac) as interaction_count
+from ia_experiment e, ia_exp2annot e2a, ia_controlledvocab topic_i, ia_annotation a,
+     ia_experiment_xref x, ia_controlledvocab db, ia_controlledvocab q, ia_int2exp i2e
+where e.ac = e2a.experiment_ac
+      and e.ac = i2e.experiment_ac
+      and e2a.annotation_ac = a.ac
+      and a.topic_ac = topic_i.ac
+      and topic_i.shortlabel in ('curation-request')
+      and e.ac = x.parent_ac
+      and x.database_ac = db.ac
+      and db.shortlabel = 'pubmed'
+      and x.qualifier_ac = q.ac
+      and q.shortlabel = 'primary-reference'
+group by e.ac, e.shortlabel, e.created, a.description, x.primaryid;
