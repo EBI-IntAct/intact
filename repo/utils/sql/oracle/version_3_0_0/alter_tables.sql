b3d664fed11f2e51e1fd771556ebd4164d96d23d
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2014-10-06 14:29:37 +0000

    updated constraints

--- repo/utils/sql/oracle/version_3_0_0/alter_tables.sql
+++ repo/utils/sql/oracle/version_3_0_0/alter_tables.sql
@@ -23,13 +23,13 @@ alter table ia_interactor add constraint FK_complex2evidencetype foreign key (ev
 alter table ia_component add (maxstoichiometry number(10,0), category varchar2(31 char)); -- add max stoichiometry for participant and category
 alter table ia_component_audit add (maxstoichiometry number(10,0), category varchar2(31 char)); -- add max stoichiometry for participant and category
 
-alter table ia_sequence_chunk drop constraint SYS_C00178400; -- remove not null constarin on parent_ac to enable oneToMany unidirectional
-alter table ia_sequence_chunk_audit drop constraint SYS_C00178407 -- remove not null constarin on parent_ac to enable oneToMany unidirectional
+alter table ia_sequence_chunk drop constraint SYS_C0018509; -- remove not null constarin on parent_ac to enable oneToMany unidirectional
+alter table ia_sequence_chunk_audit drop constraint SYS_C0018518; -- remove not null constarin on parent_ac to enable oneToMany unidirectional
 
-alter table ia_feature drop constraint SYS_C00178104; -- remove not null constarin on parent_ac to enable orphan removal
-alter table ia_feature_audit drop constraint SYS_C00178139; -- remove not null constarin on parent_ac to enable orphan removal
+alter table ia_feature drop constraint SYS_C0018155; -- remove not null constarin on parent_ac to enable orphan removal
+alter table ia_feature_audit drop constraint SYS_C0018196; -- remove not null constarin on parent_ac to enable orphan removal
 
 alter table ia_interactor_xref add (category varchar2(31 char), pubmed varchar2(255 char), evidencetype_ac varchar2(30 char)); -- add complex GO xref category
 alter table ia_interactor_xref_audit add (category varchar2(31 char), pubmed varchar2(255 char), evidencetype_ac varchar2(30 char)); -- add complex GO xref category
-alter table ia_interactor_xref add constraint FK_GO2ECO foreign key (evidencetype_ac) references ia_controlledvocab
+alter table ia_interactor_xref add constraint FK_GO2ECO foreign key (evidencetype_ac) references ia_controlledvocab;
 
