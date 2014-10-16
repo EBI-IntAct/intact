e6b4c5e8b858629cea8aa037d2891bcae7725e1a
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2014-10-16 13:24:04 +0000

    [maven-release-plugin]  copy for tag core-2.6.8

---
+++ repo/core/tags/core-2.6.8/intact-jami/src/main/java/uk/ac/ebi/intact/jami/model/lifecycle/AbstractLifeCycleEvent.java
@@ -0,0 +1,163 @@
+package uk.ac.ebi.intact.jami.model.lifecycle;
+
+import org.hibernate.annotations.ForeignKey;
+import org.hibernate.annotations.Index;
+import org.hibernate.annotations.Target;
+import org.hibernate.annotations.Type;
+import psidev.psi.mi.jami.model.CvTerm;
+import uk.ac.ebi.intact.jami.model.AbstractIntactPrimaryObject;
+import uk.ac.ebi.intact.jami.model.extension.IntactCvTerm;
+import uk.ac.ebi.intact.jami.model.user.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+/**
+ * Abstract class for publication lifecycle
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ */
+@Entity
+@Inheritance( strategy = InheritanceType.TABLE_PER_CLASS )
+public abstract class AbstractLifeCycleEvent extends AbstractIntactPrimaryObject implements LifeCycleEvent {
+
+    private transient LifeCycleEventType event;
+
+    private User who;
+
+    private Date when;
+
+    private String note;
+
+    private transient CvTerm cvEvent;
+
+    protected AbstractLifeCycleEvent() {
+    }
+
+    public AbstractLifeCycleEvent(LifeCycleEventType event, User who, Date when, String note) {
+        this.event = event;
+        this.who = who;
+        this.when = when;
+        this.note = note;
+    }
+
+    /**
+     * Date is set to current time.
+     * @param event
+     * @param who
+     * @param note
+     */
+    public AbstractLifeCycleEvent(LifeCycleEventType event, User who, String note) {
+        this( event, who, new Date(), note );
+    }
+
+    @Transient
+    /**
+     * NOTE: in the future, should be persisted and cvEvent should be removed
+     */
+    public LifeCycleEventType getEvent() {
+        if (this.event == null){
+            this.event = LifeCycleEventType.CREATED;
+        }
+        return event;
+    }
+
+    public void setEvent( LifeCycleEventType event ) {
+        this.event = event != null ? event : LifeCycleEventType.CREATED;
+        this.cvEvent = this.event.toCvTerm();
+    }
+
+    @ManyToOne( optional = false, targetEntity = User.class)
+    @JoinColumn( name = "user_ac", referencedColumnName = "ac")
+    @ForeignKey(name="FK_LIFECYCLE_EVENT_USER")
+    @Index( name = "idx_event_who" )
+    @Target(User.class)
+    public User getWho() {
+        return who;
+    }
+
+    public void setWho( User who ) {
+        this.who = who;
+    }
+
+    @Column(name = "when_date")
+    @Temporal( TemporalType.TIMESTAMP )
+    public Date getWhen() {
+        return when;
+    }
+
+    public void setWhen( Date when ) {
+        this.when = when;
+    }
+
+    @Lob
+    @Type(type = "org.hibernate.type.StringClobType")
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote( String note ) {
+        this.note = note;
+    }
+
+    @ManyToOne( targetEntity = IntactCvTerm.class, optional = false)
+    @JoinColumn( name = "event_ac", referencedColumnName = "ac")
+    @ForeignKey(name="FK_LIFECYCLE_EVENT_EVENT")
+    @Index( name = "idx_event_event" )
+    @Target(IntactCvTerm.class)
+    @Deprecated
+    @NotNull
+    /**
+     * NOTE: in the future, should be persisted and cvEvent should be removed
+     * @deprecated use getEvent instead
+     */
+    public CvTerm getCvEvent() {
+        if (this.cvEvent == null){
+            this.cvEvent = getEvent().toCvTerm();
+        }
+        return this.cvEvent;
+    }
+
+    @Deprecated
+    /**
+     * @deprecated use setEvent instead
+     */
+    public void setCvEvent( CvTerm event ) {
+        this.cvEvent = event;
+        if (event != null){
+            this.event = LifeCycleEventType.toLifeCycleEventType(event);
+        }
+    }
+
+    /**
+     * Overrides serialization for xrefs and annotations (inner classes not serializable)
+     * @param oos
+     * @throws java.io.IOException
+     */
+    private void writeObject(ObjectOutputStream oos)
+            throws IOException {
+        // default serialization
+        oos.defaultWriteObject();
+        // write the event
+        oos.writeObject(getCvEvent());
+    }
+
+    /**
+     * Overrides serialization for xrefs and annotations (inner classes not serializable)
+     * @param ois
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    private void readObject(ObjectInputStream ois)
+            throws ClassNotFoundException, IOException {
+        // default deserialization
+        ois.defaultReadObject();
+        // read default xrefs
+        setCvEvent((CvTerm)ois.readObject());
+    }
+}
---
+++ repo/core/tags/core-2.6.8/intact-jami/src/main/java/uk/ac/ebi/intact/jami/synchronizer/impl/ComplexExperimentBCSynchronizer.java
@@ -0,0 +1,322 @@
+package uk.ac.ebi.intact.jami.synchronizer.impl;
+
+import org.apache.commons.collections.map.IdentityMap;
+import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.utils.ExperimentUtils;
+import psidev.psi.mi.jami.utils.clone.ExperimentCloner;
+import uk.ac.ebi.intact.jami.context.SynchronizerContext;
+import uk.ac.ebi.intact.jami.merger.ExperimentMergerEnrichOnly;
+import uk.ac.ebi.intact.jami.model.extension.IntactCvTerm;
+import uk.ac.ebi.intact.jami.model.extension.IntactExperiment;
+import uk.ac.ebi.intact.jami.model.extension.IntactOrganism;
+import uk.ac.ebi.intact.jami.model.extension.IntactPublication;
+import uk.ac.ebi.intact.jami.synchronizer.AbstractIntactDbSynchronizer;
+import uk.ac.ebi.intact.jami.synchronizer.FinderException;
+import uk.ac.ebi.intact.jami.synchronizer.PersisterException;
+import uk.ac.ebi.intact.jami.synchronizer.SynchronizerException;
+import uk.ac.ebi.intact.jami.utils.IntactUtils;
+import uk.ac.ebi.intact.jami.utils.comparator.IntactComparator;
+import uk.ac.ebi.intact.jami.utils.comparator.UnambiguousIntactExperimentComparator;
+
+import javax.persistence.Query;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+/**
+ * Default synchronizer for experiments attached to complexes.
+ * This synchronizer is only for backward compatibility with intact core and should be deleted when intact-core is not needed anymore
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>21/01/14</pre>
+ */
+
+public class ComplexExperimentBCSynchronizer extends AbstractIntactDbSynchronizer<Experiment, IntactExperiment> {
+
+    private Map<Experiment, IntactExperiment> persistedObjects;
+    private Map<Experiment, IntactExperiment> convertedObjects;
+
+    private IntactComparator<Experiment> experimentComparator;
+
+    public ComplexExperimentBCSynchronizer(SynchronizerContext context){
+        super(context, IntactExperiment.class);
+        // to keep track of persisted cvs
+        this.experimentComparator = new UnambiguousIntactExperimentComparator();
+
+        this.persistedObjects = new TreeMap<Experiment, IntactExperiment>(this.experimentComparator);
+        this.convertedObjects = new IdentityMap();
+    }
+
+    public IntactExperiment find(Experiment experiment) throws FinderException {
+        if (experiment == null){
+            return null;
+        }
+        else if (this.persistedObjects.containsKey(experiment)){
+            return this.persistedObjects.get(experiment);
+        }
+        else{
+            IntactPublication fetchedPublication = null;
+            if (experiment.getPublication() != null){
+                fetchedPublication = getContext().getPublicationSynchronizer().find(experiment.getPublication());
+                // the publication does not exist so the experiment does not exist
+                if (fetchedPublication == null || fetchedPublication.getAc() == null){
+                    return null;
+                }
+            }
+            IntactOrganism fetchedOrganism = null;
+            if (experiment.getHostOrganism() != null){
+                fetchedOrganism = getContext().getOrganismSynchronizer().find(experiment.getHostOrganism());
+                // the organism does not exist so the experiment does not exist
+                if (fetchedOrganism == null || fetchedOrganism.getAc() == null){
+                    return null;
+                }
+            }
+            IntactCvTerm fetchedDetectionMethod = null;
+            if (experiment.getInteractionDetectionMethod() != null){
+                fetchedDetectionMethod = getContext().getInteractionDetectionMethodSynchronizer().find(experiment.getInteractionDetectionMethod());
+                // the detection method does not exist so the experiment does not exist
+                if (fetchedDetectionMethod == null || fetchedDetectionMethod.getAc() == null){
+                    return null;
+                }
+            }
+
+            Query query = getEntityManager().createQuery("select e from IntactExperiment e " +
+                    (fetchedOrganism != null ? "join e.hostOrganism as h " : "" ) +
+                    (fetchedDetectionMethod != null ? "join e.interactionDetectionMethod as det " : "" ) +
+                    (fetchedPublication != null ? "join e.publication as p " : "" ) +
+                    "where "+
+                    (fetchedOrganism != null ? "h.ac = :orgAc " : "e.hostOrganism is null " ) +
+                    (fetchedDetectionMethod != null ? "and det.ac = :detAc " : "and e.interactionDetectionMethod is null " ) +
+                    (fetchedPublication != null ? "and p.ac = :pubAc " : "and e.publication is null " ));
+            if (fetchedOrganism != null){
+                query.setParameter("orgAc", fetchedOrganism.getAc());
+            }
+            if (fetchedDetectionMethod != null){
+                query.setParameter("detAc", fetchedDetectionMethod.getAc());
+            }
+            if (fetchedPublication != null){
+                query.setParameter("pubAc", fetchedPublication.getAc());
+            }
+
+            Collection<IntactExperiment> results = query.getResultList();
+            if (results.size() == 1){
+                return results.iterator().next();
+            }
+            else if (results.size() > 1){
+                throw new FinderException("The experiment "+experiment.toString() + " can match "+results.size()+" experiments in the database and we cannot determine which one is valid.");
+            }
+            return null;
+        }
+    }
+
+    public void synchronizeProperties(IntactExperiment intactExperiment) throws FinderException, PersisterException, SynchronizerException {
+        // then check shortlabel/synchronize
+        prepareAndSynchronizeShortLabel(intactExperiment);
+        // then check interaction detection method
+        prepareInteractionDetectionMethod(intactExperiment, true);
+        // then check participant identification method
+        prepareParticipantIdentificationMethod(intactExperiment, true);
+        // then check organism
+        prepareHostOrganism(intactExperiment, true);
+        // then check annotations
+        prepareAnnotations(intactExperiment, true);
+        // then check xrefs
+        prepareXrefs(intactExperiment, true);
+    }
+
+    public void clearCache() {
+        this.persistedObjects.clear();
+        this.convertedObjects.clear();
+    }
+    @Override
+    protected Object extractIdentifier(IntactExperiment object) {
+        return object.getAc();
+    }
+
+    @Override
+    protected IntactExperiment instantiateNewPersistentInstance(Experiment object, Class<? extends IntactExperiment> intactClass) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        IntactExperiment exp = new IntactExperiment(object.getPublication());
+        ExperimentCloner.copyAndOverrideExperimentPropertiesAndInteractionEvidences(object, exp);
+        return exp;
+    }
+
+    @Override
+    protected void storeInCache(Experiment originalObject, IntactExperiment persistentObject, IntactExperiment existingInstance) {
+        if (existingInstance != null){
+            this.persistedObjects.put(originalObject, existingInstance);
+        }
+        else{
+            this.persistedObjects.put(originalObject, persistentObject);
+        }
+    }
+
+    @Override
+    protected IntactExperiment fetchObjectFromCache(Experiment object) {
+        return this.persistedObjects.get(object);
+    }
+
+    @Override
+    protected boolean isObjectStoredInCache(Experiment object) {
+        return this.persistedObjects.containsKey(object);
+    }
+
+    @Override
+    protected boolean containsObjectInstance(Experiment object) {
+        return this.convertedObjects.containsKey(object);
+    }
+
+    @Override
+    protected void removeObjectInstanceFromIdentityCache(Experiment object) {
+         this.convertedObjects.remove(object);
+    }
+
+    @Override
+    protected IntactExperiment fetchMatchingObjectFromIdentityCache(Experiment object) {
+        return this.convertedObjects.get(object);
+    }
+
+    @Override
+    protected void convertPersistableProperties(IntactExperiment intactExperiment) throws SynchronizerException, PersisterException, FinderException {
+        // then check interaction detection method
+        prepareInteractionDetectionMethod(intactExperiment, false);
+        // then check participant identification method
+        prepareParticipantIdentificationMethod(intactExperiment, false);
+        // then check organism
+        prepareHostOrganism(intactExperiment, false);
+        // then check annotations
+        prepareAnnotations(intactExperiment, false);
+        // then check xrefs
+        prepareXrefs(intactExperiment, false);
+    }
+
+    @Override
+    protected void storeObjectInIdentityCache(Experiment originalObject, IntactExperiment persistableObject) {
+        this.convertedObjects.put(originalObject, persistableObject);
+    }
+
+    @Override
+    protected boolean isObjectDirty(Experiment originalObject) {
+        return !this.experimentComparator.canCompare(originalObject);
+    }
+
+    protected void prepareHostOrganism(IntactExperiment intactExperiment, boolean enableSynchronization) throws PersisterException, FinderException, SynchronizerException {
+        Organism host = intactExperiment.getHostOrganism();
+        if (host != null){
+            intactExperiment.setHostOrganism(enableSynchronization ?
+                    getContext().getOrganismSynchronizer().synchronize(host, true) :
+                    getContext().getOrganismSynchronizer().convertToPersistentObject(host));
+        }
+    }
+
+    protected void prepareInteractionDetectionMethod(IntactExperiment intactExperiment, boolean enableSynchronization) throws PersisterException, FinderException, SynchronizerException {
+        CvTerm detectionMethod = intactExperiment.getInteractionDetectionMethod();
+        if (detectionMethod != null){
+            intactExperiment.setInteractionDetectionMethod(enableSynchronization ?
+                    getContext().getInteractionDetectionMethodSynchronizer().synchronize(detectionMethod, true) :
+                    getContext().getInteractionDetectionMethodSynchronizer().convertToPersistentObject(detectionMethod));
+        }
+    }
+
+    protected void prepareXrefs(IntactExperiment intactExperiment, boolean enableSynchronization) throws FinderException, PersisterException, SynchronizerException {
+        if (intactExperiment.areXrefsInitialized()){
+            List<Xref> xrefsToPersist = new ArrayList<Xref>(intactExperiment.getXrefs());
+            for (Xref xref : xrefsToPersist){
+                // do not persist or merge xrefs because of cascades
+                Xref expRef = enableSynchronization ?
+                        getContext().getExperimentXrefSynchronizer().synchronize(xref, false) :
+                        getContext().getExperimentXrefSynchronizer().convertToPersistentObject(xref);
+                // we have a different instance because needed to be synchronized
+                if (expRef != xref){
+                    intactExperiment.getXrefs().remove(xref);
+                    intactExperiment.getXrefs().add(expRef);
+                }
+            }
+        }
+    }
+
+    protected void prepareAnnotations(IntactExperiment intactExperiment, boolean enableSynchronization) throws FinderException, PersisterException, SynchronizerException {
+        if (intactExperiment.areAnnotationsInitialized()){
+            List<Annotation> annotationsToPersist = new ArrayList<Annotation>(intactExperiment.getAnnotations());
+            for (Annotation annotation : annotationsToPersist){
+                // do not persist or merge annotations because of cascades
+                Annotation expAnnotation = enableSynchronization ?
+                        getContext().getExperimentAnnotationSynchronizer().synchronize(annotation, false) :
+                        getContext().getExperimentAnnotationSynchronizer().convertToPersistentObject(annotation);
+                // we have a different instance because needed to be synchronized
+                if (expAnnotation != annotation){
+                    intactExperiment.getAnnotations().remove(annotation);
+                    intactExperiment.getAnnotations().add(expAnnotation);
+                }
+            }
+        }
+    }
+
+    protected void prepareParticipantIdentificationMethod(IntactExperiment intactExperiment, boolean enableSynchronization) throws FinderException, PersisterException, SynchronizerException {
+        CvTerm detectionMethod = intactExperiment.getParticipantIdentificationMethod();
+        if (detectionMethod != null){
+            intactExperiment.setParticipantIdentificationMethod(enableSynchronization ?
+                    getContext().getParticipantDetectionMethodSynchronizer().synchronize(detectionMethod, true) :
+                    getContext().getParticipantDetectionMethodSynchronizer().convertToPersistentObject(detectionMethod));
+        }
+        else{
+            detectionMethod = ExperimentUtils.extractMostCommonParticipantDetectionMethodFrom(intactExperiment);
+            intactExperiment.setParticipantIdentificationMethod(enableSynchronization ?
+                    getContext().getParticipantDetectionMethodSynchronizer().synchronize(detectionMethod, true) :
+                    getContext().getParticipantDetectionMethodSynchronizer().convertToPersistentObject(detectionMethod));
+        }
+    }
+
+    protected void prepareAndSynchronizeShortLabel(IntactExperiment intactExperiment) throws SynchronizerException {
+        // first initialise shortlabel if not done
+        if (intactExperiment.getShortLabel() == null){
+            intactExperiment.setShortLabel(intactExperiment.getHostOrganism() != null ?
+                    intactExperiment.getHostOrganism().getCommonName() :
+                    IntactUtils.generateAutomaticExperimentShortlabelFor(intactExperiment, IntactUtils.MAX_SHORT_LABEL_LEN));
+        }
+
+        // then synchronize with database
+        String name;
+        List<String> existingExperiments;
+        do{
+            name = intactExperiment.getShortLabel().trim().toLowerCase();
+            existingExperiments = Collections.EMPTY_LIST;
+
+            // check if short name already exist, if yes, synchronize with existing label
+            Query query = getEntityManager().createQuery("select e.shortLabel from IntactExperiment e " +
+                    "where (e.shortLabel = :name or e.shortLabel like :nameWithSuffix) "
+                    + (intactExperiment.getAc() != null ? "and e.ac <> :expAc" : ""));
+            query.setParameter("name", name);
+            query.setParameter("nameWithSuffix", name+"-%");
+            if (intactExperiment.getAc() != null){
+                query.setParameter("expAc", intactExperiment.getAc());
+            }
+            existingExperiments = query.getResultList();
+            if (!existingExperiments.isEmpty()){
+                String nameInSync = IntactUtils.synchronizeShortlabel(name, existingExperiments, IntactUtils.MAX_SHORT_LABEL_LEN, true);
+                if (!nameInSync.equals(name)){
+                    intactExperiment.setShortLabel(nameInSync);
+                }
+                else{
+                    break;
+                }
+            }
+            else{
+                intactExperiment.setShortLabel(name);
+            }
+        }
+        while(!existingExperiments.isEmpty());
+    }
+
+    @Override
+    protected void initialiseDefaultMerger() {
+        super.setIntactMerger(new ExperimentMergerEnrichOnly());
+    }
+
+    @Override
+    public void deleteRelatedProperties(IntactExperiment intactParticipant){
+        for (InteractionEvidence f : intactParticipant.getInteractionEvidences()){
+            getContext().getInteractionSynchronizer().delete(f);
+        }
+        intactParticipant.getInteractionEvidences().clear();
+    }
+}
