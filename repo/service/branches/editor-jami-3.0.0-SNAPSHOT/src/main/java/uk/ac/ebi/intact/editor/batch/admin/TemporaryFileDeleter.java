74b9a115873f2a994fa34c8005324a0f4770785b
mdumousseau@yahoo.com <marine3@users.noreply.github.com>
2014-12-17 16:28:47 +0000

    Fixed bug importer

--- repo/service/branches/editor-jami-3.0.0-SNAPSHOT/src/main/java/uk/ac/ebi/intact/editor/batch/admin/TemporaryFileDeleter.java
+++ repo/service/branches/editor-jami-3.0.0-SNAPSHOT/src/main/java/uk/ac/ebi/intact/editor/batch/admin/TemporaryFileDeleter.java
@@ -1,7 +1,11 @@
 package uk.ac.ebi.intact.editor.batch.admin;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 import psidev.psi.mi.jami.batch.SimpleJobListener;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * This job listener will delete any temporary file created with job id successful
@@ -13,8 +17,26 @@ import psidev.psi.mi.jami.batch.SimpleJobListener;
 
 public class TemporaryFileDeleter extends SimpleJobListener{
 
+    private List<String> filesToDeletes;
 
     public void afterJob(JobExecution jobExecution) {
         super.afterJob(jobExecution);
+
+        if (jobExecution.getExitStatus() != null && jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)){
+             for (String fileName : filesToDeletes){
+                 File file = new File(fileName);
+                 if (file.exists()){
+                     file.delete();
+                 }
+             }
+        }
+    }
+
+    public List<String> getFilesToDeletes() {
+        return filesToDeletes;
+    }
+
+    public void setFilesToDeletes(List<String> filesToDeletes) {
+        this.filesToDeletes = filesToDeletes;
     }
 }
