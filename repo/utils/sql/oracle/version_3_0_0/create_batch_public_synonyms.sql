fa066a76c758d422ec4c780ed131522cb4fc1a38
mdumousseau@yahoo.com <marine3@users.noreply.github.com>
2015-02-06 14:47:41 +0000

    added spring batch tables

---
+++ repo/utils/sql/oracle/version_3_0_0/create_batch_public_synonyms.sql
@@ -0,0 +1,19 @@
+create or replace public synonym BATCH_JOB_INSTANCE for IA_META_TAB.BATCH_JOB_INSTANCE;
+
+create or replace public synonym BATCH_JOB_EXECUTION for IA_META_TAB.BATCH_JOB_EXECUTION;
+
+create or replace public synonym BATCH_JOB_EXECUTION_PARAMS for IA_META_TAB.BATCH_JOB_EXECUTION_PARAMS;
+
+create or replace public synonym BATCH_STEP_EXECUTION for IA_META_TAB.BATCH_STEP_EXECUTION;
+
+create or replace public synonym BATCH_STEP_EXECUTION_CONTEXT for IA_META_TAB.BATCH_STEP_EXECUTION_CONTEXT;
+
+create or replace public synonym BATCH_JOB_EXECUTION_CONTEXT for IA_META_TAB.BATCH_JOB_EXECUTION_CONTEXT;
+
+create or replace public synonym BATCH_STEP_EXECUTION_SEQ for IA_META_TAB.BATCH_STEP_EXECUTION_SEQ;
+create or replace public synonym BATCH_JOB_EXECUTION_SEQ for IA_META_TAB.BATCH_JOB_EXECUTION_SEQ;
+create or replace public synonym BATCH_JOB_SEQ for IA_META_TAB.BATCH_JOB_SEQ;
+
+
+
+
