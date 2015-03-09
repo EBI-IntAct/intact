d827bbedf025bc0c22ffcf61dbae2b72be2831cb
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2015-03-09 13:20:23 +0000

    fixed bug in cloners

--- repo/service/trunk/editor/src/main/java/uk/ac/ebi/intact/editor/controller/curate/cloner/CvTermCloner.java
+++ repo/service/trunk/editor/src/main/java/uk/ac/ebi/intact/editor/controller/curate/cloner/CvTermCloner.java
@@ -1,11 +1,11 @@
 package uk.ac.ebi.intact.editor.controller.curate.cloner;
 
-import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.model.Alias;
+import psidev.psi.mi.jami.model.Annotation;
+import psidev.psi.mi.jami.model.CvTerm;
+import psidev.psi.mi.jami.model.OntologyTerm;
 import uk.ac.ebi.intact.jami.dao.IntactDao;
-import uk.ac.ebi.intact.jami.model.extension.CvTermAlias;
-import uk.ac.ebi.intact.jami.model.extension.CvTermAnnotation;
-import uk.ac.ebi.intact.jami.model.extension.CvTermXref;
-import uk.ac.ebi.intact.jami.model.extension.IntactCvTerm;
+import uk.ac.ebi.intact.jami.model.extension.*;
 
 /**
  * Cv cloner
@@ -15,14 +15,14 @@ import uk.ac.ebi.intact.jami.model.extension.IntactCvTerm;
  * @since <pre>02/12/14</pre>
  */
 
-public class CvTermCloner extends AbstractCvTermCloner<CvTerm, IntactCvTerm>{
+public class CvTermCloner extends AbstractCvTermCloner<CvTerm, IntactCvTerm> {
     @Override
     protected Annotation instantiateAnnotation(CvTerm topic, String value) {
         return new CvTermAnnotation(topic, value);
     }
 
     @Override
-    protected Xref instantiateXref(CvTerm database, String id, String version, CvTerm qualifier) {
+    protected AbstractIntactXref instantiateXref(CvTerm database, String id, String version, CvTerm qualifier) {
         return new CvTermXref(database, id, version, qualifier);
     }
 
